3a65d716015d19cdaee9326927ffc8b3db7c5cdd
Ed L <beartechtalks@gmail.com>
2020-08-31 09:53:44 +0100

    fix grammar in theia listing

--- components/gitpod-db/src/typeorm/migration/1554360209832-UpdateRepoWhitelist.ts
+++ components/gitpod-db/src/typeorm/migration/1554360209832-UpdateRepoWhitelist.ts
@@ -15,7 +15,7 @@ export class UpdateRepoWhitelist1554360209832 implements MigrationInterface {
         { url: 'https://github.com/gitpod-io/django-locallibrary-tutorial', description: '**Python** - Tutorial "Local Library" website written in Django', priority: 10 },
         { url: 'https://github.com/gitpod-io/gs-spring-boot.git', description: '**Java** - Building an Application with Spring Boot', priority: 9 },
         { url: 'https://github.com/gitpod-io/symfony-demo.git', description: '**PHP** - Symfony Demo Application', priority: 5 },
-        { url: 'https://github.com/theia-ide/theia.git', description: '**Theia** - Deep dive into Gitpods open-source IDE. (TypeScript)', priority: 4 }
+        { url: 'https://github.com/theia-ide/theia.git', description: '**Theia** - Deep dive into Gitpod\'s open-source IDE (written in TypeScript).', priority: 4 }
     ]
 
     public async up(queryRunner: QueryRunner): Promise<any> {
