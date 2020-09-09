5ce60b70c090d768794ca01c6a8fae4583101754
Sean Hellum <seanhellum45@gmail.com>
2020-09-09 13:06:31 +0000

    Use `dependency_update` again

--- install/aws-full-terraform/modules/gitpod/main.tf
+++ install/aws-full-terraform/modules/gitpod/main.tf
@@ -2,15 +2,13 @@
 
 # https://www.terraform.io/docs/providers/helm/r/release.html
 resource "helm_release" "gitpod" {
-  name          = "gitpod"
-  repository    = var.helm.repository
-  chart         = var.helm.chart
-  recreate_pods = true
-  wait          = true
-  timeout       = 600
-  # Is buggy: https://github.com/hashicorp/terraform-provider-helm/issues/405
-  # see below for workaround
-  #dependency_update = true
+  name              = "gitpod"
+  repository        = var.helm.repository
+  chart             = var.helm.chart
+  recreate_pods     = true
+  wait              = true
+  timeout           = 600
+  dependency_update = true
 
   values = flatten([
     data.template_file.gitpod_values_main.rendered,
@@ -18,21 +16,6 @@ resource "helm_release" "gitpod" {
     [for path in var.gitpod.valueFiles : file(path)],
     var.values
   ])
-
-  depends_on = [
-    null_resource.helm_dependency_update
-  ]
-}
-
-# Workaround for "helm_release.dependency_update" (see above) from https://github.com/hashicorp/terraform-provider-helm/issues/405#issuecomment-621910104
-resource "null_resource" "helm_dependency_update" {
-  provisioner "local-exec" {
-    command = "helm dependency update ${var.helm.repository}${var.helm.chart}"
-  }
-
-  triggers = {
-    always_run = "${timestamp()}"
-  }
 }
 
 #
--- install/aws-full-terraform/versions.tf
+++ install/aws-full-terraform/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = "1.6.1"
     }
+    helm = {
+        source = "hashicorp/helm"
+        version = ">= 1.3.0"
+    }
   }
   required_version = ">= 0.13"
 }
