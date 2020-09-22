e40f2532d6d34705adf0a65a2d15d4bb8d031b4c
Christian Weichel <christian.weichel@typefox.io>
2020-09-22 06:30:28 +0000

    [server] Add missing license header

--- components/server/src/auth/resource-access.spec.ts
+++ components/server/src/auth/resource-access.spec.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2020 TypeFox GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
 import { suite, test } from "mocha-typescript";
 import * as chai from 'chai';
 const expect = chai.expect;
