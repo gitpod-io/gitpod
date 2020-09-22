6250fb3a3fab953240705b76045d221cb83eb463
Christian Weichel <christian.weichel@typefox.io>
2020-09-22 06:30:45 +0000

    [supervisor-api] Enable colons in last path segment on the REST API

--- components/supervisor-api/generate.sh
+++ components/supervisor-api/generate.sh
@@ -5,7 +5,7 @@ PROTOC_INCLUDE="-I. -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_
 GO111MODULE=on  go get github.com/golang/protobuf/protoc-gen-go@v1.3.5 
 GO111MODULE=off go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 protoc $PROTOC_INCLUDE --go_out=plugins=grpc:go *.proto
-protoc $PROTOC_INCLUDE --grpc-gateway_out=logtostderr=true,paths=source_relative:go *.proto
+protoc $PROTOC_INCLUDE --grpc-gateway_out=allow_colon_final_segments=true,logtostderr=true,paths=source_relative:go *.proto
 
 # GO111MODULE=on go get github.com/golang/mock/mockgen@latest
 # cd go
