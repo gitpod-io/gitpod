2d8db0ce52e38d82fe8d59f503ba084938f6fbe3
Christian Weichel <christian.weichel@typefox.io>
2020-09-22 06:30:28 +0000

    [server] Add missing license header

--- components/server/src/auth/resource-access.ts
+++ components/server/src/auth/resource-access.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2020 TypeFox GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
 import { Workspace, WorkspaceInstance, User, Snapshot, GitpodToken, Token } from "@gitpod/gitpod-protocol";
 
 declare var resourceInstance: GuardedResource;
