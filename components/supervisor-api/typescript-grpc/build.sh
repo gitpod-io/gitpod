bdfbc5cc0a4bbba75d062235aee4ac8bf35a893e
Christian Weichel <christian.weichel@typefox.io>
2020-09-24 12:42:27 +0000

    [supervisor-api] Fix yarn workspace build for supervisor API packages

---
+++ components/supervisor-api/typescript-grpc/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+THIRD_PARTY_INCLUDES=${PROTOLOC:-$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway}
+if [ ! -d $THIRD_PARTY_INCLUDES/third_party/googleapis ]; then
+    tmpdir=$(mktemp -d)
+    git clone https://github.com/grpc-ecosystem/grpc-gateway $tmpdir
+    THIRD_PARTY_INCLUDES=$tmpdir
+fi
+
+mkdir -p lib
+export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party/googleapis -I /usr/lib/protoc/include"
+
+protoc $PROTO_INCLUDE --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` --js_out=import_style=commonjs,binary:lib --grpc_out=grpc_js:src -I${PROTOLOC:-..} ${PROTOLOC:-..}/*.proto && \
+protoc $PROTO_INCLUDE --plugin=protoc-gen-ts=`which protoc-gen-ts` --ts_out=grpc_js:lib -I${PROTOLOC:-..} ${PROTOLOC:-..}/*.proto                                                               && \
+sed -i '/google_api_annotations_pb/d' lib/*.js
---
+++ components/supervisor-api/typescript-grpcweb/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+THIRD_PARTY_INCLUDES=${PROTOLOC:-$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway}
+if [ ! -d $THIRD_PARTY_INCLUDES/third_party/googleapis ]; then
+    tmpdir=$(mktemp -d)
+    git clone https://github.com/grpc-ecosystem/grpc-gateway $tmpdir
+    THIRD_PARTY_INCLUDES=$tmpdir
+fi
+
+mkdir -p src
+export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party/googleapis -I /usr/lib/protoc/include"
+
+mkdir -p src
+protoc $PROTO_INCLUDE --plugin="protoc-gen-ts=`which protoc-gen-ts`" --js_out="import_style=commonjs,binary:src" --ts_out="service=grpc-web:src" -I${PROTOLOC:-..} ${PROTOLOC:-..}/*.proto
