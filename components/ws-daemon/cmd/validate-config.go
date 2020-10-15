8b60eab69b24a8676f1f06a9c9c842bd2b7025a1
Christian Weichel <christian.weichel@typefox.io>
2020-10-15 13:01:09 +0000

    [ws-daemon] Support direct validation of helm chart produced config

---
+++ components/ws-daemon/cmd/validate-config.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2020 TypeFox GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/gitpod-io/gitpod/common-go/log"
+	"github.com/spf13/cobra"
+)
+
+// can be used with the helm-template like so:
+// helm template ../../chart/ | sed -n '/ws-daemon-configmap.yaml/,$p' | sed '/---/q' | yq r - 'data[config.json]' | go run main.go validate-config
+
+var validateConfigCmd = &cobra.Command{
+	Use:   "validate-config",
+	Short: "reads a ws-daemon configuration from STDIN and validates it",
+	Run: func(cmd *cobra.Command, args []string) {
+		ctnt, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.WithError(err).Fatal("cannot read configuration")
+		}
+
+		var cfg config
+		err = json.Unmarshal(ctnt, &cfg)
+		if err != nil {
+			fmt.Println(string(ctnt))
+			log.WithError(err).Fatal("cannot unmarshal configuration")
+		}
+
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		enc.Encode(cfg)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(validateConfigCmd)
+}
