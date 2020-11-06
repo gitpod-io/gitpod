7d8c4e614a75915033a7147207f6000f613dca27
Anton Kosyakov <anton.kosyakov@typefox.io>
2020-11-06 10:17:36 +0000

    [supervisor] fetch loading screen async

--- components/theia/app/scripts/postgenerate.js
+++ components/theia/app/scripts/postgenerate.js
@@ -6,13 +6,24 @@
 
 // @ts-check
 
-const os = require('os');
 const fs = require('fs');
 const path = require('path');
 
 const filePath = path.resolve(__dirname, '../lib/index.html');
-fs.writeFileSync(filePath, fs.readFileSync(filePath, { encoding: 'utf-8' }).replace('<script', [
-    '<meta id="gitpod-ide-capabilities" data-settings="' + JSON.stringify({ service: true }).replace(/"/g, '&quot;') + '">',
-    '<script type="text/javascript"  src="/_supervisor/frontend/main.js"  charset="utf-8"></script>',
-    '<script'
-].join(os.EOL + '  ')), { encoding: 'utf-8' });
+fs.writeFileSync(filePath, `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+    <meta id="gitpod-ide-capabilities" data-settings="{&quot;service&quot;:true}">
+</head>
+
+<body>
+	<script type="text/javascript" src="/_supervisor/frontend/main.js" charset="utf-8"></script>
+	<div class="theia-preload"></div>
+	<script type="text/javascript" src="./bundle.js" charset="utf-8"></script>
+</body>
+
+</html>`, { encoding: 'utf-8' });
