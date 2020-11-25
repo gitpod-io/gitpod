0880cf181abdca55cfeedca6333ef518ffe51b60
Gero Posmyk-Leinemann <gero@gitpod.io>
2020-11-25 14:42:53 +0000

    test 3

--- .werft/build.js
+++ .werft/build.js
@@ -65,7 +65,10 @@ async function build(context, version) {
      */
     werft.phase("build", "build running");
     // Build using the dev-http-cache gitpod-dev to make 'yarn install' more stable
-    const cachingHttpProxyUrl = "http://dev-http-cache:3129";
+    const proxyName = "dev-http-cache";
+    const clusterLocalProxyAddr = exec(`dig +short ${proxyName}}`, { silent: true }).stdout.trim();
+    werft.log(`Resolved dev: '${clusterLocalProxyAddr}'`);
+    const cachingHttpProxyUrl = `http://${clusterLocalProxyAddr}:3129`;
     const buildEnv = {
         "HTTP_PROXY": cachingHttpProxyUrl,
         "HTTPS_PROXY": cachingHttpProxyUrl,
