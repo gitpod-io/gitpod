ab4e5be7ceb3cefc87898739fc452f3e9f5d7dd8
gtsiolis <tsiolis.g@gmail.com>
2020-11-27 14:41:08 +0000

    Improve feature preview settings

--- components/dashboard/src/components/feature-settings.tsx
+++ components/dashboard/src/components/feature-settings.tsx
@@ -8,6 +8,7 @@ import * as protocol from '@gitpod/gitpod-protocol';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 import Checkbox from '@material-ui/core/Checkbox';
+import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
 import debounce = require('lodash.debounce');
 import { IDESettings } from './ide-settings';
@@ -50,11 +51,17 @@ export class FeatureSettings extends React.Component<FeatureSettingsProps, Featu
 
     render() {
         return <Grid xs={12}>
-            <div><small>This option enables a Beta preview of some of the latest functionalities and features that are being actively developed. Some of them are user-facing features while others
-                take place in the backstage, see <a href="https://www.gitpod.io/docs/feature-preview/" target="_blank">relevant documentation</a>. If you have suggestions on how we can improve a
-                feature, please provide feedback by <a href="https://github.com/gitpod-io/gitpod/issues/new/choose" target="_blank">opening an issue</a>.
-            </small></div>
-            <FormControlLabel control={<Checkbox checked={this.state.featurePreview} onChange={() => this.updateFeatureFlags(!this.state.featurePreview)} />} label={"Enable Feature Preview"} />
+            <FormControlLabel
+                control={<Checkbox color="primary" checked={this.state.featurePreview} onChange={() => this.updateFeatureFlags(!this.state.featurePreview)} />}
+                label={
+                <>
+                    Enable Feature Preview
+                    <Typography variant="caption">
+                    This will enable a beta preview of some of the latest functionalities and features that are being actively developed.&nbsp;
+                    <a href="https://www.gitpod.io/docs/feature-preview/" target="_blank">Learn more</a>
+                    </Typography>
+                </>
+                } />
             { this.state.featurePreview && <div>
                 <h4>Default IDE</h4>
                 <div><small>By default Gitpod uses Eclipse Theia as IDE. However, other IDEs may become available in the future. Choose below to try a different IDE.</small></div>
