e1b4b7478ba92b385f4ea111eeee3c4082720f51
Anton Kosyakov <anton.kosyakov@typefox.io>
2020-11-30 10:45:18 +0000

    fix #2332: pull user settings state up

--- components/dashboard/src/components/feature-settings.tsx
+++ components/dashboard/src/components/feature-settings.tsx
@@ -10,75 +10,46 @@ import Grid from '@material-ui/core/Grid';
 import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
-import debounce = require('lodash.debounce');
 import { IDESettings } from './ide-settings';
 
 export class FeatureSettingsProps {
     service: protocol.GitpodService;
     user: protocol.User;
+    onChange: (update: Partial<protocol.User>) => void
 }
 
-export class FeatureSettingsState {
-    featurePreview: boolean
-    additionalData: protocol.AdditionalUserData
-}
-
-export class FeatureSettings extends React.Component<FeatureSettingsProps, FeatureSettingsState> {
-
-    constructor(props: FeatureSettingsProps) {
-        super(props);
-    }
-
-    private setStateFromUser(user: protocol.User) {
-        this.setState(prevState => {
-            const additionalData = user.additionalData || {};
-            return { ...prevState, 
-                additionalData,
-                featurePreview: additionalData.featurePreview || false,
-            };
-        });
-    }
-
-    componentWillMount() {
-        this.setStateFromUser((this.props.user));
-    }
-
-    componentDidUpdate(prevProps: FeatureSettingsProps) {
-        if (this.props.user !== prevProps.user) {
-            this.setStateFromUser((this.props.user));
-        }
-    }
+export class FeatureSettings extends React.Component<FeatureSettingsProps> {
 
     render() {
+        const { featurePreview } = this;
         return <Grid xs={12}>
             <FormControlLabel
-                control={<Checkbox color="primary" checked={this.state.featurePreview} onChange={() => this.updateFeatureFlags(!this.state.featurePreview)} />}
+                control={<Checkbox color="primary" checked={featurePreview} onChange={this.updateFeatureFlags} />}
                 label={
-                <>
-                    Enable Feature Preview
+                    <>
+                        Enable Feature Preview
                     <Typography variant="caption">
-                    This will enable a beta preview of some of the latest functionalities and features that are being actively developed.&nbsp;
+                            This will enable a beta preview of some of the latest functionalities and features that are being actively developed.&nbsp;
                     <a href="https://www.gitpod.io/docs/feature-preview/" target="_blank">Learn more</a>
-                    </Typography>
-                </>
+                        </Typography>
+                    </>
                 } />
-            { this.state.featurePreview && <div>
+            {featurePreview && <div>
                 <h4>Default IDE</h4>
                 <div><small>By default Gitpod uses Eclipse Theia as IDE. However, other IDEs may become available in the future. Choose below to try a different IDE.</small></div>
-                <IDESettings service={this.props.service} user={this.props.user} />
-            </div> }
+                <IDESettings service={this.props.service} user={this.props.user} onChange={this.props.onChange} />
+            </div>}
         </Grid>
     }
 
-    protected updateFeatureFlags = debounce(async (enable: boolean) => {
-        try {
-            const additionalData = (this.state.additionalData || {});
-            additionalData.featurePreview = enable;
-            const user = await this.props.service.server.updateLoggedInUser({ additionalData });
-            this.setStateFromUser(user);
-        } catch (e) {
-            console.error('Failed to update IDE settings:', e);
-        }
-    }, 150);
-    
+    private get featurePreview(): boolean {
+        return this.props.user.additionalData?.featurePreview || false;
+    }
+
+    private updateFeatureFlags = () => {
+        const additionalData = (this.props.user.additionalData || {});
+        additionalData.featurePreview = !this.featurePreview;
+        this.props.onChange({ additionalData });
+    };
+
 }
