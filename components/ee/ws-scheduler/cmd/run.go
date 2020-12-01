192bb320a64c096b27302f38bca6e2e8960c4f01
Christian Weichel <christian.weichel@typefox.io>
2020-12-01 00:14:20 +0000

    [ws-scheduler] Add Prometheus and pprof support

--- components/ee/ws-scheduler/cmd/run.go
+++ components/ee/ws-scheduler/cmd/run.go
@@ -6,13 +6,17 @@ package cmd
 
 import (
 	"context"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	"github.com/gitpod-io/gitpod/common-go/pprof"
 	"github.com/gitpod-io/gitpod/ws-scheduler/pkg/scaler"
 	"github.com/gitpod-io/gitpod/ws-scheduler/pkg/scheduler"
 )
@@ -63,6 +67,29 @@ var runCmd = &cobra.Command{
 				log.Info("ws-scaler shut down")
 			}()
 			log.Info("ws-scaler is up and running. Stop with SIGINT or CTRL+C")
+		}
+
+		if config.Prometheus.Addr != "" {
+			reg := prometheus.NewRegistry()
+			reg.MustRegister(
+				prometheus.NewGoCollector(),
+				prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
+			)
+
+			handler := http.NewServeMux()
+			handler.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+
+			go func() {
+				err := http.ListenAndServe(config.Prometheus.Addr, handler)
+				if err != nil {
+					log.WithError(err).Error("Prometheus metrics server failed")
+				}
+			}()
+			log.WithField("addr", config.Prometheus.Addr).Info("started Prometheus metrics server")
+		}
+
+		if config.PProf.Addr != "" {
+			go pprof.Serve(config.PProf.Addr)
 		}
 
 		// Run until we're told to stop
