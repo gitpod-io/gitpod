97415ddcf1c882595419e7ea5a9f69771b38733e
Christian Weichel <christian.weichel@typefox.io>
2020-12-07 13:53:40 +0000

    [common-go] Add gRPC rate limiting support

--- components/common-go/go.mod
+++ components/common-go/go.mod
@@ -6,18 +6,16 @@ require (
 	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
 	github.com/go-test/deep v1.0.5
 	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/golang/protobuf v1.3.3
-	github.com/google/go-cmp v0.4.0
+	github.com/golang/protobuf v1.4.2
+	github.com/google/go-cmp v0.5.0
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/opentracing/opentracing-go v1.1.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.1.0
-	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4 // indirect
 	github.com/prometheus/procfs v0.0.5 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/uber-go/atomic v1.4.0 // indirect
 	github.com/uber/jaeger-client-go v2.16.1-0.20190821210114-30e625686abe+incompatible
 	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
@@ -25,8 +23,9 @@ require (
 	golang.org/x/net v0.0.0-20191112182307-2180aed22343 // indirect
 	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
 	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/grpc v1.34.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 	k8s.io/apimachinery v0.0.0
---
+++ components/common-go/grpc/ratelimit.go
@@ -0,0 +1,64 @@
+// Copyright (c) 2020 TypeFox GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package grpc
+
+import (
+	"context"
+	"time"
+
+	"github.com/gitpod-io/gitpod/common-go/util"
+	"golang.org/x/time/rate"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// RateLimit configures the reate limit for a function
+type RateLimit struct {
+	Block          bool          `json:"block"`
+	BucketSize     uint          `json:"bucketSize"`
+	RefillInterval util.Duration `json:"refillInterval"`
+}
+
+// NewRatelimitingInterceptor creates a new rate limiting interceptor
+func NewRatelimitingInterceptor(f map[string]RateLimit) RatelimitingInterceptor {
+	funcs := make(map[string]ratelimitedFunction, len(f))
+	for name, fnc := range f {
+		funcs[name] = ratelimitedFunction{
+			Block: fnc.Block,
+			L:     rate.NewLimiter(rate.Every(time.Duration(fnc.RefillInterval)), int(fnc.BucketSize)),
+		}
+	}
+	return funcs
+}
+
+// RatelimitingInterceptor limits how often a gRPC function may be called. If the limit has been
+// exceeded, we'll return resource exahusted.
+type RatelimitingInterceptor map[string]ratelimitedFunction
+
+type ratelimitedFunction struct {
+	Block bool
+	L     *rate.Limiter
+}
+
+// UnaryInterceptor creates a unary interceptor that implements the rate limiting
+func (r RatelimitingInterceptor) UnaryInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		f, ok := r[info.FullMethod]
+		if ok {
+			if f.Block {
+				err := f.L.Wait(ctx)
+				if err == context.Canceled || err == context.DeadlineExceeded {
+					return nil, err
+				}
+				return nil, status.Error(codes.ResourceExhausted, err.Error())
+			} else if !f.L.Allow() {
+				return nil, status.Error(codes.ResourceExhausted, "too many requests")
+			}
+		}
+
+		return handler(ctx, req)
+	}
+}
