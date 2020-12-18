c999d32a6e86dc7485d993ff036a9f1f4f5f3107
Anton Kosyakov <anton.kosyakov@typefox.io>
2020-12-18 15:25:03 +0000

    fix #2422: support code cli

--- components/registry-facade/pkg/registry/imagecfg.go
+++ components/registry-facade/pkg/registry/imagecfg.go
@@ -124,10 +124,22 @@ func NewConfigModifierFromLayerSource(src LayerSource) ConfigModifier {
 		if err != nil {
 			return
 		}
+		envs, err := src.Envs(ctx, spec)
+		if err != nil {
+			return
+		}
 
 		for _, l := range addons {
 			layer = append(layer, l.Descriptor)
 			cfg.RootFS.DiffIDs = append(cfg.RootFS.DiffIDs, l.DiffID)
+		}
+
+		if len(envs) > 0 {
+			parsed := parseEnvs(cfg.Config.Env)
+			for _, modifyEnv := range envs {
+				modifyEnv(parsed)
+			}
+			cfg.Config.Env = parsed.serialize()
 		}
 
 		return
