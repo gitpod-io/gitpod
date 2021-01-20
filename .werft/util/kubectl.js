1e66ed403f0528276e548a33bd388612ddae588f
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-01-20 16:01:33 +0000

    [werft] Extract build.js parts to enable reuse

--- .werft/util/kubectl.js
+++ .werft/util/kubectl.js
@@ -1,6 +1,30 @@
 const { exec } = require('./shell');
 
 
+function setKubectlContextNamespace(namespace, shellOpts) {
+    [
+        "kubectl config current-context",
+        `kubectl config set-context --current --namespace=${namespace}`
+    ].forEach(cmd => exec(cmd, shellOpts));
+}
+
+function wipeAndRecreateNamespace(namespace, shellOpts) {
+    deleteAllWorkspaces(namespace, shellOpts);
+
+    recreateNamespace(namespace, shellOpts);
+}
+
+function deleteAllWorkspaces(namespace, shellOpts) {
+    const objs = exec(`kubectl get pod -l component=workspace --namespace ${namespace} --no-headers -o=custom-columns=:metadata.name`)
+        .split("\n")
+        .map(o => o.trim())
+        .filter(o => o.length > 0);
+
+    objs.forEach(o => {
+        exec(`kubectl delete pod --namespace ${namespace} ${o}`, shellOpts);
+    });
+}
+
 function recreateNamespace(namespace, shellOpts) {
     const result = exec(`kubectl get namespace ${namespace}`, { ...shellOpts, dontCheckRc: true });
     if (result.code === 0) {
@@ -25,7 +49,29 @@ function deleteNamespace(namespace, shellOpts, wait) {
     }
 }
 
+function deleteNonNamespaceObjects(namespace, destname, shellOpts) {
+    exec(`/usr/local/bin/helm3 delete gitpod-${destname} || echo gitpod-${destname} was not installed yet`, {slice: 'predeploy cleanup'});
+    exec(`/usr/local/bin/helm3 delete jaeger-${destname} || echo jaeger-${destname} was not installed yet`, {slice: 'predeploy cleanup'});
+
+    let objs = [];
+    ["ws-scheduler", "node-daemon", "cluster", "workspace", "jaeger", "jaeger-agent", "ws-sync", "ws-manager-node", "ws-daemon", "registry-facade"].forEach(comp => 
+        ["ClusterRole", "ClusterRoleBinding", "PodSecurityPolicy"].forEach(kind =>
+            exec(`kubectl get ${kind} -l component=${comp} --no-headers -o=custom-columns=:metadata.name | grep ${namespace}-ns`, { dontCheckRc: true })
+                .split("\n")
+                .map(o => o.trim())
+                .filter(o => o.length > 0)
+                .forEach(obj => objs.push({ 'kind': kind, 'obj': obj }))
+        )
+    )
+
+    objs.forEach(o => {
+        exec(`kubectl delete ${o.kind} ${o.obj}`, shellOpts);
+    });
+}
+
 
 module.exports = {
-    recreateNamespace
+    setKubectlContextNamespace,
+    wipeAndRecreateNamespace,
+    deleteNonNamespaceObjects,
 }
