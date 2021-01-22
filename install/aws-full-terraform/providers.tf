0caee2a5f189971d3d6b9c4ca3762b8722e92561
supra08 <rick.das08@gmail.com>
2021-01-22 14:03:47 +0000

    Remove deprecated config from terraform kubernetes provider

--- install/aws-full-terraform/providers.tf
+++ install/aws-full-terraform/providers.tf
@@ -14,7 +14,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.gitpod_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.gitpod_cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.default.token
-  load_config_file       = false
 }
 
 provider "helm" {
