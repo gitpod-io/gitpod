a9361ae9ef5c277881fecd78f12c2089c6f9110d
Wulf Thimm <wulf@gitpod.io>
2021-01-26 11:03:33 +0000

    [terraform,AWS] removed output resources /werft with-installer

--- install/aws-terraform/modules/gitpod/output.tf
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright (c) 2020 Gitpod GmbH. All rights reserved.
- * Licensed under the MIT License. See License-MIT.txt in the project root for license information.
- */
-
-output "external_dns" {
-  value = data.kubernetes_service.proxy.load_balancer_ingress.0.hostname
-}
