bb2eae2d3b6e7c87be60662ea48239ce47aaaf85
Christian Weichel <christian.weichel@typefox.io>
2021-01-29 10:39:41 +0000

    [build] Send slack notification when build fails

--- .werft/build.js
+++ .werft/build.js
@@ -4,6 +4,7 @@ const { werft, exec, gitTag } = require('./util/shell.js');
 const { sleep } = require('./util/util.js');
 const { wipeAndRecreateNamespace, setKubectlContextNamespace, deleteNonNamespaceObjects } = require('./util/kubectl.js');
 const { issueAndInstallCertficate } = require('./util/certs.js');
+const https = require('https');
 
 const GCLOUD_SERVICE_ACCOUNT_PATH = "/mnt/secrets/gcp-sa/service-account.json";
 
@@ -11,7 +12,55 @@ const context = JSON.parse(fs.readFileSync('context.json'));
 
 const version = parseVersion(context);
 build(context, version)
-    .catch((err) => process.exit(1));
+    .catch((err) => {
+        if (context.Repository.ref === "refs/heads/master") {
+            const repo = context.Repository.host + "/" + context.Repository.owner + "/" + context.Repository.repo;
+            const data = JSON.stringify({
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": ":X: *build failure*\n_Repo:_ `" + repo + "`\n_Build:_ `" + context.Name + "`"
+                        },
+                        "accessory": {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Go to Werft",
+                                "emoji": true
+                            },
+                            "value": "click_me_123",
+                            "url": "https://werft.gitpod-dev.com/job/" + context.Name,
+                            "action_id": "button-action"
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "```\n" + err + "\n```"
+                        }
+                    }
+                ]
+            });
+            const req = https.request({
+                hostname: "hooks.slack.com",
+                port: 443,
+                path: process.env.SLACK_NOTIFICATION_PATH.trim(),
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': data.length,
+                }
+            }, () => process.exit(1));
+            req.on('error', () => process.exit(1));
+            req.write(data);
+            req.end();
+        } else {
+            process.exit(1);
+        }
+    });
 
 function parseVersion(context) {
     let buildConfig = context.Annotations || {};
