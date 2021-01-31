100903abe281d6d704e3dcbc98b47349c812ebeb
Pablo Brincat <pablo.brin@gmail.com>
2021-02-01 00:07:53 +0200

    temp fix: terraform-provider-kubernetes
    temp fix: terraform-provider-kubernetes
    # See https://github.com/hashicorp/terraform/issues/26211#issuecomment-770456033
    version = "~> 1.10.0"

--- install/gcp-terraform/environment/installer/providers.tf
+++ install/gcp-terraform/environment/installer/providers.tf
@@ -10,11 +10,12 @@ provider "google" {
 data "google_client_config" "provider" {}
 
 provider "kubernetes" {
-  load_config_file = "false"
-
   host                   = module.kubernetes.cluster.endpoint
   token                  = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(module.kubernetes.cluster.master_auth[0].cluster_ca_certificate)
+  load_config_file = "false"
+  # See https://github.com/hashicorp/terraform/issues/26211#issuecomment-770456033
+  version = "~> 1.10.0"
 }
 
 provider "helm" {
