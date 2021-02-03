fa6a1a29702d912853500e846feffbd1c0354e69
Christian Weichel <christian.weichel@typefox.io>
2021-02-03 08:07:49 +0000

    [leeway] Update leeway to include vet fix
    from https://github.com/gitpod-io/leeway/commit/cb49e6c4b0cee38884aa78f0782d667af6d3ad7d
    
    Fixes gitpod-io/gitpod#2986
    
    /werft no-preview

--- .werft/build.js
+++ .werft/build.js
@@ -130,8 +130,7 @@ async function build(context, version) {
     };
     const imageRepo = publishRelease ? "gcr.io/gitpod-io/self-hosted" : "eu.gcr.io/gitpod-core-dev/build";
 
-    // TODO https://github.com/gitpod-io/leeway/issues/47
-    // exec(`leeway vet --ignore-warnings`);
+    exec(`leeway vet --ignore-warnings`);
     exec(`leeway build --werft=true -c ${cacheLevel} ${dontTest ? '--dont-test':''} -Dversion=${version} -DimageRepoBase=eu.gcr.io/gitpod-core-dev/dev dev:all`, buildEnv);
     if (publishRelease) {
         exec(`gcloud auth activate-service-account --key-file "/mnt/secrets/gcp-sa-release/service-account.json"`);
