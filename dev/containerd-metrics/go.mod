ab4d10a8cf41cf0d27a144203ead4f74723168e3
Christian Weichel <christian.weichel@typefox.io>
2021-02-12 14:06:52 +0000

    [containerd-metrics] Export img/layer extract time

---
+++ dev/containerd-metrics/go.mod
@@ -0,0 +1,86 @@
+module github.com/gitpod-io/gitpod/containerd-metrics
+
+go 1.15
+
+require (
+	github.com/Microsoft/go-winio v0.4.16 // indirect
+	github.com/Microsoft/hcsshim v0.8.14 // indirect
+	github.com/containerd/containerd v1.4.3
+	github.com/containerd/continuity v0.0.0-20210208174643-50096c924a4e // indirect
+	github.com/containerd/fifo v0.0.0-20210129194248-f8e8fdba47ef // indirect
+	github.com/containerd/ttrpc v1.0.2 // indirect
+	github.com/containerd/typeurl v1.0.1
+	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
+	github.com/gitpod-io/gitpod/common-go v0.0.0-00010101000000-000000000000
+	github.com/gitpod-io/gitpod/registry-facade/api v0.0.0-00010101000000-000000000000
+	github.com/gogo/googleapis v1.4.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
+	github.com/opencontainers/selinux v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.7.0
+	github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635 // indirect
+	github.com/urfave/cli v1.22.5
+	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c
+	google.golang.org/api v0.39.0
+	google.golang.org/grpc v1.35.0
+	k8s.io/api v0.20.1
+	k8s.io/apimachinery v0.20.1
+	k8s.io/client-go v0.0.0
+)
+
+replace github.com/gitpod-io/gitpod/common-go => ../../components/common-go // leeway
+
+replace github.com/gitpod-io/gitpod/registry-facade/api => ../../components/registry-facade-api/go // leeway
+
+replace k8s.io/api => k8s.io/api v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/apimachinery => k8s.io/apimachinery v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/apiserver => k8s.io/apiserver v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/cli-runtime => k8s.io/cli-runtime v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/client-go => k8s.io/client-go v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/cloud-provider => k8s.io/cloud-provider v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/code-generator => k8s.io/code-generator v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/component-base => k8s.io/component-base v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/cri-api => k8s.io/cri-api v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kube-proxy => k8s.io/kube-proxy v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kubelet => k8s.io/kubelet v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/metrics => k8s.io/metrics v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/component-helpers => k8s.io/component-helpers v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/controller-manager => k8s.io/controller-manager v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kubectl => k8s.io/kubectl v0.20.1 // leeway indirect from components/common-go:lib
+
+replace k8s.io/mount-utils => k8s.io/mount-utils v0.20.1 // leeway indirect from components/common-go:lib
---
+++ dev/containerd-metrics/main.go
@@ -0,0 +1,268 @@
+// Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/containerd/containerd"
+	apievents "github.com/containerd/containerd/api/events"
+	"github.com/containerd/containerd/events"
+	"github.com/containerd/typeurl"
+	cli "github.com/urfave/cli/v2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+
+	"github.com/gitpod-io/gitpod/common-go/log"
+	regapi "github.com/gitpod-io/gitpod/registry-facade/api"
+)
+
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:  "export",
+				Usage: "Starts exporting metrics",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:      "socket",
+						TakesFile: true,
+						Usage:     "path to the containerd socket",
+						Required:  true,
+					},
+					&cli.StringFlag{
+						Name:  "namespace",
+						Usage: "kubernetes namespace",
+						Value: "default",
+					},
+					&cli.BoolFlag{
+						Name:  "verbose",
+						Value: false,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					log.Init("containerd-metrics", "", true, c.Bool("verbose"))
+					return serveContainerdMetrics(c.String("socket"), c.String("namespace"))
+				},
+			},
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Prep struct {
+	T      time.Time
+	Parent string
+	Name   string
+}
+type Commit struct {
+	Dur    time.Duration
+	Parent string
+	Name   string
+	Key    string
+}
+
+type Image struct {
+	Name      string
+	TotalPrep time.Duration
+	Layer     []Layer
+}
+
+type Layer struct {
+	ID   string
+	Prep time.Duration
+}
+
+var (
+	prepByKey    = make(map[string]*Prep)
+	commitByKey  = make(map[string]*Commit)
+	commitByName = make(map[string]*Commit)
+)
+
+func serveContainerdMetrics(socket, namespace string) error {
+	k8s, err := rest.InClusterConfig()
+	if err != nil {
+		return err
+	}
+	clientSet, err := kubernetes.NewForConfig(k8s)
+	if err != nil {
+		return err
+	}
+	lw := cache.NewFilteredListWatchFromClient(clientSet.CoreV1().RESTClient(), "pods", namespace, func(options *metav1.ListOptions) {
+		options.LabelSelector = "gpwsman=true"
+	})
+	informer := cache.NewSharedIndexInformer(lw, &corev1.Pod{}, 300*time.Second, cache.Indexers{
+		"instanceID": instanceIDIndex,
+	})
+	stop := make(chan struct{})
+	defer close(stop)
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			idx, _ := instanceIDIndex(obj)
+			log.WithField("idx", idx).Debug("found new pod")
+		},
+	})
+	go informer.Run(stop)
+
+	log.Info("waiting for Kubernetes cache sync")
+	cache.WaitForCacheSync(stop, informer.HasSynced)
+	log.Info("Kubernetes cache synced")
+
+	client, err := containerd.New(socket, containerd.WithDefaultNamespace("k8s.io"))
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	evts, errs := client.EventService().Subscribe(ctx)
+	for {
+		var e *events.Envelope
+		select {
+		case err := <-errs:
+			return err
+		case <-sigChan:
+			return nil
+		case e = <-evts:
+		}
+
+		evt, err := typeurl.UnmarshalAny(e.Event)
+		if err != nil {
+			log.WithError(err).Warn("skipping event")
+			continue
+		}
+
+		switch evt := evt.(type) {
+		case *apievents.SnapshotPrepare:
+			prepByKey[evt.Key] = &Prep{
+				T:      time.Now(),
+				Parent: evt.Parent,
+			}
+			log.WithField("obj", *prepByKey[evt.Key]).Debug("prep")
+		case *apievents.SnapshotCommit:
+			p, ok := prepByKey[evt.Key]
+			if !ok {
+				log.WithField("key", evt.Key).WithField("name", evt.Name).Debug("found commit without prep")
+				continue
+			}
+			c := &Commit{
+				Dur:    time.Since(p.T),
+				Name:   evt.Name,
+				Parent: p.Parent,
+				Key:    evt.Key,
+			}
+			commitByKey[evt.Key] = c
+			commitByName[evt.Name] = c
+			log.WithField("obj", *c).Debug("commit")
+		case *apievents.SnapshotRemove:
+			log.WithField("obj", evt).Info("snapshot remove")
+		case *apievents.ImageDelete:
+			log.WithField("obj", evt).Info("image delete")
+		case *apievents.ContainerCreate:
+			segs := strings.Split(evt.Image, "/")
+			var instanceID string
+			if len(segs) == 3 {
+				segs = strings.Split(segs[2], ":")
+				instanceID = segs[0]
+			}
+			imageSpec := getImageSpec(instanceID, informer)
+			pushContainerMetrics(evt.ID, evt.Image, instanceID, imageSpec)
+		}
+	}
+}
+
+func instanceIDIndex(obj interface{}) ([]string, error) {
+	meta, err := meta.Accessor(obj)
+	if err != nil {
+		return []string{""}, fmt.Errorf("object has no meta: %v", err)
+	}
+	return []string{meta.GetLabels()["workspaceID"]}, nil
+}
+
+func getImageSpec(instanceID string, informer cache.SharedIndexInformer) *regapi.ImageSpec {
+	if instanceID == "" {
+		return nil
+	}
+	objs, _ := informer.GetIndexer().ByIndex("instanceID", instanceID)
+	if len(objs) == 0 {
+		return nil
+	}
+	pod, ok := objs[0].(*corev1.Pod)
+	if !ok {
+		return nil
+	}
+
+	ispec := pod.Annotations["gitpod/imageSpec"]
+	spec, err := regapi.ImageSpecFromBase64(ispec)
+	if err != nil {
+		log.WithError(err).WithField("instanceId", instanceID).Debug("cannot decode image spec")
+		return nil
+	}
+
+	return spec
+}
+
+func pushContainerMetrics(id, image, instanceID string, imageSpec *regapi.ImageSpec) {
+	var img Image
+	img.Name = image
+
+	initialPrep, ok := prepByKey[id]
+	if !ok {
+		log.WithField("image", image).WithField("id", id).Debug("image witout prep")
+		return
+	}
+
+	var c *Commit
+	c, ok = commitByName[initialPrep.Parent]
+	for ok {
+		img.Layer = append(img.Layer, Layer{
+			ID:   c.Name,
+			Prep: c.Dur,
+		})
+		img.TotalPrep += c.Dur
+		c, ok = commitByName[c.Parent]
+	}
+
+	for i, j := 0, len(img.Layer)-1; i < j; i, j = i+1, j-1 {
+		img.Layer[i], img.Layer[j] = img.Layer[j], img.Layer[i]
+	}
+
+	var (
+		originalImage string
+		ideImage      string
+		isWorkspace   bool
+	)
+	if imageSpec != nil {
+		originalImage = imageSpec.BaseRef
+		ideImage = imageSpec.IdeRef
+		isWorkspace = true
+	}
+
+	log.WithFields(map[string]interface{}{
+		"instanceId":       instanceID,
+		"image":            img,
+		"id":               id,
+		"isWorkspace":      isWorkspace,
+		"originalImageRef": originalImage,
+		"ideImageRef":      ideImage,
+	}).Info("image pulled")
+}
