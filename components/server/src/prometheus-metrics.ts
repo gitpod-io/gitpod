c6ec2770d7d905329ce6f3e305413593432eb1b6
Anton Kosyakov <anton.kosyakov@typefox.io>
2021-02-17 10:34:07 +0000

    [server] http RED metrics

--- components/server/src/prometheus-metrics.ts
+++ components/server/src/prometheus-metrics.ts
@@ -65,3 +65,26 @@ const apiCallUserCounter = new prometheusClient.Counter({
 export function increaseApiCallUserCounter(method: string, user: string) {
     apiCallUserCounter.inc({ method, user });
 }
+
+const httpRequestTotal = new prometheusClient.Counter({
+    name: 'gitpod_server_http_requests_total',
+    help: 'Total amount of HTTP requests per express route',
+    labelNames: ['method', 'route', 'statusCode'],
+    registers: [prometheusClient.register],
+});
+
+export function increaseHttpRequestCounter(method: string, route: string, statusCode: number) {
+    httpRequestTotal.inc({ method, route, statusCode });
+}
+
+const httpRequestDuration = new prometheusClient.Histogram({
+    name: 'gitpod_server_http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'statusCode'],
+    buckets: [0.01, 0.05, 0.1, 0.5, 1, 5, 10],
+    registers: [prometheusClient.register],
+});
+
+export function observeHttpRequestDuration(method: string, route: string, statusCode: number, durationInSeconds: number) {
+    httpRequestDuration.observe({ method, route, statusCode }, durationInSeconds);
+}
