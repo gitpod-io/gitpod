e1a5cf5a836bbad1f88065fdbe372ac73947feb9
Christian Weichel <christian.weichel@typefox.io>
2021-02-18 10:17:11 +0000

    [supervisor-api] Vendor gRPC gateway annotations
    Instead of trying to check it out from the grpc gateway repo,
    who recently ditched those files to encourage consumers to
    vendor those files themselves.
    
    /werft no-preview

--- components/supervisor-api/typescript-grpcweb/build.sh
+++ components/supervisor-api/typescript-grpcweb/build.sh
@@ -4,14 +4,14 @@
 # See License-AGPL.txt in the project root for license information.
 
 
-THIRD_PARTY_INCLUDES=${PROTOLOC:-$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway}
-if [ ! -d $THIRD_PARTY_INCLUDES/third_party/googleapis ]; then
-    tmpdir=$(mktemp -d)
-    git clone https://github.com/grpc-ecosystem/grpc-gateway $tmpdir
-    THIRD_PARTY_INCLUDES=$tmpdir
+THIRD_PARTY_INCLUDES=${PROTOLOC:-..}
+if [ ! -d $THIRD_PARTY_INCLUDES/third_party/google/api ]; then
+    echo "missing $THIRD_PARTY_INCLUDES/third_party/google/api"
+    exit -1
 fi
 
+
 mkdir -p lib
-export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party/googleapis -I /usr/lib/protoc/include"
+export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party -I /usr/lib/protoc/include"
 
 protoc $PROTO_INCLUDE --plugin="protoc-gen-ts=`which protoc-gen-ts`" --js_out="import_style=commonjs,binary:lib" --ts_out="service=grpc-web:lib" -I${PROTOLOC:-..} ${PROTOLOC:-..}/*.proto
--- components/supervisor-api/typescript-rest/build.sh
+++ components/supervisor-api/typescript-rest/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# Copyright (c) 2020 Gitpod GmbH. All rights reserved.
+# Licensed under the GNU Affero General Public License (AGPL).
+# See License-AGPL.txt in the project root for license information.
 
 GO111MODULES=off go get -v github.com/grpc-ecosystem/grpc-gateway/protoc-gen-openapiv2
-THIRD_PARTY_INCLUDES=${PROTOLOC:-$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway}
-if [ ! -d $THIRD_PARTY_INCLUDES/third_party/googleapis ]; then
-    tmpdir=$(mktemp -d)
-    git clone https://github.com/grpc-ecosystem/grpc-gateway $tmpdir
-    THIRD_PARTY_INCLUDES=$tmpdir
+THIRD_PARTY_INCLUDES=${PROTOLOC:-..}
+if [ ! -d $THIRD_PARTY_INCLUDES/third_party/google/api ]; then
+    echo "missing $THIRD_PARTY_INCLUDES/third_party/google/api"
+    exit -1
 fi
 
+
 mkdir -p lib
-export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party/googleapis -I /usr/lib/protoc/include"
+export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party -I /usr/lib/protoc/include"
 
 protoc $PROTO_INCLUDE --openapiv2_out=logtostderr=true:. -I${PROTOLOC:-..} ${PROTOLOC:-..}/*.proto && \
 mv *.swagger.json lib && \
