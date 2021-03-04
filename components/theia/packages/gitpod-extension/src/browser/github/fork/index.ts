96a178e7759b3d603c5dd3a477637cc838036842
Alex Tugarev <alex@gitpod.io>
2021-03-04 12:40:33 +0000

    move theia app to gitpod-io/theia-app

--- components/theia/packages/gitpod-extension/src/browser/github/fork/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2020 Gitpod GmbH. All rights reserved.
- * Licensed under the GNU Affero General Public License (AGPL).
- * See License-AGPL.txt in the project root for license information.
- */
-
-
-export * from './github-forks-loader';
-export * from './github-fork-creator';
--- components/theia/packages/gitpod-extension/src/browser/github/review-conversation-view/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2020 Gitpod GmbH. All rights reserved.
- * Licensed under the GNU Affero General Public License (AGPL).
- * See License-AGPL.txt in the project root for license information.
- */
-
-export * from './review-conversation-widget';
-export * from './review-conversation-container';
-export * from './review-conversation-contribution';
