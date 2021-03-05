87088508cdc8302f2534c62f9c87db04f06864a5
Anton Kosyakov <anton@gitpod.io>
2021-03-05 14:57:38 +0000

    [supervisor-api] fix generate.sh script
    and add missing copyrights

--- components/supervisor-api/generate.sh
+++ components/supervisor-api/generate.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-PROTOC_INCLUDE="-I. -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I /usr/lib/protoc/include"
+THIRD_PARTY_INCLUDES=${PROTOLOC:-.}
+if [ ! -d $THIRD_PARTY_INCLUDES/third_party/google/api ]; then
+    echo "missing $THIRD_PARTY_INCLUDES/third_party/google/api"
+    exit -1
+fi
+
+export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party -I /usr/lib/protoc/include"
 
 go get github.com/golang/protobuf/protoc-gen-go
-go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-protoc $PROTOC_INCLUDE --go_out=plugins=grpc:go *.proto
-protoc $PROTOC_INCLUDE --grpc-gateway_out=logtostderr=true,paths=source_relative:go *.proto
+GO111MODULE=off go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+protoc -I. $PROTO_INCLUDE --go_out=plugins=grpc:go *.proto
+protoc -I. $PROTO_INCLUDE --grpc-gateway_out=logtostderr=true,paths=source_relative:go *.proto
+leeway run components:update-license-header
--- components/ws-manager-api/typescript/src/core_pb.js
+++ components/ws-manager-api/typescript/src/core_pb.js
@@ -1,4 +1,10 @@
 /**
+ * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+/**
  * @fileoverview
  * @enhanceable
  * @suppress {messageConventions} JS Compiler reports an error if a variable or
