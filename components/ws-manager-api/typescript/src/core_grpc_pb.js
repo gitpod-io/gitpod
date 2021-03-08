09ad24f6aa018c24b86402814d72d0da7a105016
Anton Kosyakov <anton@gitpod.io>
2021-03-08 08:43:32 +0000

    [supervisor-api] fix generate.sh script
    and add missing copyrights

--- components/ws-manager-api/typescript/src/core_grpc_pb.js
+++ components/ws-manager-api/typescript/src/core_grpc_pb.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
 // GENERATED CODE -- DO NOT EDIT!
 
 'use strict';
