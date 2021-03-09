85789b6454b6ab8126c23572513df984dd261bb8
Jan Keromnes <janx@linux.com>
2021-03-09 12:46:35 +0000

    [dashboard] Re-implement create workspace page (Gitpod URL prefix)

---
+++ components/dashboard/src/start/WorkspaceLogs.tsx
@@ -0,0 +1,5 @@
+export default function WorkspaceLogs() {
+  return <pre className="m-6 p-4 h-60 w-11/12 lg:w-3/5 flex-shrink-0 rounded-lg" style={{ color: '#8E8787', background: '#ECE7E5' }}>
+    ... logs ...
+  </pre>;
+}
