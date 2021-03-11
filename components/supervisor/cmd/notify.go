f37686de61464065b2288e58825b83e8c146a6b8
Christian Weichel <christian.weichel@typefox.io>
2021-03-11 12:32:58 +0000

    [supervisor] Minor post-notification PR cleanup

--- components/supervisor/cmd/notify.go
+++ components/supervisor/cmd/notify.go
@@ -15,17 +15,23 @@ import (
 )
 
 var notifyCmd = &cobra.Command{
-	Use:   "notify <level> <message> <actions...>",
-	Short: "Notifies the user of an external event",
-	Args:  cobra.MinimumNArgs(2),
+	Use:    "notify <message>",
+	Short:  "Notifies the user of an external event",
+	Args:   cobra.ExactArgs(1),
+	Hidden: true, // this is not official user-facing functionality, but just for debugging
 	Run: func(cmd *cobra.Command, args []string) {
 		client := api.NewNotificationServiceClient(dialSupervisor())
 
+		level := api.NotifyRequest_INFO
+		lv, _ := cmd.Flags().GetString("level")
+		if l, ok := api.NotifyRequest_Level_value[strings.ToUpper(lv)]; ok {
+			level = api.NotifyRequest_Level(l)
+		}
+
 		var (
+			message     = args[0]
+			actions, _  = cmd.Flags().GetStringArray("actions")
 			ctx, cancel = context.WithTimeout(context.Background(), 1*time.Minute)
-			level       = toLevel(args[0])
-			message     = args[1]
-			actions     = args[2:]
 		)
 		defer cancel()
 
@@ -52,4 +58,11 @@ func toLevel(arg string) api.NotifyRequest_Level {
 
 func init() {
 	rootCmd.AddCommand(notifyCmd)
+
+	var levels []string
+	for k := range api.NotifyRequest_Level_value {
+		levels = append(levels, strings.ToLower(k))
+	}
+	notifyCmd.Flags().String("level", "info", "notification severity - must be one of "+strings.Join(levels, ", "))
+	notifyCmd.Flags().StringArray("actions", nil, "actions to offer to the user")
 }
