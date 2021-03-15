768bea8d73d4c6bf275455343e584cc4ff522bd0
Jan Keromnes <janx@linux.com>
2021-03-15 18:15:36 +0000

    [dashboard] Merge Account and Settings pages into a single sub-menu, align names with UX spec

--- components/dashboard/src/settings/DefaultIDE.tsx
+++ components/dashboard/src/settings/DefaultIDE.tsx
@@ -3,7 +3,7 @@ import settingsMenu from './settings-menu';
 
 export default function DefaultIDE() {
     return <div>
-        <SettingsPage title='Settings' subtitle='Configure your default IDE' menuEntries={settingsMenu}>
+        <SettingsPage title='Default IDE' subtitle='Configure your default IDE' menuEntries={settingsMenu}>
             <div className="lg:px-28 px-10 flex pt-10">
                 Default IDE
             </div>
