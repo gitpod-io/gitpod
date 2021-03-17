e83498970f51ba77fbdadb4ab5e22e21785a4bef
Sven Efftinge <sven.efftinge@typefox.io>
2021-03-17 10:59:40 +0100

    Merge pull request #3457 from gitpod-io/minor-updates
    [dashboard] improved workspaces

--- components/dashboard/src/components/Modal.tsx
+++ components/dashboard/src/components/Modal.tsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
-
 export default function Modal(props: {
     children: React.ReactChild[] | React.ReactChild,
     visible: boolean,
     closeable?: boolean,
-    onClose?: () => void
+    onClose: () => void
 }) {
-    const [visible, setVisible] = useState(props.visible);
-    const hide = () => setVisible(false);
-    if (!visible) {
+    if (!props.visible) {
         return null;
     }
+    setTimeout(() => window.addEventListener('click', props.onClose, {once: true}), 0);
     return (
-        <div className="absolute top-0 left-0 bg-black bg-opacity-40 z-50 w-screen h-screen" >
+        <div className="fixed top-0 -left-2 bg-black bg-opacity-70 z-50 w-screen h-screen" >
             <div className="bg-transparent h-1/3" />
-            <div className="bg-white rounded-md px-6 py-4 max-w-lg mx-auto">
+            <div className="bg-white border rounded-xl p-6 max-w-lg mx-auto">
                 {props.closeable !== false && (
-                    <div className="float-right cursor-pointer" onClick={hide}>&#10006;</div>
+                    <div className="float-right cursor-pointer" onClick={props.onClose}>&#10006;</div>
                 )}
-
                 {props.children}
             </div>
         </div>
