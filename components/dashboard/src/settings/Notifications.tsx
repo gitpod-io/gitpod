d5e488b91837c5444eefab07b89d6c04db4dfd88
Jan Keromnes <janx@linux.com>
2021-03-17 18:56:14 +0000

    [dashboard] Adjust styles in Settings menu and Account page

--- components/dashboard/src/settings/Notifications.tsx
+++ components/dashboard/src/settings/Notifications.tsx
@@ -4,9 +4,7 @@ import settingsMenu from "./settings-menu";
 export default function Notifications() {
     return <div>
         <SettingsPage title='Notifications' subtitle='Email notification preferences' menuEntries={settingsMenu}>
-            <div className="lg:px-28 px-10 flex pt-10">
-                Notifications
-            </div>
+            <h3>Notifications</h3>
         </SettingsPage>
     </div>;
 }
