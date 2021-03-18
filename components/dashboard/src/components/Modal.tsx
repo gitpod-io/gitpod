597fd83033020b232a300acca07ed53b76b63657
Sven Efftinge <sven.efftinge@typefox.io>
2021-03-18 16:53:09 +0000

    [dashboard] addition workspaces list features

--- components/dashboard/src/components/Modal.tsx
+++ components/dashboard/src/components/Modal.tsx
@@ -2,20 +2,29 @@ export default function Modal(props: {
     children: React.ReactChild[] | React.ReactChild,
     visible: boolean,
     closeable?: boolean,
+    className?: string,
     onClose: () => void
 }) {
     if (!props.visible) {
         return null;
     }
-    setTimeout(() => window.addEventListener('click', props.onClose, {once: true}), 0);
+    setTimeout(() => window.addEventListener('click', props.onClose, { once: true }), 0);
     return (
-        <div className="fixed top-0 -left-2 bg-black bg-opacity-70 z-50 w-screen h-screen" >
-            <div className="bg-transparent h-1/3" />
-            <div className="bg-white border rounded-xl p-6 max-w-lg mx-auto">
-                {props.closeable !== false && (
-                    <div className="float-right cursor-pointer" onClick={props.onClose}>&#10006;</div>
-                )}
-                {props.children}
+        <div className="fixed top-0 left-0 bg-black bg-opacity-70 z-50 w-screen h-screen" >
+            <div className="w-screen h-screen align-middle" style={{display: 'table-cell'}}>
+                <div className={"relative bg-white border rounded-xl p-6 max-w-lg mx-auto text-gray-600" + props.className}>
+                    {props.closeable !== false && (
+                        <div className="absolute right-9 top-8 cursor-pointer" onClick={props.onClose}>
+                            <svg version="1.1" width="14px" height="14px"
+                                viewBox="0 0 100 100">
+                                <line x1="0" y1="0" x2="100" y2="100" stroke="currentColor" strokeWidth="10px" />
+                                <line x1="0" y1="100" x2="100" y2="0" stroke="currentColor" strokeWidth="10px" />
+                            </svg>
+                        </div>
+
+                    )}
+                    {props.children}
+                </div>
             </div>
         </div>
     );
--- components/dashboard/src/workspaces/Workspaces.tsx
+++ components/dashboard/src/workspaces/Workspaces.tsx
@@ -1,24 +1,35 @@
 import React from "react";
-import { WorkspaceInfo } from "@gitpod/gitpod-protocol";
+import { WhitelistedRepository, WorkspaceInfo } from "@gitpod/gitpod-protocol";
 import Header from "../components/Header";
 import DropDown from "../components/DropDown"
 import { WorkspaceModel } from "./workspace-model";
 import { WorkspaceEntry } from "./WorkspaceEntry";
+import Modal from "../components/Modal";
+import { getGitpodService, gitpodHostUrl } from "../service/service";
 
 export interface WorkspacesProps {
 }
 
 export interface WorkspacesState {
     workspaces: WorkspaceInfo[];
+    isTemplateModelOpen: boolean;
+    repos: WhitelistedRepository[];
 }
 
 export class Workspaces extends React.Component<WorkspacesProps, WorkspacesState> {
 
-    protected workspaceModel: WorkspaceModel;
+    protected workspaceModel: WorkspaceModel | undefined;
 
     constructor(props: WorkspacesProps) {
         super(props);
+    }
+
+    async componentDidMount() {
         this.workspaceModel = new WorkspaceModel(this.setWorkspaces);
+        const repos = await getGitpodService().server.getFeaturedRepositories();
+        this.setState({
+            repos
+        })
     }
 
     protected setWorkspaces = (workspaces: WorkspaceInfo[]) => {
@@ -28,46 +39,121 @@ export class Workspaces extends React.Component<WorkspacesProps, WorkspacesState
     }
 
     render() {
-        const onActive = () => this.workspaceModel.active = true;
-        const onRecent = () => this.workspaceModel.active = false;
+        const wsModel = this.workspaceModel;
+        const toggleTemplateModal = () => this.setState({
+            isTemplateModelOpen: !this.state?.isTemplateModelOpen
+        });
+        const onActive = () => wsModel!.active = true;
+        const onAll = () => wsModel!.active = false;
         return <>
-            <Header title="Workspaces" subtitle="Manage past workspaces" />
+            <Header title="Workspaces" subtitle="Manage all workspaces and see pending changes." />
 
             <div className="lg:px-28 px-10 pt-8 flex">
                 <div className="flex">
                     <div className="py-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 490 490">
-                            <path fill="none" stroke="#777" stroke-width="36" stroke-linecap="round"
+                            <path fill="none" stroke="#999" strokeWidth="36" strokeLinecap="round"
                                 d="m280,278a153,153 0 1,0-2,2l170,170m-91-117 110,110-26,26-110-110" />
                         </svg>
                     </div>
-                    <input className="border-0" type="text" placeholder="Search Workspace" onChange={(v) => { console.log(v) }} />
+                    <input className="border-0" type="text" placeholder="Search Workspace" onChange={(v) => { if (wsModel) wsModel.setSearch(v.target.value) }} />
                 </div>
                 <div className="flex-1" />
                 <div className="py-3">
-                    <DropDown entries={[{
+                    <DropDown prefix="Filter: " contextMenuWidth="w-32" entries={[{
                         title: 'Active',
                         onClick: onActive
                     }, {
-                        title: 'Past',
-                        onClick: onRecent
+                        title: 'All',
+                        onClick: onAll
+                    }]} />
+                </div>
+                <div className="py-3 pl-3">
+                    <DropDown prefix="Limit: " contextMenuWidth="w-32" entries={[{
+                        title: '50',
+                        onClick: () => { if (wsModel) wsModel.limit = 50; }
+                    }, {
+                        title: '100',
+                        onClick: () => { if (wsModel) wsModel.limit = 100; }
+                    }, {
+                        title: '200',
+                        onClick: () => { if (wsModel) wsModel.limit = 200; }
                     }]} />
                 </div>
             </div>
-            <div className="lg:px-28 px-10 flex flex-col space-y-2">
-                <div className="py-3 flex justify-between space-x-2 text-sm text-gray-400 border-t border-b border-gray-200">
-                    <div className="w-10"></div>
-                    <div className="w-3/12">Name</div>
-                    <div className="w-4/12">Context</div>
-                    <div className="w-2/12">Pending Changes</div>
-                    <div className="w-2/12">Last Active</div>
-                    <div className="w-8"></div>
+            {wsModel && (
+                this.state?.workspaces.length > 0 || wsModel.searchTerm ?
+                    <div className="lg:px-28 px-10 flex flex-col space-y-2">
+                        <div className="px-6 py-3 flex justify-between space-x-2 text-sm text-gray-400 border-t border-b border-gray-200">
+                            <div className="w-6"></div>
+                            <div className="w-3/12">Name</div>
+                            <div className="w-4/12">Context</div>
+                            <div className="w-2/12">Pending Changes</div>
+                            <div className="w-2/12">Last Active</div>
+                            <div className="w-8"></div>
+                        </div>
+                        {
+                            wsModel.active || wsModel.searchTerm ? null :
+                                <div className="whitespace-nowrap flex space-x-2 py-6 px-6 w-full justify-between bg-gitpod-kumquat-light rounded-xl">
+                                    <div className="pr-3 self-center w-6">
+                                        <div className="relative rounded-full w-3 h-3 bg-red">
+                                            <div className="absolute text-xs text-gray-100"
+                                                style={{
+                                                    left: "5px",
+                                                    top: "-2px"
+                                                }}
+                                                ref={(node) => {
+                                                    if (node) {
+                                                        node.style.setProperty("font-size", "10px", "important");
+                                                    }
+                                                }}>!</div>
+                                        </div>
+                                    </div>
+                                    <div className="flex-1 flex flex-col overflow-x-auto">
+                                        <div className="text-red font-semibold">Garbage Collection</div>
+                                        <p className="text-gray-500">Unpinned workspaces that have been stopped for more than 14 days will be automatically deleted. <a className="text-blue-600 underline underline-thickness-thin underline-offset-small hover:text-gray-800" href="https://www.gitpod.io/docs/life-of-workspace/#garbage-collection">Learn more</a></p>
+                                    </div>
+                                </div>
+                        }
+                        {
+                            this.state?.workspaces.map(e => {
+                                return <WorkspaceEntry key={e.workspace.id} desc={e} model={wsModel} />
+                            })
+                        }
+                    </div>
+                    :
+                    <div className="lg:px-28 px-10 flex flex-col space-y-2">
+                        <div className="px-6 py-3 flex justify-between space-x-2 text-sm text-gray-400 border-t border-gray-200 h-96">
+                            <div className="flex flex-col items-center w-96 m-auto">
+                                <h3 className="text-center pb-3">No Active Workspaces</h3>
+                                <div className="text-center pb-6 text-gray-500">Prefix a git repository URL with gitpod.io/# or open a workspace template. <a className="text-gray-400 underline underline-thickness-thin underline-offset-small hover:text-gray-600" href="https://www.gitpod.io/docs/getting-started/">Learn how to get started</a></div>
+                                <button onClick={toggleTemplateModal}>Select Template</button>
+                            </div>
+                        </div>
+                    </div>
+            )}
+            <Modal onClose={toggleTemplateModal} visible={!!this.state?.isTemplateModelOpen}>
+                <h3 className="">Template            </h3>
+                {/* separator */}
+                <div className="absolute border-b pb-3 left-0 top-16 w-full"></div>
+                <div className="pt-8">
+                    <p>Select a template to open a workspace</p>
+                    <div className="space-y-2 pt-4 overflow-y-scroll h-80">
+                        {this.state?.repos && this.state.repos.map(r => {
+                            const url = gitpodHostUrl.withContext(r.url).toString();
+                            return <a key={r.name} href={url} className="rounded-xl group hover:bg-gray-100 flex p-4 my-1">
+                                <div className="w-full">
+                                    <p className="text-base text-gray-800 font-semibold">{r.name}</p>
+                                    <p>{r.url}</p>
+                                </div>
+                                <div className="flex">
+                                    <button>Open</button>
+                                </div>
+                            </a>;
+                        })}
+                    </div>
                 </div>
-                {
-                    this.state?.workspaces.map(e => {
-                        return <WorkspaceEntry key={e.workspace.id} {...e} />
-                    })
-                }
-            </div></>;
+            </Modal>
+        </>;
     }
 }
