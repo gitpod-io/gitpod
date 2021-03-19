4a8e91178f2633b59cad9255d885265deef53501
Jan Keromnes <janx@linux.com>
2021-03-19 12:07:12 +0000

    [dashboard] Improve new workspace start experience ⚡

--- components/dashboard/src/start/StartWorkspace.tsx
+++ components/dashboard/src/start/StartWorkspace.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { DisposableCollection, WorkspaceInstance } from "@gitpod/gitpod-protocol";
-import { GitpodHostUrl } from "@gitpod/gitpod-protocol/lib/util/gitpod-host-url";
-import { getGitpodService } from "../service/service";
+import { getGitpodService, gitpodHostUrl } from "../service/service";
 import { StartPage, StartPhase } from "./StartPage";
 
 export interface StartWorkspaceProps {
@@ -117,7 +116,7 @@ export default class StartWorkspace extends React.Component<StartWorkspaceProps,
 
   async ensureWorkspaceAuth(instanceID: string) {
     if (!document.cookie.includes(`${instanceID}_owner_`)) {
-      const authURL = new GitpodHostUrl(window.location.toString()).asWorkspaceAuth(instanceID);
+      const authURL = gitpodHostUrl.asWorkspaceAuth(instanceID);
       const response = await fetch(authURL.toString());
       if (response.redirected) {
         this.redirectTo(response.url);
@@ -144,8 +143,8 @@ export default class StartWorkspace extends React.Component<StartWorkspaceProps,
   }
 
   render() {
-    let phase = StartPhase.Checking;
-    let statusMessage = undefined;
+    let phase = StartPhase.Preparing;
+    let statusMessage = <p className="text-base text-gray-400">Preparing workspace …</p>;
 
     switch (this.state?.workspaceInstance?.status.phase) {
       // unknown indicates an issue within the system in that it cannot determine the actual phase of
@@ -156,8 +155,8 @@ export default class StartWorkspace extends React.Component<StartWorkspaceProps,
       // Preparing means that we haven't actually started the workspace instance just yet, but rather
       // are still preparing for launch. This means we're building the Docker image for the workspace.
       case "preparing":
-        phase = StartPhase.Building;
-        statusMessage = <p className="text-base text-gray-400">Building Image …</p>;
+        phase = StartPhase.Preparing;
+        statusMessage = <p className="text-base text-gray-400">Building image …</p>;
         break;
 
       // Pending means the workspace does not yet consume resources in the cluster, but rather is looking for
@@ -165,22 +164,22 @@ export default class StartWorkspace extends React.Component<StartWorkspaceProps,
       // workspace, the workspace will be in Pending state until that happened.
       case "pending":
         phase = StartPhase.Preparing;
-        statusMessage = <p className="text-base text-gray-400">Allocating Resources …</p>;
+        statusMessage = <p className="text-base text-gray-400">Allocating resources …</p>;
         break;
 
       // Creating means the workspace is currently being created. That includes downloading the images required
       // to run the workspace over the network. The time spent in this phase varies widely and depends on the current
       // network speed, image size and cache states.
       case "creating":
-        phase = StartPhase.Preparing;
-        statusMessage = <p className="text-base text-gray-400">Pulling Container Image …</p>;
+        phase = StartPhase.Creating;
+        statusMessage = <p className="text-base text-gray-400">Pulling container image …</p>;
         break;
 
       // Initializing is the phase in which the workspace is executing the appropriate workspace initializer (e.g. Git
       // clone or backup download). After this phase one can expect the workspace to either be Running or Failed.
       case "initializing":
         phase = StartPhase.Starting;
-        statusMessage = <p className="text-base text-gray-400">Cloning Repository …</p>; // TODO Loading Prebuild ...
+        statusMessage = <p className="text-base text-gray-400">Cloning repository …</p>; // TODO Loading prebuild ...
         break;
 
       // Running means the workspace is able to actively perform work, either by serving a user through Theia,
@@ -194,17 +193,39 @@ export default class StartWorkspace extends React.Component<StartWorkspaceProps,
       // When in this state, we expect it to become running or stopping anytime soon.
       case "interrupted":
         phase = StartPhase.Running;
-        statusMessage = <p className="text-base text-gray-400">Checking On Workspace …</p>;
+        statusMessage = <p className="text-base text-gray-400">Checking workspace …</p>;
         break;
 
       // Stopping means that the workspace is currently shutting down. It could go to stopped every moment.
       case "stopping":
-        statusMessage = <p className="text-base text-gray-400">Stopping …</p>;
+        phase = StartPhase.Stopping;
+        statusMessage = <div>
+          <div className="flex space-x-3 items-center rounded-xl py-2 px-4 h-16 w-72 mt-4 bg-gray-100">
+            <div className="rounded-full w-3 h-3 text-sm bg-gitpod-kumquat">&nbsp;</div>
+            <div>
+              <p className="text-gray-700 font-semibold">{this.state.workspaceInstance.workspaceId}</p>
+              <p>{this.state.contextUrl}</p>
+            </div>
+          </div>
+        </div>;
         break;
 
       // Stopped means the workspace ended regularly because it was shut down.
       case "stopped":
-        statusMessage = <p className="text-base text-gray-400">Stopped</p>;
+        phase = StartPhase.Stopped;
+        statusMessage = <div>
+            <div className="flex space-x-3 items-center rounded-xl py-2 px-4 h-16 w-72 mt-4 bg-gray-100">
+            <div className="rounded-full w-3 h-3 text-sm bg-gray-300">&nbsp;</div>
+            <div>
+              <p className="text-gray-700 font-semibold">{this.state.workspaceInstance.workspaceId}</p>
+              <p>{this.state.contextUrl}</p>
+            </div>
+          </div>
+          <div className="mt-10 flex space-x-2">
+            <button className="px-4 py-2 text-gray-500 bg-white font-semibold border-gray-500" onClick={() => this.redirectTo(gitpodHostUrl.asDashboard().toString())}>Go to Dashboard</button>
+            <button className="px-4 py-2 text-gray-50 bg-green-600 font-semibold border-green-800" onClick={() => this.redirectTo(gitpodHostUrl.asStart(this.state.workspaceInstance?.workspaceId).toString())}>Open Workspace</button>
+          </div>
+        </div>;
         break;
     }
 
