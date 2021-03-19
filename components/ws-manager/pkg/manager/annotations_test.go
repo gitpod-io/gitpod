4f21308dd4d92110ffd1d34af0b149ca32634a19
Christian Weichel <chris@gitpod.io>
2021-03-19 09:37:59 +0000

    [ws-manager] Update tests to work without PLIS

--- components/ws-manager/pkg/manager/annotations_test.go
+++ components/ws-manager/pkg/manager/annotations_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/gitpod-io/gitpod/ws-manager/api"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestMarkWorkspace(t *testing.T) {
@@ -79,6 +80,59 @@ func TestMarkWorkspace(t *testing.T) {
 			}
 			if len(modificationPosterior) != len(test.ExpectedResult) {
 				t.Errorf("annotations do not match expectation: %v != %v", test.ExpectedResult, modificationPosterior)
+			}
+		})
+	}
+}
+
+func TestModifyFinalizer(t *testing.T) {
+	finalizer := "gitpod.io/finalizer"
+	tests := []struct {
+		Description string
+		InitialSet  []string
+		Finalizer   string
+		Add         bool
+		ExpectedSet []string
+		ExpectedErr string
+	}{
+		{"add finalizer", nil, finalizer, true, []string{finalizer}, ""},
+		{"add finalizer noop", []string{finalizer}, finalizer, true, []string{finalizer}, ""},
+		{"remove finalizer", []string{finalizer}, finalizer, false, nil, ""},
+		{"remove finalizer noop", []string{}, finalizer, false, nil, ""},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Description, func(t *testing.T) {
+			manager := forTestingOnlyGetManager(t)
+			startCtx, err := forTestingOnlyCreateStartWorkspaceContext(manager, "foo", api.WorkspaceType_REGULAR)
+			if err != nil {
+				t.Errorf("cannot create test pod start context; this is a bug in the unit test itself: %v", err)
+				return
+			}
+
+			pod, err := manager.createDefiniteWorkspacePod(startCtx)
+			if err != nil {
+				t.Errorf("cannot create test pod; this is a bug in the unit test itself: %v", err)
+				return
+			}
+			pod.Finalizers = test.InitialSet
+
+			err = manager.Clientset.Create(context.Background(), pod)
+			if err != nil {
+				t.Errorf("cannot create test pod; this is a bug in the unit test itself: %v", err)
+				return
+			}
+
+			err = manager.modifyFinalizer(context.Background(), startCtx.Request.Id, test.Finalizer, test.Add)
+			if err != nil && err.Error() != test.ExpectedErr {
+				t.Errorf("modifyFinalizer failed: %v", err)
+				return
+			}
+
+			pod, _ = manager.findWorkspacePod(context.Background(), startCtx.Request.Id)
+
+			if diff := cmp.Diff(pod.Finalizers, test.ExpectedSet); diff != "" {
+				t.Errorf("unexpected blob hash (-want +got):\n%s", diff)
 			}
 		})
 	}
