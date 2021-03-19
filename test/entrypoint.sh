a41d7f597c83755f9d0dc3d4876396bf02d205a2
Cornelius A. Ludmann <cornelius@gitpod.io>
2021-03-19 16:39:50 +0000

    Add missing license headers

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+# Licensed under the GNU Affero General Public License (AGPL).
+# See License-AGPL.txt in the project root for license information.
+
 set -ex
 
 for i in $(ls /tests/*.test); do
