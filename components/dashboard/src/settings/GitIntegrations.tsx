fdf8bb3c73ae6e09dcd6fea42bc249dbfe0a4ea0
X_AUTHOR_NAME <alex@gitpod.io>
2021-03-22 15:14:09 +0000

    [dashboard] Add Git Providers

--- components/dashboard/src/settings/GitIntegrations.tsx
+++
@@ -1,9 +0,0 @@
-import { SettingsPage } from "./SettingsPage";
-
-export default function GitIntegrations() {
-    return <div>
-        <SettingsPage title='Integrations' subtitle='Manage permissions for git providers and integrations.'>
-            <h3>Git Providers</h3>
-        </SettingsPage>
-    </div>;
-}
