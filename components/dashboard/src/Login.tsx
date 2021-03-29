12d2e12bad4c68f79a2af3a35ae1d20ed4794f6c
Sven Efftinge <sven.efftinge@typefox.io>
2021-03-29 16:25:57 +0000

    Update Login.tsx

--- components/dashboard/src/Login.tsx
+++ components/dashboard/src/Login.tsx
@@ -4,15 +4,40 @@
  * See License-AGPL.txt in the project root for license information.
  */
 
-import { gitpod, gitpodIcon, terminal } from './images';
+import { gitpod, gitpodIcon } from './images';
 import { AuthProviderInfo } from "@gitpod/gitpod-protocol";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "./user-context";
 import { getGitpodService, gitpodHostUrl, reconnectGitpodService } from "./service/service";
 import { iconForAuthProvider, simplifyProviderName } from "./provider-utils";
+import automate from "./images/welcome/automate.svg";
+import code from "./images/welcome/code.svg";
+import collaborate from "./images/welcome/collaborate.svg";
+import customize from "./images/welcome/customize.svg";
+import fresh from "./images/welcome/fresh.svg";
+import prebuild from "./images/welcome/prebuild.svg";
+
+
+
+function Item(props: {icon: string, iconSize?: string, text:string}) {
+    const iconSize = props.iconSize || 28;
+    return <div className="flex-col items-center w-1/3 px-3">
+        <img src={props.icon} className={`w-${iconSize} m-auto h-24`} />
+        <div className="text-gray-400 text-sm w-36 h-20 text-center">{props.text}</div>
+    </div>;
+}
+
+export function markLoggedIn() {
+    document.cookie = "gitpod-user=loggedIn;max-age=" + 60*60*24*365;
+}
+
+export function hasLoggedInBefore() {
+    return document.cookie.match("gitpod-user=loggedIn");
+}
 
 export function Login() {
     const { setUser } = useContext(UserContext);
+    const showWelcome = !hasLoggedInBefore();
 
     const [authProviders, setAuthProviders] = useState<AuthProviderInfo[]>([]);
 
@@ -21,7 +46,7 @@ export function Login() {
             setAuthProviders(await getGitpodService().server.getAuthProviders());
         })();
 
-        window.addEventListener("message", (event) => {
+        const listener = (event: MessageEvent<any>) => {
             // todo: check event.origin
 
             if (event.data === "auth-success") {
@@ -34,9 +59,14 @@ export function Login() {
                 (async () => {
                     reconnectGitpodService();
                     setUser(await getGitpodService().server.getLoggedInUser());
+                    markLoggedIn();
                 })();
             }
-        })
+        };
+        window.addEventListener("message", listener);
+        return () => {
+            window.removeEventListener("message", listener);
+        }
     }, [])
 
     const openLogin = (host: string) => {
@@ -48,36 +78,43 @@ export function Login() {
     }
 
     return (<div id="login-container" className="z-50 flex w-screen h-screen">
-        <div id="feature-section" className="flex-grow bg-gray-100 w-1/2 hidden lg:block">
-            <div id="feature-section-column" className="flex max-w-2xl h-full ml-auto pt-6">
-                <div className="flex flex-col space-y-12 pl-6 pr-24 m-auto">
-                    <div>
+        {showWelcome ? <div id="feature-section" className="flex-grow bg-gray-100 w-1/2 hidden lg:block">
+            <div id="feature-section-column" className="flex max-w-xl h-full mx-auto pt-6">
+                <div className="flex flex-col px-8 my-auto ml-auto">
+                    <div className="mb-12">
                         <img src={gitpod} className="h-8" />
                     </div>
-                    <div>
-                        <h1 className="xl:text-7xl text-5xl">Save Time<br /> with Prebuilds</h1>
+                    <div className="mb-10">
+                        <h1 className="text-5xl mb-3">Welcome to Gitpod</h1>
+                        <div className="text-gray-400 text-lg">
+                            Spin up fresh, automated dev environments for each task in the cloud, in seconds.
+                        </div>
                     </div>
-                    <div className="text-gray-400 text-lg">
-                        Gitpod continuously builds your git branches like a CI server. This means no more waiting for dependencies to be downloaded and builds to finish. <a className="underline underline-thickness-thin underline-offset-small hover:text-gray-600" href="https://www.gitpod.io/docs/prebuilds/" target="gitpod-docs">Learn more about Prebuilds</a>
+                    <div className="flex mb-10">
+                        <Item icon={code} iconSize="16" text="Always Ready&#x2011;To&#x2011;Code"/>
+                        <Item icon={customize} text="Personalize your Workspace"/>
+                        <Item icon={automate} text="Automate Your Development Setup"/>
                     </div>
-                    <div>
-                        <img src={terminal} className="h-64 -ml-8" />
+                    <div className="flex">
+                        <Item icon={prebuild} text="Continuously Prebuild Your Project"/>
+                        <Item icon={collaborate} text="Collaborate With Your Team"/>
+                        <Item icon={fresh} text="Fresh Workspace For Each New Task"/>
                     </div>
                 </div>
             </div>
-        </div>
-        <div id="login-section" className="flex-grow flex lg:w-1/2 w-full">
-            <div id="login-section-column" className="flex-grow max-w-2xl flex flex-col h-100 mx-auto lg:mx-0">
+        </div>: null}
+        <div id="login-section" className={"flex-grow flex w-full" + (showWelcome ? " lg:w-1/2" : "")}>
+            <div id="login-section-column" className={"flex-grow max-w-2xl flex flex-col h-100 mx-auto" + (showWelcome ? " lg:my-0" : "")}>
                 <div className="flex-grow h-100 flex flex-row items-center justify-center" >
                     <div className="rounded-xl px-10 py-10 mx-auto">
                         <div className="mx-auto pb-8">
                             <img src={gitpodIcon} className="h-16 mx-auto" />
                         </div>
                         <div className="mx-auto text-center pb-8 space-y-2">
-                            <h1 className="text-3xl">Log in to Gitpod</h1>
+                            <h1 className="text-3xl">Log in{showWelcome? ' to Gitpod' : ''}</h1>
                             <h2 className="uppercase text-sm text-gray-400">ALWAYS READY-TO-CODE</h2>
                         </div>
-                        <div className="flex flex-col space-y-3">
+                        <div className="flex flex-col space-y-3 items-center">
                             {authProviders.map(ap => {
                                 return (
                                     <button key={"button" + ap.host} className="btn-login flex-none w-56 h-10 p-0 inline-flex" onClick={() => openLogin(ap.host)}>
