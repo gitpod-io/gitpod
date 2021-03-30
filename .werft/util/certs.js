558041eeeb9f8ceb23213d43462e8fb463d253f6
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-03-30 09:49:05 +0000

    [werft] Add "as-ws-cluster" and "with-ws-cluster" flags

--- .werft/util/certs.js
+++ .werft/util/certs.js
@@ -1,7 +1,14 @@
 const { exec } = require('./shell');
 const { sleep } = require('./util.js');
 
-async function issueAndInstallCertficate(werft, pathToTerraform, gcpSaPath, namespace, dnsZoneDomain, domain, ip, namespaceRecreatedPromise, certificateSecretName) {
+async function issueCertficate(werft, pathToTerraform, gcpSaPath, namespace, dnsZoneDomain, domain, ip, additionalWsSubdomains) {
+    const subdomains = ["", "*.", "*.ws-dev."];
+    if (Array.isArray(subdomains)) {
+        for (const sd of additionalWsSubdomains) {
+            subdomains.push(`*.ws-${additionalWsSubdomains}.`);
+        }
+    }
+
     // Always use 'terraform apply' to make sure the certificate is present and up-to-date
     await exec(`set -x \
         && cd ${pathToTerraform} \
@@ -12,7 +19,7 @@ async function issueAndInstallCertficate(werft, pathToTerraform, gcpSaPath, name
             -var 'dns_zone_domain=${dnsZoneDomain}' \
             -var 'domain=${domain}' \
             -var 'public_ip=${ip}' \
-            -var 'subdomains=["", "*.", "*.ws-dev."]'`, {slice: 'certificate', async: true});
+            -var 'subdomains=[${subdomains.map(s => `"${s}"`).join(", ")}]'`, {slice: 'certificate', async: true});
 
     werft.log('certificate', `waiting until certificate certs/${namespace} is ready...`)
     let notReadyYet = true;
@@ -26,21 +33,21 @@ async function issueAndInstallCertficate(werft, pathToTerraform, gcpSaPath, name
 
         sleep(5000);
     }
+}
 
-    werft.log('certificate', 'waiting for preview env namespace being re-created...');
-    await namespaceRecreatedPromise;
-
-    werft.log('certificate', `copying certificate from "certs/${namespace}" to "${namespace}/${certificateSecretName}"`);
+async function installCertficate(werft, fromNamespace, toNamespace, certificateSecretName) {
+    werft.log('certificate', `copying certificate from "certs/${fromNamespace}" to "${toNamespace}/${certificateSecretName}"`);
     // certmanager is configured to create a secret in the namespace "certs" with the name "${namespace}".
-    exec(`kubectl get secret ${namespace} --namespace=certs -o yaml \
+    exec(`kubectl get secret ${fromNamespace} --namespace=certs -o yaml \
         | yq d - 'metadata.namespace' \
         | yq d - 'metadata.uid' \
         | yq d - 'metadata.resourceVersion' \
         | yq d - 'metadata.creationTimestamp' \
-        | sed 's/${namespace}/${certificateSecretName}/g' \
-        | kubectl apply --namespace=${namespace} -f -`);
+        | sed 's/${fromNamespace}/${certificateSecretName}/g' \
+        | kubectl apply --namespace=${toNamespace} -f -`);
 }
 
 module.exports = {
-    issueAndInstallCertficate,
+    issueCertficate,
+    installCertficate,
 }
