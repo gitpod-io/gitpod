c0dc265646776f8c7e230d45a3860dad41f101fc
Jan Keromnes <janx@linux.com>
2021-04-12 16:07:40 +0000

    [dashboard] Implement dark theme toggle defaulting to OS preference

--- components/dashboard/src/App.tsx
+++ components/dashboard/src/App.tsx
@@ -56,6 +56,14 @@ function App() {
         })();
     }, []);
 
+    useEffect(() => {
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [localStorage.theme]);
+
     if (loading) {
         return <Loading />
     }
--- components/dashboard/src/start/StartPage.tsx
+++ components/dashboard/src/start/StartPage.tsx
@@ -79,7 +79,7 @@ export interface StartWorkspaceError {
 export function StartPage(props: StartPageProps) {
   const { phase, error } = props;
   let title = props.title || getPhaseTitle(phase, error);
-  return <div className="w-screen h-screen bg-white align-middle">
+  return <div className="w-screen h-screen align-middle">
     <div className="flex flex-col mx-auto items-center text-center h-screen">
       <div className="h-1/3"></div>
       <img src={gitpodIcon} className={`h-16 flex-shrink-0 ${(error || phase === StartPhase.Stopped) ? '' : 'animate-bounce'}`} />
