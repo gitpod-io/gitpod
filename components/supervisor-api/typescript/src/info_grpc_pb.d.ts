d6a73bcaea93d85e8f7a76960b60fb07243a31b3
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-04-14 17:41:10 -0400

    [supervisor-api] Update grpc code

--- components/supervisor-api/typescript/src/info_grpc_pb.d.ts
+++ components/supervisor-api/typescript/src/info_grpc_pb.d.ts
@@ -9,7 +9,7 @@
 
 /* tslint:disable */
 
-import * as grpc from "grpc";
+import * as grpc from "@grpc/grpc-js";
 import * as info_pb from "./info_pb";
 
 interface IInfoServiceService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
