e08d79114929fe5a6755ec2bf4955780803e41a4
Autofix <autofix-bot@users.noreply.github.com>
2021-04-28 03:06:22 +0000

    Autofix: trailing-spaces

--- components/dashboard/src/App.tsx
+++ components/dashboard/src/App.tsx
@@ -81,7 +81,7 @@ function App() {
         }
     }, []);
 
-    if ((window.location.hostname === 'gitpod.io' || window.location.hostname === 'gitpod-staging.com' || window.location.hostname.endsWith('gitpod-dev.com')) 
+    if ((window.location.hostname === 'gitpod.io' || window.location.hostname === 'gitpod-staging.com' || window.location.hostname.endsWith('gitpod-dev.com'))
         && window.location.pathname === '/' && window.location.hash === '' && !loading && !user) {
         window.location.href = `https://www.gitpod.io`;
         return <div></div>;
