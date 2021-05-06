38faf384f799c8cd91263e9d44f63d88bc7477dd
Christian Weichel <chris@gitpod.io>
2021-05-06 05:34:21 +0000

    [gpctl] Updated bash completion

--- dev/gpctl/cmd/completion.go
+++ dev/gpctl/cmd/completion.go
@@ -4,79 +4,71 @@
 
 package cmd
 
-//go:generate sh -c "cd .. && echo \\# generated using go generate in components/gpctl > ../../devops/images/workspaces/gitpod-dev/gpctl_completion; go run main.go completion bash >> ../../devops/images/workspaces/gitpod-dev/gpctl_completion"
-
 import (
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-const (
-	bashCompletionFunc = `__gpctl_parse_get_workspace_url()
-{
-    local gpctl_output out
-    if gpctl_output=$(gpctl workspaces list --output-template '{{ range .Status }}{{ .Id }} {{ .Spec.Url | trimPrefix "http://" | trimPrefix "https://" }} {{ end }}' | tr " " "\n" 2>/dev/null); then
-        out=($(echo "${gpctl_output}" | awk '{print $1}'))
-        COMPREPLY=( $( compgen -W "${out[*]}" -- "$cur" ) )
-    fi
-}
-__gpctl_get_workspace_url()
-{
-    __gpctl_parse_get_workspace_url
-    if [[ $? -eq 0 ]]; then
-        return 0
-    fi
-}
-__gpctl_parse_get_imagebuild_ref()
-{
-	local gpctl_output out
-    if gpctl_output=$(gpctl imagebuilds list -o jsonpath | tr " " "\n" 2>/dev/null); then
-        out=($(echo "${gpctl_output}" | awk '{print $1}'))
-        COMPREPLY=( $( compgen -W "${out[*]}" -- "$cur" ) )
-    fi
-}
-__gpctl_get_imagebuild_ref()
-{
-	__gpctl_parse_get_imagebuild_ref
-    if [[ $? -eq 0 ]]; then
-        return 0
-    fi
-}
-__gpctl_custom_func() {
-    case ${last_command} in
-        gpctl_workspaces_describe | gpctl_workspaces_stop | gpctl_workspaces_snapshot)
-            __gpctl_get_workspace_url
-            return
-			;;
-		gpctl_imagebuilds_logs)
-            __gpctl_get_imagebuild_ref
-            return
-            ;;
-        *)
-            ;;
-    esac
-}
-`
-)
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate completion script",
+	Long: `To load completions:
 
-// bashCompletionCmd represents the bashCompletion command
-var bashCompletionCmd = &cobra.Command{
-	Use:       "completion bash|zsh",
-	Short:     "Provides shell completion for gpctl. Use with `. <(gpctl completion)`",
-	Hidden:    true,
-	Args:      cobra.ExactValidArgs(1),
-	ValidArgs: []string{"bash", "zsh"},
+Bash:
+
+  $ source <(gpctl completion bash)
+
+  # To load completions for each session, execute once:
+  # Linux:
+  $ gpctl completion bash > /etc/bash_completion.d/gpctl
+  # macOS:
+  $ gpctl completion bash > /usr/local/etc/bash_completion.d/gpctl
+
+Zsh:
+
+  # If shell completion is not already enabled in your environment,
+  # you will need to enable it.  You can execute the following once:
+
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+  # To load completions for each session, execute once:
+  $ gpctl completion zsh > "${fpath[1]}/_gpctl"
+
+  # You will need to start a new shell for this setup to take effect.
+
+fish:
+
+  $ gpctl completion fish | source
+
+  # To load completions for each session, execute once:
+  $ gpctl completion fish > ~/.config/fish/completions/gpctl.fish
+
+PowerShell:
+
+  PS> gpctl completion powershell | Out-String | Invoke-Expression
+
+  # To load completions for every new session, run:
+  PS> gpctl completion powershell > gpctl.ps1
+  # and source this file from your PowerShell profile.
+`,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		switch args[0] {
 		case "bash":
-			rootCmd.GenBashCompletion(os.Stdout)
+			cmd.Root().GenBashCompletion(os.Stdout)
 		case "zsh":
-			rootCmd.GenZshCompletion(os.Stdout)
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
 		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(bashCompletionCmd)
+	rootCmd.AddCommand(completionCmd)
 }
--- dev/gpctl/go.mod
+++ dev/gpctl/go.mod
@@ -16,7 +16,7 @@ require (
 	github.com/huandu/xstrings v1.2.0 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/spf13/cobra v0.0.5
+	github.com/spf13/cobra v1.1.3
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	google.golang.org/grpc v1.37.0
 	google.golang.org/protobuf v1.26.0
