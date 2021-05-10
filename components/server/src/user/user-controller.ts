c680b451fd96eb99ba7662945eb40feace24e95d
rl-gitpod <robert@gitpod.io>
2021-05-10 01:51:32 +0000

    Use full url to return to

--- components/server/src/user/user-controller.ts
+++ components/server/src/user/user-controller.ts
@@ -278,7 +278,7 @@ export class UserController {
                 log.info(`AUTHORIZE: ${JSON.stringify(req.query)}`);
 
                 if (!req.isAuthenticated() || !User.is(req.user)) {
-                    res.redirect(`${this.env.hostUrl}/api/login?returnTo=${req.query}`)
+                    res.redirect(`${this.env.hostUrl}/api/login?returnTo=${this.env.hostUrl}/${req.originalUrl}`)
                 }
 
                 const user = req.user as User;
