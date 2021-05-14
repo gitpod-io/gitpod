360759883778f3414e5c54fdfc8cae16068a19c9
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-05-14 09:03:19 -0400

    Generation of grpc code should also include typescript components

--- components/supervisor-api/typescript-grpcweb/build.sh
+++ components/supervisor-api/typescript-grpcweb/build.sh
@@ -3,8 +3,9 @@
 # Licensed under the GNU Affero General Public License (AGPL).
 # See License-AGPL.txt in the project root for license information.
 
+DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd -P)
 
-THIRD_PARTY_INCLUDES=${PROTOLOC:-..}
+THIRD_PARTY_INCLUDES=${PROTOLOC:-$DIR/..}
 if [ ! -d $THIRD_PARTY_INCLUDES/third_party/google/api ]; then
     echo "missing $THIRD_PARTY_INCLUDES/third_party/google/api"
     exit -1
@@ -15,7 +16,7 @@ mkdir -p lib
 export PROTO_INCLUDE="-I$THIRD_PARTY_INCLUDES/third_party -I /usr/lib/protoc/include"
 
 protoc $PROTO_INCLUDE \
-    --plugin="protoc-gen-ts=`which protoc-gen-ts`" \
+    --plugin="protoc-gen-ts=$DIR/node_modules/.bin/protoc-gen-ts" \
     --js_out="import_style=commonjs,binary:lib" \
-    --ts_out="grpc_js,service=grpc-web:lib" \
+    --ts_out="service=grpc-web:lib" \
     -I${PROTOLOC:-..} ${PROTOLOC:-..}/*.proto
