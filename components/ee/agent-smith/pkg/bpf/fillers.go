0f0af636258e6f8bba4b2a1c46bca371966cf3f2
Lorenzo Fontana <fontanalorenz@gmail.com>
2021-06-07 16:28:36 +0000

    Fix license header for agent-smith

--- components/ee/agent-smith/pkg/bpf/fillers.go
+++ components/ee/agent-smith/pkg/bpf/fillers.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+// Licensed under the Gitpod Enterprise Source Code License,
+// See License.enterprise.txt in the project root folder.
+
 package bpf
 
 // fillersHash needs to be in sync with the generated ppm_fillers.go file
