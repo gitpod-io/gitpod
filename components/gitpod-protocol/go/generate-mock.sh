a1a94454f39fdf46079c629e4d937875555a03a8
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-06-10 18:18:47 -0400

    [gitpod-protocol] Refactor generation of gitpod-schema.json

---
+++ components/gitpod-protocol/go/generate-mock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+# Licensed under the GNU Affero General Public License (AGPL).
+# See License-AGPL.txt in the project root for license information.
+
+set -x
+
+go get github.com/golang/mock/mockgen@v1.5.0
+
+mockgen \
+    -package=protocol \
+    -self_package=github.com/gitpod-io/gitpod/gitpod-protocol \
+    -source=gitpod-service.go > mock.go_tmp > mock.go_tmp
+
+mv mock.go_tmp mock.go
+
+leeway run components:update-license-header
--- components/gitpod-protocol/hack/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-go get -u github.com/a-h/generate/...
-node -e "new (require(\"typescript-parser\").TypescriptParser)().parseFiles([\"../src/gitpod-service.ts\", \"../src/protocol.ts\", \"../src/workspace-instance.ts\"], \"../..\").then(a => console.log(JSON.stringify(a)))" | \
-jq '[.[0] | .declarations[2].methods | .[] | {name: .name, params: .parameters, result: .type}]' | \
-go run generate.go
