9c8737800a2e4627ab699502ab4a8df28a613bcf
Lorenzo Fontana <fontanalorenz@gmail.com>
2021-06-14 10:18:27 +0000

    WORKSPACE_KERNEL environment variable
    Needed because the werft build executor and the final workspace are not
    guaranteed to run the same kernel.
    
    Also simplified the scripts by using the workspace kernel coming from
    the workspace's package manager directly instead of re-downloading.

--- components/ee/agent-smith/scripts/prepare-bpf-dev-environment.sh
+++ components/ee/agent-smith/scripts/prepare-bpf-dev-environment.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
-vmlinux_url="http://security.ubuntu.com/ubuntu/pool/main/l/linux-signed-gke-5.4/linux-image-5.4.0-1033-gke_5.4.0-1033.35~18.04.1_amd64.deb"
-modules_url="http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke-5.4/linux-modules-5.4.0-1033-gke_5.4.0-1033.35~18.04.1_amd64.deb"
+if [ ! "$WORKSPACE_KERNEL" == "$(uname -r)" ]; then
+    echo "The WORKSPACE_KERNEL environment variable and the current running workspace kernel are not equal, please update the WORKSPACE_KERNEL variable accordingly in the workspace dockerfile: dev/image/Dockerfile"
+    echo "WORKPACE_KERNEL=${WORKSPACE_KERNEL}"
+    echo "Current kernel=$(uname -r)"
+    exit 1
+fi
+
 img_url="https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.tar.gz"
 
 script_dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -12,8 +17,6 @@ outdir="${script_dirname}/_output"
 rm -Rf $outdir
 mkdir -p $outdir
 
-curl -L -o "${outdir}/linux-image.deb" $vmlinux_url
-curl -L -o "${outdir}/linux-modules.deb" $modules_url
 curl -L -o "${outdir}/rootfs.tar.gz" $img_url
 
 cd $outdir
@@ -25,16 +28,15 @@ qemu-img resize bionic-server-cloudimg-amd64.img +20G
 sudo virt-customize -a bionic-server-cloudimg-amd64.img --run-command 'resize2fs /dev/sda'
 
 sudo virt-customize -a bionic-server-cloudimg-amd64.img --root-password password:root
-sudo virt-customize -a bionic-server-cloudimg-amd64.img --copy-in linux-modules.deb:/root
 
-sudo virt-customize -a bionic-server-cloudimg-amd64.img --run-command 'dpkg -i /root/linux-modules.deb'
+# copy kernel modules
+sudo virt-customize -a bionic-server-cloudimg-amd64.img --copy-in /lib/modules/$WORKSPACE_KERNEL:/lib/modules
 
 sudo virt-customize -a bionic-server-cloudimg-amd64.img --run-command 'echo "[Network]\nDHCP=ipv4" > /etc/systemd/network/20-dhcp.network'
 
 sudo virt-customize -a bionic-server-cloudimg-amd64.img --run-command 'apt remove openssh-server -y && apt install openssh-server -y'
 sudo virt-customize -a bionic-server-cloudimg-amd64.img --run-command "sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
 sudo virt-customize -a bionic-server-cloudimg-amd64.img --run-command "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
-ar x linux-image.deb
-tar -xvf data.tar.xz
+
 
 echo "BPF environment is ready"
