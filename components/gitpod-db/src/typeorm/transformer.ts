aafd37088bd83d68c5691729e1c91acca725a65b
Cornelius A. Ludmann <cornelius@gitpod.io>
2021-07-13 12:24:34 +0000

    Revert "[typeorm] Catch JSON error in SIMPLE_JSON transformer"
    This reverts commit 1e78c6c96e6d5c6367ba4f7a6ab73d6e9a389cde.

--- components/gitpod-db/src/typeorm/transformer.ts
+++ components/gitpod-db/src/typeorm/transformer.ts
@@ -6,7 +6,6 @@
 
 import { ValueTransformer } from "typeorm/decorator/options/ValueTransformer";
 import { EncryptionService } from "@gitpod/gitpod-protocol/lib/encryption/encryption-service";
-import { log } from "@gitpod/gitpod-protocol/lib/util/logging";
 
 
 export namespace Transformer {
@@ -46,12 +45,7 @@ export namespace Transformer {
                 return JSON.stringify(value || defaultValue);
             },
             from(value: any): any {
-                try {
-                    return typeof value === 'object' ? value : JSON.parse(value);
-                } catch (e) {
-                    log.error(`Cannot parse JSON during TypeORM transformation. Returning default value '${JSON.stringify(defaultValue)}' instead. Value: ${JSON.stringify(value)}`, e);
-                    return defaultValue;
-                }
+                return JSON.parse(value);
             }
         };
     }
