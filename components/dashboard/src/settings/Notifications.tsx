264071fc8b50c8f55b789aa571399a406f110d11
Cornelius A. Ludmann <cornelius@gitpod.io>
2021-07-19 17:10:52 +0000

    [dashboard] Fix email notifications checkbox

--- components/dashboard/src/settings/Notifications.tsx
+++ components/dashboard/src/settings/Notifications.tsx
@@ -4,7 +4,7 @@
  * See License-AGPL.txt in the project root for license information.
  */
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { getGitpodService } from "../service/service";
 import { UserContext } from "../user-context";
 import CheckBox from "../components/CheckBox";
@@ -13,15 +13,16 @@ import settingsMenu from "./settings-menu";
 
 export default function Notifications() {
     const { user, setUser } = useContext(UserContext);
-    const isTransactionalMail = !user?.additionalData?.emailNotificationSettings?.disallowTransactionalEmails;
-    const toggleTransactionalMail = async ()=>{
+
+    const [isTransactionalMail, setTransactionMail] = useState(!user?.additionalData?.emailNotificationSettings?.disallowTransactionalEmails);
+    const toggleTransactionalMail = async () => {
         if (user) {
             user.additionalData = {
-                ... {
-                    ... user.additionalData,
+                ...{
+                    ...user.additionalData,
                     emailNotificationSettings: {
-                        ... user.additionalData?.emailNotificationSettings,
-                        disallowTransactionalEmails: !!isTransactionalMail
+                        ...user.additionalData?.emailNotificationSettings,
+                        disallowTransactionalEmails: isTransactionalMail
                     }
                 }
             }
@@ -29,31 +30,34 @@ export default function Notifications() {
                 additionalData: user.additionalData
             });
             setUser(user);
+            setTransactionMail(!isTransactionalMail);
         }
     };
-    const isMarketingMail = !!user?.allowsMarketingCommunication;
-    const toggleMarketingMail = async ()=> {
+
+    const [isMarketingMail, setMarketingMail] = useState(!!user?.allowsMarketingCommunication);
+    const toggleMarketingMail = async () => {
         if (user) {
-            user.allowsMarketingCommunication = !user?.allowsMarketingCommunication;
+            user.allowsMarketingCommunication = !isMarketingMail;
             await getGitpodService().server.updateLoggedInUser({
                 allowsMarketingCommunication: user.allowsMarketingCommunication
             });
             setUser(user);
+            setMarketingMail(!isMarketingMail);
         }
     }
     return <div>
-        <PageWithSubMenu subMenu={settingsMenu}  title='Notifications' subtitle='Choose when to be notified.'>
+        <PageWithSubMenu subMenu={settingsMenu} title='Notifications' subtitle='Choose when to be notified.'>
             <h3>Email Notification Preferences</h3>
             <CheckBox
                 title="Account Notifications"
                 desc="Receive emails about changes to your account"
                 checked={isTransactionalMail}
-                onChange={toggleTransactionalMail}/>
+                onChange={toggleTransactionalMail} />
             <CheckBox
                 title="Product Notifications"
                 desc="Receive emails about product updates and news"
                 checked={isMarketingMail}
-                onChange={toggleMarketingMail}/>
+                onChange={toggleMarketingMail} />
         </PageWithSubMenu>
     </div>;
 }
