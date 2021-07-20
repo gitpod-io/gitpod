15988767a13d84f10d94dc9b52fa099421ba6cd2
Christian Weichel <chris@gitpod.io>
2021-07-20 20:03:17 +0000

    [image-builder-mk3] Improve in-build log output

--- components/image-builder-bob/cmd/root.go
+++ components/image-builder-bob/cmd/root.go
@@ -20,7 +20,7 @@ var rootCmd = &cobra.Command{
 
 // Execute runs the root command
 func Execute() {
-	log.Init("bob", "", true, true)
+	log.Init("bob", "", false, false)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
