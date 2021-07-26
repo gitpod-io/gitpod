9b9f3d7228f679b1c9a9f2eb29934c5c38964ee2
Alex Tugarev <alex@gitpod.io>
2021-07-26 14:32:05 +0000

    Revert "DO NOT MERGE THIS"
    This reverts commit 2114062978cf4ac91b0a2e4c1f73a79fcf91fe2e.

--- components/licensor/typescript/ee/src/index.ts
+++ components/licensor/typescript/ee/src/index.ts
@@ -5,7 +5,7 @@
  */
 
 import { injectable, inject, postConstruct } from 'inversify';
-import { init, Instance, dispose, inspect, validate } from "./nativemodule";
+import { init, Instance, dispose, isEnabled, hasEnoughSeats, canUsePrebuild, inspect, validate } from "./nativemodule";
 import { Feature, LicensePayload } from './api';
 
 export const LicenseKeySource = Symbol("LicenseKeySource");
@@ -38,30 +38,27 @@ export class LicenseEvaluator {
 
     public async reloadLicense() {
         this.dispose()
-        // await this.init()
+        await this.init()
     }
 
     public validate(): { msg?: string, valid: boolean } {
-        // const v = validate(this.instanceID);
-        // if (v.valid) {
+        const v = validate(this.instanceID);
+        if (v.valid) {
             return { valid: true };
-        // }
-        // return { msg: v.msg, valid: false };
+        }
+        return { msg: v.msg, valid: false };
     }
 
     public isEnabled(feature: Feature): boolean {
-        // return isEnabled(this.instanceID, feature);
-        return true;
+        return isEnabled(this.instanceID, feature);
     }
 
     public hasEnoughSeats(seats: number): boolean {
-        // return hasEnoughSeats(this.instanceID, seats);
-        return true;
+        return hasEnoughSeats(this.instanceID, seats);
     }
 
     public canUsePrebuild(totalPrebuildSecondsSpent: number): boolean {
-        // return canUsePrebuild(this.instanceID, totalPrebuildSecondsSpent);
-        return true;
+        return canUsePrebuild(this.instanceID, totalPrebuildSecondsSpent);
     }
 
     public inspect(): LicensePayload {
