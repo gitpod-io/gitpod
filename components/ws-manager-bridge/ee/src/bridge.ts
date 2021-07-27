8b16b13b5e60363527eae4efc25ae13f9dbd321e
Christian Weichel <chris@gitpod.io>
2021-07-27 18:20:28 +0000

    [ws-manager-bridge] Don't interpret failed tasks as aborted prebuild

--- components/ws-manager-bridge/ee/src/bridge.ts
+++ components/ws-manager-bridge/ee/src/bridge.ts
@@ -75,12 +75,13 @@ export class WorkspaceManagerBridgeEE extends WorkspaceManagerBridge {
                     prebuild.error = status.conditions!.timeout;
                     headlessUpdateType = HeadlessWorkspaceEventType.AbortedTimedOut;
                 } else if (!!status.conditions!.failed) {
-                    prebuild.state = "aborted"
+                    prebuild.state = "aborted";
                     prebuild.error = status.conditions!.failed;
                     headlessUpdateType = HeadlessWorkspaceEventType.Aborted;
                 } else if (!!status.conditions!.headlessTaskFailed) {
-                    prebuild.state = "aborted"
+                    prebuild.state = "available";
                     prebuild.error = status.conditions!.headlessTaskFailed;
+                    prebuild.snapshot = status.conditions!.snapshot;
                     headlessUpdateType = HeadlessWorkspaceEventType.FinishedButFailed;
                 } else {
                     prebuild.state = "available";
