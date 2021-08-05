d4c58005ef79b07676fa0319cdca7c0a3ef3da84
George Tsiolis <tsiolis.g@gmail.com>
2021-08-05 14:41:13 +0000

    Update right menu style

--- components/dashboard/src/components/PillMenuItem.tsx
+++ components/dashboard/src/components/PillMenuItem.tsx
@@ -12,7 +12,7 @@ export default function PillMenuItem(p: {
     link?: string,
     onClick?: (event: React.MouseEvent) => void
 }) {
-    const classes = 'flex block text-sm font-medium dark:text-gray-200 px-3 px-0 py-1.5 rounded-md transition ease-in-out ' +
+    const classes = 'flex block font-medium dark:text-gray-200 px-2 py-1 rounded-lg transition ease-in-out ' +
         (p.selected
             ? 'bg-gray-200 dark:bg-gray-700'
             : 'text-gray-600 hover:bg-gray-100 dark:hover:bg-gray-800');
