558f29b1485f81764031a222c370e4094128ed5f
Jan Keromnes <janx@linux.com>
2021-08-09 07:15:08 +0000

    [dashboard] Disallow team names that might conflict with dashboard URLs

---
+++ components/gitpod-db/src/the-big-username-blocklist.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+// Source: https://github.com/marteinn/The-Big-Username-Blacklist-JS/blob/master/src/index.js
+declare module 'the-big-username-blacklist' {
+    export function validate (username: string): boolean;
+    export var list: string[];
+}
