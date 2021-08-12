b589cd69b09d66e3f941209966692bd1a539c3f6
Leonardo Di Donato <leodidonato@gmail.com>
2021-08-12 17:53:56 +0200

    new(agent-smith): autofill support
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- components/ee/agent-smith/pkg/bpf/fillers_table.go
+++ components/ee/agent-smith/pkg/bpf/fillers_table.go
@@ -8,20 +8,46 @@ const (
 	PPM_MAX_AUTOFILL_ARGS = (1 << 2)
 )
 
+type PPMAutofillParamType uint32
+
+const (
+	APT_REG  PPMAutofillParamType = 0
+	APT_SOCK PPMAutofillParamType = 1
+)
+
+type PPMAutofillArgIDType int16
+
+const (
+	AF_ID_USEDEFAULT PPMAutofillArgIDType = -2
+	AF_ID_RETVAL     PPMAutofillArgIDType = -1
+	AF_ID_1ST        PPMAutofillArgIDType = 0
+	AF_ID_2ND        PPMAutofillArgIDType = 1
+	AF_ID_3RD        PPMAutofillArgIDType = 2
+)
+
 type PPMAutofillArg struct {
-	ID         int16
+	ID         PPMAutofillArgIDType
 	DefaultVal float64
 }
 
 type PPMEventEntry struct {
 	FillerCallback [8]uint8 // unused in the BPF implementation, kept for offset
-	FillerID       uint32
+	FillerID       PPMFillerID
 	NAutoFillArgs  uint16
-	ParamType      uint32
+	ParamType      PPMAutofillParamType
 	AutofillArg    [PPM_MAX_AUTOFILL_ARGS]PPMAutofillArg
 }
 
-var FillersTable = map[uint32]PPMEventEntry{
+var FillersTable = map[PPMEEventType]PPMEventEntry{
+	PPME_SOCKET_CONNECT_E: {
+		FillerID:      PPM_FILLER_sys_autofill,
+		NAutoFillArgs: 1,
+		ParamType:     APT_SOCK,
+		AutofillArg:   [PPM_MAX_AUTOFILL_ARGS]PPMAutofillArg{{ID: AF_ID_1ST}},
+	},
+	PPME_SOCKET_CONNECT_X: {
+		FillerID: PPM_FILLER_sys_connect_x,
+	},
 	PPME_SYSCALL_EXECVE_19_E: {
 		FillerID: PPM_FILLER_sys_execve_e,
 	},
