b85c375952b0a5c7780971863d8854c836e9707c
Leonardo Di Donato <leodidonato@gmail.com>
2021-08-12 17:55:40 +0200

    update(agent-smith): add connect syscall to syscall table
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- components/ee/agent-smith/pkg/bpf/syscall_table.go
+++ components/ee/agent-smith/pkg/bpf/syscall_table.go
@@ -4,16 +4,19 @@
 
 package bpf
 
-import "golang.org/x/sys/unix"
+import (
+	"golang.org/x/sys/unix"
+)
 
 type SyscallEventPair struct {
 	Flags          uint32
-	EnterEventType uint32
-	ExitEventType  uint32
+	EnterEventType PPMEEventType
+	ExitEventType  PPMEEventType
 }
 
 var SyscallsTable = map[uint32]SyscallEventPair{
-	unix.SYS_EXECVE: {Flags: UF_USED | UF_NEVER_DROP | UF_SIMPLEDRIVER_KEEP, EnterEventType: PPME_SYSCALL_EXECVE_19_E, ExitEventType: PPME_SYSCALL_EXECVE_19_X},
+	unix.SYS_CONNECT: {Flags: UF_USED | UF_NEVER_DROP | UF_SIMPLEDRIVER_KEEP, EnterEventType: PPME_SOCKET_CONNECT_E, ExitEventType: PPME_SOCKET_CONNECT_X}, // fixme > do we want UF_NEVER_DROP?
+	unix.SYS_EXECVE:  {Flags: UF_USED | UF_NEVER_DROP | UF_SIMPLEDRIVER_KEEP, EnterEventType: PPME_SYSCALL_EXECVE_19_E, ExitEventType: PPME_SYSCALL_EXECVE_19_X},
 	// unix.SYS_CLONE:  {Flags: UF_USED | UF_NEVER_DROP | UF_SIMPLEDRIVER_KEEP, EnterEventType: PPME_SYSCALL_CLONE_20_E, ExitEventType: PPME_SYSCALL_CLONE_20_X},
 	// unix.SYS_FORK:   {Flags: UF_USED | UF_NEVER_DROP | UF_SIMPLEDRIVER_KEEP, EnterEventType: PPME_SYSCALL_FORK_E, ExitEventType: PPME_SYSCALL_FORK_X},
 	// unix.SYS_VFORK:  {Flags: UF_USED | UF_NEVER_DROP | UF_SIMPLEDRIVER_KEEP, EnterEventType: PPME_SYSCALL_VFORK_E, ExitEventType: PPME_SYSCALL_VFORK_X},
--- components/ee/agent-smith/pkg/bpf/syscalls.go
+++ components/ee/agent-smith/pkg/bpf/syscalls.go
@@ -14,14 +14,18 @@ const (
 	UF_ATOMIC            uint32 = (1 << 4) // The handler should not block (interrupt context)
 )
 
+type PPMEEventType uint32
+
 // ppme event types
 const (
-	PPME_SYSCALL_FORK_E      uint32 = 182
-	PPME_SYSCALL_FORK_X      uint32 = 183
-	PPME_SYSCALL_VFORK_E     uint32 = 184
-	PPME_SYSCALL_VFORK_X     uint32 = 185
-	PPME_SYSCALL_CLONE_20_E  uint32 = 222
-	PPME_SYSCALL_CLONE_20_X  uint32 = 223
-	PPME_SYSCALL_EXECVE_19_E uint32 = 292
-	PPME_SYSCALL_EXECVE_19_X uint32 = 293
+	PPME_SOCKET_CONNECT_E    PPMEEventType = 22
+	PPME_SOCKET_CONNECT_X    PPMEEventType = 23
+	PPME_SYSCALL_FORK_E      PPMEEventType = 182
+	PPME_SYSCALL_FORK_X      PPMEEventType = 183
+	PPME_SYSCALL_VFORK_E     PPMEEventType = 184
+	PPME_SYSCALL_VFORK_X     PPMEEventType = 185
+	PPME_SYSCALL_CLONE_20_E  PPMEEventType = 222
+	PPME_SYSCALL_CLONE_20_X  PPMEEventType = 223
+	PPME_SYSCALL_EXECVE_19_E PPMEEventType = 292
+	PPME_SYSCALL_EXECVE_19_X PPMEEventType = 293
 )
