64246285eb1e9022777fbc86c62dfdca70d3001b
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-08-12 19:19:22 -0400

    [docker-up] Update docker-compose and slirp4netns

--- components/docker-up/dependencies.sh
+++ components/docker-up/dependencies.sh
@@ -6,8 +6,8 @@
 set -euo pipefail
 
 DOCKER_VERSION=19.03.15
-DOCKER_COMPOSE_VERSION=1.28.5
-SLIRP4NETNS_VERSION=v1.1.9
+DOCKER_COMPOSE_VERSION=1.29.2
+SLIRP4NETNS_VERSION=v1.1.12
 
 curl -o docker.tgz      -fsSL https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz
 curl -o docker-compose  -fsSL https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-Linux-x86_64
