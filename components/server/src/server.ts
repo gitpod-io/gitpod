b726e23c11806eb168e4a7bfee9aec2a89436b35
Laurie T. Malau <laurie@gitpod.io>
2021-08-13 10:50:48 +0000

    [server] unsubscribe controller for email campaign

--- components/server/src/server.ts
+++ components/server/src/server.ts
@@ -41,6 +41,7 @@ import { CodeSyncService } from './code-sync/code-sync-service';
 import { increaseHttpRequestCounter, observeHttpRequestDuration } from './prometheus-metrics';
 import { OAuthController } from './oauth-server/oauth-controller';
 import { HeadlessLogController } from './workspace/headless-log-controller';
+import { NewsletterSubscriptionController } from './user/newsletter-subscription-controller';
 import { Config } from './config';
 
 @injectable()
@@ -73,6 +74,7 @@ export class Server<C extends GitpodClient, S extends GitpodServer> {
 
     @inject(HostContextProvider) protected readonly hostCtxProvider: HostContextProvider;
     @inject(OAuthController) protected readonly oauthController: OAuthController;
+    @inject(NewsletterSubscriptionController) protected readonly newsletterSubscriptionController: NewsletterSubscriptionController;
 
     protected readonly eventEmitter = new EventEmitter();
     protected app?: express.Application;
@@ -260,6 +262,7 @@ export class Server<C extends GitpodClient, S extends GitpodServer> {
         app.use('/workspace-download', this.workspaceDownloadService.apiRouter);
         app.use('/code-sync', this.codeSyncService.apiRouter);
         app.use('/headless-logs', this.headlessLogController.apiRouter);
+        app.use(this.newsletterSubscriptionController.apiRouter);
         app.use("/version", (req: express.Request, res: express.Response, next: express.NextFunction) => {
             res.send(this.env.version);
         });
