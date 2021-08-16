70c591d0da033c49038f3bda0779199e25e20568
Prince Rachit Sinha <prince@gitpod.io>
2021-08-16 14:40:15 +0000

    Fail werft if cert copy fails

--- .werft/util/certs.ts
+++ .werft/util/certs.ts
@@ -93,5 +93,7 @@ export async function installCertficate(werft, params: InstallCertificateParams)
     }
     if (!notReadyYet) {
         werft.log('certificate', `copied certificate from "${params.certNamespace}/${params.certName}" to "${params.destinationNamespace}/${params.certSecretName}"`);
+    } else {
+        werft.fail('certificate', `failed to copy certificate from "${params.certNamespace}/${params.certName}" to "${params.destinationNamespace}/${params.certSecretName}"`)
     }
 }
