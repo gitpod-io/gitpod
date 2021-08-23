730209850ad1c68c3fd203da8684cb27386b2aab
Alex Tugarev <alex@gitpod.io>
2021-08-23 08:14:47 +0000

    [db] remove "on update" from "creationTime" columns

---
+++ components/gitpod-db/src/typeorm/migration/1629706076011-FixCreationTimeColumns.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class FixCreationTimeColumns1629706076011 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE d_b_app_installation MODIFY creationTime timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6);");
+        await queryRunner.query("ALTER TABLE d_b_prebuilt_workspace MODIFY creationTime timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6);");
+        await queryRunner.query("ALTER TABLE d_b_snapshot MODIFY creationTime timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6);");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
