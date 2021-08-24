d8777beaec0971cb1c3b73ad55e10258041f3605
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-08-24 13:45:27 -0400

    label

--- scripts/build-selector.sh
+++ scripts/build-selector.sh
@@ -18,6 +18,7 @@ affinity:
           operator: In
           values:
           - "true"
+      - labelSelector:
         - key: kubernetes.io/hostname
           operator: In
           values:
@@ -35,6 +36,7 @@ affinity:
               operator: In
               values:
               - "true"
+          - labelSelector:
             - key: kubernetes.io/hostname
               operator: In
               values:
