a74edad10343240047b811ef3a291cfe46453340
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-08-25 11:54:53 -0400

    Update go format

--- components/ws-daemon-api/go/tools.go
+++ components/ws-daemon-api/go/tools.go
@@ -2,6 +2,7 @@
 // Licensed under the GNU Affero General Public License (AGPL).
 // See License-AGPL.txt in the project root for license information.
 
+//go:build tools
 // +build tools
 
 // helper to avoid adding/removing dependencies required by grpc but not by the module itself
