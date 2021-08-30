c869cd0531fd2efb807ac3b46921ff57ad2e8cea
Laurie T. Malau <laurie@gitpod.io>
2021-08-30 14:26:21 +0000

    [db] Fix string booleans to be booleans
    Fixes #5328

---
+++ components/gitpod-db/src/typeorm/migration/1630333424897-ReplaceStringBooleansWithBooleans.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+import {MigrationInterface, QueryRunner} from "typeorm";
+import { columnExists } from "./helper/helper";
+
+export class ReplaceStringBooleansWithBooleans1630333424897 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        if (await columnExists(queryRunner, "d_b_user", "additionalData")) {
+            await queryRunner.query("update d_b_user set additionalData = json_set(additionalData, '$.emailNotificationSettings.allowsChangelogMail', true) where json_extract(additionalData, '$.emailNotificationSettings.allowsChangelogMail') = 'true'");
+            await queryRunner.query("update d_b_user set additionalData = json_set(additionalData, '$.emailNotificationSettings.allowsChangelogMail', false) where json_extract(additionalData, '$.emailNotificationSettings.allowsChangelogMail') = 'false'")
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
