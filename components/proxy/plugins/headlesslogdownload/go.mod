b86bf2a931dd9cf43994e7a2f2b308c4e31d55a2
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-09-01 11:32:49 +0000

    [proxy, server] Introduce '/headless-log-download' endpoint for transparent downloags of headless logs

---
+++ components/proxy/plugins/headlesslogdownload/go.mod
@@ -0,0 +1,5 @@
+module github.com/gitpod-io/gitpod/proxy/plugins/headlesslogdownload
+
+go 1.17
+
+require github.com/caddyserver/caddy/v2 v2.4.3
--- components/server/src/workspace/headless-log-controller.ts
+++ components/server/src/workspace/headless-log-controller.ts
@@ -6,7 +6,7 @@
 
 import { inject, injectable } from "inversify";
 import * as express from 'express';
-import { HEADLESS_LOG_STREAM_STATUS_CODE, Queue, TeamMemberInfo, User } from "@gitpod/gitpod-protocol";
+import { HEADLESS_LOG_STREAM_STATUS_CODE, Queue, TeamMemberInfo, User, Workspace, WorkspaceInstance } from "@gitpod/gitpod-protocol";
 import { log } from '@gitpod/gitpod-protocol/lib/util/logging';
 import { CompositeResourceAccessGuard, OwnerResourceGuard, TeamMemberResourceGuard, WorkspaceLogAccessGuard } from "../auth/resource-access";
 import { DBWithTracing, TracedWorkspaceDB } from "@gitpod/gitpod-db/lib/traced-db";
@@ -22,6 +22,8 @@ import { BearerAuth } from "../auth/bearer-authenticator";
 import { ProjectsService } from "../projects/projects-service";
 import { HostContextProvider } from "../auth/host-context-provider";
 
+export const HEADLESS_LOGS_PATH_PREFIX = "/headless-logs";
+export const HEADLESS_LOG_DOWNLOAD_PATH_PREFIX = "/headless-log-download";
 
 @injectable()
 export class HeadlessLogController {
@@ -33,69 +35,26 @@ export class HeadlessLogController {
     @inject(TeamDB) protected readonly teamDb: TeamDB;
     @inject(HostContextProvider) protected readonly hostContextProvider: HostContextProvider;
 
-    get apiRouter(): express.Router {
+    get headlessLogs(): express.Router {
         const router = express.Router();
 
         router.use(this.auth.restHandlerOptionally);
-        router.get("/:instanceId/:terminalId", asyncHandler(async (req: express.Request, res: express.Response) => {
-            const span = opentracing.globalTracer().startSpan("/headless-logs/");
+        router.get("/:instanceId/:terminalId", [authenticateAndAuthorize, asyncHandler(async (req: express.Request, res: express.Response) => {
+            const span = opentracing.globalTracer().startSpan(HEADLESS_LOGS_PATH_PREFIX);
             const params = { instanceId: req.params.instanceId, terminalId: req.params.terminalId };
-            if (!(isWithFunctionAccessGuard(req) && req.functionGuard?.canAccess(accesHeadlessLogs)) && !(req.isAuthenticated() && User.is(req.user))) {
-                res.sendStatus(403);
-                log.warn("unauthenticated headless log request", params);
-                return;
-            }
-
-            const user = req.user as User;
-            if (!User.is(user)) {
-                res.sendStatus(401);
-                return;
-            }
-            if (user.blocked) {
-                res.sendStatus(403);
-                log.warn("blocked user attempted to fetch workspace cookie", { ...params, userId: user.id });
-                return;
-            }
+            const user = req.user as User;  // verified by authenticateAndAuthorize
 
             const instanceId = params.instanceId;
-            const [workspace, instance] = await Promise.all([
-                this.workspaceDb.trace({span}).findByInstanceId(instanceId),
-                this.workspaceDb.trace({span}).findInstanceById(instanceId),
-            ]);
-            if (!workspace) {
-                res.sendStatus(404);
-                log.warn(`workspace for instanceId ${instanceId} not found`);
+            const ws = await this.authorizeHeadlessLogAccess(span, user, instanceId, res);
+            if (!ws) {
                 return;
             }
-            if (!instance) {
-                res.sendStatus(404);
-                log.warn(`instance ${instanceId} not found`);
-                return;
-            }
-            let teamMembers: TeamMemberInfo[] = [];
-            if (workspace?.projectId) {
-                const p = await this.projectService.getProject(workspace.projectId);
-                if (p?.teamId) {
-                    teamMembers = await this.teamDb.findMembersByTeam(p.teamId);
-                }
-            }
-            const logCtx = { userId: user.id, instanceId, workspaceId: workspace.id };
-            log.debug(logCtx, "/headless-log/");
-            log.debug("VERSION: " + req.httpVersion);
+            const { workspace, instance } = ws;
+
+            const logCtx = { userId: user.id, instanceId, workspaceId: workspace!.id };
+            log.debug(logCtx, HEADLESS_LOGS_PATH_PREFIX);
 
             try {
-                // [gpl] It's a bit sad that we have to duplicate this access check... but that's due to the way our API code is written
-                const resourceGuard = new CompositeResourceAccessGuard([
-                    new OwnerResourceGuard(user.id),
-                    new TeamMemberResourceGuard(user.id),
-                    new WorkspaceLogAccessGuard(user, this.hostContextProvider),
-                ]);
-                if (!await resourceGuard.canAccess({ kind: 'workspace', subject: workspace, teamMembers }, 'get')) {
-                    res.sendStatus(403);
-                    log.warn(logCtx, "unauthenticated headless log access");
-                    return;
-                }
-
                 const head = {
                     'Content-Type': 'text/html; charset=utf-8',  // is text/plain, but with that node.js won't stream...
                     'Transfer-Encoding': 'chunked',
@@ -142,22 +101,118 @@ export class HeadlessLogController {
                 res.write(`\n${HEADLESS_LOG_STREAM_STATUS_CODE}: 500`);
                 res.end();
             }
-        }));
-        router.get("/", (req: express.Request, res: express.Response) => {
-            // This is an error case: every request should match the handler above
-            const user = req.user as User;
-            if (!User.is(user)) {
-                res.sendStatus(401);
-                return;
-            }
-            if (user.blocked) {
-                res.sendStatus(403);
-                return;
-            }
-
-            log.error({ userId: user.id }, "/headless-logs: malformed request", { path: req.path });
-            res.status(400);
-        });
+        })]);
+        router.get("/", malformedRequestHandler);
         return router;
     }
+
+    get headlessLogDownload(): express.Router {
+        const router = express.Router();
+
+        router.use(this.auth.restHandlerOptionally);
+        router.get("/:instanceId/:taskId", [authenticateAndAuthorize, asyncHandler(async (req: express.Request, res: express.Response) => {
+            const span = opentracing.globalTracer().startSpan(HEADLESS_LOG_DOWNLOAD_PATH_PREFIX);
+            const params = { instanceId: req.params.instanceId, taskId: req.params.taskId };
+            const user = req.user as User;  // verified by authenticateAndAuthorize
+
+            const instanceId = params.instanceId;
+            const ws = await this.authorizeHeadlessLogAccess(span, user, instanceId, res);
+            if (!ws) {
+                return;
+            }
+            const { workspace, instance } = ws;
+
+            const logCtx = { userId: user.id, instanceId, workspaceId: workspace!.id };
+            log.debug(logCtx, HEADLESS_LOG_DOWNLOAD_PATH_PREFIX);
+
+            try {
+                const taskId = params.taskId;
+                const downloadUrl = await this.headlessLogService.getHeadlessLogDownloadUrl(user.id, instance, taskId);
+                res.send(downloadUrl);  // cmp. headless_log_download.go
+            } catch (err) {
+                log.error(logCtx, "error retrieving headless log download URL", err);
+                res.status(500);
+            }
+        })]);
+        router.get("/", malformedRequestHandler);
+        return router;
+    }
+
+    protected async authorizeHeadlessLogAccess(span: opentracing.Span, user: User, instanceId: string, res: express.Response): Promise<{ workspace: Workspace, instance: WorkspaceInstance } | undefined> {
+        const [workspace, instance] = await Promise.all([
+            this.workspaceDb.trace({span}).findByInstanceId(instanceId),
+            this.workspaceDb.trace({span}).findInstanceById(instanceId),
+        ]);
+        if (!workspace) {
+            res.sendStatus(404);
+            log.warn(`workspace for instanceId ${instanceId} not found`);
+            return undefined;
+        }
+        if (!instance) {
+            res.sendStatus(404);
+            log.warn(`instance ${instanceId} not found`);
+            return undefined;
+        }
+        const logCtx = { userId: user.id, instanceId, workspaceId: workspace.id };
+
+        let teamMembers: TeamMemberInfo[] = [];
+        if (workspace?.projectId) {
+            const p = await this.projectService.getProject(workspace.projectId);
+            if (p?.teamId) {
+                teamMembers = await this.teamDb.findMembersByTeam(p.teamId);
+            }
+        }
+
+        // [gpl] It's a bit sad that we have to duplicate this access check... but that's due to the way our API code is written
+        const resourceGuard = new CompositeResourceAccessGuard([
+            new OwnerResourceGuard(user.id),
+            new TeamMemberResourceGuard(user.id),
+            new WorkspaceLogAccessGuard(user, this.hostContextProvider),
+        ]);
+        if (!await resourceGuard.canAccess({ kind: 'workspaceLog', subject: workspace, teamMembers }, 'get')) {
+            res.sendStatus(403);
+            log.warn(logCtx, "unauthenticated headless log access");
+            return undefined;
+        }
+
+        return { workspace, instance };
+    }
+}
+
+function authenticateAndAuthorize(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const params = { instanceId: req.params.instanceId, terminalId: req.params.terminalId };
+    if (!(isWithFunctionAccessGuard(req) && req.functionGuard?.canAccess(accesHeadlessLogs)) && !(req.isAuthenticated() && User.is(req.user))) {
+        res.sendStatus(403);
+        log.warn("unauthenticated headless log request", params);
+        return;
+    }
+
+    const user = req.user as User;
+    if (!User.is(user)) {
+        res.sendStatus(401);
+        return;
+    }
+    if (user.blocked) {
+        res.sendStatus(403);
+        log.warn("blocked user attempted to access headless log", { ...params, userId: user.id });
+        return;
+    }
+
+    next();
+}
+
+function malformedRequestHandler(req: express.Request, res: express.Response) {
+    // This is an error case: every request should match the handler above
+    const user = req.user as User;
+    if (!User.is(user)) {
+        res.sendStatus(401);
+        return;
+    }
+    if (user.blocked) {
+        res.sendStatus(403);
+        return;
+    }
+
+    log.error({ userId: user.id }, "malformed request", { path: req.path });
+    res.status(400);
 }
