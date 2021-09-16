9e060ed3a5bc8606cf71aadb2a2bd37fd8c9e3e9
Simon Emms <simon@gitpod.io>
2021-09-16 12:06:02 +0100

    [agent-smith]: expose the config publicly

--- components/ee/agent-smith/cmd/config-schema.go
+++ components/ee/agent-smith/cmd/config-schema.go
@@ -7,6 +7,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gitpod-io/gitpod/agent-smith/pkg/config"
 
 	"github.com/alecthomas/jsonschema"
 	"github.com/gitpod-io/gitpod/common-go/log"
@@ -18,7 +19,7 @@ var configSchemaCmd = &cobra.Command{
 	Use:   "config-schema",
 	Short: "Generates the JSON schema validating the configuration",
 	Run: func(cmd *cobra.Command, args []string) {
-		schema := jsonschema.Reflect(&config{})
+		schema := jsonschema.Reflect(&config.ServiceConfig{})
 		schema.Title = "agent-smith config schema - generated using agent-smith config-schema"
 		out, err := json.MarshalIndent(schema, "", "  ")
 		if err != nil {
