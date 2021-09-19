eb0ec84e876d20540e30aad5e9decc6d9c784b8f
Leo Di Donato <leodidonato@gmail.com>
2021-09-19 23:39:14 +0000

    new(ipshot): DoH response struct
    Signed-off-by: Leo Di Donato <leodidonato@gmail.com>

---
+++ components/ee/agent-smith/ipshot/doh/res.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2020 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package doh
+
+// Response contains the response from a DNS Over HTTPS query.
+// Both Google and Cloudflare seem to share a scheme here.
+// See:
+// https://developers.google.com/speed/public-dns/docs/dns-over-https#dns_response_in_json
+// https://developers.cloudflare.com/1.1.1.1/dns-over-https/json-format
+//
+// Such schema partially resembles the one of RFC 8427 (section 5.2).
+type Response struct {
+	Status   int  `json:"Status"` // 0=NOERROR, 2=SERVFAIL - Standard DNS response code (32 bit integer)
+	TC       bool `json:"TC"`     // Whether the response is truncated
+	RD       bool `json:"RD"`
+	RA       bool `json:"RA"`
+	AD       bool `json:"AD"` // Whether all response data was validated with DNSSEC
+	CD       bool `json:"CD"` // Whether the client asked to disable DNSSEC
+	Question []struct {
+		Name string `json:"name"` // FQDN with trailing dot
+		Type int    `json:"type"` // Standard DNS RR type
+	} `json:"Question"`
+	Answer []struct {
+		Name string `json:"name"` // Always matches name in the Question section
+		Type int    `json:"type"` // Standard DNS RR type
+		TTL  int    `json:"TTL"`  // Record's time-to-live in seconds
+		Data string `json:"data"` // Data
+	} `json:"Answer"`
+	Additional       []interface{} `json:"Additional"`
+	EdnsClientSubnet string        `json:"edns_client_subnet"` // IP address / scope prefix-length
+	Comment          string        `json:"Comment"`            // Diagnostics information in case of an error
+}
