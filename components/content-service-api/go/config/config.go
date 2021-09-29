c065ea1169f6b72dde79e78716852449f84050a3
Simon Emms <simon@gitpod.io>
2021-09-29 08:51:46 +0100

    [content-service]: publicly expose the config

--- components/content-service-api/go/config/config.go
+++ components/content-service-api/go/config/config.go
@@ -4,7 +4,14 @@
 
 package config
 
-import "os"
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"golang.org/x/xerrors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"os"
+)
 
 // StorageConfig configures the remote storage we use
 type StorageConfig struct {
@@ -101,4 +108,63 @@ type MinIOConfig struct {
 
 	Region         string `json:"region"`
 	ParallelUpload uint   `json:"parallelUpload,omitempty"`
+}
+
+type Service struct {
+	Addr string    `json:"address"`
+	TLS  TLSConfig `json:"tls"`
+}
+
+type Prometheus struct {
+	Addr string `json:"address"`
+}
+
+type PProf struct {
+	Addr string `json:"address"`
+}
+
+type ServiceConfig struct {
+	// Daemon  daemon.Config `json:"daemon"`
+	Service    Service       `json:"service"`
+	Prometheus Prometheus    `json:"prometheus"`
+	PProf      PProf         `json:"pprof"`
+	Storage    StorageConfig `json:"storage"`
+}
+
+type TLSConfig struct {
+	Authority   string `json:"ca"`
+	Certificate string `json:"crt"`
+	PrivateKey  string `json:"key"`
+}
+
+// ServerOption produces the GRPC option that configures a server to use this TLS configuration
+func (c *TLSConfig) ServerOption() (grpc.ServerOption, error) {
+	if c.Authority == "" || c.Certificate == "" || c.PrivateKey == "" {
+		return nil, nil
+	}
+
+	// Load certs
+	certificate, err := tls.LoadX509KeyPair(c.Certificate, c.PrivateKey)
+	if err != nil {
+		return nil, xerrors.Errorf("cannot load TLS certificate: %w", err)
+	}
+
+	// Create a certificate pool from the certificate authority
+	certPool := x509.NewCertPool()
+	ca, err := os.ReadFile(c.Authority)
+	if err != nil {
+		return nil, xerrors.Errorf("cannot not read ca certificate: %w", err)
+	}
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		return nil, xerrors.Errorf("failed to append ca certs")
+	}
+
+	creds := credentials.NewTLS(&tls.Config{
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{certificate},
+		ClientCAs:    certPool,
+		MinVersion:   tls.VersionTLS12,
+	})
+
+	return grpc.Creds(creds), nil
 }
