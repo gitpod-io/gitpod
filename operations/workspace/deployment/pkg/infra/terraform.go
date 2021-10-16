a3dcac247077e0d257d5b5d5a4563b4178470d8b
Prince Rachit Sinha <prince@gitpod.io>
2021-10-16 11:36:53 +0000

    Define terraform methods

--- operations/workspace/deployment/pkg/infra/terraform.go
+++ operations/workspace/deployment/pkg/infra/terraform.go
@@ -52,6 +52,7 @@ type TerraformModule struct {
 	Region         string
 	ScriptPath     string
 	ScriptPathArgs string
+	ModulePath     string
 }
 
 // CreateTerraformModule creates a terraform module for kubernetes cluster creation and other resources
@@ -81,5 +82,44 @@ func (tm *TerraformModule) CreateTerraformModule() error {
 
 // ApplyTerraformModule does an equivalent of `terraform apply -auto-approve`
 func (tm *TerraformModule) ApplyTerraformModule() error {
-	panic("Not supported yet!!")
+	modulePathDir := filepath.Dir(tm.ModulePath)
+	modulePathDirName := filepath.Base(modulePathDir)
+
+	// TODO(prs): Replace this with stable terraform-exec when it is released
+	// cd {modulePathDirName} && terraform init && terraform apply -auto-approve
+	//
+	// e.g.
+	// cd deploy/production/ws-us89/terraform && terraform init && terraform apply -auto-approve
+	commandToRun := fmt.Sprintf(`cd %s && terraform init && terraform apply -auto-approve`, modulePathDirName)
+
+	cmd := exec.Command("/bin/sh", "-c", commandToRun)
+	err := cmd.Run()
+	stdout, _ := cmd.Output()
+	fmt.Print(string(stdout))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// DestroyTerraformModule does an equivalent of `terraform destroy -auto-approve`
+func (tm *TerraformModule) DestroyTerraformModule() error {
+	modulePathDir := filepath.Dir(tm.ModulePath)
+	modulePathDirName := filepath.Base(modulePathDir)
+
+	// TODO(prs): Replace this with stable terraform-exec when it is released
+	// cd {modulePathDirName} && terraform destroy -auto-approve
+	//
+	// e.g.
+	// cd deploy/production/ws-us89/terraform && terraform destroy -auto-approve
+	commandToRun := fmt.Sprintf(`cd %s && terraform destroy -auto-approve`, modulePathDirName)
+
+	cmd := exec.Command("/bin/sh", "-c", commandToRun)
+	err := cmd.Run()
+	stdout, _ := cmd.Output()
+	fmt.Print(string(stdout))
+	if err != nil {
+		return err
+	}
+	return nil
 }
