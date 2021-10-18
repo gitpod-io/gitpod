df13af2835bded9c7ce324e4e80d589ea8327ae8
Prince Rachit Sinha <prince@gitpod.io>
2021-10-18 09:28:49 +0000

    Broken: Add license header

--- operations/workspace/deployment/pkg/step/step.go
+++ operations/workspace/deployment/pkg/step/step.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
 package step
 
 // IStep is an interface that should be implemented by all steps of cluster lifecycle
