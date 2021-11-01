5fb1089bd8eca57b5089b3387553150292105748
Prince Rachit Sinha <prince@gitpod.io>
2021-11-01 08:11:10 +0000

    [ws-deployment] Refactor deployment code for parallel processing

--- operations/workspace/deployment/pkg/orchestrate/deploy.go
+++ operations/workspace/deployment/pkg/orchestrate/deploy.go
@@ -5,26 +5,48 @@
 package orchestrate
 
 import (
-	"sync"
-
 	log "github.com/gitpod-io/gitpod/common-go/log"
 	"github.com/gitpod-io/gitpod/ws-deployment/pkg/common"
 	"github.com/gitpod-io/gitpod/ws-deployment/pkg/step"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/xerrors"
 )
 
+// Deploy creates given workspace clusters and then deploys gitpod on them
 func Deploy(context *common.ProjectContext, clusters []*common.WorkspaceCluster) error {
-	var wg sync.WaitGroup
-	wg.Add(len(clusters))
-
-	for _, cluster := range clusters {
-		go func(context *common.ProjectContext, cluster *common.WorkspaceCluster) {
-			defer wg.Done()
-			err := step.CreateCluster(context, cluster)
+	eg := new(errgroup.Group)
+	for _, c := range clusters {
+		c := c
+		eg.Go(func() error {
+			err := createCluster(context, c)
 			if err != nil {
-				log.Log.Infof("error creating cluster %s: %s", cluster.Name, err)
+				return err
 			}
-		}(context, cluster)
+			err = installGitpod(context, c)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
 	}
-	wg.Wait()
+	if err := eg.Wait(); err != nil {
+		return xerrors.New(err.Error())
+	}
 	return nil
+}
+
+// TODO(prs): Add implementation once we have scripts in ops repo tested and
+// install step implemented
+func installGitpod(context *common.ProjectContext, cluster *common.WorkspaceCluster) error {
+	log.Log.Infof("received request to install gitpod on cluster: %s cannot be processed. Implementation pending", cluster.Name)
+	return nil
+}
+
+func createCluster(context *common.ProjectContext, cluster *common.WorkspaceCluster) error {
+	// TODO(prs): add retry logic below
+	err := step.CreateCluster(context, cluster)
+	if err != nil {
+		log.Log.Infof("error creating cluster %s: %s", cluster.Name, err)
+	}
+	return err
 }
