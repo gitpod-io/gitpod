96807fb737cb2093a6abd7152297fffdca81af82
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-11-01 14:17:42 +0000

    [werft] Cleanup build.ts around KUBECONFIG

--- .werft/util/gpctl.ts
+++ .werft/util/gpctl.ts
@@ -1,43 +1,33 @@
 import * as shell from 'shelljs';
-
-function buildRequiredFlags(pathToKubeConfig: string): string {
-    if (pathToKubeConfig != "") {
-        return ` --kubeconfig=${pathToKubeConfig}`
-    }
-    return ""
-}
+import { ExecOptions } from './shell';
 
 export function buildGpctlBinary() {
     shell.exec(`cd /workspace/dev/gpctl && go build && cd -`)
 }
 
-export function printClustersList(pathToKubeConfig: string): string {
-    let cmd = `/workspace/dev/gpctl/gpctl clusters list` + buildRequiredFlags(pathToKubeConfig)
-    const result = shell.exec(cmd).trim()
+export function printClustersList(shellOpts: ExecOptions): string {
+    const result = shell.exec(`/workspace/dev/gpctl/gpctl clusters list`, { ...shellOpts, async: false }).trim()
     return result
 }
 
-export function uncordonCluster(pathToKubeConfig: string, name: string): string {
-    let cmd = `/workspace/dev/gpctl/gpctl clusters uncordon --name=${name}` + buildRequiredFlags(pathToKubeConfig)
-    const result = shell.exec(cmd).trim()
+export function uncordonCluster(name: string, shellOpts: ExecOptions): string {
+    const result = shell.exec(`/workspace/dev/gpctl/gpctl clusters uncordon --name=${name}`, { ...shellOpts, async: false }).trim();
     return result
 }
 
-export function registerCluster(pathToKubeConfig: string, name: string, url: string): string {
-    let cmd = `/workspace/dev/gpctl/gpctl clusters register \
+export function registerCluster(name: string, url: string, shellOpts: ExecOptions): string {
+    const cmd = `/workspace/dev/gpctl/gpctl clusters register \
 	--name ${name} \
 	--hint-cordoned \
 	--hint-govern \
 	--tls-path ./wsman-tls \
-	--url ${url}` + buildRequiredFlags(pathToKubeConfig)
-
-    const result = shell.exec(cmd).trim()
+	--url ${url}`;
+    const result = shell.exec(cmd, { ...shellOpts, async: false }).trim();
     return result
 }
 
-export function getClusterTLS(pathToKubeConfig: string): string {
-    let cmd = `/workspace/dev/gpctl/gpctl clusters get-tls-config` + buildRequiredFlags(pathToKubeConfig)
-    const result = shell.exec(cmd).trim()
+export function getClusterTLS(shellOpts: ExecOptions): string {
+    const result = shell.exec(`/workspace/dev/gpctl/gpctl clusters get-tls-config`, { ...shellOpts, async: false }).trim()
     return result
 }
 
--- .werft/util/util.ts
+++ .werft/util/util.ts
@@ -1,3 +1,5 @@
+import { ExecOptions } from "./shell";
+
 export async function sleep(millis: number) {
     return new Promise((resolve) => {
         setTimeout(resolve, millis);
@@ -5,8 +7,24 @@ export async function sleep(millis: number) {
 }
 
 export function validateIPaddress(ipaddress) {
-    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
-      return true
-    }
-    return false
+  if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
+    return true
   }
+  return false
+}
+
+export function env(k8sConfigPath: string, _parent?: ExecOptions): ExecOptions {
+    const parent = _parent || {};
+    if (!parent.env) {
+        parent.env = {
+            ...process.env,
+        };
+    };
+    return {
+        ...parent,
+        env: {
+            ...parent.env,
+            "KUBECONFIG": k8sConfigPath,
+        },
+    }
+}
