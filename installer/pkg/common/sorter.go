0b6ac76a75f424a8cd3377323d00dade9f9d9459
Christian Weichel <chris@gitpod.io>
2021-11-07 11:24:20 +0000

    [installer] Improve rendered YAML output

--- installer/pkg/common/sorter.go
+++ installer/pkg/common/sorter.go
@@ -5,10 +5,13 @@
 package common
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"fmt"
 	"regexp"
-	"sigs.k8s.io/yaml"
 	"sort"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
 )
 
 // Those occurring earlier in the list get installed before those occurring later in the list.
@@ -18,8 +21,8 @@ var sortOrder = []string{
 	"Namespace",
 	"NetworkPolicy",
 	"ResourceQuota",
-	"Issuer",      // added
-	"Certificate", // added
+	"Issuer",
+	"Certificate",
 	"LimitRange",
 	"PodSecurityPolicy",
 	"PodDisruptionBudget",
@@ -44,7 +47,7 @@ var sortOrder = []string{
 	"Pod",
 	"ReplicationController",
 	"ReplicaSet",
-	"StatefulSet", // moved above Deployment
+	"StatefulSet",
 	"Deployment",
 	"HorizontalPodAutoscaler",
 	"Job",
@@ -53,21 +56,21 @@ var sortOrder = []string{
 	"APIService",
 }
 
-type k8sChart struct {
+type runtimeObject struct {
 	metav1.TypeMeta `json:",inline"`
+	Metadata        metav1.ObjectMeta `json:"metadata"`
 	Content         string
 }
 
-func DependencySortingRenderFunc(charts []string) ([]string, error) {
-	sortData := make([]k8sChart, 0)
-
-	for _, o := range charts {
+func DependencySortingRenderFunc(objects []string) ([]string, error) {
+	sortData := make([]runtimeObject, 0, len(objects))
+	for _, o := range objects {
 		// Assume multi-document YAML
 		re := regexp.MustCompile("(^|\n)---")
 		items := re.Split(o, -1)
 
 		for _, p := range items {
-			var v k8sChart
+			var v runtimeObject
 			err := yaml.Unmarshal([]byte(p), &v)
 			if err != nil {
 				return nil, err
@@ -90,9 +93,14 @@ func DependencySortingRenderFunc(charts []string) ([]string, error) {
 		return scoreI < scoreJ
 	})
 
-	out := make([]string, 0)
+	out := make([]string, 0, len(sortData))
 	for _, d := range sortData {
-		out = append(out, d.Content)
+		ctnt := strings.Trim(d.Content, "\n")
+		if len(strings.TrimSpace(ctnt)) == 0 {
+			continue
+		}
+		ctnt = fmt.Sprintf("# %s/%s %s\n%s", d.TypeMeta.APIVersion, d.TypeMeta.Kind, d.Metadata.Name, ctnt)
+		out = append(out, ctnt)
 	}
 
 	return out, nil
