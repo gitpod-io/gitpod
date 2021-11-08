3c8b60c0f64daaccc3e4989bc429a81bc6ea8d20
André Duarte <mail@andreduarte.net>
2021-11-08 13:01:46 +0000

    [jetbrains] Clean up JetBrains backend plugin

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/ide/jetbrains/backend/listeners/MyApplicationActivationListener.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/ide/jetbrains/backend/listeners/MyApplicationActivationListener.kt
@@ -4,12 +4,10 @@
 
 package io.gitpod.ide.jetbrains.backend.listeners
 
-import com.intellij.ide.ApplicationInitializedListener
 import com.intellij.openapi.application.ApplicationActivationListener
-import com.intellij.openapi.components.ServiceManager
-import io.gitpod.ide.jetbrains.backend.services.HeartbeatService
-import com.intellij.openapi.wm.IdeFrame
 import com.intellij.openapi.components.service
+import com.intellij.openapi.wm.IdeFrame
+import io.gitpod.ide.jetbrains.backend.services.HeartbeatService
 
 class MyApplicationActivationListener : ApplicationActivationListener {
     override fun applicationActivated(ideFrame: IdeFrame) {
--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/ide/jetbrains/backend/services/HeartbeatService.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/ide/jetbrains/backend/services/HeartbeatService.kt
@@ -8,33 +8,27 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.logger
 import io.gitpod.gitpodprotocol.api.ConnectionHelper
-import io.gitpod.gitpodprotocol.api.GitpodServer
 import io.gitpod.gitpodprotocol.api.entities.SendHeartBeatOptions
-import io.gitpod.ide.jetbrains.backend.services.ControllerStatusProvider.Companion.ControllerStatus
+import io.gitpod.ide.jetbrains.backend.services.ControllerStatusService.ControllerStatus
 import io.gitpod.ide.jetbrains.backend.utils.Retrier.retry
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.runBlocking
+import java.io.IOException
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.concurrent.thread
 import kotlin.random.Random.Default.nextInt
-import java.lang.Thread
 
 @Service
-class HeartbeatService() : Disposable {
+class HeartbeatService : Disposable {
     private val logger = logger<HeartbeatService>()
-    private val fetchToken: suspend () -> String = { AuthTokenService.fetchToken() }
-    private val controllerStatusProvider = ControllerStatusProvider()
 
     @Suppress("MagicNumber")
     private val intervalInSeconds = 30
 
-    private val uri = "wss://${System.getenv("GITPOD_HOST").split("//").last()}/api/v1"
-    private val origin = System.getenv("GITPOD_WORKSPACE_URL")
-    private val instanceId = System.getenv("GITPOD_INSTANCE_ID")
-
-    private val server = AtomicReference<GitpodServer>()
+    private val heartbeatClient = AtomicReference<HeartbeatClient>()
     private val status = AtomicReference(
         ControllerStatus(
             connected = false,
@@ -44,7 +38,7 @@ class HeartbeatService() : Disposable {
     private val closed = AtomicBoolean(false)
 
     init {
-        logger.info("Service initiated")
+        logger.info("Service initiating")
 
         @Suppress("MagicNumber")
         thread(name = "gitpod-heartbeat", contextClassLoader = this.javaClass.classLoader) {
@@ -59,15 +53,27 @@ class HeartbeatService() : Disposable {
 
     private suspend fun checkActivity(maxIntervalInSeconds: Int) {
         logger.info("Checking activity")
-        val status = controllerStatusProvider.fetch()
+        val status = try {
+            ControllerStatusService.fetch()
+        } catch (e: IOException) {
+            logger.error(e.message, e)
+            return@checkActivity
+        }
         val previousStatus = this.status.getAndSet(status)
 
-        if (status.connected != previousStatus.connected) {
-            return sendHeartbeat(wasClosed = !status.connected)
+        val wasClosed: Boolean? = when {
+            status.connected != previousStatus.connected -> !status.connected
+            status.connected && status.secondsSinceLastActivity <= maxIntervalInSeconds -> false
+            else -> null
         }
 
-        if (status.connected && status.secondsSinceLastActivity <= maxIntervalInSeconds) {
-            return sendHeartbeat(wasClosed = false)
+        if (wasClosed != null) {
+            @Suppress("TooGenericExceptionCaught")
+            return try {
+                sendHeartbeat(wasClosed)
+            } catch (e: Exception) {
+                logger.error("Failed to send heartbeat with wasClosed=$wasClosed", e)
+            }
         }
     }
 
@@ -79,19 +85,18 @@ class HeartbeatService() : Disposable {
     @Synchronized
     private suspend fun sendHeartbeat(wasClosed: Boolean = false) {
         retry(2, logger) {
-            if (server.get() == null) {
-                server.set(createServer())
+            if (heartbeatClient.get() == null) {
+                heartbeatClient.set(createHeartbeatClient())
             }
 
             @Suppress("TooGenericExceptionCaught") // Unsure what exceptions might be thrown
             try {
-                val s = server.get()!!
-                s.sendHeartBeat(SendHeartBeatOptions(instanceId, wasClosed)).await()
+                heartbeatClient.get()!!(wasClosed).await()
                 logger.info("Heartbeat sent with wasClosed=$wasClosed")
             } catch (e: Exception) {
                 // If connection fails for some reason,
                 // remove the reference to the existing server.
-                server.set(null)
+                heartbeatClient.set(null)
                 throw e
             }
         }
@@ -102,12 +107,22 @@ class HeartbeatService() : Disposable {
      * @throws IOException
      * @throws IllegalStateException
      */
-    private suspend fun createServer(): GitpodServer {
-        logger.info("Creating GitpodServer")
-        val token = fetchToken()
-        val server = ConnectionHelper().connect(uri, origin, token).server()
-        return server
+    private suspend fun createHeartbeatClient(): HeartbeatClient {
+        logger.info("Creating HeartbeatClient")
+        val supervisorInfo = SupervisorInfoService.fetch()
+
+        val server = ConnectionHelper().connect(
+            "wss://${supervisorInfo.host.split("//").last()}/api/v1",
+            supervisorInfo.workspaceUrl,
+            supervisorInfo.authToken
+        ).server()
+
+        return { wasClosed: Boolean ->
+            server.sendHeartBeat(SendHeartBeatOptions(supervisorInfo.instanceId, wasClosed))
+        }
     }
 
     override fun dispose() = closed.set(true)
 }
+
+typealias HeartbeatClient = (Boolean) -> CompletableFuture<Void>
