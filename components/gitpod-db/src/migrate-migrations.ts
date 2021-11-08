606a4014625c3ff2eaf096cbf509af20830910fd
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-11-08 14:19:19 +0000

    [db] Move migrations from *.sql to TS

---
+++ components/gitpod-db/src/migrate-migrations.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+
+import { TypeORM } from "./typeorm/typeorm";
+import { Config } from "./config";
+import { MigrateMigrations0_2_0 } from "./typeorm/migrate-migrations-0_2_0";
+
+
+async function migrateMigrationsTable() {
+    const config = new Config();
+    const typeorm = new TypeORM(config, {});
+    const conn = await typeorm.getConnection();
+
+    const runner = conn.createQueryRunner();
+    const migration_0_2_0 = new MigrateMigrations0_2_0();
+    await migration_0_2_0.up(runner);
+
+    conn.close();
+    console.log("successfully migrated 'migrations' table.");
+}
+
+migrateMigrationsTable().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
