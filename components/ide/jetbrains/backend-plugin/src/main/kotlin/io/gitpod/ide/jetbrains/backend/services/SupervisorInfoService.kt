5324b95a3800b509b392668f7dd2bd0db08435dc
André Duarte <mail@andreduarte.net>
2021-11-08 13:01:46 +0000

    [jetbrains] Clean up JetBrains backend plugin

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/ide/jetbrains/backend/services/SupervisorInfoService.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/ide/jetbrains/backend/services/SupervisorInfoService.kt
@@ -6,25 +6,41 @@ package io.gitpod.ide.jetbrains.backend.services
 
 import com.intellij.openapi.diagnostic.logger
 import io.gitpod.ide.jetbrains.backend.utils.Retrier.retry
+import io.gitpod.supervisor.api.Info
+import io.gitpod.supervisor.api.Info.WorkspaceInfoRequest
+import io.gitpod.supervisor.api.InfoServiceGrpc
 import io.gitpod.supervisor.api.Token.GetTokenRequest
 import io.gitpod.supervisor.api.TokenServiceGrpc
 import io.grpc.ManagedChannelBuilder
 import kotlinx.coroutines.guava.asDeferred
 
-object AuthTokenService {
-    private val logger = logger<AuthTokenService>()
+object SupervisorInfoService {
+    private val logger = logger<SupervisorInfoService>()
     private const val SUPERVISOR_ADDRESS = "localhost:22999"
 
+    data class Info(
+        val host: String,
+        val workspaceUrl: String,
+        val instanceId: String,
+        val authToken: String
+    )
+
     @Suppress("MagicNumber")
-    suspend fun fetchToken(): String =
+    suspend fun fetch(): Info =
         retry(3, logger) {
             val channel = ManagedChannelBuilder
                 .forTarget(SUPERVISOR_ADDRESS)
                 .usePlaintext()
                 .build()
 
+            val infoResponse: io.gitpod.supervisor.api.Info.WorkspaceInfoResponse = InfoServiceGrpc
+                .newFutureStub(channel)
+                .workspaceInfo(WorkspaceInfoRequest.newBuilder().build())
+                .asDeferred()
+                .await()
+
             val request = GetTokenRequest.newBuilder()
-                .setHost(System.getenv("GITPOD_HOST").split("//").last())
+                .setHost(infoResponse.gitpodHost.split("//").last())
                 .addScope("function:sendHeartBeat")
                 .setKind("gitpod")
                 .build()
@@ -35,6 +51,11 @@ object AuthTokenService {
                 .asDeferred()
                 .await()
 
-            response.token
+            Info(
+                host = infoResponse.gitpodHost,
+                workspaceUrl = infoResponse.workspaceUrl,
+                instanceId = infoResponse.instanceId,
+                authToken = response.token
+            )
         }
 }
