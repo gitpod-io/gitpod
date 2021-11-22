51f4f87f48f0148502f4b4ed6ae4d07634a84c88
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-11-22 13:17:18 +0000

    [db] Index DBTokenEntry.expiryDate

---
+++ components/gitpod-db/src/typeorm/migration/1637586292251-IndexTokenEntryExpiryDate.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+import { indexExists } from "./helper/helper";
+
+export class IndexTokenEntryExpiryDate1637586292251 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const TABLE_NAME = "d_b_token_entry";
+        const INDEX_NAME = "ind_expiryDate";
+
+        if (!(await indexExists(queryRunner, TABLE_NAME, INDEX_NAME))) {
+            await queryRunner.query(`CREATE INDEX ${INDEX_NAME} ON ${TABLE_NAME} (expiryDate)`);
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
