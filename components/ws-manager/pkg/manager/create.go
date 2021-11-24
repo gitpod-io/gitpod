18e4c955d0b5a5b14d4c2165558eef4300a9e809
Pudong Zheng <tianshi8650@gmail.com>
2021-11-24 07:14:11 +0000

    fix workspace leak

--- components/ws-manager/pkg/manager/create.go
+++ components/ws-manager/pkg/manager/create.go
@@ -365,6 +365,7 @@ func (m *Manager) createDefiniteWorkspacePod(startContext *startWorkspaceContext
 			Namespace:   m.Config.Namespace,
 			Labels:      labels,
 			Annotations: annotations,
+			Finalizers:  []string{"gitpod.io/finalizer"},
 		},
 		Spec: corev1.PodSpec{
 			AutomountServiceAccountToken: &boolFalse,
