c18d39bac57b309d49e55a012a002d0e10864b02
Christian Weichel <chris@gitpod.io>
2021-12-01 20:36:36 +0000

    [ws-daemon] Retry /dev/fuse setup

--- components/ws-daemon/pkg/daemon/cgroup_customizer.go
+++ components/ws-daemon/pkg/daemon/cgroup_customizer.go
@@ -6,8 +6,10 @@ package daemon
 
 import (
 	"context"
+	"time"
 
 	"github.com/containerd/cgroups"
+	"github.com/gitpod-io/gitpod/common-go/log"
 	"github.com/gitpod-io/gitpod/ws-daemon/pkg/dispatch"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"golang.org/x/xerrors"
@@ -32,34 +34,58 @@ func (c *CgroupCustomizer) WorkspaceAdded(ctx context.Context, ws *dispatch.Work
 	if disp == nil {
 		return xerrors.Errorf("no dispatch available")
 	}
-
 	cgroupPath, err := disp.Runtime.ContainerCGroupPath(context.Background(), ws.ContainerID)
 	if err != nil {
-		return xerrors.Errorf("cannot start governer: %w", err)
+		return xerrors.Errorf("cannot get container (%s) cgroup path: %w", ws.ContainerID, err)
 	}
 
-	control, err := cgroups.Load(c.customV1, cgroups.StaticPath(cgroupPath))
+	enableDevFus := func() error {
+		control, err := cgroups.Load(c.customV1, cgroups.StaticPath(cgroupPath))
+		if err != nil {
+			return err
+		}
 
-	if err != nil {
-		return xerrors.Errorf("error loading cgroup at path: %s %w", cgroupPath, err)
-	}
-
-	res := &specs.LinuxResources{
-		Devices: []specs.LinuxDeviceCgroup{
-			// /dev/fuse
-			{
-				Type:   "c",
-				Minor:  &fuseDeviceMinor,
-				Major:  &fuseDeviceMajor,
-				Access: "rwm",
-				Allow:  true,
+		err = control.Update(&specs.LinuxResources{
+			Devices: []specs.LinuxDeviceCgroup{
+				// /dev/fuse
+				{
+					Type:   "c",
+					Minor:  &fuseDeviceMinor,
+					Major:  &fuseDeviceMajor,
+					Access: "rwm",
+					Allow:  true,
+				},
 			},
-		},
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
 	}
 
-	if err := control.Update(res); err != nil {
-		return xerrors.Errorf("cgroup update failed: %w", err)
-	}
+	// We'll try for some time to modify the cgroup. On some systems the cgroup is not fully present
+	// by the time this function gets called. A future CRI-based container integration would benefit
+	// from containerd internal synchronisation. We don't have that synchronisation today though, hence
+	// need to supendously retry.
+	go func() {
+		t := time.NewTicker(500 * time.Millisecond)
+		defer t.Stop()
+
+		for range t.C {
+			if ctx.Err() != nil {
+				return
+			}
+
+			err := enableDevFus()
+			if err != nil {
+				log.WithField("cgroup", cgroupPath).WithFields(ws.OWI()).WithError(err).Debug("cannot enable /dev/fuse in cgroup")
+				continue
+			}
+
+			return
+		}
+	}()
 
 	return nil
 }
