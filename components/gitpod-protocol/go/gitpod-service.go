f153cbb1b2160fb8a2495ba1ffefcc1df2811e1c
Pudong Zheng <tianshi8650@gmail.com>
2021-12-07 10:18:35 +0000

    [gp-cli] allow to create snapshot

--- components/gitpod-protocol/go/gitpod-service.go
+++ components/gitpod-protocol/go/gitpod-service.go
@@ -73,7 +73,7 @@ type APIInterface interface {
 	SendFeedback(ctx context.Context, feedback string) (res string, err error)
 	RegisterGithubApp(ctx context.Context, installationID string) (err error)
 	TakeSnapshot(ctx context.Context, options *TakeSnapshotOptions) (res string, err error)
-	WaitForSnapshot(ctx context.Context, options *WaitForSnapshotOptions) (err error)
+	WaitForSnapshot(ctx context.Context, snapshotId string) (err error)
 	GetSnapshots(ctx context.Context, workspaceID string) (res []*string, err error)
 	StoreLayout(ctx context.Context, workspaceID string, layoutData string) (err error)
 	GetLayout(ctx context.Context, workspaceID string) (res string, err error)
@@ -1279,14 +1279,14 @@ func (gp *APIoverJSONRPC) TakeSnapshot(ctx context.Context, options *TakeSnapsho
 }
 
 // WaitForSnapshot calls waitForSnapshot on the server
-func (gp *APIoverJSONRPC) WaitForSnapshot(ctx context.Context, options *WaitForSnapshotOptions) (err error) {
+func (gp *APIoverJSONRPC) WaitForSnapshot(ctx context.Context, snapshotId string) (err error) {
 	if gp == nil {
 		err = errNotConnected
 		return
 	}
 	var _params []interface{}
 
-	_params = append(_params, options)
+	_params = append(_params, snapshotId)
 
 	var result string
 	err = gp.C.Call(ctx, "waitForSnapshot", _params, &result)
@@ -1939,11 +1939,7 @@ type GenerateNewGitpodTokenOptions struct {
 type TakeSnapshotOptions struct {
 	LayoutData  string `json:"layoutData,omitempty"`
 	WorkspaceID string `json:"workspaceId,omitempty"`
-}
-
-// WaitForSnapshotOptions is the WaitForSnapshotOptions message type
-type WaitForSnapshotOptions struct {
-	SnapshotID string `json:"snapshotID,omitempty"`
+	DontWait    bool   `json:"dontWait",omitempty`
 }
 
 // PreparePluginUploadParams is the PreparePluginUploadParams message type
