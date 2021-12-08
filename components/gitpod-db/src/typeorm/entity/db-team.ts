996148641b883db20cdb1f88b61324de108e72e3
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-12-08 14:22:39 +0000

    [db] Cleanup

--- components/gitpod-db/src/typeorm/entity/db-team.ts
+++ components/gitpod-db/src/typeorm/entity/db-team.ts
@@ -4,12 +4,13 @@
  * See License-AGPL.txt in the project root for license information.
  */
 
+import { Team } from "@gitpod/gitpod-protocol";
 import { Entity, Column, PrimaryColumn } from "typeorm";
 import { TypeORM } from "../typeorm";
 
 @Entity()
 // on DB but not Typeorm: @Index("ind_lastModified", ["_lastModified"])   // DBSync
-export class DBTeam {
+export class DBTeam implements Team {
   @PrimaryColumn(TypeORM.UUID_COLUMN_TYPE)
   id: string;
 
