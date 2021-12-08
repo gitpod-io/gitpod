fccd4a7e8a80857b7bf03599149346baf2843ad7
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-12-08 08:57:47 +0000

    [protocol] Code cleanup

--- components/gitpod-protocol/src/messaging/node/connection.ts
+++ components/gitpod-protocol/src/messaging/node/connection.ts
@@ -7,56 +7,12 @@
 
 import * as ws from "ws";
 import * as http from "http";
-import * as https from "https";
-import * as url from "url";
 import * as net from "net";
-import { MessageConnection } from "vscode-jsonrpc";
-import { createWebSocketConnection, IWebSocket } from "vscode-ws-jsonrpc";
+import { IWebSocket } from "vscode-ws-jsonrpc";
 import { log } from '../../util/logging';
-
-export interface IServerOptions {
-    readonly server: http.Server | https.Server;
-    readonly path?: string;
-    matches?(request: http.IncomingMessage): boolean;
-}
-
-export function createServerWebSocketConnection(options: IServerOptions, onConnect: (connection: MessageConnection) => void): void {
-    openJsonRpcSocket(options, socket => {
-        onConnect(createWebSocketConnection(socket, console));
-    });
-}
-
-export function openJsonRpcSocket(options: IServerOptions, onOpen: (socket: IWebSocket) => void): void {
-    openSocket(options, socket => {
-        const webSocket = toIWebSocket(socket);
-        onOpen(webSocket);
-    });
-}
 
 export interface OnOpen {
     (webSocket: ws, request: http.IncomingMessage, socket: net.Socket, head: Buffer): void;
-}
-
-export function openSocket(options: IServerOptions, onOpen: OnOpen): void {
-    const wss = new ws.Server({
-        noServer: true,
-            perMessageDeflate: {
-                // don't compress if a message is less than 256kb
-                threshold: 256 * 1024
-            }
-    });
-    options.server.on('upgrade', (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
-        const pathname = request.url ? url.parse(request.url).pathname : undefined;
-        if (options.path && pathname === options.path || options.matches && options.matches(request)) {
-            wss.handleUpgrade(request, socket, head, webSocket => {
-                if (webSocket.readyState === webSocket.OPEN) {
-                    onOpen(webSocket, request, socket, head);
-                } else {
-                    webSocket.on('open', () => onOpen(webSocket, request, socket, head));
-                }
-            });
-        }
-    });
 }
 
 export function toIWebSocket(webSocket: ws) {
