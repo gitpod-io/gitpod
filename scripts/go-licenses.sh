0f0f6485eea6d8cae50e88578500db1d32caa539
Moritz Eysholdt <moritz@gitpod.io>
2021-12-09 14:13:07 +0000

    Updated license list for golang dependencies
    Via
    GITHUB_TOKEN=(...) ./scripts/go-licenses.sh

--- scripts/go-licenses.sh
+++ scripts/go-licenses.sh
@@ -9,21 +9,24 @@ cd "$(dirname "$0")/.." || exit
 out=$PWD
 
 tmpdir=$(mktemp -d)
-(cd "$tmpdir" || exit; curl -L https://github.com/mitchellh/golicense/releases/download/v0.1.1/golicense_0.1.1_linux_x86_64.tar.gz | tar xzv)
+(cd "$tmpdir" || exit; curl -L https://github.com/mitchellh/golicense/releases/download/v0.2.0/golicense_0.2.0_linux_x86_64.tar.gz | tar xzv)
 golicense="$tmpdir"/golicense
 
-for i in ls components; do
-    if [ -f "components/$i/go.mod" ]; then
+cd "components" || exit
+for i in *; do
+    echo "trying $i"
+    if [ -f "$i/go.mod" ]; then
         echo "building $i"
-        cd components/"$i" || exit
+        cd "$i" || exit
         go build -o exec
 
         echo "checking $i"
-        timeout 60 "$golicense" -plain -license=true exec >> "$out"/licenses.$i.txt
+        timeout 60 "$golicense" -plain -license=true exec >> "$out/licenses.$i.txt"
         rm exec
         cd - || exit
     fi
 done
+cd ..
 
 cat licenses.*.txt | sort | tr -s ' ' | uniq | sed -r 's/\s+/,/' | column -s , -t > "$out"/License.third-party.go.txt
 echo "Output written to $out/License.third-party.go.txt"
