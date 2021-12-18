233337f9e43345687cf7ab120a21fa9011783451
Florian Imdahl <git@ffflorian.de>
2021-12-18 16:12:19 +0100

    feat: Extend workspace timeout

---
+++ components/gitpod-cli/cmd/timeout-extend.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package cmd
+
+import (
+	"context"
+	"time"
+
+	gitpod "github.com/gitpod-io/gitpod/gitpod-cli/pkg/gitpod"
+	"github.com/spf13/cobra"
+)
+
+// timeoutExtendCmd represents the timeoutExtendCmd command
+var timeoutExtendCmd = &cobra.Command{
+	Use:   "timeout-extend",
+	Short: "Boost the timeout of this workspace",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		wsInfo, err := gitpod.GetWSInfo(ctx)
+		if err != nil {
+			fail(err.Error())
+		}
+		client, err := gitpod.ConnectToServer(ctx, wsInfo, []string{
+			"function:setWorkspaceTimeout",
+			"180m",
+			"resource:workspace::" + wsInfo.WorkspaceId + "::get/update",
+		})
+		if err != nil {
+			fail(err.Error())
+		}
+		err = client.StopWorkspace(ctx, wsInfo.WorkspaceId)
+		if err != nil {
+			fail(err.Error())
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(timeoutExtendCmd)
+}
