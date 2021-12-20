481af81678e0d4c68b7bc1e850eb09d51ad8d5f3
Simon Emms <simon@gitpod.io>
2021-12-20 08:53:02 +0000

    [installer]: refactor the workspace component with ide ownership

--- installer/pkg/components/workspace/constants.go
+++ installer/pkg/components/workspace/constants.go
@@ -9,14 +9,6 @@ const (
 	ContainerPort                = 23000
 	DefaultWorkspaceImage        = "gitpod/workspace-full"
 	DefaultWorkspaceImageVersion = "latest"
-	CodeIDEImage                 = "ide/code"
-	CodeIDEImageStableVersion    = "commit-0d1e8cc5dcc8ccb38efcc2cd6404316b6e7bf4e9" // stable version that will be updated manually on demand
-	CodeDesktopIDEImage          = "ide/code-desktop"
-	CodeDesktopInsidersIDEImage  = "ide/code-desktop-insiders"
-	IntelliJDesktopIDEImage      = "ide/intellij"
-	GoLandDesktopIdeImage        = "ide/goland"
-	PyCharmDesktopIdeImage       = "ide/pycharm"
-	PhpStormDesktopIdeImage      = "ide/phpstorm"
 	DockerUpImage                = "docker-up"
 	SupervisorImage              = "supervisor"
 	WorkspacekitImage            = "workspacekit"
