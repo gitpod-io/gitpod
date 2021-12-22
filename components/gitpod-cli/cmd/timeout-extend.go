60b24b5f31bfa3dfdf2e966b72e7ee5a9bc156cd
Florian Imdahl <git@ffflorian.de>
2021-12-22 19:57:41 +0100

    wording

--- components/gitpod-cli/cmd/timeout-extend.go
+++ components/gitpod-cli/cmd/timeout-extend.go
@@ -16,7 +16,7 @@ import (
 // timeoutExtendCmd represents the timeoutExtendCmd command
 var timeoutExtendCmd = &cobra.Command{
 	Use:   "timeout-extend",
-	Short: "Extend the timeout of this workspace",
+	Short: "Extend the timeout of the current workspace to 180 minutes",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
