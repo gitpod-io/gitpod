1d6c8e1545335f20f3c8b5be99db0cebad9282ec
Filip Troníček <filip@gitpod.io>
2022-01-03 21:12:38 +0000

    Add version command

---
+++ components/gitpod-cli/cmd/version.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package cmd
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	// Version : current version
+	Version string = strings.TrimSpace(version)
+	//go:embed version.txt
+	version string
+)
+
+// urlCmd represents the url command
+var versionCmd = &cobra.Command{
+	Short: "Prints the version of the CLI",
+	Args: cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
