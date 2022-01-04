46df01a04ec46ec8e20861c771b9e6604d8e38ca
Gero Posmyk-Leinemann <gero@gitpod.io>
2022-01-04 09:25:31 +0000

    [db] DROPT TABLE d_b_payment_source_info

---
+++ components/gitpod-db/src/typeorm/migration/1641287965022-DropDBPaymentSourceInfo.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class DropDBPaymentSourceInfo1641287965022 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query("DROP TABLE `d_b_payment_source_info`");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
