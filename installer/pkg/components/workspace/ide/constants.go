14bb245ba43988044a6e69d3486cb90ce120e771
Jean Pierre <jeanp413@hotmail.com>
2022-01-05 14:22:08 +0000

    [ide] Update code stable image
    Notable Changes:
    -  https://github.com/gitpod-io/gitpod/pull/7451

--- installer/pkg/components/workspace/ide/constants.go
+++ installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-0d1e8cc5dcc8ccb38efcc2cd6404316b6e7bf4e9" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-2533c9bc91273bb35f7bb067b7127e2cc925a387" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
