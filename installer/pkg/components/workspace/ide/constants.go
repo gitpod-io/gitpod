972dfeb50d56993982eaaa8be4dfbd3cf339143b
Jean Pierre <jeanp413@hotmail.com>
2022-01-12 05:39:18 +0000

    [code] Port telemetry to stable

--- installer/pkg/components/workspace/ide/constants.go
+++ installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-2533c9bc91273bb35f7bb067b7127e2cc925a387" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-dc3be5ab7e8dd88d7b3d9c40fa8a2b2130106c2d" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
