57f30617d75659f75059e721d572207f27530e71
Simon Emms <simon@gitpod.io>
2022-01-13 16:44:17 +0000

    [installation-telemetry]: get installationAdmin data from server

---
+++ components/installation-telemetry/pkg/server/installationAdmin.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gitpod-io/gitpod/installation-telemetry/pkg/common"
+)
+
+type InstallationAdminSettings struct {
+	SendTelemetry bool `json:"sendTelemetry"`
+}
+
+type InstallationAdminData struct {
+	ID       string                    `json:"id"`
+	Settings InstallationAdminSettings `json:"settings"`
+}
+
+func GetInstallationAdminData(config common.Config) (*InstallationAdminData, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/installation-admin/data", config.Server))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data InstallationAdminData
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
