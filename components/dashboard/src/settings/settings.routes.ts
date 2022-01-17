49dd9740365ed340232b8ac1e6ab8a932265eef9
William Ghelfi <trumbitta@gmail.com>
2022-01-17 12:01:54 +0000

    refactor(dashboard): turn some magic strings into configuration constants
    This also helps with other places where we may want to use the same values.
    E.g. e2e tests

---
+++ components/dashboard/src/settings/settings.routes.ts
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+export const settingsPathMain = '/settings';
+
+export const settingsPathAccount = '/account';
+export const settingsPathIntegrations = '/integrations';
+export const settingsPathNotifications = '/notifications';
+export const settingsPathPlans = '/plans';
+export const settingsPathPreferences = '/preferences';
+
+export const settingsPathTeams = '/teams';
+export const settingsPathTeamsJoin = [settingsPathTeams, 'join'].join('/');
+export const settingsPathTeamsNew = [settingsPathTeams, 'new'].join('/');
+
+export const settingsPathVariables = '/variables';
