c7ba9832d78bf857ff1affad9d4cd0c0bc977150
Jan Keromnes <jan.keromnes@typefox.io>
2022-01-18 09:11:20 +0100

    Fix DB migration order

--- components/gitpod-db/src/typeorm/migration/1642493449937-ProjectEnvVars.ts
+++ components/gitpod-db/src/typeorm/migration/1642493449937-ProjectEnvVars.ts
@@ -6,7 +6,7 @@
 
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class ProjectEnvVars1639735838107 implements MigrationInterface {
+export class ProjectEnvVars1642493449937 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE IF NOT EXISTS `d_b_project_env_var` (`id` char(36) NOT NULL, `projectId` char(36) NOT NULL, `name` varchar(255) NOT NULL, `value` text NOT NULL, `censored` tinyint(4) NOT NULL, `creationTime` varchar(255) NOT NULL, `deleted` tinyint(4) NOT NULL DEFAULT '0', `_lastModified` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (`id`, `projectId`), KEY `ind_projectid` (projectId), KEY `ind_dbsync` (`_lastModified`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
