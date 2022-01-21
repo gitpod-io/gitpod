f2ef9bd5e56830cc5be36e3d9d6ae398a9d42718
William Ghelfi <trumbitta@gmail.com>
2022-01-21 14:22:23 +0000

    style(local-app-api): batch-format with Prettier

--- components/local-app-api/typescript-grpcweb/scripts/publish.js
+++ components/local-app-api/typescript-grpcweb/scripts/publish.js
@@ -1,4 +1,3 @@
-
 /**
  * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
  * Licensed under the GNU Affero General Public License (AGPL).
@@ -10,7 +9,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const supervisorPckPath = path.join(process.cwd(), 'components-supervisor-api-typescript-grpcweb--publish/components-supervisor-api-typescript-grpcweb--lib/package');
+const supervisorPckPath = path.join(
+  process.cwd(),
+  'components-supervisor-api-typescript-grpcweb--publish/components-supervisor-api-typescript-grpcweb--lib/package',
+);
 const localAppPckPath = path.join(process.cwd(), 'components-local-app-api-typescript-grpcweb--lib/package');
 
 const supervisorPck = JSON.parse(fs.readFileSync(path.join(supervisorPckPath, 'package.json'), 'utf-8'));
@@ -19,4 +21,7 @@ localAppPck.dependencies['@gitpod/supervisor-api-grpcweb'] = supervisorPck.versi
 fs.writeFileSync(path.join(localAppPckPath, 'package.json'), JSON.stringify(localAppPck, undefined, 2), 'utf-8');
 
 process.argv.push('components-local-app-api-typescript-grpcweb--lib/package');
-require(path.join(process.cwd(), 'components-local-app-api-typescript-grpcweb--scripts/components-gitpod-protocol--scripts/publish.js'));
+require(path.join(
+  process.cwd(),
+  'components-local-app-api-typescript-grpcweb--scripts/components-gitpod-protocol--scripts/publish.js',
+));
--- components/local-app-api/typescript-grpcweb/webpack.config.js
+++ components/local-app-api/typescript-grpcweb/webpack.config.js
@@ -8,30 +8,30 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/localapp.ts',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "ts-loader"
-            },
-            {
-                test: /\.js$/,
-                use: ["source-map-loader"],
-                enforce: "pre",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    output: {
-        filename: 'localapp.js',
-        path: path.resolve('./lib'),
-        libraryTarget: 'umd',
-        globalObject: 'typeof self !== \'undefined\' ? self : this'
-    },
-    externals: {
-        '@improbable-eng/grpc-web': 'commonjs2 @improbable-eng/grpc-web',
-    },
-    mode: 'production'
+  entry: './src/localapp.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    filename: 'localapp.js',
+    path: path.resolve('./lib'),
+    libraryTarget: 'umd',
+    globalObject: "typeof self !== 'undefined' ? self : this",
+  },
+  externals: {
+    '@improbable-eng/grpc-web': 'commonjs2 @improbable-eng/grpc-web',
+  },
+  mode: 'production',
 };
