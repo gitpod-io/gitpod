44104c9a56ce04408d234de51c346455365210d1
Pavel Tumik <18602811+sagor999@users.noreply.github.com>
2022-01-22 03:02:20 +0000

    add which test is being executed

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -12,6 +12,7 @@ export PATH=$PATH:/tests
 FAILURE_COUNT=0
 # shellcheck disable=SC2045
 for i in $(find /tests/ -name "*.test" | sort); do
+    echo "running test: $i"
     "$i" "$@";
     TEST_STATUS=$?
     if [ "$TEST_STATUS" -ne "0" ]; then
