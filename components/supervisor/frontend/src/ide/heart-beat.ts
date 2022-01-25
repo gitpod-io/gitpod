84a496d03cae1b0debbd8a0ab114c4d3eb01ffd6
Anton Kosyakov <anton@gitpod.io>
2022-01-25 10:57:02 +0000

    [supervisor] resume hearbeating if window unload was cancelled

--- components/supervisor/frontend/src/ide/heart-beat.ts
+++ components/supervisor/frontend/src/ide/heart-beat.ts
@@ -4,6 +4,8 @@
  * See License-AGPL.txt in the project root for license information.
  */
 
+import { DisposableCollection, Disposable } from '@gitpod/gitpod-protocol/lib/util/disposable';
+
 let lastActivity = 0;
 const updateLastActivitiy = () => {
     lastActivity = new Date().getTime();
@@ -13,11 +15,12 @@ export const track = (w: Window) => {
     w.document.addEventListener('keydown', updateLastActivitiy, { capture: true });
 }
 
-let intervalHandle: NodeJS.Timer | undefined;
+let toCancel: DisposableCollection | undefined;
 export function schedule(instanceId: string): void {
-    if (intervalHandle !== undefined) {
+    if (toCancel) {
         return;
     }
+    toCancel = new DisposableCollection()
     const sendHeartBeat = async (wasClosed?: true) => {
         try {
             await window.gitpod.service.server.sendHeartBeat({ instanceId, wasClosed });
@@ -26,12 +29,26 @@ export function schedule(instanceId: string): void {
         }
     }
     sendHeartBeat();
-    window.addEventListener('beforeunload', () => {
+    let unloadTimeout: any;
+    const beforeUnloadListener = () => {
+        unloadTimeout = setTimeout(() => {
+            // if unload was cancelled then resume heartbeating
+            sendHeartBeat();
+        }, 2000);
         sendHeartBeat(true);
-    }, { once: true });
+    };
+    const unloadListener = () => {
+        if (unloadTimeout) {
+            clearTimeout(unloadTimeout);
+        }
+    }
+    window.addEventListener('beforeunload', beforeUnloadListener);
+    window.addEventListener('unload', unloadListener);
+    toCancel.push(Disposable.create(() => window.removeEventListener('beforeunload', beforeUnloadListener)));
+    toCancel.push(Disposable.create(() => window.removeEventListener('unload', unloadListener)));
 
     let activityInterval = 30000;
-    intervalHandle = setInterval(() => {
+    const intervalHandle = setInterval(() => {
         // add an additional random value between 5 and 15 seconds
         const randomInterval = Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000;
         if (lastActivity + activityInterval + randomInterval < new Date().getTime()) {
@@ -40,11 +57,12 @@ export function schedule(instanceId: string): void {
         }
         sendHeartBeat();
     }, activityInterval);
+    toCancel.push(Disposable.create(() => clearInterval(intervalHandle)));
 }
 
 export const cancel = () => {
-    if (intervalHandle !== undefined) {
-        clearInterval(intervalHandle);
-        intervalHandle = undefined;
+    if (toCancel) {
+        toCancel.dispose();
+        toCancel = undefined;
     }
 }
