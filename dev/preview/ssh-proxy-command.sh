81c8edcea45a51ad56637cb2e3d2cfca80157418
Pudong Zheng <tianshi8650@gmail.com>
2022-01-26 03:57:46 +0000

    change vm-ssh port to 2200
    (cherry picked from commit 6926c575f4485b4275cb151eeba0320c23347905)

--- dev/preview/ssh-proxy-command.sh
+++ dev/preview/ssh-proxy-command.sh
@@ -12,11 +12,11 @@ do
     esac
 done
 
-pkill -f "kubectl --context=harvester (.*)${PORT}:22"
+pkill -f "kubectl --context=harvester (.*)${PORT}:2200"
 kubectl \
     --context=harvester \
     --kubeconfig=/home/gitpod/.kube/config \
-    -n "${NAMESPACE}" port-forward service/proxy "${PORT}:22" > /dev/null 2>&1 &
+    -n "${NAMESPACE}" port-forward service/proxy "${PORT}:2200" > /dev/null 2>&1 &
 
 # Wait for the port to be read
 while true; do
