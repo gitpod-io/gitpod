f0fa6420c1b7bdc71e7882e7081ec64918871859
Cornelius A. Ludmann <cornelius@gitpod.io>
2022-02-03 19:30:58 +0000

    ansible docker image
    /werft no-preview

--- test-setups/ansible/utils/gcp-ssh-wrapper.sh
+++ test-setups/ansible/utils/gcp-ssh-wrapper.sh
@@ -6,19 +6,6 @@
 # https://unix.stackexchange.com/a/568099/385206
 # https://binx.io/blog/2021/03/10/how-to-tell-ansible-to-use-gcp-iap-tunneling/
 
-GCP_PROJECT=clu-self-hosted-playground
-GCP_ZONE=europe-west3-a
-GCP_AUTH_FILE=/tmp/service-account.json
-
-if [[ "$(gcloud config get-value project)" != "$GCP_PROJECT" ]]; then
-    if [[ -f "$GCP_AUTH_FILE" ]]; then
-      gcloud auth activate-service-account --key-file="$GCP_AUTH_FILE"
-    else
-      exit 1
-    fi
-    gcloud config set project "$GCP_PROJECT"
-fi
-
 # "host" is next to the last, cmd is the last argument of ssh command.
 host="${@: -2: 1}"
 cmd="${@: -1: 1}"
@@ -29,7 +16,6 @@ socket="/tmp/ansible-ssh-${host}-22-iap"
 exec gcloud compute ssh "$host" \
   --tunnel-through-iap \
   --quiet\
-  --zone "$GCP_ZONE"\
   --command "$cmd" \
   -- \
   -C \
---
+++ test-setups/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+# Licensed under the GNU Affero General Public License (AGPL).
+# See License-AGPL.txt in the project root for license information.
+
+set -euo pipefail
+
+if [[ "$#" -lt 1 ]]; then
+    echo "Usage: $0 <playbook name> [<ansible-playbook args>]" >&2
+    printf "\nAvailable playbooks:\n" >&2
+    for f in /ansible/*.yaml; do
+        echo "- $(basename "$f" .yaml)" >&2
+    done
+    exit 1
+fi
+
+playbook="$1"
+shift
+
+if [[ "$playbook" == gcp* ]]; then
+    echo "Using /ansible/ansible.gcp.cfg ..."
+    export ANSIBLE_CONFIG=/ansible/ansible.gcp.cfg
+
+    # Hack to configure the GCP project into inventory.gcp.yaml
+    gcp_project=$(yq e '.gcp.project' /ansible/vars/gcp.yaml)
+    yq e -i ".projects = [\"${gcp_project}\"]" /ansible/inventory/inventory.gcp.yaml
+fi
+
+ansible-playbook "$@" "/ansible/${playbook}.yaml"
