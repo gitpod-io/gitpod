7547d114825da48a825e7b011d826f1afccc75d9
Tarun Pothulapati <tarun@gitpod.io>
2022-02-07 08:09:56 +0000

    telemetry: add more fields to data
    Fixes https://github.com/gitpod-io/gitpod/issues/7866
    
    This PR updates the `installation-admin-controller` to also retrieve
    more data to send with telemetry. These are not part of the
    `installationAdminDb` as we do not want to store this in the database
    but lazily retrieve whenever a request is sent to `/data` endpoint
    of the `installation-admin` express app unlike the `uuid` and settings
    which need to be stored and updated.
    
    The following fields are added:
    - `totalUsers` : specifies the total number of users in the instance
    - `totalWorkspaces`: specifies the total number of **regular** workspaces in the instance
    - `totalInstances`: specifies the total number of **regular** workspace instances in the gitpod instance
    
    Signed-off-by: Tarun Pothulapati <tarun@gitpod.io>

--- components/server/src/installation-admin/installation-admin-controller.ts
+++ components/server/src/installation-admin/installation-admin-controller.ts
@@ -6,17 +6,25 @@
 
 import { injectable, inject } from 'inversify';
 import * as express from 'express';
-import { InstallationAdminDB } from '@gitpod/gitpod-db/lib';
+import { InstallationAdminDB, UserDB, WorkspaceDB } from '@gitpod/gitpod-db/lib';
+import { Data } from '@gitpod/gitpod-protocol'
 
 @injectable()
 export class InstallationAdminController {
     @inject(InstallationAdminDB) protected readonly installationAdminDb: InstallationAdminDB;
+    @inject(UserDB) protected readonly userDb: UserDB
+    @inject(WorkspaceDB) protected readonly workspaceDb: WorkspaceDB
 
     public create(): express.Application {
         const app = express();
 
         app.get('/data', async (req: express.Request, res: express.Response) => {
-            const data = await this.installationAdminDb.getData();
+            const data: Data = {
+                installationAdmin: await this.installationAdminDb.getData(),
+                totalUsers: await this.userDb.getUserCount(true),
+                totalWorkspaces: await this.workspaceDb.getWorkspaceCount(),
+                totalInstances: await this.workspaceDb.getInstanceCount(),
+            } as Data;
 
             res.status(200).json(data);
         });
