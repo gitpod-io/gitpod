9c3b5f6223844878ea506903530366788970b3e9
ArthurSens <arthursens2005@gmail.com>
2022-02-23 10:10:22 +0000

    gitpod.yml: Fix core-dev namespace setting for long branches
    Signed-off-by: ArthurSens <arthursens2005@gmail.com>

--- scripts/branch-namespace.sh
+++ scripts/branch-namespace.sh
@@ -2,7 +2,7 @@
 
 branch=$(git symbolic-ref HEAD 2>&1) || error "cannot set kubectl namespace: no branch"
 currentContext=$(kubectl config current-context 2>&1) || error "cannot set kubectl namespace: no current context"
-namespace=staging-$(echo "$branch" | awk '{ sub(/^refs\/heads\//, ""); $0 = tolower($0); gsub(/[^-a-z0-9]/, "-"); print }')
+namespace=staging-$(echo "${branch:0:19}" | awk '{ sub(/^refs\/heads\//, ""); $0 = tolower($0); gsub(/[^-a-z0-9]/, "-"); print }')
 
 echo "Setting kubectl namespace: $namespace"
 kubectl config set-context "$currentContext" --namespace "$namespace"
