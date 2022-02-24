b1c83db3d765f6b5b63680e0739445585c54d9ed
Jean Pierre <jeanp413@hotmail.com>
2022-02-24 17:42:09 +0000

    Update stable with vscode_session telemetry fix

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-9216e2bf490666d468012ab797ecc2703968da47" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-890de80174c7dd9d073d63ef2b1f9eac68f063c1" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
