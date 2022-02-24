59960698e77749603876cf7ed24a87f7974fd652
Thomas Schubart <thomas@gitpod.io>
2022-02-24 10:37:13 +0000

    Map additional ids

--- components/docker-up/docker-up/main.go
+++ components/docker-up/docker-up/main.go
@@ -82,12 +82,6 @@ func main() {
 		logger.Fatalf("Docker socket already exists at %s.\nIn a Gitpod workspace Docker will start automatically when used.\nIf all else fails, please remove %s and try again.", dockerSocketFN, dockerSocketFN)
 	}
 
-	if _, exists := os.LookupEnv("DOCKER_DAEMON_ARGS"); exists {
-		log.Debug("DOCKER_DAEMON_ARGS exists")
-	} else {
-		log.Debug("DOCKER_DAEMON_ARGS does not exist")
-	}
-
 	err = ensurePrerequisites()
 	if err != nil {
 		log.WithError(err).Fatal("failed")
@@ -235,9 +229,13 @@ func adaptSubid(oldfile string, id int) error {
 
 	if id != 0 {
 		newfile.WriteString(fmt.Sprintf("gitpod:%d:%d\n", 1, id))
-	}
+		newfile.WriteString("gitpod:33333:1\n")
 
-	newfile.WriteString("gitpod:33333:1\n")
+	} else {
+		newfile.WriteString("gitpod:33333:1\n")
+		newfile.WriteString(fmt.Sprintf("gitpod:%d:%d\n", 1, 33332))
+		newfile.WriteString(fmt.Sprintf("gitpod:%d:%d\n", 33334, 32200))
+	}
 
 	uidScanner := bufio.NewScanner(uid)
 	for uidScanner.Scan() {
