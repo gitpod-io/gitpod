46648e6bd6653b7986bb44bd3fcefe3cf60689ef
Cornelius A. Ludmann <cornelius@gitpod.io>
2022-02-25 13:16:04 +0000

    [testing] Add integration tests
    /werft no-preview
    /werft with-contrib

---
+++ test-installations/ansible/roles/gitpod-installer/files/run-tests.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+# Licensed under the GNU Affero General Public License (AGPL).
+# See License-AGPL.txt in the project root for license information.
+
+returncode=0
+
+shopt -s globstar nullglob
+for file in /tests/**/*.test
+do
+    echo "$file"
+    $file -kubeconfig=/etc/rancher/k3s/k3s.yaml
+    rc=$?
+    if [[ "$rc" != "0" ]]; then
+        echo "$file failed with code $rc"
+        returncode=$rc
+    fi
+done
+
+exit $returncode
