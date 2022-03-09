afa2bd1cb8bd359ff5abb8bfc8df94210bad860e
Mads Hartmann <mads379@gmail.com>
2022-03-09 15:05:01 +0000

    Allow for branch name lengths of 45
    This relaxes the branch name restriction from 20 characters to 45. This is achieved by constructing the preview name based on the first 10 characters of the branch name and then the first 10 characters of the hashed value of the branch name.
    
    All places that (to my knowledge) rely on the preview name has been updated.
    
    Fixes https://github.com/gitpod-io/ops/issues/1252

--- dev/preview/portforward-monitoring-satellite-harvester.sh
+++ dev/preview/portforward-monitoring-satellite-harvester.sh
@@ -3,7 +3,9 @@
 # Exposes Prometheus and Grafana's UI
 #
 
-VM_NAME="$(git symbolic-ref HEAD 2>&1 | awk '{ sub(/^refs\/heads\//, ""); $0 = tolower($0); gsub(/[^-a-z0-9]/, "-"); print }')"
+source ./dev/preview/util/preview-name-from-branch.sh
+
+VM_NAME="$(preview-name-from-branch)"
 NAMESPACE="preview-${VM_NAME}"
 
 function log {
