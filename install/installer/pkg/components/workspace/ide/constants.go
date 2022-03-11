8384e7c3568cbbc8dcbc121675e05f60e22a8ae6
mustard <huiwen@gitpod.io>
2022-03-11 08:05:17 +0000

    [code] update stable to 1.65.2

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-a5d259949a3fa8d3bbfbb4cb19fd2c3fb0cdf00b" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-93edb90833d8d662f4c1a64b2ad8ced456e62862" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
