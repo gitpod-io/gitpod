ad2d2b212b8c1f77416f20ff252205334d07df7f
Geoffrey Huntley <ghuntley@ghuntley.com>
2022-03-17 12:13:29 +0000

    format code

--- components/server/src/gitea/convert.ts
+++ components/server/src/gitea/convert.ts
@@ -1,26 +1,32 @@
-import { Repository } from '@gitpod/gitpod-protocol';
-import { RepoURL } from '../repohost';
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+import { Repository } from "@gitpod/gitpod-protocol";
+import { RepoURL } from "../repohost";
 import { Gitea } from "./api";
 
-export function convertRepo(repo: Gitea.Repository): Repository | undefined  {
-  if (!repo.clone_url || !repo.name || !repo.owner?.login) {
-    return undefined;
-  }
+export function convertRepo(repo: Gitea.Repository): Repository | undefined {
+    if (!repo.clone_url || !repo.name || !repo.owner?.login) {
+        return undefined;
+    }
 
-  const host = RepoURL.parseRepoUrl(repo.clone_url)!.host;
+    const host = RepoURL.parseRepoUrl(repo.clone_url)!.host;
 
-  return {
-    host,
-    owner: repo.owner.login,
-    name: repo.name,
-    cloneUrl: repo.clone_url,
-    description: repo.description,
-    avatarUrl: repo.avatar_url,
-    webUrl: repo.html_url, // TODO: is this correct?
-    defaultBranch: repo.default_branch,
-    private: repo.private,
-    fork: undefined // TODO: load fork somehow
-  }
+    return {
+        host,
+        owner: repo.owner.login,
+        name: repo.name,
+        cloneUrl: repo.clone_url,
+        description: repo.description,
+        avatarUrl: repo.avatar_url,
+        webUrl: repo.html_url, // TODO: is this correct?
+        defaultBranch: repo.default_branch,
+        private: repo.private,
+        fork: undefined, // TODO: load fork somehow
+    };
 }
 
 // export function convertBranch(repo: Gitea.Repository): Branch | undefined  {
