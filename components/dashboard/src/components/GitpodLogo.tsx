ec92f47ee95bf8a22c7d9f669d5ba42f1cea5272
Emily Trau <emily@gitpod.io>
2022-03-24 15:37:50 +0000

    licenses

--- components/dashboard/src/components/GitpodLogo.tsx
+++ components/dashboard/src/components/GitpodLogo.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
 // import React from "react";
 import { useLabsStorage } from "../settings/LabsStorage";
 import gitpodLogo from "../images/gitpod.svg";
--- components/dashboard/src/settings/LabsStorage.tsx
+++ components/dashboard/src/settings/LabsStorage.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
 import React, { useState, useContext, useCallback, useEffect } from "react";
 import owoify from "owoifyx";
 
