057b22cd6667d07c9cf6395f9d1c4e6943cbe67e
Filip Troníček <filip@gitpod.io>
2022-03-29 22:03:55 +0000

    Add license headers

--- components/server/src/devcontainer-converter/cli.ts
+++ components/server/src/devcontainer-converter/cli.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
 import yargs = require("yargs/yargs");
 import { toGitpod } from "./converter";
 import fs = require("fs");
