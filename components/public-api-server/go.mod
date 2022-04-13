cb7fa7468f61ee24383396770b69f33adc94accc
Milan Pavlik <milan@gitpod.io>
2022-04-13 08:37:44 +0000

    [public-api-server] Simple gRPC server
     Please enter the commit message for your changes. Lines starting

--- components/public-api-server/go.mod
+++ components/public-api-server/go.mod
@@ -53,3 +53,14 @@ replace k8s.io/kubectl => k8s.io/kubectl v0.23.4 // leeway indirect from compone
 replace k8s.io/mount-utils => k8s.io/mount-utils v0.23.4 // leeway indirect from components/common-go:lib
 
 replace k8s.io/pod-security-admission => k8s.io/pod-security-admission v0.23.4 // leeway indirect from components/common-go:lib
+
+require google.golang.org/grpc v1.45.0
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
--- components/public-api-server/main.go
+++ components/public-api-server/main.go
@@ -6,15 +6,66 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
-	addr := ":9000"
-	err := http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte(`hello world`))
-	}))
-	if err != nil {
-		fmt.Println("Failed to server on", addr, err)
+	errors := make(chan error)
+	defer close(errors)
+
+	go func() {
+		addr := ":9000"
+		fmt.Println("Serving HTTP", addr)
+		err := http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte(`hello world\n`))
+		}))
+		if err != nil {
+			fmt.Println("Failed to serve HTTP on", addr, err)
+			errors <- err
+		}
+	}()
+
+	go func() {
+		if err := serveGRPC(); err != nil {
+			fmt.Println("Failed to serve gRPC", err)
+			errors <- err
+		}
+	}()
+
+	// detect OS signals
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-errors:
+		fmt.Println("Encountered errors when running servers", err)
+		os.Exit(1)
+	case <-sigs:
+		fmt.Println("Received termination signal, shutting down.")
+		// TODO(milanpavlik): Graceful server shutdown
+		os.Exit(0)
 	}
+}
+
+func serveGRPC() error {
+	port := 9001
+	srv := grpc.NewServer()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return fmt.Errorf("failed to acquire port %d", port)
+	}
+
+	fmt.Println("Serving gRPC", listener.Addr().String())
+	if serveErr := srv.Serve(listener); err != nil {
+		return fmt.Errorf("failed to serve gRPC: %w", serveErr)
+	}
+
+	return nil
 }
