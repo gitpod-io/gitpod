7f68f2374cd91a62778ff484d7c8b9c540ddcd67
Christian Weichel <chris@gitpod.io>
2022-04-13 10:00:53 +0000

    [nsinsider] Make veth0 match container MTU

--- components/ws-daemon/nsinsider/main.go
+++ components/ws-daemon/nsinsider/main.go
@@ -284,11 +284,16 @@ func main() {
 					}
 					targetPid := c.Int("target-pid")
 
+					eth0, err := netlink.LinkByName(containerIf)
+					if err != nil {
+						return xerrors.Errorf("cannot get container network device %s: %w", containerIf, err)
+					}
+
 					veth := &netlink.Veth{
 						LinkAttrs: netlink.LinkAttrs{
 							Name:  vethIf,
 							Flags: net.FlagUp,
-							MTU:   1500,
+							MTU:   eth0.Attrs().MTU,
 						},
 						PeerName:      cethIf,
 						PeerNamespace: netlink.NsPid(targetPid),
