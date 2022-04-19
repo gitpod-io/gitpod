dee048c5108bbe4d1cda43172c90ac506aebe991
Sven Efftinge <sven@gitpod.io>
2022-04-19 13:44:13 +0000

    [protocol] fix generate id for bad repo names

--- components/gitpod-protocol/src/util/generate-workspace-id.spec.ts
+++ components/gitpod-protocol/src/util/generate-workspace-id.spec.ts
@@ -14,7 +14,7 @@ const expect = chai.expect;
 @suite
 class TestGenerateWorkspaceId {
     @test public async testGenerateWorkspaceId() {
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 10; i++) {
             const id = await generateWorkspaceID();
             expect(new GitpodHostUrl().withWorkspacePrefix(id, "eu").workspaceId).to.equal(id);
         }
@@ -32,13 +32,20 @@ class TestGenerateWorkspaceId {
             ["foo", "bar", "foo-bar-"],
             ["f", "bar", ".{2,16}-bar-"],
             ["gitpod-io", "gitpod", "gitpodio-gitpod-"],
+            ["breatheco-de", "python-flask-api-tutorial", "breathecode-pythonflask-"],
+            ["short", "muchlongerthaneleven", "short-muchlongerthanel-"],
+            ["muchlongerthaneleven", "short", "muchlongerthanel-short-"],
             [
                 'this is rather long and has some "§$"% special chars',
                 "also here pretty long and needs abbreviation",
-                "thisisratherlon-alsohere-",
+                "thisisrathe-alsoherepre-",
             ],
-            ["breatheco-de", "python-flask-api-tutorial", "breathecode-pythonflaska-"],
             ["UPPER", "CaSe", "upper-case-"],
+            [
+                "superlongfirstsegment",
+                "---------",
+                "superlong" /* we don't mantch for the whole first segment, because it has different length depending on the animal that is used to replace the -------*/,
+            ],
         ];
         for (const d of data) {
             const id = await generateWorkspaceID(d[0], d[1]);
