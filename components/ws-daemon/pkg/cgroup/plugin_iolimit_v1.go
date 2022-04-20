112606693b00cd6adaa67c84a3500c18ad54d1ad
Christian Weichel <chris@gitpod.io>
2022-04-20 18:28:32 +0000

    [ws-daemon] Regularly update cgroup v1 IO limits

--- components/ws-daemon/pkg/cgroup/plugin_iolimit_v1.go
+++ components/ws-daemon/pkg/cgroup/plugin_iolimit_v1.go
@@ -7,6 +7,7 @@ package cgroup
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/gitpod-io/gitpod/common-go/log"
 	"github.com/intel/goresctrl/pkg/blockio"
@@ -49,22 +50,34 @@ func (c *IOLimiterV1) Name() string  { return "iolimiter-v1" }
 func (c *IOLimiterV1) Type() Version { return Version1 }
 
 func (c *IOLimiterV1) Apply(ctx context.Context, basePath, cgroupPath string) error {
-	log.WithField("cgroupPath", cgroupPath).Debug("starting io limiting")
-	err := blockio.SetCgroupClass(cgroupPath, "default")
-	if err != nil {
-		return err
-	}
-
 	go func() {
-		<-ctx.Done()
-		// Prior to shutting down though, we need to reset the IO limits to ensure we don't have
-		// processes stuck in the uninterruptable "D" (disk sleep) state. This would prevent the
-		// workspace pod from shutting down.
-		err := blockio.SetCgroupClass(cgroupPath, "reset")
-		if err != nil {
-			log.WithError(err).WithField("cgroupPath", cgroupPath).Error("cannot write IO limits")
+		log.WithField("cgroupPath", cgroupPath).Debug("starting io limiting")
+		// We are racing workspacekit and the interaction with disks.
+		// If we did this just once there's a chance we haven't interacted with all
+		// devices yet, and hence would not impose IO limits on them.
+		ticker := time.NewTicker(30 * time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				// Prior to shutting down though, we need to reset the IO limits to ensure we don't have
+				// processes stuck in the uninterruptable "D" (disk sleep) state. This would prevent the
+				// workspace pod from shutting down.
+
+				err := blockio.SetCgroupClass(cgroupPath, "reset")
+				if err != nil {
+					log.WithError(err).WithField("cgroupPath", cgroupPath).Error("cannot write IO limits")
+				}
+				log.WithField("cgroupPath", cgroupPath).Debug("stopping io limiting")
+				return
+			case <-ticker.C:
+				err := blockio.SetCgroupClass(cgroupPath, "default")
+				if err != nil {
+					log.WithError(err).WithField("cgroupPath", cgroupPath).Error("cannot write IO limits")
+				}
+			}
 		}
 	}()
-
 	return nil
 }
