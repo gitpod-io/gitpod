a1e9617e9f4b85ce1ea02f00a3831e86b1d7f855
Simon Emms <simon@gitpod.io>
2022-04-20 09:52:18 +0000

    [code]: update stable to 1.66.2
    Copy of #9381 for hotfix release

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-93edb90833d8d662f4c1a64b2ad8ced456e62862" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-c89358b24c9577d1fd986d57387f3847f7dfb06f" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
