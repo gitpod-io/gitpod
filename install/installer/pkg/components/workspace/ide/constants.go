fdf7e8f94fee4a0f36ec2a6d2ef76d22432b9b28
Jean Pierre <jeanp413@hotmail.com>
2022-04-21 19:11:37 +0000

    [code] Update stable to 1.66.2 with proxy fix

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-c89358b24c9577d1fd986d57387f3847f7dfb06f" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-e4d4c1cc703e3adc62e4cfe935f37d5d2eeb6df3" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
