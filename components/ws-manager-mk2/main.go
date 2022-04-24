b0443d9c34371783c8e9b8a2bca2e6e84b238624
Christian Weichel <chris@gitpod.io>
2022-04-24 16:12:09 +0000

    [ws-manager-mk2] Add validating webhook

--- components/ws-manager-mk2/main.go
+++ components/ws-manager-mk2/main.go
@@ -73,6 +73,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Workspace")
 		os.Exit(1)
 	}
+	if err = (&workspacev1.Workspace{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Workspace")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
