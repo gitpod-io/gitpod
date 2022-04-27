2ed14b37b2828aced3153a52be0773858cbd4e77
Milan Pavlik <milan@gitpod.io>
2022-04-27 19:28:58 +0000

    [public-api] Add mock implementation of GetWorkspace

---
+++ components/public-api-server/pkg/apiv1/workspace_test.go
@@ -0,0 +1,35 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package apiv1
+
+import (
+	"context"
+	v1 "github.com/gitpod-io/gitpod/public-api/v1"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestWorkspaceService_GetWorkspace(t *testing.T) {
+	svc := NewWorkspaceService()
+
+	workspaceID := "some-workspace-id"
+	resp, err := svc.GetWorkspace(context.Background(), &v1.GetWorkspaceRequest{
+		WorkspaceId: workspaceID,
+	})
+	require.NoError(t, err)
+	require.Equal(t, &v1.GetWorkspaceResponse{
+		ResponseStatus: nil,
+		Result: &v1.Workspace{
+			WorkspaceId: workspaceID,
+			OwnerId:     "mock_owner",
+			ProjectId:   "mock_project_id",
+			Context: &v1.WorkspaceContext{
+				ContextUrl: "https://github.com/gitpod-io/gitpod",
+				Details:    nil,
+			},
+			Description: "This is a mock response",
+		},
+	}, resp)
+}
