c17d7eaff68ec7a125266dde1a2bf9007b8ed8ff
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2022-05-03 11:00:21 -0400

    Revert "Refactor backup restauration"
    This reverts commit bbaa49876a555c2e4332e33ef35e8a5b1848598a.

--- components/content-service/pkg/archive/tar.go
+++ components/content-service/pkg/archive/tar.go
@@ -5,13 +5,19 @@
 package archive
 
 import (
+	"archive/tar"
 	"context"
 	"io"
+	"os"
+	"os/exec"
+	"path"
+	"sort"
+	"syscall"
 	"time"
 
-	"github.com/containers/storage/pkg/archive"
-	"github.com/containers/storage/pkg/idtools"
+	"github.com/moby/moby/pkg/system"
 	"github.com/opentracing/opentracing-go"
+	"golang.org/x/xerrors"
 
 	"github.com/gitpod-io/gitpod/common-go/log"
 	"github.com/gitpod-io/gitpod/common-go/tracing"
@@ -60,29 +66,141 @@ func ExtractTarbal(ctx context.Context, src io.Reader, dst string, opts ...TarOp
 		opt(&cfg)
 	}
 
-	uidMaps := make([]idtools.IDMap, len(cfg.UIDMaps))
-	for i, m := range cfg.UIDMaps {
-		uidMaps[i] = idtools.IDMap{
-			ContainerID: m.ContainerID,
-			HostID:      m.HostID,
-			Size:        m.Size,
-		}
-	}
-	gidMaps := make([]idtools.IDMap, len(cfg.GIDMaps))
-	for i, m := range cfg.GIDMaps {
-		gidMaps[i] = idtools.IDMap{
-			ContainerID: m.ContainerID,
-			HostID:      m.HostID,
-			Size:        m.Size,
-		}
+	pr, pw := io.Pipe()
+	src = io.TeeReader(src, pw)
+	tarReader := tar.NewReader(pr)
+
+	type Info struct {
+		UID, GID  int
+		IsSymlink bool
+		Xattrs    map[string]string
 	}
 
-	err = archive.Untar(src, dst, &archive.TarOptions{
-		UIDMaps:     uidMaps,
-		GIDMaps:     gidMaps,
-		Compression: archive.Uncompressed,
-	})
+	finished := make(chan bool)
+	m := make(map[string]Info)
+
+	go func() {
+		defer close(finished)
+		for {
+			hdr, err := tarReader.Next()
+			if err == io.EOF {
+				finished <- true
+				return
+			}
+
+			if err != nil {
+				log.WithError(err).Error("error reading tar")
+				return
+			}
+
+			m[hdr.Name] = Info{
+				UID:       hdr.Uid,
+				GID:       hdr.Gid,
+				IsSymlink: (hdr.Linkname != ""),
+				//nolint:staticcheck
+				Xattrs: hdr.Xattrs,
+			}
+		}
+	}()
+
+	// Be explicit about the tar flags. We want to restore the exact content without changes
+	tarcmd := exec.Command(
+		"tar",
+		"--extract",
+		"--preserve-permissions",
+		"--xattrs", "--xattrs-include=security.capability",
+	)
+	tarcmd.Dir = dst
+	tarcmd.Stdin = src
+
+	var msg []byte
+	msg, err = tarcmd.CombinedOutput()
+	if err != nil {
+		return xerrors.Errorf("tar %s: %s", dst, err.Error()+";"+string(msg))
+	}
+
+	log.WithField("log", string(msg)).Debug("decompressing tar stream log")
+
+	<-finished
+
+	// lets create a sorted list of pathes and chown depth first.
+	paths := make([]string, 0, len(m))
+	for path := range m {
+		paths = append(paths, path)
+	}
+	sort.Sort(sort.Reverse(sort.StringSlice(paths)))
+
+	// We need to remap the UID and GID between the host and the container to avoid permission issues.
+	for _, p := range paths {
+		v := m[p]
+		uid := toHostID(v.UID, cfg.UIDMaps)
+		gid := toHostID(v.GID, cfg.GIDMaps)
+
+		if v.IsSymlink {
+			continue
+		}
+
+		err = remapFile(path.Join(dst, p), uid, gid, v.Xattrs)
+		if err != nil {
+			log.WithError(err).WithField("uid", uid).WithField("gid", gid).WithField("path", p).Warn("cannot chown")
+		}
+	}
 
 	log.WithField("duration", time.Since(start).Milliseconds()).Debug("untar complete")
-	return
+	return nil
+}
+
+func toHostID(containerID int, idMap []IDMapping) int {
+	for _, m := range idMap {
+		if (containerID >= m.ContainerID) && (containerID <= (m.ContainerID + m.Size - 1)) {
+			hostID := m.HostID + (containerID - m.ContainerID)
+			return hostID
+		}
+	}
+	return containerID
+}
+
+// remapFile changes the UID and GID of a file preserving existing file mode bits.
+func remapFile(name string, uid, gid int, xattrs map[string]string) error {
+	// current info of the file before any change
+	fileInfo, err := os.Stat(name)
+	if err != nil {
+		return err
+	}
+
+	// nothing to do for symlinks
+	if fileInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
+		return nil
+	}
+
+	// changing UID or GID can break files with suid/sgid
+	err = os.Lchown(name, uid, gid)
+	if err != nil {
+		return err
+	}
+
+	// restore original permissions
+	err = os.Chmod(name, fileInfo.Mode())
+	if err != nil {
+		return err
+	}
+
+	for key, value := range xattrs {
+		if err := system.Lsetxattr(name, key, []byte(value), 0); err != nil {
+			log.WithField("name", key).WithField("value", value).WithField("file", name).WithError(err).Error("restoring extended attributes")
+			if err == syscall.ENOTSUP || err == syscall.EPERM {
+				continue
+			}
+
+			return err
+		}
+	}
+
+	// restore file times
+	fileTime := fileInfo.Sys().(*syscall.Stat_t)
+	return os.Chtimes(name, timespecToTime(fileTime.Atim), timespecToTime(fileTime.Mtim))
+}
+
+func timespecToTime(ts syscall.Timespec) time.Time {
+	return time.Unix(int64(ts.Sec), int64(ts.Nsec))
 }
