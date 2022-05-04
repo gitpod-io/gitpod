4b94c911683521016479439e9beefe7e1ddc307c
Andrew Farries <andrew@gitpod.io>
2022-05-04 14:23:44 +0000

    Refactor: Move proxy service gen into its own file

--- install/installer/pkg/components/proxy/objects.go
+++ install/installer/pkg/components/proxy/objects.go
@@ -5,11 +5,7 @@
 package proxy
 
 import (
-	"fmt"
-
 	"github.com/gitpod-io/gitpod/installer/pkg/common"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
 var Objects = common.CompositeRenderFunc(
@@ -17,32 +13,6 @@ var Objects = common.CompositeRenderFunc(
 	deployment,
 	networkpolicy,
 	rolebinding,
-	func(cfg *common.RenderContext) ([]runtime.Object, error) {
-		ports := map[string]common.ServicePort{
-			ContainerHTTPName: {
-				ContainerPort: ContainerHTTPPort,
-				ServicePort:   ContainerHTTPPort,
-			},
-			ContainerHTTPSName: {
-				ContainerPort: ContainerHTTPSPort,
-				ServicePort:   ContainerHTTPSPort,
-			},
-			MetricsContainerName: {
-				ContainerPort: PrometheusPort,
-				ServicePort:   PrometheusPort,
-			},
-		}
-		if cfg.Config.SSHGatewayHostKey != nil {
-			ports[ContainerSSHName] = common.ServicePort{
-				ContainerPort: ContainerSSHPort,
-				ServicePort:   ContainerSSHPort,
-			}
-		}
-		return common.GenerateService(Component, ports, func(service *corev1.Service) {
-			service.Spec.Type = corev1.ServiceTypeLoadBalancer
-			service.Annotations["external-dns.alpha.kubernetes.io/hostname"] = fmt.Sprintf("%s,*.%s,*.ws.%s", cfg.Config.Domain, cfg.Config.Domain, cfg.Config.Domain)
-			service.Annotations["cloud.google.com/neg"] = `{"exposed_ports": {"80":{},"443": {}}}`
-		})(cfg)
-	},
+	service,
 	common.DefaultServiceAccount(Component),
 )
