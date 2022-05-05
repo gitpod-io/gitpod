3455274e20cddc73ff05d57cbfd7161791dcab63
Andrew Farries <andrew@gitpod.io>
2022-05-05 09:40:06 +0000

    Add tests for proxy services
    Ensure that annotations added through the experimental config are
    present on the rendered services.

---
+++ install/installer/pkg/components/ide-proxy/service_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the MIT License. See License-MIT.txt in the project root for license information.
+
+package ide_proxy
+
+import (
+	"testing"
+
+	"github.com/gitpod-io/gitpod/installer/pkg/common"
+	"github.com/gitpod-io/gitpod/installer/pkg/config/v1"
+	"github.com/gitpod-io/gitpod/installer/pkg/config/v1/experimental"
+	"github.com/gitpod-io/gitpod/installer/pkg/config/versions"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestServiceAnnotations(t *testing.T) {
+	annotations := map[string]string{"hello": "world"}
+
+	ctx := renderContextWithIDEProxyConfig(t, &experimental.IDEProxyConfig{ServiceAnnotations: annotations})
+
+	objects, err := service(ctx)
+	require.NoError(t, err)
+
+	require.Len(t, objects, 1, "must render only one object")
+
+	svc := objects[0].(*corev1.Service)
+	for k, v := range annotations {
+		require.Equalf(t, annotations[k], svc.Annotations[k],
+			"expected to find annotation %q:%q on ide-proxy service, but found %q:%q", k, v, k, svc.Annotations[k])
+	}
+}
+
+func renderContextWithIDEProxyConfig(t *testing.T, proxyConfig *experimental.IDEProxyConfig) *common.RenderContext {
+	ctx, err := common.NewRenderContext(config.Config{
+		Experimental: &experimental.Config{
+			IDE: &experimental.IDEConfig{
+				IDEProxyConfig: proxyConfig,
+			},
+		},
+	}, versions.Manifest{
+		Components: versions.Components{
+			PublicAPIServer: versions.Versioned{
+				Version: "commit-test-latest",
+			},
+		},
+	}, "test-namespace")
+	require.NoError(t, err)
+
+	return ctx
+}
