2f266fd4366fa45bb73b87ec32a1fa4a46d332f4
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2022-05-06 08:46:53 -0400

    [content-service] Refactor GCS test to use a fake server

--- components/content-service/pkg/storage/gcloud_test.go
+++ components/content-service/pkg/storage/gcloud_test.go
@@ -6,22 +6,42 @@ package storage
 
 import (
 	"context"
-	"io"
 	"testing"
 
-	"golang.org/x/xerrors"
+	"cloud.google.com/go/storage"
+	"github.com/fsouza/fake-gcs-server/fakestorage"
+	"google.golang.org/api/option"
 
 	"github.com/gitpod-io/gitpod/content-service/api/config"
 	"github.com/gitpod-io/gitpod/content-service/pkg/archive"
 )
 
 func TestObjectAccessToNonExistentObj(t *testing.T) {
-	storage := objDoesNotExistGCloudStorage{
-		Delegate: &DirectGCPStorage{
-			WorkspaceName: "foobar",
-			Stage:         config.StageDevStaging,
-		},
+	server := *fakestorage.NewServer([]fakestorage.Object{})
+	defer server.Stop()
+
+	args := []option.ClientOption{
+		option.WithoutAuthentication(),
+		option.WithEndpoint(server.URL()),
+		option.WithHTTPClient(server.HTTPClient()),
 	}
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+
+	client, err := storage.NewClient(ctx, args...)
+	if err != nil {
+		t.Errorf("error creating GCS gcsClient: %v", err)
+	}
+
+	storage := DirectGCPStorage{
+		WorkspaceName: "foobar",
+		Stage:         config.StageDevStaging,
+		client:        client,
+	}
+
+	storage.ObjectAccess = storage.defaultObjectAccess
+
 	var mappings []archive.IDMapping
 	found, err := storage.Download(context.Background(), "/tmp", "foo", mappings)
 	if err != nil {
@@ -30,45 +50,4 @@ func TestObjectAccessToNonExistentObj(t *testing.T) {
 	if found {
 		t.Errorf("gcloud storage reported object found despite it being non-existent")
 	}
-}
-
-type objDoesNotExistGCloudStorage struct {
-	Delegate *DirectGCPStorage
-}
-
-// Init does nothing
-func (rs *objDoesNotExistGCloudStorage) Init(ctx context.Context, owner, workspace, instance string) error {
-	return rs.Delegate.Init(ctx, owner, workspace, instance)
-}
-
-// EnsureExists does nothing
-func (rs *objDoesNotExistGCloudStorage) EnsureExists(ctx context.Context) error {
-	return nil
-}
-
-// Download always returns false and does nothing
-func (rs *objDoesNotExistGCloudStorage) Download(ctx context.Context, destination string, name string, mappings []archive.IDMapping) (bool, error) {
-	rs.Delegate.ObjectAccess = func(ctx context.Context, bkt, obj string) (io.ReadCloser, bool, error) {
-		return nil, false, nil
-	}
-
-	return rs.Delegate.Download(ctx, destination, name, mappings)
-}
-
-// Download always returns false and does nothing
-func (rs *objDoesNotExistGCloudStorage) DownloadSnapshot(ctx context.Context, destination string, name string, mappings []archive.IDMapping) (bool, error) {
-	rs.Delegate.ObjectAccess = func(ctx context.Context, bkt, obj string) (io.ReadCloser, bool, error) {
-		return nil, false, nil
-	}
-
-	return rs.Delegate.DownloadSnapshot(ctx, destination, name, mappings)
-}
-
-func (rs *objDoesNotExistGCloudStorage) Qualify(name string) string {
-	return rs.Delegate.Qualify(name)
-}
-
-// Upload does nothing
-func (rs *objDoesNotExistGCloudStorage) Upload(ctx context.Context, source string, name string, opts ...UploadOption) error {
-	return xerrors.Errorf("not supported")
 }
