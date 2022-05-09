a2b2283005d29aa8385a8365ede09e7186713db8
Jan Keromnes <janx@linux.com>
2022-05-09 16:21:35 +0000

    [payment] Auto-format chargebee/team-subscription-handler.ts

--- components/ee/payment-endpoint/src/chargebee/team-subscription-handler.ts
+++ components/ee/payment-endpoint/src/chargebee/team-subscription-handler.ts
@@ -4,17 +4,17 @@
  * See License.enterprise.txt in the project root folder.
  */
 
-import { inject, injectable } from 'inversify';
+import { inject, injectable } from "inversify";
 
-import { TeamSubscriptionDB } from '@gitpod/gitpod-db/lib/team-subscription-db';
-import { log, LogContext } from '@gitpod/gitpod-protocol/lib/util/logging';
-import { Plans } from '@gitpod/gitpod-protocol/lib/plans';
-import { TeamSubscription } from '@gitpod/gitpod-protocol/lib/team-subscription-protocol';
-import { getCancelledAt, getStartDate } from './chargebee-subscription-helper';
-import { Chargebee as chargebee } from './chargebee-types';
-import { EventHandler } from './chargebee-event-handler';
+import { TeamSubscriptionDB } from "@gitpod/gitpod-db/lib/team-subscription-db";
+import { log, LogContext } from "@gitpod/gitpod-protocol/lib/util/logging";
+import { Plans } from "@gitpod/gitpod-protocol/lib/plans";
+import { TeamSubscription } from "@gitpod/gitpod-protocol/lib/team-subscription-protocol";
+import { getCancelledAt, getStartDate } from "./chargebee-subscription-helper";
+import { Chargebee as chargebee } from "./chargebee-types";
+import { EventHandler } from "./chargebee-event-handler";
 import { TeamSubscriptionService } from "../accounting/team-subscription-service";
-import { Config } from '../config';
+import { Config } from "../config";
 
 @injectable()
 export class TeamSubscriptionHandler implements EventHandler<chargebee.SubscriptionEventV2> {
@@ -23,7 +23,7 @@ export class TeamSubscriptionHandler implements EventHandler<chargebee.Subscript
     @inject(TeamSubscriptionService) protected readonly service: TeamSubscriptionService;
 
     canHandle(event: chargebee.Event<any>): boolean {
-        if (event.event_type.startsWith('subscription')) {
+        if (event.event_type.startsWith("subscription")) {
             const evt = event as chargebee.Event<chargebee.SubscriptionEventV2>;
             const plan = Plans.getById(evt.content.subscription.plan_id);
             if (!plan) {
@@ -53,17 +53,23 @@ export class TeamSubscriptionHandler implements EventHandler<chargebee.Subscript
         return true;
     }
 
-    async mapToTeamSubscription(userId: string, eventType: chargebee.EventType, chargebeeSubscription: chargebee.Subscription) {
+    async mapToTeamSubscription(
+        userId: string,
+        eventType: chargebee.EventType,
+        chargebeeSubscription: chargebee.Subscription,
+    ) {
         await this.db.transaction(async (db) => {
             const subs = await db.findTeamSubscriptions({
                 userId,
-                paymentReference: chargebeeSubscription.id
+                paymentReference: chargebeeSubscription.id,
             });
             if (subs.length === 0) {
                 // Sanity check: If we try to create too many slots here we OOM, so we error instead.
                 const quantity = chargebeeSubscription.plan_quantity;
                 if (quantity > this.config.maxTeamSlotsOnCreation) {
-                    throw new Error(`(TS ${chargebeeSubscription.id}): nr of slots on creation (${quantity}) is higher than configured maximum (${this.config.maxTeamSlotsOnCreation}). Skipping creation!`);
+                    throw new Error(
+                        `(TS ${chargebeeSubscription.id}): nr of slots on creation (${quantity}) is higher than configured maximum (${this.config.maxTeamSlotsOnCreation}). Skipping creation!`,
+                    );
                 }
 
                 const ts = TeamSubscription.create({
@@ -78,12 +84,12 @@ export class TeamSubscriptionHandler implements EventHandler<chargebee.Subscript
                 await db.storeTeamSubscriptionEntry(ts);
                 await this.service.addSlots(ts, quantity);
             } else {
-                const oldSubscription = subs.find(s => s.paymentReference === chargebeeSubscription.id);
+                const oldSubscription = subs.find((s) => s.paymentReference === chargebeeSubscription.id);
                 if (!oldSubscription) {
                     throw new Error(`Cannot find TeamSubscription for paymentReference ${chargebeeSubscription.id}!`);
                 }
 
-                if (eventType === 'subscription_cancelled') {
+                if (eventType === "subscription_cancelled") {
                     const cancelledAt = getCancelledAt(chargebeeSubscription);
                     oldSubscription.endDate = cancelledAt;
                     await this.service.deactivateAllSlots(oldSubscription, new Date(cancelledAt));
