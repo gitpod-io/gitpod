cc819bb5333299fa98689a5bd0c35cd2a8b3b8a4
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2022-05-12 19:59:17 -0400

    [ws-daemon] Refactor watch of configuration file

--- components/ws-daemon/pkg/config/config.go
+++ components/ws-daemon/pkg/config/config.go
@@ -6,21 +6,15 @@ package config
 
 import (
 	"bytes"
-	"context"
-	"crypto/sha256"
 	"crypto/tls"
-	"encoding/hex"
 	"encoding/json"
-	"io"
 	"os"
-	"time"
 
 	"golang.org/x/xerrors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
 	common_grpc "github.com/gitpod-io/gitpod/common-go/grpc"
-	"github.com/gitpod-io/gitpod/common-go/log"
 	"github.com/gitpod-io/gitpod/ws-daemon/pkg/daemon"
 )
 
@@ -39,63 +33,6 @@ func Read(fn string) (*Config, error) {
 	}
 
 	return &cfg, nil
-}
-
-// watchConfig watches the configuration file and if changed reloads the static layer
-func Watch(fn string, cb func(context.Context, *daemon.Config) error) {
-	hashConfig := func() (hash string, err error) {
-		f, err := os.Open(fn)
-		if err != nil {
-			return "", err
-		}
-		defer f.Close()
-
-		h := sha256.New()
-		_, err = io.Copy(h, f)
-		if err != nil {
-			return "", err
-		}
-
-		return hex.EncodeToString(h.Sum(nil)), nil
-	}
-	reloadConfig := func() error {
-		cfg, err := Read(fn)
-		if err != nil {
-			return err
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
-		defer cancel()
-
-		return cb(ctx, &cfg.Daemon)
-	}
-
-	var (
-		tick    = time.NewTicker(30 * time.Second)
-		oldHash string
-	)
-	defer tick.Stop()
-	for range tick.C {
-		currentHash, err := hashConfig()
-		if err != nil {
-			log.WithError(err).Warn("cannot check if config has changed")
-		}
-
-		if oldHash == "" {
-			oldHash = currentHash
-		}
-		if currentHash == oldHash {
-			continue
-		}
-		oldHash = currentHash
-
-		err = reloadConfig()
-		if err == nil {
-			log.Info("configuration was updated - reloaded static layer config")
-		} else {
-			log.WithError(err).Error("cannot reload config - config hot reloading did not work")
-		}
-	}
 }
 
 type Config struct {
