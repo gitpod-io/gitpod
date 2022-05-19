0f8e528f7b38c6cdf9d3a3e14045adcfb1acd6dd
Andrew Farries <andrew@gitpod.io>
2022-05-19 11:57:18 +0000

    Add test for workspace URL templates

--- install/installer/pkg/components/ws-manager/configmap_test.go
+++ install/installer/pkg/components/ws-manager/configmap_test.go
@@ -5,12 +5,16 @@
 package wsmanager
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/gitpod-io/gitpod/installer/pkg/common"
+	"github.com/gitpod-io/gitpod/installer/pkg/config/v1"
 	configv1 "github.com/gitpod-io/gitpod/installer/pkg/config/v1"
+	"github.com/gitpod-io/gitpod/installer/pkg/config/versions"
 	wsmancfg "github.com/gitpod-io/gitpod/ws-manager/api/config"
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -130,6 +134,61 @@ func TestBuildWorkspaceTemplates(t *testing.T) {
 			if diff := cmp.Diff(test.Expectation, act); diff != "" {
 				t.Errorf("Expectation mismatch (-want +got):\n%s", diff)
 			}
+		})
+	}
+}
+
+func TestWorkspaceURLTemplates(t *testing.T) {
+	tests := []struct {
+		Name                             string
+		Domain                           string
+		InstallationShortname            string
+		ExpectedWorkspaceUrlTemplate     string
+		ExpectedWorkspacePortURLTemplate string
+	}{
+		{
+			Name:                             "With an installation shortname",
+			Domain:                           "example.com",
+			InstallationShortname:            "eu02",
+			ExpectedWorkspaceUrlTemplate:     "https://{{ .Prefix }}.ws-eu02.example.com",
+			ExpectedWorkspacePortURLTemplate: "https://{{ .WorkspacePort }}-{{ .Prefix }}.ws-eu02.example.com",
+		},
+		{
+			Name:                             "Without an installation shortname",
+			Domain:                           "example.com",
+			InstallationShortname:            "",
+			ExpectedWorkspaceUrlTemplate:     "https://{{ .Prefix }}.ws.example.com",
+			ExpectedWorkspacePortURLTemplate: "https://{{ .WorkspacePort }}-{{ .Prefix }}.ws.example.com",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			ctx, err := common.NewRenderContext(config.Config{
+				Domain: test.Domain,
+				Metadata: configv1.Metadata{
+					InstallationShortname: test.InstallationShortname,
+				},
+				ObjectStorage: configv1.ObjectStorage{
+					InCluster: pointer.Bool(true),
+				},
+			}, versions.Manifest{}, "test_namespace")
+			require.NoError(t, err)
+
+			objs, err := configmap(ctx)
+			require.NoError(t, err)
+
+			cfgmap, ok := objs[0].(*corev1.ConfigMap)
+			require.Truef(t, ok, "configmap function did not return a configmap")
+
+			configJson, ok := cfgmap.Data["config.json"]
+			require.Truef(t, ok, "configmap data did not contain %q key", "config.json")
+
+			serviceConfig := wsmancfg.ServiceConfiguration{}
+			json.Unmarshal([]byte(configJson), &serviceConfig)
+
+			require.Equal(t, test.ExpectedWorkspaceUrlTemplate, serviceConfig.Manager.WorkspaceURLTemplate)
+			require.Equal(t, test.ExpectedWorkspacePortURLTemplate, serviceConfig.Manager.WorkspacePortURLTemplate)
 		})
 	}
 }
