56d2115917f261381b1e48e6fab8a166ccac15f8
Jean Pierre <jeanp413@hotmail.com>
2022-05-20 21:11:39 +0000

    Update VS Code Stable to 1.67.2

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-d5b6d5fcb1d4c6bd2c8a30a8366ea400a96830ef" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-9e0960d85f0c30b1ce57c9d14e6a905bc46f427c" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
