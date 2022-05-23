acc55c422bb83491b90168a23f699e8066408a00
Victor Nogueira <felladrin@gmail.com>
2022-05-23 08:47:09 +0000

    Update Gateway Plugin to work on JetBrains Gateway 2022.2

--- components/ide/jetbrains/gateway-plugin/build.gradle.kts
+++ components/ide/jetbrains/gateway-plugin/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.1.5"
+    id("org.jetbrains.intellij") version "1.6.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.1.2"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
