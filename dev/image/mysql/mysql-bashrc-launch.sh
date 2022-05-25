ff10fd3636865a17c58be4054a89750d86d716d2
Milan Pavlik <milan@gitpod.io>
2022-05-25 13:45:06 +0000

    Fix deps

--- dev/image/mysql/mysql-bashrc-launch.sh
+++ dev/image/mysql/mysql-bashrc-launch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+# Licensed under the GNU Affero General Public License (AGPL).
+# See License-AGPL.txt in the project root for license information.
+
 
 # this script is intended to be called from .bashrc
 # This is a workaround for not having something like supervisord
