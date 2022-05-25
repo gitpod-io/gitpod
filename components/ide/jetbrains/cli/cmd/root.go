8a1930a9ea5eeb955bb3ae0564f9e53e1a9d1c98
Victor Nogueira <victor@gitpod.io>
2022-05-25 08:07:33 +0000

    Wait the Backend Plugin to be ready and the Controller Client to be connected before executing "gp preview" on JetBrains IDEs

--- components/ide/jetbrains/cli/cmd/root.go
+++ components/ide/jetbrains/cli/cmd/root.go
@@ -5,7 +5,11 @@
 package cmd
 
 import (
+	"errors"
+	"log"
+	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -19,6 +23,18 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func getCliApiUrl() *url.URL {
+	var backendPort = 63342
+	if _, fileStatError := os.Stat("/ide-desktop/bin/idea-cli-dev"); !errors.Is(fileStatError, os.ErrNotExist) {
+		backendPort = backendPort + 1
+	}
+	parsedUrl, urlParseError := url.Parse("http://localhost:" + strconv.Itoa(backendPort) + "/api/gitpod/cli")
+	if urlParseError != nil {
+		log.Fatal(urlParseError)
+	}
+	return parsedUrl
 }
 
 func init() {}
