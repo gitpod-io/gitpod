d0f11b876cbab03f231f9bf32939f552cea6f169
Milan Pavlik <milan@gitpod.io>
2022-06-01 08:00:47 +0000

    [worspace-instance] Deprecate deployedTime field

--- components/gitpod-db/src/typeorm/entity/db-workspace-instance.ts
+++ components/gitpod-db/src/typeorm/entity/db-workspace-instance.ts
@@ -40,6 +40,7 @@ export class DBWorkspaceInstance implements WorkspaceInstance {
     })
     startedTime?: string;
 
+    // DEPRECATED
     @Column({
         default: "",
         transformer: Transformer.MAP_EMPTY_STR_TO_UNDEFINED,
