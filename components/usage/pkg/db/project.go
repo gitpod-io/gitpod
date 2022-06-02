e84166f7b03d34bfdced98a882b6893d2a1bd632
Milan Pavlik <milan@gitpod.io>
2022-06-02 13:36:52 +0200

    Update components/usage/pkg/db/project.go
    Co-authored-by: Jan Keromnes <jan.keromnes@typefox.io>

--- components/usage/pkg/db/project.go
+++ components/usage/pkg/db/project.go
@@ -29,7 +29,7 @@ type Project struct {
 	MarkedDeleted bool `gorm:"column:markedDeleted;type:tinyint;default:0;" json:"markedDeleted"`
 
 	// deleted is reserved for use by db-sync
-	_ int32 `gorm:"column:deleted;type:tinyint;default:0;" json:"deleted"`
+	_ bool `gorm:"column:deleted;type:tinyint;default:0;" json:"deleted"`
 }
 
 // TableName sets the insert table name for this struct type
