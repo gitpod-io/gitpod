bb6a4c27b3035174c01c4deee7eca64472a733d9
Milan Pavlik <milan@gitpod.io>
2022-06-06 20:25:33 +0000

    [usage] Handle empty value varchar time

--- components/usage/pkg/db/types_test.go
+++ components/usage/pkg/db/types_test.go
@@ -6,6 +6,7 @@ package db
 
 import (
 	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
 	"testing"
 	"time"
 )
@@ -75,5 +76,83 @@ func TestVarcharTime_Scan(t *testing.T) {
 				Error: err != nil,
 			})
 		})
+	}
+}
+
+func TestVarcharTime_Value_ISO8601(t *testing.T) {
+	for _, scenario := range []struct {
+		Time     VarcharTime
+		Expected string
+	}{
+		{
+			Time:     NewVarcharTime(time.Date(2019, 05, 10, 9, 54, 28, 185000000, time.UTC)),
+			Expected: "2019-05-10T09:54:28.185Z",
+		},
+		{
+			Time:     VarcharTime{},
+			Expected: "",
+		},
+	} {
+		wireFormat, err := scenario.Time.Value()
+		require.NoError(t, err)
+		require.Equal(t, scenario.Expected, wireFormat)
+	}
+}
+
+func TestVarcharTime_String_ISO8601(t *testing.T) {
+	for _, scenario := range []struct {
+		Time     VarcharTime
+		Expected string
+	}{
+		{
+			Time:     NewVarcharTime(time.Date(2019, 05, 10, 9, 54, 28, 185000000, time.UTC)),
+			Expected: "2019-05-10T09:54:28.185Z",
+		},
+		{
+			Time:     VarcharTime{},
+			Expected: "",
+		},
+	} {
+		require.Equal(t, scenario.Expected, scenario.Time.String())
+	}
+}
+
+func TestVarcharTime_SerializeAndDeserialize(t *testing.T) {
+	// Custom table to be able to exercise serialization easily, independent of other models
+	type VarcharModel struct {
+		ID   int         `gorm:"primaryKey"`
+		Time VarcharTime `gorm:"column:time;type:varchar(255);"`
+	}
+
+	conn := ConnectForTests(t)
+	require.NoError(t, conn.AutoMigrate(&VarcharModel{}))
+
+	conn.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&VarcharModel{})
+
+	for _, scenario := range []struct {
+		Description string
+		Input       VarcharModel
+		Expected    VarcharModel
+	}{
+		{
+			Description: "empty value for VarcharTime",
+			Input: VarcharModel{
+				ID:   1,
+				Time: VarcharTime{},
+			},
+			Expected: VarcharModel{
+				ID:   1,
+				Time: VarcharTime{},
+			},
+		},
+	} {
+		tx := conn.Create(scenario.Input)
+		require.NoError(t, tx.Error)
+
+		var read VarcharModel
+		tx = conn.First(&read, scenario.Input.ID)
+		require.NoError(t, tx.Error)
+
+		require.Equal(t, scenario.Expected, read)
 	}
 }
