3c02797c92ade4c038396ffe2c3e09eba2c7c5f4
Pavel Tumik <18602811+sagor999@users.noreply.github.com>
2022-06-10 00:14:04 +0000

    fix index on volume snapshot table

--- components/gitpod-db/src/typeorm/migration/1654628106102-VolumeSnapshotAddWSId.ts
+++ components/gitpod-db/src/typeorm/migration/1654628106102-VolumeSnapshotAddWSId.ts
@@ -13,7 +13,7 @@ export class VolumeSnapshotAddWSId1654628106102 implements MigrationInterface {
             await queryRunner.query("DROP TABLE `d_b_volume_snapshot`");
         }
         await queryRunner.query(
-            `CREATE TABLE IF NOT EXISTS d_b_volume_snapshot (  id char(36) NOT NULL, workspaceId char(36) NOT NULL, creationTime timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), volumeHandle varchar(255) NOT NULL, PRIMARY KEY (id), KEY ind_5iziusbz752p4jv3e8erq3wc7n (workspaceId), KEY ind_dbsync (creationTime)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`,
+            `CREATE TABLE IF NOT EXISTS d_b_volume_snapshot (  id char(36) NOT NULL, workspaceId char(36) NOT NULL, creationTime timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), volumeHandle varchar(255) NOT NULL, PRIMARY KEY (id), KEY ind_workspaceId_creationTime (workspaceId, creationTime), KEY ind_dbsync (creationTime)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`,
         );
     }
 
