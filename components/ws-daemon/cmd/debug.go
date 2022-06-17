ad7e8b4d87f20ca7851aaa0f94ee8aa200042fac
Christian Weichel <chris@gitpod.io>
2022-06-17 07:50:24 +0000

    Make content init work on Ubuntu

---
+++ components/ws-daemon/cmd/debug.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2020 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// debugCmd represents the generate command
+var debugCmd = &cobra.Command{
+	Use:   "debug",
+	Short: "Helps debug parts of ws-dameon",
+	Args:  cobra.ExactArgs(1),
+}
+
+func init() {
+	rootCmd.AddCommand(debugCmd)
+}
