f6288c0d60c95a0e0fb407bae3b1cb6cf829a25c
Andrew Farries <andrew@gitpod.io>
2022-06-17 10:56:49 +0000

    Make installer render usage configmap

---
+++ install/installer/pkg/components/usage/configmap.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the MIT License. See License-MIT.txt in the project root for license information.
+
+package usage
+
+import (
+	"fmt"
+
+	"github.com/gitpod-io/gitpod/installer/pkg/common"
+	"github.com/gitpod-io/gitpod/installer/pkg/config/v1/experimental"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func configmap(ctx *common.RenderContext) ([]runtime.Object, error) {
+	schedule := "1h"
+	_ = ctx.WithExperimental(func(ucfg *experimental.Config) error {
+		if ucfg.WebApp != nil && ucfg.WebApp.Usage != nil && ucfg.WebApp.Usage.Schedule != "" {
+			schedule = ucfg.WebApp.Usage.Schedule
+		}
+		return nil
+	})
+
+	return []runtime.Object{
+		&corev1.ConfigMap{
+			TypeMeta: common.TypeMetaConfigmap,
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("%s-config", Component),
+				Namespace: ctx.Namespace,
+				Labels:    common.DefaultLabels(Component),
+			},
+			Data: map[string]string{
+				"schedule": schedule,
+			},
+		},
+	}, nil
+}
