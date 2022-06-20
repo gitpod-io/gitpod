e0d413cee80280532b3a2ab05df94bb8341ffbcb
Nandaja Varma <nandaja.varma@gmail.com>
2022-06-20 08:49:14 +0000

    [installer] Add upgrade test werft flag

--- .werft/build.ts
+++ .werft/build.ts
@@ -62,8 +62,8 @@ async function run(context: any) {
 
     if (!config.withPreview || config.publishRelease) {
         werft.phase("deploy", "not deploying");
-        console.log("running without preview environment or publish-release is set");
-        return
+        console.log("no-preview or publish-release is set");
+        return;
     }
 
     try {
