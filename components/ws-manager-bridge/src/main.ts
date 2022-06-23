7e0807601db239259af2b5576987ffb1ee76ec59
Gero Posmyk-Leinemann <gero@gitpod.io>
2022-06-23 15:31:29 +0000

    [bridge] Move MetaInstanceController into Bridge.controlInstances

--- components/ws-manager-bridge/src/main.ts
+++ components/ws-manager-bridge/src/main.ts
@@ -14,7 +14,6 @@ import { TypeORM } from "@gitpod/gitpod-db/lib/typeorm/typeorm";
 import { TracingManager } from "@gitpod/gitpod-protocol/lib/util/tracing";
 import { ClusterServiceServer } from "./cluster-service-server";
 import { BridgeController } from "./bridge-controller";
-import { MetaInstanceController } from "./meta-instance-controller";
 
 log.enableJSONLogging("ws-manager-bridge", undefined, LogrusLogLevel.getFromEnv());
 
@@ -48,9 +47,6 @@ export const start = async (container: Container) => {
 
         const clusterServiceServer = container.get<ClusterServiceServer>(ClusterServiceServer);
         await clusterServiceServer.start();
-
-        const metaInstanceController = container.get<MetaInstanceController>(MetaInstanceController);
-        metaInstanceController.start();
 
         process.on("SIGTERM", async () => {
             log.info("SIGTERM received, stopping");
