3225597af9d2fea113b32f9913e3f5881a284f2b
Andrew Farries <andrew@gitpod.io>
2022-06-24 13:28:19 +0000

    Add `gp timeout show` command

---
+++ components/gitpod-cli/cmd/timeout-show.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	gitpod "github.com/gitpod-io/gitpod/gitpod-cli/pkg/gitpod"
+	"github.com/spf13/cobra"
+)
+
+// showTimeoutCommand shows the workspace timeout
+var showTimeoutCommand = &cobra.Command{
+	Use:   "show",
+	Short: "Show the current workspace timeout",
+	Run: func(_ *cobra.Command, _ []string) {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		wsInfo, err := gitpod.GetWSInfo(ctx)
+		if err != nil {
+			fail(err.Error())
+		}
+		client, err := gitpod.ConnectToServer(ctx, wsInfo, []string{
+			"function:getWorkspaceTimeout",
+			"resource:workspace::" + wsInfo.WorkspaceId + "::get/update",
+		})
+		if err != nil {
+			fail(err.Error())
+		}
+
+		res, err := client.GetWorkspaceTimeout(ctx, wsInfo.WorkspaceId)
+		if err != nil {
+			fail(err.Error())
+		}
+
+		// Try to use `DurationRaw` but fall back to `Duration` in case of
+		// old server component versions that don't expose it.
+		if res.DurationRaw != "" {
+			fmt.Println(res.DurationRaw)
+		} else {
+			fmt.Println(res.Duration)
+		}
+	},
+}
+
+func init() {
+	timeoutCmd.AddCommand(showTimeoutCommand)
+}
