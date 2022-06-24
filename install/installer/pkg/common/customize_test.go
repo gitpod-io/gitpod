5e0e0a863cd8f90382bb3fc426590a2c9408fd3c
Simon Emms <simon@gitpod.io>
2022-06-24 13:19:15 +0000

    [installer]: allow for customization of labels

--- install/installer/pkg/common/customize_test.go
+++ install/installer/pkg/common/customize_test.go
@@ -193,3 +193,189 @@ func TestCustomizeAnnotation(t *testing.T) {
 		})
 	}
 }
+
+func TestCustomizeLabel(t *testing.T) {
+	testCases := []struct {
+		Name           string
+		Customization  []config.Customization
+		Component      string
+		TypeMeta       metav1.TypeMeta
+		ExistingLabels []func() map[string]string
+		Expect         map[string]string
+	}{
+		{
+			Name:          "no customization",
+			Customization: nil,
+			Component:     "component",
+			TypeMeta:      common.TypeMetaDeployment,
+			Expect:        map[string]string{},
+		},
+		{
+			Customization: []config.Customization{},
+			Name:          "empty customization",
+			Component:     "component",
+			TypeMeta:      common.TypeMetaDeployment,
+			Expect:        map[string]string{},
+		},
+		{
+			Customization: []config.Customization{
+				{
+					TypeMeta: common.TypeMetaBatchJob,
+					Metadata: metav1.ObjectMeta{
+						Name: "component",
+						Labels: map[string]string{
+							"key1": "value1",
+						},
+					},
+				},
+			},
+			Name:      "ignore different typeMeta labels",
+			Component: "component",
+			TypeMeta:  common.TypeMetaDeployment,
+			Expect:    map[string]string{},
+		},
+		{
+			Customization: []config.Customization{
+				{
+					TypeMeta: common.TypeMetaDeployment,
+					Metadata: metav1.ObjectMeta{
+						Name: "component2",
+						Labels: map[string]string{
+							"key1": "value1",
+						},
+					},
+				},
+			},
+			Name:      "ignore same typeMeta, different name labels",
+			Component: "component",
+			TypeMeta:  common.TypeMetaDeployment,
+			Expect:    map[string]string{},
+		},
+		{
+			Customization: []config.Customization{
+				{
+					TypeMeta: common.TypeMetaDeployment,
+					Metadata: metav1.ObjectMeta{
+						Name: "component",
+						Labels: map[string]string{
+							"key1": "value1",
+						},
+					},
+				},
+			},
+			Name:      "single component labels",
+			Component: "component",
+			TypeMeta:  common.TypeMetaDeployment,
+			Expect: map[string]string{
+				"key1": "value1",
+			},
+		},
+		{
+			Customization: []config.Customization{
+				{
+					TypeMeta: common.TypeMetaDeployment,
+					Metadata: metav1.ObjectMeta{
+						Name: "component",
+						Labels: map[string]string{
+							"key1": "value1",
+							"key2": "value2",
+						},
+					},
+				},
+				{
+					TypeMeta: common.TypeMetaDeployment,
+					Metadata: metav1.ObjectMeta{
+						Name: "component",
+						Labels: map[string]string{
+							"key3": "value3",
+						},
+					},
+				},
+			},
+			Name:      "multiple component labels",
+			Component: "component",
+			TypeMeta:  common.TypeMetaDeployment,
+			Expect: map[string]string{
+				"key1": "value1",
+				"key2": "value2",
+				"key3": "value3",
+			},
+		},
+		{
+			Customization: []config.Customization{
+				{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "*",
+						Kind:       "*",
+					},
+					Metadata: metav1.ObjectMeta{
+						Name: "*",
+						Labels: map[string]string{
+							"key1": "value1",
+						},
+					},
+				},
+			},
+			Name:      "wildcard labels",
+			Component: "component",
+			TypeMeta:  common.TypeMetaDeployment,
+			Expect: map[string]string{
+				"key1": "value1",
+			},
+		},
+		{
+			Customization: []config.Customization{
+				{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "*",
+						Kind:       "*",
+					},
+					Metadata: metav1.ObjectMeta{
+						Name: "*",
+						Labels: map[string]string{
+							"key1": "value1",
+							"key2": "override",
+							"key3": "",
+						},
+					},
+				},
+			},
+			Name:      "override input, do not override existing",
+			Component: "component",
+			TypeMeta:  common.TypeMetaDeployment,
+			ExistingLabels: []func() map[string]string{
+				func() map[string]string {
+					return map[string]string{
+						"key2": "original",
+					}
+				},
+			},
+			Expect: map[string]string{
+				"key1": "value1",
+				"key2": "original",
+				"key3": "",
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			ctx, err := common.NewRenderContext(config.Config{
+				Customization: &testCase.Customization,
+			}, versions.Manifest{}, "test_namespace")
+			require.NoError(t, err)
+
+			result := common.CustomizeLabel(ctx, testCase.Component, testCase.TypeMeta, testCase.ExistingLabels...)
+
+			// These all have the default labels - add these in
+			expectation := common.DefaultLabels(testCase.Component)
+			for k, v := range testCase.Expect {
+				expectation[k] = v
+			}
+
+			if !reflect.DeepEqual(expectation, result) {
+				t.Errorf("expected %v but got %v", expectation, result)
+			}
+		})
+	}
+}
