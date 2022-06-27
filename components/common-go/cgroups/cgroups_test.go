ba44a7ad192325b497a7f1ef8fe80c712fa5f598
Thomas Schubart <thomas@gitpod.io>
2022-06-27 13:48:49 +0000

    [common] Introduce cgroup library

--- components/common-go/cgroups/cgroups_test.go
+++ components/common-go/cgroups/cgroups_test.go
@@ -5,9 +5,12 @@
 package cgroups
 
 import (
+	"math"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var cgroupPath = []string{"/kubepods", "burstable", "pods234sdf", "234as8df34"}
@@ -85,5 +88,49 @@ func verifyCpuControllerToggled(t *testing.T, path string, enabled bool) {
 		t.Fatalf("%s should have enabled cpu controller", path)
 	} else if !enabled && string(content) == "+cpu" {
 		t.Fatalf("%s should not have enabled cpu controller", path)
+	}
+}
+
+func TestReadSingleValue(t *testing.T) {
+	scenarios := []struct {
+		name     string
+		content  string
+		expected uint64
+	}{
+		{
+			name:     "cgroup2 max value",
+			content:  "max",
+			expected: math.MaxUint64,
+		},
+		{
+			name:     "cgroup1 max value",
+			content:  "-1",
+			expected: math.MaxUint64,
+		},
+		{
+			name:     "valid value",
+			content:  "100000",
+			expected: 100_000,
+		},
+	}
+
+	for _, s := range scenarios {
+		t.Run(s.name, func(t *testing.T) {
+			f, err := os.CreateTemp("", "cgroup_test*")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if _, err := f.Write([]byte(s.content)); err != nil {
+				t.Fatal(err)
+			}
+
+			v, err := ReadSingleValue(f.Name())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t, s.expected, v)
+		})
 	}
 }
