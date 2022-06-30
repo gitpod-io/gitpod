1f9ed57eb196def0dc3912e21f8c415eb4049361
Andrew Farries <andrew@gitpod.io>
2022-06-30 15:20:17 +0000

    Remove unused deletedAt column

--- components/gitpod-db/src/typeorm/migration/1656408949484-AddBlockedRepositoryTable.ts
+++ components/gitpod-db/src/typeorm/migration/1656408949484-AddBlockedRepositoryTable.ts
@@ -9,7 +9,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class AddBlockedRepositoryTable1656408949484 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE \`d_b_blocked_repository\` (\`id\` int NOT NULL AUTO_INCREMENT, \`urlRegexp\` varchar(255) NOT NULL, \`blockUser\` tinyint NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, \`deleted\` tinyint NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+            `CREATE TABLE \`d_b_blocked_repository\` (\`id\` int NOT NULL AUTO_INCREMENT, \`urlRegexp\` varchar(255) NOT NULL, \`blockUser\` tinyint NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted\` tinyint NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
         );
     }
 
