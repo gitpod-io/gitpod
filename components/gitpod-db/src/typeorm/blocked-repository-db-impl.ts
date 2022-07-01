543d66f914d46b36557236a2e98bfa07fb2048a3
Andrew Farries <andrew@gitpod.io>
2022-07-01 10:33:49 +0000

    [server] Add APIs for working with blocked repos
    * Create, list, delete.

--- components/gitpod-db/src/typeorm/blocked-repository-db-impl.ts
+++ components/gitpod-db/src/typeorm/blocked-repository-db-impl.ts
@@ -23,6 +23,50 @@ export class TypeORMBlockedRepositoryDBImpl implements BlockedRepositoryDB {
         return (await this.getEntityManager()).getRepository<DBBlockedRepository>(DBBlockedRepository);
     }
 
+    public async createBlockedRepository(urlRegexp: string, blockUser: boolean): Promise<BlockedRepository> {
+        const blockedRepositoryRepo = await this.getBlockedRepositoryRepo();
+
+        return await blockedRepositoryRepo.save({ urlRegexp: urlRegexp, blockUser: blockUser, deleted: false });
+    }
+
+    public async deleteBlockedRepository(id: number): Promise<boolean> {
+        const blockedRepositoryRepo = await this.getBlockedRepositoryRepo();
+
+        const result = await blockedRepositoryRepo.delete(id);
+        return !!result.affected;
+    }
+
+    public async findAllBlockedRepositories(
+        offset: number,
+        limit: number,
+        orderBy: keyof BlockedRepository,
+        orderDir: "DESC" | "ASC",
+        searchTerm?: string,
+        minCreationDate?: Date,
+        maxCreationDate?: Date,
+    ): Promise<{ total: number; rows: BlockedRepository[] }> {
+        const blockedRepositoryRepo = await this.getBlockedRepositoryRepo();
+
+        const qBuilder = blockedRepositoryRepo.createQueryBuilder("br").where(`br.deleted = 0`);
+        if (searchTerm) {
+            qBuilder.andWhere(`br.urlRegexp LIKE :searchTerm`, { searchTerm: "%" + searchTerm + "%" });
+        }
+        if (minCreationDate) {
+            qBuilder.andWhere("br.createdAt >= :minCreationDate", {
+                minCreationDate: minCreationDate.toISOString(),
+            });
+        }
+        if (maxCreationDate) {
+            qBuilder.andWhere("br.createdAt < :maxCreationDate", {
+                maxCreationDate: maxCreationDate.toISOString(),
+            });
+        }
+        qBuilder.orderBy("br." + orderBy, orderDir);
+        qBuilder.skip(offset).take(limit).select();
+        const [rows, total] = await qBuilder.getManyAndCount();
+        return { total, rows };
+    }
+
     public async findBlockedRepositoryByURL(contextURL: string): Promise<BlockedRepository | undefined> {
         const blockedRepositoryRepo = await this.getBlockedRepositoryRepo();
 
