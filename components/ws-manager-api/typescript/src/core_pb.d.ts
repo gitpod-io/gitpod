351834665016048cb8d039f1919839d5969add84
Christian Weichel <chris@gitpod.io>
2022-07-04 08:59:47 +0000

    [ws-manager] Ship env vars as secret

--- components/ws-manager-api/typescript/src/core_pb.d.ts
+++ components/ws-manager-api/typescript/src/core_pb.d.ts
@@ -1261,6 +1261,7 @@ export enum WorkspaceFeatureFlag {
     FULL_WORKSPACE_BACKUP = 4,
     FIXED_RESOURCES = 5,
     PERSISTENT_VOLUME_CLAIM = 7,
+    PROTECTED_SECRETS = 8,
 }
 
 export enum WorkspaceType {
