0e2aa5683bf8a239665d64d93a85f12660dcd437
Liam Bennett <liam@gitpod.io>
2022-07-05 09:20:01 +0000

    WIP

--- dev/preview/werft-credential-helper.sh
+++ dev/preview/werft-credential-helper.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl --silent localhost:22999/_supervisor/v1/token/git/github.com/ | jq -r '.token'
+if [[ -f $GITHUB_TOKEN_PATH ]]; then
+    cat "$GITHUB_TOKEN_PATH"
+else
+    curl --silent localhost:22999/_supervisor/v1/token/git/github.com/ | jq -r '.token'
+fi
