adc5f2ae3d80600dfa50ea0a96cc9f7485f37fe6
mustard <mhqnwt@gmail.com>
2022-07-06 08:55:00 +0000

    [code] update stable to 1.68.2 with conn telemetry

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-5cff80fdce975a70553712505e3eebf73bfdf5fa" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-9ea9102852a77fc925bdabac4ad95b3932181795" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
