76685779e5afec531e0629c1442e05c3c0b447ae
Mads Hartmann <mads379@gmail.com>
2022-07-07 13:02:55 +0000

    Implement --watch mode for previewctl install-context

--- dev/preview/previewctl/cmd/install_context.go
+++ dev/preview/previewctl/cmd/install_context.go
@@ -19,25 +19,53 @@ var (
 
 func installContextCmd(logger *logrus.Logger) *cobra.Command {
 
+	// Used to ensure that we only install contexts
+	var lastSuccessfulPreviewEnvironment *preview.Preview = nil
+
+	install := func(timeout time.Duration) error {
+		p, err := preview.New("", logger)
+
+		if err != nil {
+			return err
+		}
+
+		if lastSuccessfulPreviewEnvironment != nil && lastSuccessfulPreviewEnvironment.Same(p) {
+			logger.Infof("The preview envrionment hasn't changed")
+			return nil
+		}
+
+		err = p.InstallContext(true, timeout)
+		if err == nil {
+			lastSuccessfulPreviewEnvironment = p
+		}
+		return err
+	}
+
 	cmd := &cobra.Command{
 		Use:   "install-context",
 		Short: "Installs the kubectl context of a preview environment.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			p, err := preview.New(branch, logger)
-			if err != nil {
-				return err
-			}
 
-			err = p.InstallContext(watch, timeout)
-			if err != nil {
-				logger.WithFields(logrus.Fields{"err": err}).Fatal("Failed to install context.")
+			if watch {
+				for range time.Tick(15 * time.Second) {
+					// We're using a short timeout here to handle the scenario where someone switches
+					// to a branch that doens't have a preview envrionment. In that case the default
+					// timeout would mean that we would block for 10 minutes, potentially missing
+					// if the user changes to a new branch that does that a preview.
+					err := install(30 * time.Second)
+					if err != nil {
+						logger.WithFields(logrus.Fields{"err": err}).Info("Failed to install context. Trying again soon.")
+					}
+				}
+			} else {
+				return install(timeout)
 			}
 
 			return nil
 		},
 	}
 
-	cmd.Flags().BoolVar(&watch, "watch", false, "If wait is enabled, previewctl will keep trying to install the kube-context every 30 seconds.")
-	cmd.Flags().DurationVarP(&timeout, "timeout", "t", 10*time.Minute, "Timeout for the watch flag.")
+	cmd.Flags().BoolVar(&watch, "watch", false, "If watch is enabled, previewctl will keep trying to install the kube-context every 15 seconds.")
+	cmd.Flags().DurationVarP(&timeout, "timeout", "t", 10*time.Minute, "Timeout before considering the installation failed")
 	return cmd
 }
