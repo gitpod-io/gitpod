59a317a320260368029d95a711a32caffbd0fdff
mustard <mhqnwt@gmail.com>
2022-07-11 19:33:20 +0000

    [code] upgrade stable code with ports onOpen fix

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-d7b839fa361b26f075b1cbacd058e1cda532f0b9" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-42da3e469c53b727dcf5aca6487727cc5c034095" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
