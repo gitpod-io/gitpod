962222aa77f6642a56c661379f64579cd64348ed
Simon Emms <simon@gitpod.io>
2022-07-12 10:52:27 +0000

    [installer]: add output test for the render function

---
+++ install/installer/cmd/render_test.go
@@ -0,0 +1,82 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the MIT License. See License-MIT.txt in the project root for license information.
+
+package cmd
+
+import (
+	"flag"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	update = flag.Bool("update", false, "update the golden files of this test")
+)
+
+func init() {
+	// Ensure that the randomisation always returns the same values
+	rootOpts.SeedValue = 42
+	setSeed()
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	os.Exit(m.Run())
+}
+
+func TestRender(t *testing.T) {
+	dir, err := ioutil.ReadDir("testdata/render")
+	require.NoError(t, err)
+
+	var testCases []struct {
+		Name string
+	}
+
+	for _, d := range dir {
+		if d.IsDir() {
+			testCases = append(testCases, struct{ Name string }{
+				Name: d.Name(),
+			})
+		}
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			rootOpts.VersionMF = "testdata/render/versions.yaml"
+			renderOpts.ConfigFN = "testdata/render/" + testCase.Name + "/config.yaml"
+			goldenPath := "testdata/render/" + testCase.Name + "/output.golden"
+
+			f, err := os.OpenFile(goldenPath, os.O_RDWR, 0644)
+			defer f.Close()
+			require.NoError(t, err)
+
+			// Generate the YAML from the render function
+			yaml, err := renderFn()
+			require.NoError(t, err)
+
+			// Concatenate the []strings with a newline between
+			got := ""
+			for _, item := range yaml {
+				got += item + "\n"
+			}
+
+			if *update {
+				_, err := f.WriteString(got)
+				require.NoError(t, err)
+				return
+			}
+
+			// Get the expected output from the golden file
+			content, err := ioutil.ReadAll(f)
+			if err != nil {
+				require.NoError(t, err)
+			}
+
+			// Compare
+			require.Equal(t, string(content), got)
+		})
+	}
+}
