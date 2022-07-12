7e3682f27b0f2d82187364f724867ca9bee8f3ec
Andrea Falzetti <andrea@gitpod.io>
2022-07-12 20:52:07 +0000

    [code] Update stable to 1.69.1

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-42da3e469c53b727dcf5aca6487727cc5c034095" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-cc059a57be4856e56a5e83de87e6c9b81263e3fe" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
