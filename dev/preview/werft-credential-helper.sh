b109199b8cb249d0fe9bbd4326df12d61640a20d
Mads Hartmann <mads379@gmail.com>
2022-07-12 11:46:17 +0000

    Make werft-credential-helper work outside workspaces

--- dev/preview/werft-credential-helper.sh
+++ dev/preview/werft-credential-helper.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl --silent localhost:22999/_supervisor/v1/token/git/github.com/ | jq -r '.token'
+if [[ -f $WERFT_GITHUB_TOKEN_PATH ]]; then
+    cat "$WERFT_GITHUB_TOKEN_PATH"
+else
+    curl --silent localhost:22999/_supervisor/v1/token/git/github.com/ | jq -r '.token'
+fi
