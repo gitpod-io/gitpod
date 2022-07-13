b9a9a9b9fc09848a48e2dd5a72ecb6642fb08e87
Andrew Farries <andrew@gitpod.io>
2022-07-13 10:05:55 +0000

    Add tests for usage record creation

---
+++ components/usage/pkg/db/workspace_instance_usage_test.go
@@ -0,0 +1,101 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package db_test
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/gitpod-io/gitpod/usage/pkg/db"
+	"github.com/gitpod-io/gitpod/usage/pkg/db/dbtest"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCanCreateUsageRecords(t *testing.T) {
+	teamID := uuid.New().String()
+	teamAttributionID := db.NewTeamAttributionID(teamID)
+	instanceID := uuid.New()
+	ctx := context.Background()
+
+	scenarios := []struct {
+		Name         string
+		UsageRecords []db.WorkspaceInstanceUsage
+	}{
+		{
+			Name: "One workspace instance",
+			UsageRecords: []db.WorkspaceInstanceUsage{{
+				WorkspaceID:   instanceID,
+				AttributionID: teamAttributionID,
+				StartedAt:     time.Now(),
+				StoppedAt:     sql.NullTime{},
+				CreditsUsed:   0,
+				GenerationId:  0,
+				Deleted:       false,
+			}},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		conn := dbtest.ConnectForTests(t)
+		t.Run(scenario.Name, func(t *testing.T) {
+			t.Cleanup(func() {
+				require.NoError(t, conn.Where(instanceID).Delete(&db.WorkspaceInstanceUsage{}).Error)
+			})
+
+			require.NoError(t, db.CreateUsageRecords(ctx, conn, scenario.UsageRecords))
+		})
+	}
+}
+
+func TestCanHandleDuplicateRecords(t *testing.T) {
+	teamID := uuid.New().String()
+	teamAttributionID := db.NewTeamAttributionID(teamID)
+	instanceID := uuid.New()
+	instanceStartTime := time.Now()
+	ctx := context.Background()
+
+	scenarios := []struct {
+		Name         string
+		UsageRecords []db.WorkspaceInstanceUsage
+	}{
+		{
+			Name: "The same instance twice",
+			UsageRecords: []db.WorkspaceInstanceUsage{
+				{
+					WorkspaceID:   instanceID,
+					AttributionID: teamAttributionID,
+					StartedAt:     instanceStartTime,
+					StoppedAt:     sql.NullTime{},
+					CreditsUsed:   0,
+					GenerationId:  0,
+					Deleted:       false,
+				},
+				{
+					WorkspaceID:   instanceID,
+					AttributionID: teamAttributionID,
+					StartedAt:     instanceStartTime,
+					StoppedAt:     sql.NullTime{},
+					CreditsUsed:   0,
+					GenerationId:  0,
+					Deleted:       false,
+				},
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		conn := dbtest.ConnectForTests(t)
+		t.Run(scenario.Name, func(t *testing.T) {
+			t.Cleanup(func() {
+				require.NoError(t, conn.Where(instanceID).Delete(&db.WorkspaceInstanceUsage{}).Error)
+			})
+
+			require.NoError(t, db.CreateUsageRecords(ctx, conn, scenario.UsageRecords))
+		})
+	}
+}
