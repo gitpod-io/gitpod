8870f755db9dce0b119a9fbc0e7b1a3cc0098428
Filip Troníček <filip@gitpod.io>
2022-07-13 16:46:40 +0000

    Add our Extension control manifest to the IDE proxy
    Move steps around
    
    use --parents
    
    debug
    
    Remove `ls` command
    
    Add marketplace URL to configmap
    
    Update code commit
    
    Remove `vscode-webview.net`
    
    Add another copy
    
    Add the manifest to the repo
    
    Remove unused code
    
    Fix URL replacement
    
    Update code commit
    
    Update code commit
    
    Add CORS header

--- install/installer/pkg/components/blobserve/configmap.go
+++ install/installer/pkg/components/blobserve/configmap.go
@@ -40,18 +40,6 @@ func configmap(ctx *common.RenderContext) ([]runtime.Object, error) {
 					PrePull: []string{},
 					Workdir: "/ide",
 					Replacements: []blobserve.StringReplacement{{
-						Search:      "vscode-webview.net",
-						Replacement: ctx.Config.Domain,
-						Path:        "/ide/out/vs/workbench/workbench.web.api.js",
-					}, {
-						Search:      "vscode-webview.net",
-						Replacement: ctx.Config.Domain,
-						Path:        "/ide/out/vs/workbench/workbench.web.main.js",
-					}, {
-						Search:      "vscode-webview.net",
-						Replacement: ctx.Config.Domain,
-						Path:        "/ide/out/vs/workbench/services/extensions/worker/extensionHostWorker.js",
-					}, {
 						Search:      "vscode-cdn.net",
 						Replacement: ctx.Config.Domain,
 						Path:        "/ide/out/vs/workbench/workbench.web.api.js",
@@ -70,6 +58,10 @@ func configmap(ctx *common.RenderContext) ([]runtime.Object, error) {
 					}, {
 						Search:      "open-vsx.org",
 						Replacement: openVSXProxyUrl,
+						Path:        "/ide/out/vs/workbench/workbench.web.main.js",
+					}, {
+						Search:      "ide.gitpod.io/code/markeplace.json",
+						Replacement: fmt.Sprintf("ide.%s/code/marketplace.json", ctx.Config.Domain),
 						Path:        "/ide/out/vs/workbench/workbench.web.main.js",
 					}},
 					InlineStatic: []blobserve.InlineReplacement{{
