586da5742709d2c06caedbd4c393abd4618c0ee3
Tarun Pothulapati <tarun@gitpod.io>
2022-07-18 06:32:20 +0000

    [local-preview] honour `DO_NOT_TRACK` env variable
    Currently, There is no way to disable telemetry for `local-preview`
    as we run the cronjob manaully during init, when Gitpod is ready.
    
    This fixes that by honouring [`DO_NOT_TRACK` envrionment variable](https://consoledonottrack.com/)
    , which when set prevent the script from triggering the cronjob manually.
    
    Signed-off-by: Tarun Pothulapati <tarun@gitpod.io>

--- install/preview/prettylog/main.go
+++ install/preview/prettylog/main.go
@@ -24,7 +24,7 @@ var msgs = []struct {
 	{Msg: "preparing system", Success: "extracting images to download ahead"},
 	{Msg: "downloading images", Success: "--output-split-files"},
 	{Msg: "preparing Gitpod preview installation", Success: "rm -rf /var/lib/rancher/k3s/server/manifests/gitpod"},
-	{Msg: "starting Gitpod", Success: "gitpod-telemetry-init"},
+	{Msg: "starting Gitpod", Success: "Gitpod pods are ready"},
 	{Msg: fmt.Sprintf("Gitpod is running. Visit https://%s to access the dashboard", os.Getenv("DOMAIN"))},
 }
 
