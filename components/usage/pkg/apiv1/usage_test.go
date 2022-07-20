0d27d25b7cdd0cc0314b3b36b30d13bae97fd675
Milan Pavlik <milan@gitpod.io>
2022-07-20 14:30:05 +0000

    [usage] ListUsage filters by time range

--- components/usage/pkg/apiv1/usage_test.go
+++ components/usage/pkg/apiv1/usage_test.go
@@ -7,8 +7,6 @@ package apiv1
 import (
 	"context"
 	"database/sql"
-	"testing"
-
 	"github.com/gitpod-io/gitpod/common-go/baseserver"
 	v1 "github.com/gitpod-io/gitpod/usage-api/v1"
 	"github.com/gitpod-io/gitpod/usage/pkg/db"
@@ -20,68 +18,118 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"testing"
+	"time"
 )
 
 func TestUsageService_ListBilledUsage(t *testing.T) {
-	srv := baseserver.NewForTests(t,
-		baseserver.WithGRPC(baseserver.MustUseRandomLocalAddress(t)),
-	)
-
-	dbconn := dbtest.ConnectForTests(t)
-	v1.RegisterUsageServiceServer(srv.GRPC(), NewUsageService(dbconn))
-	baseserver.StartServerForTests(t, srv)
-
-	conn, err := grpc.Dial(srv.GRPCAddress(), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	require.NoError(t, err)
-
-	client := v1.NewUsageServiceClient(conn)
 	ctx := context.Background()
 
-	const attributionID = "team:123-456-789"
-	instanceId := uuid.New()
-	startedAt := timestamppb.Now()
-	instanceUsages := []db.WorkspaceInstanceUsage{
-		{
-			InstanceID:    instanceId,
-			AttributionID: attributionID,
-			StartedAt:     startedAt.AsTime(),
-			StoppedAt:     sql.NullTime{},
-			CreditsUsed:   0,
-			GenerationID:  0,
-		},
-	}
-	dbtest.CreateWorkspaceInstanceUsageRecords(t, dbconn, instanceUsages...)
+	attributionID := db.NewTeamAttributionID(uuid.New().String())
 
 	type Expectation struct {
 		Code        codes.Code
 		InstanceIds []string
 	}
 
-	scenarios := []struct {
-		name          string
-		AttributionID string
-		Expect        Expectation
-	}{
+	type Scenario struct {
+		name      string
+		Instances []db.WorkspaceInstanceUsage
+		Request   *v1.ListBilledUsageRequest
+		Expect    Expectation
+	}
+
+	scenarios := []Scenario{
 		{
-			name:          "returns one usage record",
-			AttributionID: attributionID,
+			name:      "fails when From is after To",
+			Instances: nil,
+			Request: &v1.ListBilledUsageRequest{
+				AttributionId: string(attributionID),
+				From:          timestamppb.New(time.Date(2022, 07, 1, 13, 0, 0, 0, time.UTC)),
+				To:            timestamppb.New(time.Date(2022, 07, 1, 12, 0, 0, 0, time.UTC)),
+			},
 			Expect: Expectation{
-				Code:        codes.OK,
-				InstanceIds: []string{instanceId.String()},
+				Code:        codes.InvalidArgument,
+				InstanceIds: nil,
 			},
 		},
+		{
+			name:      "fails when time range is greater than 31 days",
+			Instances: nil,
+			Request: &v1.ListBilledUsageRequest{
+				AttributionId: string(attributionID),
+				From:          timestamppb.New(time.Date(2022, 7, 1, 13, 0, 0, 0, time.UTC)),
+				To:            timestamppb.New(time.Date(2022, 8, 1, 13, 0, 1, 0, time.UTC)),
+			},
+			Expect: Expectation{
+				Code:        codes.InvalidArgument,
+				InstanceIds: nil,
+			},
+		},
+		(func() Scenario {
+			start := time.Date(2022, 07, 1, 13, 0, 0, 0, time.UTC)
+			attrID := db.NewTeamAttributionID(uuid.New().String())
+			var instances []db.WorkspaceInstanceUsage
+			var instanceIDs []string
+			for i := 0; i < 10; i++ {
+				instance := dbtest.NewWorkspaceInstanceUsage(t, db.WorkspaceInstanceUsage{
+					AttributionID: attrID,
+					StartedAt:     start.Add(time.Duration(i) * 24 * time.Hour),
+					StoppedAt: sql.NullTime{
+						Time:  start.Add(time.Duration(i)*24*time.Hour + time.Hour),
+						Valid: true,
+					},
+				})
+				instances = append(instances, instance)
+
+				instanceIDs = append(instanceIDs, instance.InstanceID.String())
+			}
+
+			return Scenario{
+				name:      "filters results to specified time range",
+				Instances: instances,
+				Request: &v1.ListBilledUsageRequest{
+					AttributionId: string(attrID),
+					From:          timestamppb.New(start),
+					To:            timestamppb.New(start.Add(5 * 24 * time.Hour)),
+				},
+				Expect: Expectation{
+					Code:        codes.OK,
+					InstanceIds: instanceIDs[0:5],
+				},
+			}
+		})(),
 	}
 
 	for _, scenario := range scenarios {
 		t.Run(scenario.name, func(t *testing.T) {
-			resp, err := client.ListBilledUsage(ctx, &v1.ListBilledUsageRequest{
-				AttributionId: scenario.AttributionID,
-			})
+			dbconn := dbtest.ConnectForTests(t)
+			dbtest.CreateWorkspaceInstanceUsageRecords(t, dbconn, scenario.Instances...)
+
+			srv := baseserver.NewForTests(t,
+				baseserver.WithGRPC(baseserver.MustUseRandomLocalAddress(t)),
+			)
+
+			v1.RegisterUsageServiceServer(srv.GRPC(), NewUsageService(dbconn))
+			baseserver.StartServerForTests(t, srv)
+
+			conn, err := grpc.Dial(srv.GRPCAddress(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			require.NoError(t, err)
+
+			client := v1.NewUsageServiceClient(conn)
+
+			resp, err := client.ListBilledUsage(ctx, scenario.Request)
+			require.Equal(t, scenario.Expect.Code, status.Code(err))
+
+			if err != nil {
+				return
+			}
+
 			var instanceIds []string
 			for _, billedSession := range resp.Sessions {
 				instanceIds = append(instanceIds, billedSession.InstanceId)
 			}
-			require.Equal(t, scenario.Expect.Code, status.Code(err))
+
 			require.Equal(t, scenario.Expect.InstanceIds, instanceIds)
 		})
 	}
