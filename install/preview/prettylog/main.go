747f7ab6c8214804fd5be432c06e2d745afd578f
Tarun Pothulapati <tarun@gitpod.io>
2022-07-25 10:49:23 +0000

    [local-preview] Send telemetry for each event
    Currently, The telemetry of `local-preview` is the same as any self-hosted install which
    means that these telemetry cronjob is run daily, and also once when the installation is ready.
    This information isn't as useful, as the properties aren't that important for a short-lived
    installation.
    
    Instead, It would make sense to have telemetry on each event of the `local-preview`
    command itself so that we can measure the Installation success rate,  Time taken, etc
    to improve the overall UX of `local-preview`. This will be more important for us
    to understand the usage of `local-preview` itself.
    
    This PR fixes that by attaching a telemetry event to be sent, for each status update
    in `local-preview`. For this to work, A anonymous User ID is generated and used
    for all status updates.
    
    Event Schema:
    
    ```
    analytics.track({
        userId: userId,
        event: "localpreview_status",
        properties: {
            status: "checking prerequisites" | "starting gitpod" | "gitpod ready"
        }
    })
    ```
    
    Signed-off-by: Tarun Pothulapati <tarun@gitpod.io>

--- install/preview/prettylog/main.go
+++ install/preview/prettylog/main.go
@@ -12,21 +12,30 @@ import (
 	"strings"
 
 	"github.com/pterm/pterm"
+	"gopkg.in/segmentio/analytics-go.v3"
 )
 
-var msgs = []struct {
-	Fail    string
-	Success string
+const (
+	telemetryEvent = "localpreview_status"
+)
 
-	Msg string
-}{
-	{Msg: "checking prerequisites", Fail: "requires a system with at least", Success: "Gitpod Domain:"},
-	{Msg: "preparing system", Success: "extracting images to download ahead"},
-	{Msg: "downloading images", Success: "--output-split-files"},
-	{Msg: "preparing Gitpod preview installation", Success: "rm -rf /var/lib/rancher/k3s/server/manifests/gitpod"},
-	{Msg: "starting Gitpod", Success: "Gitpod pods are ready"},
-	{Msg: fmt.Sprintf("Gitpod is running. Visit https://%s to access the dashboard", os.Getenv("DOMAIN"))},
-}
+var (
+	segmentIOToken string
+	msgs           = []struct {
+		Fail    string
+		Success string
+		Status  string
+
+		Msg string
+	}{
+		{Msg: "checking prerequisites", Fail: "requires a system with at least", Success: "Gitpod Domain:", Status: "checking prerequisites"},
+		{Msg: "preparing system", Success: "extracting images to download ahead"},
+		{Msg: "downloading images", Success: "--output-split-files"},
+		{Msg: "preparing Gitpod preview installation", Success: "rm -rf /var/lib/rancher/k3s/server/manifests/gitpod"},
+		{Msg: "starting Gitpod", Success: "Gitpod pods are ready", Status: "starting gitpod"},
+		{Msg: fmt.Sprintf("Gitpod is running. Visit https://%s to access the dashboard", os.Getenv("DOMAIN")), Status: "gitpod ready"},
+	}
+)
 
 func main() {
 	dmp, err := os.OpenFile("logs.txt", os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
@@ -40,10 +49,11 @@ func main() {
 	scan := bufio.NewScanner(r)
 	var msgIdx int
 	lastSpinner, _ := pterm.DefaultSpinner.Start(msgs[msgIdx].Msg)
+	// send Telemetry update for the first phase
+	send_telemetry(msgs[msgIdx].Status)
 	for scan.Scan() {
 		line := scan.Text()
 		msg := msgs[msgIdx]
-
 		var next bool
 		switch {
 		case msg.Fail != "" && strings.Contains(line, msg.Fail):
@@ -63,6 +73,9 @@ func main() {
 			return
 		}
 		lastSpinner, _ = pterm.DefaultSpinner.Start(msgs[msgIdx].Msg)
+		// send Telemetry for phase update
+		send_telemetry(msgs[msgIdx].Status)
+
 	}
 	err = scan.Err()
 	if errors.Is(err, io.EOF) {
@@ -70,5 +83,27 @@ func main() {
 	}
 	if err != nil {
 		panic(err)
+	}
+}
+
+func send_telemetry(status string) {
+	if os.Getenv("DO_NOT_TRACK") != "1" && status != "" {
+		if segmentIOToken == "" {
+			panic("No segmentIOToken set during build")
+		}
+
+		client, _ := analytics.NewWithConfig(segmentIOToken, analytics.Config{})
+		defer func() {
+			client.Close()
+
+		}()
+
+		telemetry := analytics.Track{
+			UserId: os.Getenv("USER_ID"),
+			Event:  telemetryEvent,
+			Properties: analytics.NewProperties().
+				Set("status", status),
+		}
+		client.Enqueue(telemetry)
 	}
 }
