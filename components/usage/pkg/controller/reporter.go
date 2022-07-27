35feada89ff7ed63a4eb3d3d21ce833579d5d92d
Milan Pavlik <milan@gitpod.io>
2022-07-27 09:06:56 +0000

    [usage] Add metrics for number of usage records

--- components/usage/pkg/controller/reporter.go
+++ components/usage/pkg/controller/reporter.go
@@ -5,6 +5,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"time"
 )
@@ -29,17 +30,26 @@ var (
 		Help:      "Histogram of reconcile duration",
 		Buckets:   prometheus.LinearBuckets(30, 30, 10), // every 30 secs, starting at 30secs
 	}, []string{"outcome"})
+
+	sessionsRetrievedTotal = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Subsystem: subsystem,
+		Name:      "usage_records_retrieved_total",
+		Help:      "Number of usage records retrieved during usage collection run",
+	})
 )
 
 func RegisterMetrics(reg *prometheus.Registry) error {
-	err := reg.Register(reconcileStartedTotal)
-	if err != nil {
-		return err
+	metrics := []prometheus.Collector{
+		reconcileStartedTotal,
+		reconcileStartedDurationSeconds,
+		sessionsRetrievedTotal,
 	}
-
-	err = reg.Register(reconcileStartedDurationSeconds)
-	if err != nil {
-		return err
+	for _, metric := range metrics {
+		err := reg.Register(metric)
+		if err != nil {
+			return fmt.Errorf("failed to register metric: %w", err)
+		}
 	}
 
 	return nil
@@ -55,4 +65,8 @@ func reportUsageReconcileFinished(duration time.Duration, err error) {
 		outcome = "error"
 	}
 	reconcileStartedDurationSeconds.WithLabelValues(outcome).Observe(duration.Seconds())
+}
+
+func reportSessionsRetrievedTotal(count int) {
+	sessionsRetrievedTotal.Set(float64(count))
 }
