6e3faa703c2dbc830dd2ece79cc68a34aa6cca16
Milan Pavlik <milan@gitpod.io>
2022-07-27 08:25:41 +0000

    [usage] Add CollectUsage RPC

--- components/usage-api/typescript/src/usage/v1/usage_pb.d.ts
+++ components/usage-api/typescript/src/usage/v1/usage_pb.d.ts
@@ -134,3 +134,54 @@ export namespace BilledSession {
         credits: number,
     }
 }
+
+export class CollectUsageRequest extends jspb.Message {
+
+    hasStartTime(): boolean;
+    clearStartTime(): void;
+    getStartTime(): google_protobuf_timestamp_pb.Timestamp | undefined;
+    setStartTime(value?: google_protobuf_timestamp_pb.Timestamp): CollectUsageRequest;
+
+    hasEndTime(): boolean;
+    clearEndTime(): void;
+    getEndTime(): google_protobuf_timestamp_pb.Timestamp | undefined;
+    setEndTime(value?: google_protobuf_timestamp_pb.Timestamp): CollectUsageRequest;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): CollectUsageRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: CollectUsageRequest): CollectUsageRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: CollectUsageRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): CollectUsageRequest;
+    static deserializeBinaryFromReader(message: CollectUsageRequest, reader: jspb.BinaryReader): CollectUsageRequest;
+}
+
+export namespace CollectUsageRequest {
+    export type AsObject = {
+        startTime?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+        endTime?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    }
+}
+
+export class CollectUsageResponse extends jspb.Message {
+    clearSessionsList(): void;
+    getSessionsList(): Array<BilledSession>;
+    setSessionsList(value: Array<BilledSession>): CollectUsageResponse;
+    addSessions(value?: BilledSession, index?: number): BilledSession;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): CollectUsageResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: CollectUsageResponse): CollectUsageResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: CollectUsageResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): CollectUsageResponse;
+    static deserializeBinaryFromReader(message: CollectUsageResponse, reader: jspb.BinaryReader): CollectUsageResponse;
+}
+
+export namespace CollectUsageResponse {
+    export type AsObject = {
+        sessionsList: Array<BilledSession.AsObject>,
+    }
+}
