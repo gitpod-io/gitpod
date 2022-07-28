51433dc390879efcfa2dff583dfa3c559b18e235
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2022-07-28 18:00:40 -0400

    [ws-daemon] Configure plugins log level to Debug

--- components/ws-daemon/pkg/cgroup/plugin_fuse_v2.go
+++ components/ws-daemon/pkg/cgroup/plugin_fuse_v2.go
@@ -25,7 +25,7 @@ func (c *FuseDeviceEnablerV2) Type() Version { return Version2 }
 
 func (c *FuseDeviceEnablerV2) Apply(ctx context.Context, basePath, cgroupPath string) error {
 	fullCgroupPath := filepath.Join(basePath, cgroupPath)
-	log.WithField("cgroupPath", fullCgroupPath).Info("configuring devices")
+	log.WithField("cgroupPath", fullCgroupPath).Debug("configuring devices")
 
 	cgroupFD, err := unix.Open(fullCgroupPath, unix.O_DIRECTORY|unix.O_RDONLY|unix.O_CLOEXEC, 0600)
 	if err != nil {
