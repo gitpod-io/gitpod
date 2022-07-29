2e2a3d535732efd8c38868810b73e5fde3239002
mustard <mhqnwt@gmail.com>
2022-07-29 16:15:42 +0000

    [code] update stable code for 1.69 with in-product changelog and ports view improvement

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-050c611f28564c6c7b1e58db470f07997dfb4730" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-079f161f0aaf95d6b5e28f0709b3e629337291e9" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
