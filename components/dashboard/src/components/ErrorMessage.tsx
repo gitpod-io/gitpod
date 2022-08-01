7efea0a97f90b165afdd55e6168526424eb5e220
Gero Posmyk-Leinemann <gero@gitpod.io>
2022-08-01 14:26:46 +0000

    [dashboard] Move to new Alert component

--- components/dashboard/src/components/ErrorMessage.tsx
+++ components/dashboard/src/components/ErrorMessage.tsx
@@ -6,17 +6,15 @@
 
 import FeedbackComponent from "../feedback-form/FeedbackComponent";
 import { isGitpodIo } from "../utils";
+import Alert from "./Alert";
 
 function ErrorMessage(props: { imgSrc: string; imgAlt?: string; message: string }) {
     return (
         <>
-            <div className="mt-16 flex space-x-2 py-6 px-6 w-96 justify-between bg-gitpod-kumquat-light rounded-xl">
-                <div className="pr-3 self-center w-6">
-                    <img src={props.imgSrc} alt={props.imgAlt || "An error message"} />
-                </div>
-                <div className="flex-1 flex flex-col">
-                    <p className="text-gitpod-red text-sm">{props.message}</p>
-                </div>
+            <div className="space-y-4 mt-4">
+                <Alert closable={false} showIcon={true} type="error">
+                    <span>{props.message}</span>
+                </Alert>
             </div>
             {isGitpodIo() && (
                 <FeedbackComponent
