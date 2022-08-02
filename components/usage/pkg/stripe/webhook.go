766430278b34836e4b15e7670688301e6d5c29c8
Andrew Farries <andrew@gitpod.io>
2022-08-02 06:00:54 +0000

    Simplify json decoding of request body

--- components/usage/pkg/stripe/webhook.go
+++ components/usage/pkg/stripe/webhook.go
@@ -7,7 +7,6 @@ package stripe
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gitpod-io/gitpod/common-go/log"
@@ -24,15 +23,10 @@ func (h *WebhookHandler) Handle(w http.ResponseWriter, req *http.Request) {
 	const maxBodyBytes = int64(65536)
 
 	req.Body = http.MaxBytesReader(w, req.Body, maxBodyBytes)
-	payload, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		log.WithError(err).Error("Stripe webhook error when reading request body")
-		w.WriteHeader(http.StatusServiceUnavailable)
-		return
-	}
 
 	event := stripe.Event{}
-	if err := json.Unmarshal(payload, &event); err != nil {
+	err := json.NewDecoder(req.Body).Decode(&event)
+	if err != nil {
 		log.WithError(err).Error("Stripe webhook error while parsing event payload")
 		w.WriteHeader(http.StatusBadRequest)
 		return
