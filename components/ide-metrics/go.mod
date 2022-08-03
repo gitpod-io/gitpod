84f60b56f107481112fa5cf7209bba2fb70652d5
Pudong Zheng <tianshi8650@gmail.com>
2022-08-03 05:44:13 +0000

    Add IDE metrics server component

---
+++ components/ide-metrics/go.mod
@@ -0,0 +1,102 @@
+module github.com/gitpod-io/gitpod/ide-metrics
+
+go 1.18
+
+require (
+	github.com/gitpod-io/gitpod/common-go v0.0.0-00010101000000-000000000000
+	github.com/gitpod-io/gitpod/ide-metrics-api v0.0.0-00010101000000-000000000000
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/improbable-eng/grpc-web v0.15.0
+	github.com/prometheus/client_golang v1.12.2
+	github.com/soheilhy/cmux v0.1.5
+	github.com/spf13/cobra v1.4.0
+	google.golang.org/grpc v1.48.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.34.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/rs/cors v1.7.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
+	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
+	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	nhooyr.io/websocket v1.8.6 // indirect
+)
+
+replace github.com/gitpod-io/gitpod/gitpod-protocol => ../gitpod-protocol/go // leeway
+
+replace github.com/gitpod-io/gitpod/common-go => ../common-go // leeway
+
+replace github.com/gitpod-io/gitpod/ide-metrics-api => ../ide-metrics-api/go // leeway
+
+replace k8s.io/api => k8s.io/api v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/apimachinery => k8s.io/apimachinery v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/apiserver => k8s.io/apiserver v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/cli-runtime => k8s.io/cli-runtime v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/client-go => k8s.io/client-go v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/cloud-provider => k8s.io/cloud-provider v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/code-generator => k8s.io/code-generator v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/component-base => k8s.io/component-base v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/cri-api => k8s.io/cri-api v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kube-proxy => k8s.io/kube-proxy v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kubelet => k8s.io/kubelet v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/metrics => k8s.io/metrics v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/component-helpers => k8s.io/component-helpers v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/controller-manager => k8s.io/controller-manager v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/kubectl => k8s.io/kubectl v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/mount-utils => k8s.io/mount-utils v0.23.5 // leeway indirect from components/common-go:lib
+
+replace k8s.io/pod-security-admission => k8s.io/pod-security-admission v0.23.5 // leeway indirect from components/common-go:lib
---
+++ components/ide-metrics/pkg/server/server.go
@@ -0,0 +1,203 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/gitpod-io/gitpod/common-go/log"
+	api "github.com/gitpod-io/gitpod/ide-metrics-api"
+	"github.com/gitpod-io/gitpod/ide-metrics-api/config"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/soheilhy/cmux"
+	"google.golang.org/grpc"
+)
+
+type IDEMetricsServer struct {
+	config *config.ServiceConfiguration
+
+	registry     prometheus.Registerer
+	counterMap   map[string]*allowListCollector
+	histogramMap map[string]*allowListCollector
+
+	api.UnimplementedMetricsServiceServer
+}
+
+type allowListCollector struct {
+	Collector        prometheus.Collector
+	Labels           []string
+	AllowLabelValues map[string][]string
+}
+
+func (c *allowListCollector) Check(labels map[string]string) bool {
+	if len(c.Labels) != len(labels) {
+		return false
+	}
+	for label, value := range labels {
+		allowValues, ok := c.AllowLabelValues[label]
+		if !ok {
+			return false
+		}
+		found := false
+		for _, v := range allowValues {
+			if v == value {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func newAllowListCollector(allowList []config.LabelAllowList) *allowListCollector {
+	labels := make([]string, 0, len(allowList))
+	allowLabelValues := make(map[string][]string)
+	for _, l := range allowList {
+		labels = append(labels, l.Name)
+		allowLabelValues[l.Name] = l.AllowValues
+	}
+	return &allowListCollector{
+		Labels:           labels,
+		AllowLabelValues: allowLabelValues,
+	}
+}
+
+func (s *IDEMetricsServer) AddCounter(ctx context.Context, req *api.AddCounterRequest) (*api.AddCounterResponse, error) {
+	c, ok := s.counterMap[req.Name]
+	if !ok {
+		return nil, errors.New("metric not found")
+	}
+	if !c.Check(req.Labels) {
+		return nil, errors.New("label not allow")
+	}
+	counterVec := c.Collector.(*prometheus.CounterVec)
+	counter, err := counterVec.GetMetricWith(req.Labels)
+	if err != nil {
+		return nil, err
+	}
+	if req.Value == nil {
+		counter.Inc()
+	} else {
+		counter.Add(float64(*req.Value))
+	}
+	return &api.AddCounterResponse{}, nil
+}
+func (s *IDEMetricsServer) ObserveHistogram(ctx context.Context, req *api.ObserveHistogramRequest) (*api.ObserveHistogramResponse, error) {
+	c, ok := s.histogramMap[req.Name]
+	if !ok {
+		return nil, errors.New("metric not found")
+	}
+	if !c.Check(req.Labels) {
+		return nil, errors.New("label not allow")
+	}
+	histogramVec := c.Collector.(*prometheus.HistogramVec)
+	histogram, err := histogramVec.GetMetricWith(req.Labels)
+	if err != nil {
+		return nil, err
+	}
+	histogram.Observe(req.Value)
+	return &api.ObserveHistogramResponse{}, nil
+}
+
+func (s *IDEMetricsServer) registryCounterMetrics() {
+	for _, m := range s.config.Server.CounterMetrics {
+		if _, ok := s.counterMap[m.Name]; ok {
+			continue
+		}
+		c := newAllowListCollector(m.Labels)
+		counterVec := prometheus.NewCounterVec(prometheus.CounterOpts{
+			Name: m.Name,
+			Help: m.Help,
+		}, c.Labels)
+		c.Collector = counterVec
+		s.counterMap[m.Name] = c
+		err := s.registry.Register(counterVec)
+		if err != nil {
+			log.WithError(err).WithField("name", m.Name).Warn("registry metrics failed")
+		}
+	}
+}
+
+func (s *IDEMetricsServer) registryHistogramMetrics() {
+	for _, m := range s.config.Server.HistogramMetrics {
+		if _, ok := s.histogramMap[m.Name]; ok {
+			continue
+		}
+		c := newAllowListCollector(m.Labels)
+		histogramVec := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    m.Name,
+			Help:    m.Help,
+			Buckets: m.Buckets,
+		}, c.Labels)
+		c.Collector = histogramVec
+		s.histogramMap[m.Name] = c
+		err := s.registry.Register(histogramVec)
+		if err != nil {
+			log.WithError(err).WithField("name", m.Name).Warn("registry metrics failed")
+		}
+	}
+}
+
+func (s *IDEMetricsServer) prepareMetrics() {
+	s.registryCounterMetrics()
+	s.registryHistogramMetrics()
+}
+
+func (s *IDEMetricsServer) register(grpcServer *grpc.Server) {
+	api.RegisterMetricsServiceServer(grpcServer, s)
+}
+
+func (s *IDEMetricsServer) ReloadConfig(cfg *config.ServiceConfiguration) {
+	// reload config only add metrics now, we don't support modify or delete metrics
+	s.config = cfg
+	s.prepareMetrics()
+}
+
+func NewMetricsServer(cfg *config.ServiceConfiguration, reg prometheus.Registerer) *IDEMetricsServer {
+	s := &IDEMetricsServer{
+		registry:     reg,
+		config:       cfg,
+		counterMap:   make(map[string]*allowListCollector),
+		histogramMap: make(map[string]*allowListCollector),
+	}
+	s.prepareMetrics()
+	return s
+}
+
+func (s *IDEMetricsServer) Start() error {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", s.config.Server.Port))
+	if err != nil {
+		return err
+	}
+	m := cmux.New(l)
+	grpcMux := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldSendSettings("content-type", "application/grpc"))
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	s.register(grpcServer)
+	go grpcServer.Serve(grpcMux)
+
+	httpMux := m.Match(cmux.HTTP1Fast())
+	routes := http.NewServeMux()
+	grpcWebServer := grpcweb.WrapServer(grpcServer, grpcweb.WithWebsockets(true), grpcweb.WithWebsocketOriginFunc(func(req *http.Request) bool {
+		return true
+	}), grpcweb.WithOriginFunc(func(origin string) bool {
+		return true
+	}))
+
+	routes.Handle("/metrics-api/", http.StripPrefix("/metrics-api", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		grpcWebServer.ServeHTTP(w, r)
+	})))
+	go http.Serve(httpMux, routes)
+
+	return m.Serve()
+}
