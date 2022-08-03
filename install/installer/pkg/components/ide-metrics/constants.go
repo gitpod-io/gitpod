b408c127025e0f2ba8f7b533a2bf4189c0fb9521
Pudong Zheng <tianshi8650@gmail.com>
2022-08-03 05:54:33 +0000

    Add IDE metrics server to installer

---
+++ install/installer/pkg/components/ide-metrics/constants.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package ide_metrics
+
+const (
+	Component     = "ide-metrics"
+	ContainerPort = 3000
+	PortName      = "http"
+	ServicePort   = 3000
+	ReadinessPort = 3000
+	VolumeConfig  = "config"
+)
