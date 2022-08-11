958f6186b993a76db7ca575f3f57c98acc0e393d
Jean Pierre <jeanp413@hotmail.com>
2022-08-11 02:21:25 +0000

    [code] update stable code image to 1.70.1

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-ce3e2bc5edd1cf37d66ef0b37c38645543021831" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-034e1ea1e89e0b6a6be50de20bf67509111efa65" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
