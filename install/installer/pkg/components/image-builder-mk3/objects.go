671a3dc706fba4409be2565a7e7e2957386ebb62
Simon Emms <simon@gitpod.io>
2022-08-17 11:57:39 +0000

    [installer]: remove all references to pod security policies

--- install/installer/pkg/components/image-builder-mk3/objects.go
+++ install/installer/pkg/components/image-builder-mk3/objects.go
@@ -7,7 +7,6 @@ package image_builder_mk3
 import "github.com/gitpod-io/gitpod/installer/pkg/common"
 
 var Objects = common.CompositeRenderFunc(
-	clusterrole,
 	configmap,
 	deployment,
 	networkpolicy,
