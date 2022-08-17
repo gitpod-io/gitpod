1baf1df8fb86721e61203b1f97d386e1da081106
Jan Keromnes <janx@linux.com>
2022-08-17 16:01:47 +0000

    [dashboard] Improve usage-based billing account selection UX

--- components/dashboard/src/settings/Billing.tsx
+++ components/dashboard/src/settings/Billing.tsx
@@ -10,9 +10,7 @@ import { BillingAccountSelector } from "../components/BillingAccountSelector";
 export default function Billing() {
     return (
         <PageWithSettingsSubMenu title="Billing" subtitle="Usage-Based Billing.">
-            <h3>Usage-Based Billing</h3>
-            <h2 className="text-gray-500">Manage usage-based billing, spending limit, and payment method.</h2>
-            <div className="mt-8">
+            <div>
                 <h3>Billing Account</h3>
                 <BillingAccountSelector />
             </div>
