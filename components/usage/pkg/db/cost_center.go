8b201eea37e2e5a96094d1106bae284da5e4f539
Laurie T. Malau <laurie@gitpod.io>
2022-08-23 09:12:25 +0000

    Get CostCenter

--- components/usage/pkg/db/cost_center.go
+++ components/usage/pkg/db/cost_center.go
@@ -5,15 +5,20 @@
 package db
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"time"
 
-	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
+var CostCenterNotFound = errors.New("CostCenter not found")
+
 type CostCenter struct {
-	ID            uuid.UUID `gorm:"primary_key;column:id;type:char;size:36;" json:"id"`
-	SpendingLimit int       `gorm:"column:spendingLimit;type:int;default:0;" json:"spendingLimit"`
-	LastModified  time.Time `gorm:"column:_lastModified;type:timestamp;default:CURRENT_TIMESTAMP(6);" json:"_lastModified"`
+	ID            AttributionID `gorm:"primary_key;column:id;type:char;size:36;" json:"id"`
+	SpendingLimit int32         `gorm:"column:spendingLimit;type:int;default:0;" json:"spendingLimit"`
+	LastModified  time.Time     `gorm:"->:column:_lastModified;type:timestamp;default:CURRENT_TIMESTAMP(6);" json:"_lastModified"`
 
 	// deleted is restricted for use by db-sync
 	_ bool `gorm:"column:deleted;type:tinyint;default:0;" json:"deleted"`
@@ -22,4 +27,20 @@ type CostCenter struct {
 // TableName sets the insert table name for this struct type
 func (d *CostCenter) TableName() string {
 	return "d_b_cost_center"
+}
+
+func GetCostCenter(ctx context.Context, conn *gorm.DB, attributionId AttributionID) (*CostCenter, error) {
+	db := conn.WithContext(ctx)
+	var costCenter CostCenter
+
+	result := db.First(&costCenter, attributionId)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, CostCenterNotFound
+		}
+		return nil, fmt.Errorf("failed to get cost center: %w", result.Error)
+
+	}
+
+	return &costCenter, nil
 }
