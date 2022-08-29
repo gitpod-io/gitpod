708f94bc0394ca4a55e21ddf03b47b05640762ab
Tarun Pothulapati <tarun@gitpod.io>
2022-08-29 18:06:24 +0000

    [local-preview] fix cgroupv2 error
    Signed-off-by: Tarun Pothulapati <tarun@gitpod.io>

--- install/preview/entrypoint.sh
+++ install/preview/entrypoint.sh
@@ -43,17 +43,6 @@ fi
 
 echo "Gitpod Domain: $DOMAIN"
 
-if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
-  echo "[$(date -Iseconds)] [CgroupV2 Fix] Evacuating Root Cgroup ..."
-	# move the processes from the root group to the /init group,
-  # otherwise writing subtree_control fails with EBUSY.
-  mkdir -p /sys/fs/cgroup/init
-  busybox xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
-  # enable controllers
-  sed -e 's/ / +/g' -e 's/^/+/' <"/sys/fs/cgroup/cgroup.controllers" >"/sys/fs/cgroup/cgroup.subtree_control"
-  echo "[$(date -Iseconds)] [CgroupV2 Fix] Done"
-fi
-
 mount --make-shared /sys/fs/cgroup
 mount --make-shared /proc
 mount --make-shared /var/gitpod
