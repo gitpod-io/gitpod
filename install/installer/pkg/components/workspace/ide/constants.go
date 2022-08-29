db6f5ed420c787d367a00ef0f6c052a5df48cc52
mustard <mhqnwt@gmail.com>
2022-08-29 16:11:59 +0000

    [code] update stable code image to report workbench errors

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-c47714bb26bbdbf55f0abdd7560a7ef14f92a742" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-b86bfcbb01feb0b9c3e67a14e6ad3bb95044f51d" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
