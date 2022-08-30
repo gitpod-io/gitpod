56566a4c73d8415c41431c39792de3602d61eedd
Simon Emms <simon@gitpod.io>
2022-08-30 17:16:27 +0000

    [installer]: create build-from-envvars command on config

---
+++ install/installer/cmd/config_build-from-envvars.go
@@ -0,0 +1,40 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the MIT License. See License-MIT.txt in the project root for license information.
+
+package cmd
+
+import (
+	"github.com/gitpod-io/gitpod/installer/pkg/config"
+	"github.com/spf13/cobra"
+)
+
+// configBuildFromEnvvarsCmd represents the validate command
+var configBuildFromEnvvarsCmd = &cobra.Command{
+	Use:   "build-from-envvars",
+	Short: "Build configuration from environment variables",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if _, err := configFileExistsAndInit(); err != nil {
+			return err
+		}
+
+		_, version, cfg, err := loadConfig(configOpts.ConfigFile)
+		if err != nil {
+			return err
+		}
+
+		apiVersion, err := config.LoadConfigVersion(version)
+		if err != nil {
+			return err
+		}
+
+		if err := apiVersion.BuildFromEnvvars(cfg); err != nil {
+			return err
+		}
+
+		return saveConfigFile(cfg)
+	},
+}
+
+func init() {
+	configCmd.AddCommand(configBuildFromEnvvarsCmd)
+}
