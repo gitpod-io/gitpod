7bbdc5445e5b7f27c737aa2fce7859c34dc532dd
mustard <mhqnwt@gmail.com>
2022-09-02 02:22:50 +0000

    [code] update code stable to align all proposal apis

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-44a66f55c90b92fada502cc293e927cde62d75fc" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-d6329814c2aa34c414574fd0d1301447d6fe82c9" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
