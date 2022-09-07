d90790c8bd53a616aa04c44ab6ca6c1ab67c3dfd
mustard <mhqnwt@gmail.com>
2022-09-07 17:18:35 +0000

    [ide-service] add test cases for ide config parse

---
+++ components/ide-service/pkg/server/ideconfig_test.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	ctesting "github.com/gitpod-io/gitpod/common-go/testing"
+)
+
+func TestParseConfig(t *testing.T) {
+
+	type fixture struct {
+		IDEConfig
+	}
+	type gold struct {
+		Config IDEConfig
+		Err    string
+	}
+
+	test := ctesting.FixtureTest{
+		T:    t,
+		Path: "testdata/ideconfig_*.json",
+		Test: func(t *testing.T, input interface{}) interface{} {
+			fixture := input.(*fixture)
+
+			b, err := json.Marshal(fixture.IDEConfig)
+			if err != nil {
+				return &gold{Err: err.Error()}
+			}
+
+			config, err := ParseConfig(context.Background(), b)
+			if err != nil {
+				return &gold{Err: err.Error()}
+			}
+			return &gold{Config: *config, Err: ""}
+		},
+		Fixture: func() interface{} { return &fixture{} },
+		Gold:    func() interface{} { return &gold{} },
+	}
+	test.Run()
+}
