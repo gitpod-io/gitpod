bde626bbdbc2547e4c77569dcec4f4ccd0aeca95
mustard <mhqnwt@gmail.com>
2022-09-07 10:33:34 +0000

    [installer] add ide-service component
    Co-authored-by: Simon Emms <simon@gitpod.io>

---
+++ install/installer/pkg/components/ide-service/constants.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package ide_service
+
+const (
+	Component    = "ide-service"
+	VolumeConfig = "config"
+
+	GRPCPortName    = "grpc"
+	GRPCServicePort = 9001
+)
