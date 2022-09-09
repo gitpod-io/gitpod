8dcbb578db1b961880422422f52dc37b28c82cda
Milan Pavlik <milan@gitpod.io>
2022-09-09 13:40:29 +0000

    [usage] Refactor controller package into scheduler

--- components/usage/pkg/scheduler/scheduler.go
+++ components/usage/pkg/scheduler/scheduler.go
@@ -36,19 +36,21 @@ func (c *Scheduler) Start() {
 	log.Infof("Starting usage scheduler. Setting up %d jobs.", len(c.specs))
 
 	for _, job := range c.specs {
+		// need to re-assign job to avoid pointing to a different job spec once the `cron.FuncJob` executes.
+		j := job
 		c.cron.Schedule(job.Schedule, cron.FuncJob(func() {
 			c.runningJobs.Add(1)
 			defer c.runningJobs.Done()
 
 			now := time.Now().UTC()
-			logger := log.WithField("job_id", job.ID)
+			logger := log.WithField("job_id", j.ID)
 
-			logger.Infof("Starting scheduled job %s", job.ID)
-			reportJobStarted(job.ID)
+			logger.Infof("Starting scheduled job %s", j.ID)
+			reportJobStarted(j.ID)
 
-			err := job.Job.Run()
+			err := j.Job.Run()
 			defer func() {
-				reportJobCompleted(job.ID, time.Since(now), err)
+				reportJobCompleted(j.ID, time.Since(now), err)
 			}()
 			if err != nil {
 				logger.WithError(err).Errorf("Scheduled job %s failed.", job.ID)
@@ -63,9 +65,10 @@ func (c *Scheduler) Start() {
 
 // Stop terminates the Scheduler and awaits for all running jobs to complete.
 func (c *Scheduler) Stop() {
-	log.Info("Stopping usage controller.")
+	log.Info("Stopping scheduler.")
+	c.cron.Stop()
 
-	log.Info("Awaiting existing reconciliation runs to complete..")
+	log.Info("Awaiting existing jobs to complete.")
 	// Wait for existing jobs to finish
 	c.runningJobs.Wait()
 
