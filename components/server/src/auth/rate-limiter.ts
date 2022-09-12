7f0062a21bf4c84e283732c26fb5d61ada6d69c5
Andrew Farries <andrew@gitpod.io>
2022-09-12 12:01:42 +0000

    Add `findStripeSubscriptionId` method to server
    Add a general method for finding the stripe subscription id for a given
    attribution id. Implement the existing `findStripeSubscriptionIdForTeam`
    method in terms of the new method.

--- components/server/src/auth/rate-limiter.ts
+++ components/server/src/auth/rate-limiter.ts
@@ -206,6 +206,7 @@ const defaultFunctions: FunctionsConfig = {
     tsReassignSlot: { group: "default", points: 1 },
     getStripePublishableKey: { group: "default", points: 1 },
     getStripeSetupIntentClientSecret: { group: "default", points: 1 },
+    findStripeSubscriptionId: { group: "default", points: 1 },
     findStripeSubscriptionIdForTeam: { group: "default", points: 1 },
     createOrUpdateStripeCustomerForTeam: { group: "default", points: 1 },
     createOrUpdateStripeCustomerForUser: { group: "default", points: 1 },
