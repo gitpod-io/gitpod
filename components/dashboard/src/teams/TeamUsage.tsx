9a86d79b5ac8f873c5da4f1a06e320a6dd6f6b0c
Sven Efftinge <sven@gitpod.io>
2022-09-19 06:29:33 +0000

    [usage view] remove unused billingmode

--- components/dashboard/src/teams/TeamUsage.tsx
+++ components/dashboard/src/teams/TeamUsage.tsx
@@ -4,46 +4,27 @@
  * See License-AGPL.txt in the project root for license information.
  */
 
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { useLocation } from "react-router";
 import { getCurrentTeam, TeamsContext } from "./teams-context";
-import { getGitpodService, gitpodHostUrl } from "../service/service";
-import { BillingMode } from "@gitpod/gitpod-protocol/lib/billing-mode";
 import UsageView from "../components/UsageView";
-import { AttributionId } from "@gitpod/gitpod-protocol/lib/attribution";
 
 function TeamUsage() {
     const { teams } = useContext(TeamsContext);
     const location = useLocation();
     const team = getCurrentTeam(location, teams);
-    const [billingMode, setBillingMode] = useState<BillingMode | undefined>(undefined);
-    const [attributionId, setAttributionId] = useState<AttributionId | undefined>();
-
-    useEffect(() => {
-        if (!team) {
-            return;
-        }
-        setAttributionId({ kind: "team", teamId: team.id });
-        (async () => {
-            const billingMode = await getGitpodService().server.getBillingModeForTeam(team.id);
-            setBillingMode(billingMode);
-        })();
-    }, [team]);
-
-    useEffect(() => {
-        if (!billingMode) {
-            return;
-        }
-        if (!BillingMode.showUsageBasedBilling(billingMode)) {
-            window.location.href = gitpodHostUrl.asDashboard().toString();
-        }
-    }, [billingMode]);
-
-    if (!billingMode || !attributionId) {
+    if (!team) {
         return <></>;
     }
 
-    return <UsageView billingMode={billingMode} attributionId={attributionId} />;
+    return (
+        <UsageView
+            attributionId={{
+                kind: "team",
+                teamId: team.id,
+            }}
+        />
+    );
 }
 
 export default TeamUsage;
