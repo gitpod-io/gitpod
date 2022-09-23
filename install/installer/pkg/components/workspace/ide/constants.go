53b5b23d2004dac9f4aef5d60cfa9bf61180096c
mustard <mhqnwt@gmail.com>
2022-09-23 16:38:24 +0000

    [code] update stable browser code for error report

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-2d7f400a0046e9793b88b28ce569e5ef02fa8ba2" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-97cff52e43760ad3ad96aa82d9fba934c5f53be8" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
