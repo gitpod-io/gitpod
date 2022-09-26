409ce6bb5de3c94a57943464de782eb836099632
Milan Pavlik <milan@gitpod.io>
2022-09-26 13:09:20 +0000

    [server] Remove Workspace health monitoring endpoint

--- components/server/ee/src/monitoring-endpoint-ee.ts
+++ components/server/ee/src/monitoring-endpoint-ee.ts
@@ -5,18 +5,13 @@
  */
 
 import * as express from "express";
-import { WorkspaceHealthMonitoring } from "./workspace/workspace-health-monitoring";
-import { TraceContext } from "@gitpod/gitpod-protocol/lib/util/tracing";
-import { log } from "@gitpod/gitpod-protocol/lib/util/logging";
-import { injectable, inject } from "inversify";
+import { injectable } from "inversify";
 import { registerServerMetrics } from "../../src/prometheus-metrics";
 import * as prometheusClient from "prom-client";
 import { registerDBMetrics } from "@gitpod/gitpod-db/lib";
 
 @injectable()
-export class MonitoringEndpointsAppEE extends WorkspaceHealthMonitoring {
-    @inject(WorkspaceHealthMonitoring) protected readonly workspaceHealthMonitoring: WorkspaceHealthMonitoring;
-
+export class MonitoringEndpointsAppEE {
     public create(): express.Application {
         const registry = prometheusClient.register;
 
@@ -34,53 +29,6 @@ export class MonitoringEndpointsAppEE extends WorkspaceHealthMonitoring {
             }
         });
 
-        monApp.get("/workspace-health", async (req, res) => {
-            try {
-                const result = await checkWorkspaceHealth({}, this.workspaceHealthMonitoring, !!req.query.extra);
-                if (result.unhealthy > 0) {
-                    console.warn("not all workspaces are healthy", result);
-                    res.status(406).send(result);
-                } else {
-                    res.status(200).send(result);
-                }
-            } catch (err) {
-                log.debug("failed to check workspace health", err);
-                res.status(500).send(err);
-            }
-        });
-
         return monApp;
     }
-}
-
-async function checkWorkspaceHealth(
-    ctx: TraceContext,
-    workspaceHealthMonitoring: WorkspaceHealthMonitoring,
-    extra: boolean = false,
-) {
-    const span = TraceContext.startSpan("checkWorkspaceHealth", ctx);
-    const result = await workspaceHealthMonitoring.probeAllRunningWorkspaces({ span });
-    const numUnhealthy = result
-        .map((r) => (r.ok ? 0 : 1))
-        .reduce((acc: number, cur: number) => (acc + cur) as number, 0);
-    let returnValue = {
-        status:
-            numUnhealthy > 0 ? (numUnhealthy == result.length ? "unhealthy" : "partially healthy") : "fully healthy",
-        healthy: result.length - numUnhealthy,
-        unhealthy: numUnhealthy,
-        total: result.length,
-        timestamp: Date.now(),
-    };
-    if (extra && numUnhealthy > 0) {
-        span.finish();
-        return {
-            ...returnValue,
-            extra: {
-                unhealthyWorkspaces: result.filter((r) => !r.ok),
-                notRunningWorkspaces: result.filter((r) => r.status !== "running"),
-            },
-        };
-    }
-    span.finish();
-    return returnValue;
 }
