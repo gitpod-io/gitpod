c6c9f2b1b9d890b4a891f7f9114e637b0a017a9a
Pudong Zheng <tianshi8650@gmail.com>
2022-09-27 06:23:41 +0000

    [ide-service-api] using better grpc generator

--- components/ide-service-api/generate.sh
+++ components/ide-service-api/generate.sh
@@ -15,28 +15,7 @@ COMPONENTS_DIR=$ROOT_DIR/components
 # shellcheck disable=SC1090,SC1091
 source "$ROOT_DIR"/scripts/protoc-generator.sh
 
-# TODO (aledbf): refactor to avoid duplication
-local_go_protoc() {
-    local ROOT_DIR=$1
-    # shellcheck disable=2035
-    protoc \
-        -I/usr/lib/protoc/include -I"$COMPONENTS_DIR" -I. \
-        --go_out=go \
-        --go_opt=paths=source_relative \
-        --go-grpc_out=go \
-        --go-grpc_opt=paths=source_relative \
-        *.proto
-}
-
-go_protoc_gateway() {
-    # shellcheck disable=2035
-    protoc \
-        -I/usr/lib/protoc/include -I"$COMPONENTS_DIR" -I. \
-        --grpc-gateway_out=logtostderr=true,paths=source_relative:go \
-        *.proto
-}
-
 install_dependencies
-local_go_protoc "$COMPONENTS_DIR"
-go_protoc_gateway "$COMPONENTS_DIR"
+go_protoc "$COMPONENTS_DIR"
+typescript_ts_protoc "$COMPONENTS_DIR"
 update_license
