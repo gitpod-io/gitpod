68190ebb7653019dfc2e590f9f89020b64c166bc
Aleksandar Aleksandrov <a.g.aleksandrov@gmail.com>
2022-09-28 14:11:13 +0300

    Terraform resources for Harvester VM and LBs

--- dev/preview/infrastructure/harvester/namespace.tf
+++ dev/preview/infrastructure/harvester/namespace.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_namespace" "preview_namespace" {
   provider = k8s.harvester
   metadata {
     name = "preview-${var.preview_name}"
--- dev/preview/infrastructure/harvester/variables.tf
+++ dev/preview/infrastructure/harvester/variables.tf
@@ -14,3 +14,37 @@ variable "dev_kube_path" {
   description = "The path to the Dev Cluster kubeconfig"
   default     = "~/.kube/dev"
 }
+
+variable "vm_memory" {
+  type        = string
+  default     = "2Gi"
+  description = "Memory for the VM"
+}
+
+variable "vm_cpu" {
+  type        = number
+  default     = 2
+  description = "CPU for the VM"
+}
+
+variable "vm_image" {
+  type        = string
+  default     = "default/gitpod-k3s-202209251218"
+  description = "The image name"
+}
+
+variable "dockerhub_user" {
+  type        = string
+  description = "The dockerhub user used to pull images in k3s"
+}
+
+variable "dockerhub_password" {
+  type        = string
+  description = "The password for the dockerhub user used to pull images in k3s"
+}
+
+variable "vm_storage_class" {
+  type        = string
+  default     = "longhorn-gitpod-k3s-202209251218-onereplica"
+  description = "The storage class for the VM"
+}
