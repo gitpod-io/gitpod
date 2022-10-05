b6cabee1eeebbd325e7c18085a1b7bf7be9280f3
Milan Pavlik <milan@gitpod.io>
2022-10-05 08:49:41 +0000

    [usage] Use buf to codegen Go proto/grpc

--- components/usage-api/generate.sh
+++ components/usage-api/generate.sh
@@ -15,13 +15,12 @@ COMPONENTS_DIR="$ROOT_DIR"/components
 # shellcheck disable=SC1090,SC1091
 source "$ROOT_DIR"/scripts/protoc-generator.sh
 
-lint
+
 
 install_dependencies
-go_protoc "$COMPONENTS_DIR" "usage/v1"
-mkdir -p go/v1
-mv go/usage/v1/*.pb.go go/v1
-rm -rf go/usage
+lint
+protoc_buf_generate
+
 typescript_ts_protoc "$COMPONENTS_DIR" "usage/v1"
 
 update_license
--- components/usage-api/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/gitpod-io/gitpod/usage-api-codegen
-
-go 1.19
-
-require google.golang.org/protobuf v1.28.1
