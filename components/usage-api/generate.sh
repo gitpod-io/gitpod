8fd1a0935426e7601a9aecae0fd93ee49bae60f0
Milan Pavlik <milan@gitpod.io>
2022-10-06 12:12:36 +0000

    [usage] Use buf for ts_proto plugin codegen

--- components/usage-api/generate.sh
+++ components/usage-api/generate.sh
@@ -9,18 +9,12 @@ set -o nounset
 set -o pipefail
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/../../
-COMPONENTS_DIR="$ROOT_DIR"/components
 
 # include protoc bash functions
 # shellcheck disable=SC1090,SC1091
 source "$ROOT_DIR"/scripts/protoc-generator.sh
 
-
-
 install_dependencies
 lint
 protoc_buf_generate
-
-typescript_ts_protoc "$COMPONENTS_DIR" "usage/v1"
-
 update_license
