9490c15320c154dc01538c5481e6d142887c29d4
mustard <mhqnwt@gmail.com>
2022-10-19 07:44:24 +0000

    [preview] fix certification error and dns issue

--- dev/preview/infrastructure/harvester/dns.tf
+++ dev/preview/infrastructure/harvester/dns.tf
@@ -37,11 +37,33 @@ resource "google_dns_record_set" "root-wc-ws" {
   rrdatas      = [var.harvester_ingress_ip]
 }
 
+resource "google_dns_record_set" "root-wc-ws-dev" {
+  provider = google
+
+  name = "*.ws-dev.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
+  rrdatas      = [var.harvester_ingress_ip]
+}
+
 
 resource "google_dns_record_set" "root-wc-ws-ssh" {
   provider = google
 
   name = "*.ssh.ws.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
+  rrdatas      = [kubernetes_service.dev-svc.status[0].load_balancer[0].ingress[0].ip]
+}
+
+resource "google_dns_record_set" "root-wc-ws-dev-ssh" {
+  provider = google
+
+  name = "*.ssh.ws-dev.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
   type = "A"
   ttl  = 300
 
