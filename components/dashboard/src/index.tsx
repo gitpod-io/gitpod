637c84d8f1c0c46f47b1f8b07817d884ceb0ace1
Milan Pavlik <milan@gitpod.io>
2022-10-24 15:25:16 +0200

    [dashboard] Fix display of billing period boundaries by using only UTC times - using dayjs (#14100)

--- components/dashboard/src/index.tsx
+++ components/dashboard/src/index.tsx
@@ -19,10 +19,12 @@ import { StartWorkspaceModalContextProvider } from "./workspaces/start-workspace
 import { BrowserRouter } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
 
 import "./index.css";
 
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
 ReactDOM.render(
     <React.StrictMode>
