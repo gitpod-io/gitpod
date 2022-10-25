8bdc886be620a1c9317e4eccbc48308ad4c97ef5
Mads Hartmann <mads379@gmail.com>
2022-10-25 10:40:43 +0000

    Clean git repository before performing build
    Add additional build args

--- dev/preview/workflow/preview/build.sh
+++ dev/preview/workflow/preview/build.sh
@@ -15,8 +15,20 @@ ensure_gcloud_auth
 
 VERSION="$(preview-name-from-branch)-dev"
 
+# We have quite a few Leeway packages whose hash includes files from .gitignore.
+# Some of these files (such as build folders) are populated as part of our prebuilds
+# which means that the hash for these components will be different from a workspace
+# then in CI.
+#
+# We should fix the package definitions so that they don't include these files in the hash.
+#
+# For now we're simply cleaning the repository by deleting all files that are in .gitignore.
+git clean -dfX
+
 leeway build \
-    -DSEGMENT_IO_TOKEN="" \
+    -DSEGMENT_IO_TOKEN="$(kubectl --context=dev -n werft get secret self-hosted -o jsonpath='{.data.segmentIOToken}' | base64 -d)" \
+    -DREPLICATED_API_TOKEN="$(kubectl --context=dev -n werft get secret replicated -o jsonpath='{.data.token}' | base64 -d)" \
+    -DREPLICATED_APP="$(kubectl --context=dev -n werft get secret replicated -o jsonpath='{.data.app}' | base64 -d)" \
     -Dversion="${VERSION}" \
     --dont-retag \
     --dont-test \
