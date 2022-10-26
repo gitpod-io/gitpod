959a4a554271afae7af87c29a5d0abac0c78e0c6
Andrew Farries <andrew@gitpod.io>
2022-10-26 09:32:31 +0000

    Add prometheus metric for image builds started

--- components/server/src/prometheus-metrics.ts
+++ components/server/src/prometheus-metrics.ts
@@ -21,6 +21,7 @@ export function registerServerMetrics(registry: prometheusClient.Registry) {
     registry.registerMetric(instanceStartsFailedTotal);
     registry.registerMetric(prebuildsStartedTotal);
     registry.registerMetric(stripeClientRequestsCompletedDurationSeconds);
+    registry.registerMetric(imageBuildsStartedTotal);
 }
 
 const loginCounter = new prometheusClient.Counter({
@@ -175,4 +176,13 @@ export const stripeClientRequestsCompletedDurationSeconds = new prometheusClient
 
 export function observeStripeClientRequestsCompleted(operation: string, outcome: string, durationInSeconds: number) {
     stripeClientRequestsCompletedDurationSeconds.observe({ operation, outcome }, durationInSeconds);
+}
+
+export const imageBuildsStartedTotal = new prometheusClient.Counter({
+    name: "gitpod_server_image_builds_started_total",
+    help: "counter of the total number of image builds started on server",
+});
+
+export function increaseImageBuildsStartedTotal() {
+    imageBuildsStartedTotal.inc();
 }
