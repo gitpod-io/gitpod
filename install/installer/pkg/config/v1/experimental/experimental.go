dd16cf1dd9ad8237b6a4888ff620677679e8024e
Jan Keromnes <janx@linux.com>
2022-10-26 10:34:35 +0200

    [installer] Actually add 'inactivityPeriodForRepos' to server configmap

--- install/installer/pkg/config/v1/experimental/experimental.go
+++ install/installer/pkg/config/v1/experimental/experimental.go
@@ -232,6 +232,7 @@ type ServerConfig struct {
 	EnableLocalApp                    *bool             `json:"enableLocalApp"`
 	RunDbDeleter                      *bool             `json:"runDbDeleter"`
 	DisableWorkspaceGarbageCollection bool              `json:"disableWorkspaceGarbageCollection"`
+	InactivityPeriodForRepos          *int              `json:"inactivityPeriodForRepos"`
 
 	// @deprecated use containerRegistry.privateBaseImageAllowList instead
 	DefaultBaseImageRegistryWhiteList []string `json:"defaultBaseImageRegistryWhitelist"`
