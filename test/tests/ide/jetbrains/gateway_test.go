051603aaee0c6a68bcc03a31e95e5645ef94c0e0
Pudong Zheng <tianshi8650@gmail.com>
2022-10-27 09:01:08 +0000

    [integration-test] don't parallel run JetBrains IDE test

--- test/tests/ide/jetbrains/gateway_test.go
+++ test/tests/ide/jetbrains/gateway_test.go
@@ -25,8 +25,6 @@ import (
 	"github.com/google/go-github/v42/github"
 )
 
-var ideProjectMap map[string]string
-
 type GatewayHostStatus struct {
 	AppPid     int64  `json:"appPid"`
 	AppVersion string `json:"appVersion"`
@@ -46,14 +44,14 @@ type GatewayHostStatus struct {
 	UnattendedMode bool   `json:"unattendedMode"`
 }
 
+var (
+	userToken     string
+	roboquatToken string
+)
+
 func init() {
-	ideProjectMap = make(map[string]string)
-	ideProjectMap["goland"] = "https://github.com/gitpod-io/template-golang-cli"
-	ideProjectMap["intellij"] = "https://github.com/gitpod-io/spring-petclinic"
-	ideProjectMap["phpstorm"] = "https://github.com/gitpod-io/template-php-laravel-mysql"
-	ideProjectMap["pycharm"] = "https://github.com/gitpod-io/template-python-django"
-	ideProjectMap["rubymine"] = "https://github.com/gitpod-io/template-ruby-on-rails-postgres"
-	ideProjectMap["webstorm"] = "https://github.com/gitpod-io/template-typescript-react"
+	userToken, _ = os.LookupEnv("USER_TOKEN")
+	roboquatToken, _ = os.LookupEnv("ROBOQUAT_TOKEN")
 }
 
 func GetHttpContent(url string) ([]byte, error) {
@@ -66,150 +64,236 @@ func GetHttpContent(url string) ([]byte, error) {
 	return b, err
 }
 
-func TestJetBrainsGatewayWorkspace(t *testing.T) {
-	userToken, _ := os.LookupEnv("USER_TOKEN")
-	roboquatToken, ok := os.LookupEnv("ROBOQUAT_TOKEN")
-	if !ok {
+func JetBrainsIDETest(ctx context.Context, t *testing.T, cfg *envconf.Config, ideName string, repo string) {
+	api := integration.NewComponentAPI(ctx, cfg.Namespace(), kubeconfig, cfg.Client())
+	t.Cleanup(func() {
+		api.Done(t)
+	})
+
+	config, err := integration.GetServerConfig(cfg.Namespace(), cfg.Client())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("get or create user")
+	_, err = api.CreateUser(username, userToken)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = api.MakeUserUnleashedPlan(username)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("connecting to server...")
+	server, err := api.GitpodServer(integration.WithGitpodUser(username))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("connected to server")
+
+	t.Logf("starting workspace")
+	var nfo *protocol.WorkspaceInfo
+	var stopWs func(waitForStop bool, api *integration.ComponentAPI) (*wsmanapi.WorkspaceStatus, error)
+
+	for i := 0; i < 3; i++ {
+		nfo, stopWs, err = integration.LaunchWorkspaceFromContextURL(t, ctx, "referrer:jetbrains-gateway:"+ideName+"/"+repo, username, api)
+		if err != nil {
+			if strings.Contains(err.Error(), "code 429 message: too many requests") {
+				t.Log(err)
+				time.Sleep(10 * time.Second)
+				continue
+			}
+			t.Fatal(err)
+		} else {
+			break
+		}
+	}
+
+	defer func() {
+		sctx, scancel := context.WithTimeout(context.Background(), 5*time.Minute)
+		defer scancel()
+
+		sapi := integration.NewComponentAPI(sctx, cfg.Namespace(), kubeconfig, cfg.Client())
+		defer sapi.Done(t)
+
+		_, _ = stopWs(true, sapi)
+	}()
+
+	t.Logf("get oauth2 token")
+	oauthToken, err := api.CreateOAuth2Token(username, []string{
+		"function:getGitpodTokenScopes",
+		"function:getIDEOptions",
+		"function:getOwnerToken",
+		"function:getWorkspace",
+		"function:getWorkspaces",
+		"function:listenForWorkspaceInstanceUpdates",
+		"resource:default",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("make port 63342 public")
+	_, err = server.OpenPort(ctx, nfo.Workspace.ID, &protocol.WorkspaceInstancePort{Port: 63342, Visibility: "public"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gatewayLink := fmt.Sprintf("jetbrains-gateway://connect#gitpodHost=%s&workspaceId=%s", strings.TrimPrefix(config.HostURL, "https://"), nfo.Workspace.ID)
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: roboquatToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	githubClient := github.NewClient(tc)
+
+	t.Logf("trigger github action")
+	_, err = githubClient.Actions.CreateWorkflowDispatchEventByFileName(ctx, "gitpod-io", "gitpod", "jetbrains-integration-test.yml", github.CreateWorkflowDispatchEventRequest{
+		Ref: "main",
+		Inputs: map[string]interface{}{
+			"secret_gateway_link": gatewayLink,
+			"secret_access_token": oauthToken,
+			"secret_endpoint":     strings.TrimPrefix(nfo.LatestInstance.IdeURL, "https://"),
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checkUrl := fmt.Sprintf("https://63342-%s/codeWithMe/unattendedHostStatus?token=gitpod", strings.TrimPrefix(nfo.LatestInstance.IdeURL, "https://"))
+
+	t.Logf("waiting result")
+	testStatus := false
+	for ctx.Err() == nil {
+		time.Sleep(1 * time.Second)
+		body, _ := GetHttpContent(checkUrl)
+		var status GatewayHostStatus
+		err = json.Unmarshal(body, &status)
+		if err != nil {
+			continue
+		}
+		if len(status.Projects) == 1 && status.Projects[0].ControllerConnected {
+			testStatus = true
+			break
+		}
+	}
+	if !testStatus {
+		t.Fatal(ctx.Err())
+	}
+}
+
+func TestGoLand(t *testing.T) {
+	if roboquatToken == "" {
 		t.Skip("this test need github action run permission")
 	}
 	integration.SkipWithoutUsername(t, username)
 	integration.SkipWithoutUserToken(t, userToken)
+	f := features.New("Start a workspace using GoLand").
+		WithLabel("component", "IDE").
+		WithLabel("ide", "GoLand").
+		Assess("it can let JetBrains Gateway connect", func(_ context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+			defer cancel()
+			JetBrainsIDETest(ctx, t, cfg, "goland", "https://github.com/gitpod-io/template-golang-cli")
+			return ctx
+		}).
+		Feature()
+	testEnv.Test(t, f)
+}
 
-	var featureList []features.Feature
-
-	for n, r := range ideProjectMap {
-		ideName := n
-		repo := r
-		f := features.New("Start a workspace using "+ideName).
-			WithLabel("component", "IDE").
-			WithLabel("ide", ideName).
-			Assess("it can let JetBrains Gateway connect", func(_ context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-				ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
-				defer cancel()
-
-				api := integration.NewComponentAPI(ctx, cfg.Namespace(), kubeconfig, cfg.Client())
-				t.Cleanup(func() {
-					api.Done(t)
-				})
-
-				config, err := integration.GetServerConfig(cfg.Namespace(), cfg.Client())
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				t.Logf("get or create user")
-				_, err = api.CreateUser(username, userToken)
-				if err != nil {
-					t.Fatal(err)
-				}
-				err = api.MakeUserUnleashedPlan(username)
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				t.Logf("connecting to server...")
-				server, err := api.GitpodServer(integration.WithGitpodUser(username))
-				if err != nil {
-					t.Fatal(err)
-				}
-				t.Logf("connected to server")
-
-				t.Logf("starting workspace")
-				var nfo *protocol.WorkspaceInfo
-				var stopWs func(waitForStop bool, api *integration.ComponentAPI) (*wsmanapi.WorkspaceStatus, error)
-
-				for i := 0; i < 3; i++ {
-					nfo, stopWs, err = integration.LaunchWorkspaceFromContextURL(t, ctx, "referrer:jetbrains-gateway:"+ideName+"/"+repo, username, api)
-					if err != nil {
-						if strings.Contains(err.Error(), "code 429 message: too many requests") {
-							t.Log(err)
-							time.Sleep(10 * time.Second)
-							continue
-						}
-						t.Fatal(err)
-					} else {
-						break
-					}
-				}
-
-				defer func() {
-					sctx, scancel := context.WithTimeout(context.Background(), 5*time.Minute)
-					defer scancel()
-
-					sapi := integration.NewComponentAPI(sctx, cfg.Namespace(), kubeconfig, cfg.Client())
-					defer sapi.Done(t)
-
-					stopWs(true, sapi)
-				}()
-
-				t.Logf("get oauth2 token")
-				oauthToken, err := api.CreateOAuth2Token(username, []string{
-					"function:getGitpodTokenScopes",
-					"function:getIDEOptions",
-					"function:getOwnerToken",
-					"function:getWorkspace",
-					"function:getWorkspaces",
-					"function:listenForWorkspaceInstanceUpdates",
-					"resource:default",
-				})
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				t.Logf("make port 63342 public")
-				_, err = server.OpenPort(ctx, nfo.Workspace.ID, &protocol.WorkspaceInstancePort{Port: 63342, Visibility: "public"})
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				gatewayLink := fmt.Sprintf("jetbrains-gateway://connect#gitpodHost=%s&workspaceId=%s", strings.TrimPrefix(config.HostURL, "https://"), nfo.Workspace.ID)
-
-				ts := oauth2.StaticTokenSource(
-					&oauth2.Token{AccessToken: roboquatToken},
-				)
-				tc := oauth2.NewClient(ctx, ts)
-
-				githubClient := github.NewClient(tc)
-
-				t.Logf("trigger github action")
-				_, err = githubClient.Actions.CreateWorkflowDispatchEventByFileName(ctx, "gitpod-io", "gitpod", "jetbrains-integration-test.yml", github.CreateWorkflowDispatchEventRequest{
-					Ref: "main",
-					Inputs: map[string]interface{}{
-						"secret_gateway_link": gatewayLink,
-						"secret_access_token": oauthToken,
-						"secret_endpoint":     strings.TrimPrefix(nfo.LatestInstance.IdeURL, "https://"),
-					},
-				})
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				checkUrl := fmt.Sprintf("https://63342-%s/codeWithMe/unattendedHostStatus?token=gitpod", strings.TrimPrefix(nfo.LatestInstance.IdeURL, "https://"))
-
-				t.Logf("waiting result")
-				testStatus := false
-				for ctx.Err() == nil {
-					time.Sleep(1 * time.Second)
-					body, _ := GetHttpContent(checkUrl)
-					var status GatewayHostStatus
-					err = json.Unmarshal(body, &status)
-					if err != nil {
-						continue
-					}
-					if len(status.Projects) == 1 && status.Projects[0].ControllerConnected {
-						testStatus = true
-						break
-					}
-				}
-				if !testStatus {
-					t.Fatal(ctx.Err())
-				}
-				time.Sleep(time.Second * 10)
-				return ctx
-			}).
-			Feature()
-		featureList = append(featureList, f)
+func TestIntellij(t *testing.T) {
+	if roboquatToken == "" {
+		t.Skip("this test need github action run permission")
 	}
+	integration.SkipWithoutUsername(t, username)
+	integration.SkipWithoutUserToken(t, userToken)
+	f := features.New("Start a workspace using Intellij").
+		WithLabel("component", "IDE").
+		WithLabel("ide", "Intellij").
+		Assess("it can let JetBrains Gateway connect", func(_ context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+			defer cancel()
+			JetBrainsIDETest(ctx, t, cfg, "intellij", "https://github.com/gitpod-io/spring-petclinic")
+			return ctx
+		}).
+		Feature()
+	testEnv.Test(t, f)
+}
 
-	testEnv.TestInParallel(t, featureList...)
+func TestPhpStorm(t *testing.T) {
+	if roboquatToken == "" {
+		t.Skip("this test need github action run permission")
+	}
+	integration.SkipWithoutUsername(t, username)
+	integration.SkipWithoutUserToken(t, userToken)
+	f := features.New("Start a workspace using PhpStorm").
+		WithLabel("component", "IDE").
+		WithLabel("ide", "PhpStorm").
+		Assess("it can let JetBrains Gateway connect", func(_ context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+			defer cancel()
+			JetBrainsIDETest(ctx, t, cfg, "phpstorm", "https://github.com/gitpod-io/template-php-laravel-mysql")
+			return ctx
+		}).
+		Feature()
+	testEnv.Test(t, f)
+}
+
+func TestPyCharm(t *testing.T) {
+	if roboquatToken == "" {
+		t.Skip("this test need github action run permission")
+	}
+	integration.SkipWithoutUsername(t, username)
+	integration.SkipWithoutUserToken(t, userToken)
+	f := features.New("Start a workspace using Pycharm").
+		WithLabel("component", "IDE").
+		WithLabel("ide", "Pycharm").
+		Assess("it can let JetBrains Gateway connect", func(_ context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+			defer cancel()
+			JetBrainsIDETest(ctx, t, cfg, "pycharm", "https://github.com/gitpod-io/template-python-django")
+			return ctx
+		}).
+		Feature()
+	testEnv.Test(t, f)
+}
+
+func TestRubyMine(t *testing.T) {
+	if roboquatToken == "" {
+		t.Skip("this test need github action run permission")
+	}
+	integration.SkipWithoutUsername(t, username)
+	integration.SkipWithoutUserToken(t, userToken)
+	f := features.New("Start a workspace using RubyMine").
+		WithLabel("component", "IDE").
+		WithLabel("ide", "RubyMine").
+		Assess("it can let JetBrains Gateway connect", func(_ context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+			defer cancel()
+			JetBrainsIDETest(ctx, t, cfg, "rubymine", "https://github.com/gitpod-io/template-ruby-on-rails-postgres")
+			return ctx
+		}).
+		Feature()
+	testEnv.Test(t, f)
+}
+
+func TestWebStorm(t *testing.T) {
+	if roboquatToken == "" {
+		t.Skip("this test need github action run permission")
+	}
+	integration.SkipWithoutUsername(t, username)
+	integration.SkipWithoutUserToken(t, userToken)
+	f := features.New("Start a workspace using WebStorm").
+		WithLabel("component", "IDE").
+		WithLabel("ide", "WebStorm").
+		Assess("it can let JetBrains Gateway connect", func(_ context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+			defer cancel()
+			JetBrainsIDETest(ctx, t, cfg, "webstorm", "https://github.com/gitpod-io/template-typescript-react")
+			return ctx
+		}).
+		Feature()
+	testEnv.Test(t, f)
 }
