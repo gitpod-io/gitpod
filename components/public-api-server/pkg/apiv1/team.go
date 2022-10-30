b5708a19d60bf580201c44c7306f2bdaf7b5dcea
Milan Pavlik <milan@gitpod.io>
2022-10-30 20:20:38 +0000

    [public-api] Implement experimental ListTeams

--- components/public-api-server/pkg/apiv1/team.go
+++ components/public-api-server/pkg/apiv1/team.go
@@ -8,16 +8,15 @@ import (
 	"context"
 	"fmt"
 
-	protocol "github.com/gitpod-io/gitpod/gitpod-protocol"
-	"github.com/relvacode/iso8601"
-	"google.golang.org/protobuf/types/known/timestamppb"
-
 	connect "github.com/bufbuild/connect-go"
 	"github.com/gitpod-io/gitpod/common-go/log"
+	protocol "github.com/gitpod-io/gitpod/gitpod-protocol"
 	"github.com/gitpod-io/gitpod/public-api-server/pkg/auth"
 	"github.com/gitpod-io/gitpod/public-api-server/pkg/proxy"
 	v1 "github.com/gitpod-io/gitpod/public-api/experimental/v1"
 	"github.com/gitpod-io/gitpod/public-api/experimental/v1/v1connect"
+	"github.com/relvacode/iso8601"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func NewTeamsService(pool proxy.ServerConnectionPool) *TeamService {
@@ -41,38 +40,82 @@ func (s *TeamService) CreateTeam(ctx context.Context, req *connect.Request[v1.Cr
 		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("Name is a required argument when creating a team."))
 	}
 
-	server, err := s.connectionPool.Get(ctx, token)
+	conn, err := s.connectionPool.Get(ctx, token)
 	if err != nil {
 		log.Log.WithError(err).Error("Failed to get connection to server.")
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
 
-	team, err := server.CreateTeam(ctx, req.Msg.GetName())
+	created, err := conn.CreateTeam(ctx, req.Msg.GetName())
 	if err != nil {
+		log.WithError(err).Error("Failed to create team.")
 		return nil, proxy.ConvertError(err)
 	}
 
-	members, err := server.GetTeamMembers(ctx, team.ID)
+	team, err := s.toTeamAPIResponse(ctx, conn, created)
 	if err != nil {
-		return nil, proxy.ConvertError(err)
-	}
-
-	invite, err := server.GetGenericInvite(ctx, team.ID)
-	if err != nil {
-		return nil, proxy.ConvertError(err)
+		log.WithError(err).Error("Failed to populate team with details.")
+		return nil, err
 	}
 
 	return connect.NewResponse(&v1.CreateTeamResponse{
-		Team: &v1.Team{
-			Id:      team.ID,
-			Name:    team.Name,
-			Slug:    team.Slug,
-			Members: teamMembersToAPIResponse(members),
-			TeamInvitation: &v1.TeamInvitation{
-				Id: invite.ID,
-			},
-		},
+		Team: team,
 	}), nil
+}
+
+func (s *TeamService) ListTeams(ctx context.Context, req *connect.Request[v1.ListTeamsRequest]) (*connect.Response[v1.ListTeamsResponse], error) {
+	token := auth.TokenFromContext(ctx)
+
+	conn, err := s.connectionPool.Get(ctx, token)
+	if err != nil {
+		log.Log.WithError(err).Error("Failed to get connection to server.")
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	teams, err := conn.GetTeams(ctx)
+	if err != nil {
+		log.WithError(err).Error("Failed to list teams from server.")
+		return nil, proxy.ConvertError(err)
+	}
+
+	var response []*v1.Team
+	for _, t := range teams {
+		team, err := s.toTeamAPIResponse(ctx, conn, t)
+		if err != nil {
+			log.WithError(err).Error("Failed to populate team with details.")
+			return nil, err
+		}
+
+		response = append(response, team)
+	}
+
+	return connect.NewResponse(&v1.ListTeamsResponse{
+		Teams: response,
+	}), nil
+}
+
+func (s *TeamService) toTeamAPIResponse(ctx context.Context, conn protocol.APIInterface, team *protocol.Team) (*v1.Team, error) {
+	members, err := conn.GetTeamMembers(ctx, team.ID)
+	if err != nil {
+		log.WithError(err).Error("Failed to get team members.")
+		return nil, proxy.ConvertError(err)
+	}
+
+	invite, err := conn.GetGenericInvite(ctx, team.ID)
+	if err != nil {
+		log.WithError(err).Error("Failed to get generic invite.")
+		return nil, proxy.ConvertError(err)
+	}
+
+	return &v1.Team{
+		Id:      team.ID,
+		Name:    team.Name,
+		Slug:    team.Slug,
+		Members: teamMembersToAPIResponse(members),
+		TeamInvitation: &v1.TeamInvitation{
+			Id: invite.ID,
+		},
+	}, nil
 }
 
 func teamMembersToAPIResponse(members []*protocol.TeamMemberInfo) []*v1.TeamMember {
