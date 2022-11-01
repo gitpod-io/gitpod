f2ce3892ad01b66868a3571a41ae36d142166552
mustard <mhqnwt@gmail.com>
2022-11-01 04:39:09 +0000

    [code] update stable code to fix in-product changelog

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-63ea5f90da48ef1c968a070a8e32972ea5470a8b" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-2eb8dce567174ddb2efe12e30e8375cb2ffc4af3" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
