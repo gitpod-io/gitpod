ab71063e7c99bf1aa116f8ab0f959442723ef3f1
Filip Troníček <filip@gitpod.io>
2022-11-02 20:19:34 +0000

    Update stable to `1.73`

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-2eb8dce567174ddb2efe12e30e8375cb2ffc4af3" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-f17f584e2e252f3c1e877d28ea1edd1ab137a831" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
