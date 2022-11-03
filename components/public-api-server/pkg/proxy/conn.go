a470cdbfc21ba16546dcc8f6396dea0589c26f14
Milan Pavlik <milan@gitpod.io>
2022-11-03 10:27:10 +0000

    [public-api] Refactor to support multiple token types

--- components/public-api-server/pkg/proxy/conn.go
+++ components/public-api-server/pkg/proxy/conn.go
@@ -6,12 +6,14 @@ package proxy
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/url"
 	"time"
 
 	"github.com/gitpod-io/gitpod/common-go/log"
 	gitpod "github.com/gitpod-io/gitpod/gitpod-protocol"
+	"github.com/gitpod-io/gitpod/public-api-server/pkg/auth"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
 
 	lru "github.com/hashicorp/golang-lru"
@@ -20,7 +22,7 @@ import (
 type ServerConnectionPool interface {
 	// Get retrieves or creates a new connection for the specified token
 	// Connections must not be shared across tokens
-	Get(ctx context.Context, token string) (gitpod.APIInterface, error)
+	Get(ctx context.Context, token auth.Token) (gitpod.APIInterface, error)
 }
 
 // NoConnectionPool is a simple version of the ServerConnectionPool which always creates a new connection.
@@ -28,23 +30,32 @@ type NoConnectionPool struct {
 	ServerAPI *url.URL
 }
 
-func (p *NoConnectionPool) Get(ctx context.Context, token string) (gitpod.APIInterface, error) {
+func (p *NoConnectionPool) Get(ctx context.Context, token auth.Token) (gitpod.APIInterface, error) {
 	logger := ctxlogrus.Extract(ctx)
 
 	start := time.Now()
 	defer func() {
 		reportConnectionDuration(time.Since(start))
 	}()
-	server, err := gitpod.ConnectToServer(p.ServerAPI.String(), gitpod.ConnectToServerOpts{
-		Context: ctx,
-		Token:   token,
-		Log:     logger,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create new connection to server: %w", err)
+
+	var conn *gitpod.APIoverJSONRPC
+	switch token.Type {
+	case auth.AccessTokenType:
+		server, err := gitpod.ConnectToServer(p.ServerAPI.String(), gitpod.ConnectToServerOpts{
+			Context: ctx,
+			Token:   token.Value,
+			Log:     logger,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to create new connection to server: %w", err)
+		}
+
+		conn = server
+	default:
+		return nil, errors.New("unknown token type")
 	}
 
-	return server, nil
+	return conn, nil
 }
 
 func NewConnectionPool(address *url.URL, poolSize int) (*ConnectionPool, error) {
@@ -69,30 +80,35 @@ func NewConnectionPool(address *url.URL, poolSize int) (*ConnectionPool, error) 
 
 	return &ConnectionPool{
 		cache: cache,
-		connConstructor: func(token string) (gitpod.APIInterface, error) {
-			return gitpod.ConnectToServer(address.String(), gitpod.ConnectToServerOpts{
-				// We're using Background context as we want the connection to persist beyond the lifecycle of a single request
-				Context: context.Background(),
-				Token:   token,
-				Log:     log.Log,
-				CloseHandler: func(_ error) {
-					cache.Remove(token)
-					connectionPoolSize.Dec()
-				},
-			})
+		connConstructor: func(token auth.Token) (gitpod.APIInterface, error) {
+			switch token.Type {
+			case auth.AccessTokenType:
+				return gitpod.ConnectToServer(address.String(), gitpod.ConnectToServerOpts{
+					// We're using Background context as we want the connection to persist beyond the lifecycle of a single request
+					Context: context.Background(),
+					Token:   token.Value,
+					Log:     log.Log,
+					CloseHandler: func(_ error) {
+						cache.Remove(token)
+						connectionPoolSize.Dec()
+					},
+				})
+			default:
+				return nil, errors.New("unknown authentication token")
+			}
 		},
 	}, nil
 
 }
 
 type ConnectionPool struct {
-	connConstructor func(token string) (gitpod.APIInterface, error)
+	connConstructor func(token auth.Token) (gitpod.APIInterface, error)
 
 	// cache stores token to connection mapping
 	cache *lru.Cache
 }
 
-func (p *ConnectionPool) Get(ctx context.Context, token string) (gitpod.APIInterface, error) {
+func (p *ConnectionPool) Get(ctx context.Context, token auth.Token) (gitpod.APIInterface, error) {
 	cached, found := p.cache.Get(token)
 	reportCacheOutcome(found)
 	if found {
