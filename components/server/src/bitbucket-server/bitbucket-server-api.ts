fbd4a56aab66a6c4f27acfc2d18116c9b3ff2f23
Alex Tugarev <alex@gitpod.io>
2022-11-03 06:47:44 +0000

    [bitbucket-server] improve logging of webhook installation

--- components/server/src/bitbucket-server/bitbucket-server-api.ts
+++ components/server/src/bitbucket-server/bitbucket-server-api.ts
@@ -57,7 +57,7 @@ export class BitbucketServerApi {
             result = "error " + error?.message;
             throw error;
         } finally {
-            console.debug(`BitbucketServer GET ${fullUrl} - ${result}`);
+            console.log(`BBS: ${method} ${fullUrl} - ${result}`);
         }
     }
 
