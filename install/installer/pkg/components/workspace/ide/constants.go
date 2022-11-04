adecfbe33b1ecf67617ec34fd400049fa7f2d1a9
Pudong Zheng <tianshi8650@gmail.com>
2022-11-04 13:01:31 +0000

    [code] update stable code to add gallery metrics

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-f17f584e2e252f3c1e877d28ea1edd1ab137a831" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-6fb745a090b05048a96ab47bdab5e4a88f848b98" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
