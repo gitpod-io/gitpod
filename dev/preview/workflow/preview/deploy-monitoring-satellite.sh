032d1efdfd96bbd58dc6dafed15ca54de8dd4f08
Mads Hartmann <mads379@gmail.com>
2022-11-08 13:16:15 +0000

    Perform all operations in the temporary directory
    Apply files one at a time to help debugging

--- dev/preview/workflow/preview/deploy-monitoring-satellite.sh
+++ dev/preview/workflow/preview/deploy-monitoring-satellite.sh
@@ -39,6 +39,12 @@ kubectl \
     --context "${PREVIEW_K3S_KUBE_CONTEXT}" \
     apply -f -
 
+tmpdir=$(mktemp -d)
+manifests_dir="${tmpdir}/manifests"
+mkdir -p "${manifests_dir}"
+echo "Using ${tmpdir} with manifests in ${manifests_dir} this can be useful if you have to debug the generated manifests"
+cd "${tmpdir}"
+
 if ! command -v envsubst; then
   go install github.com/a8m/envsubst/cmd/envsubst@latest
 fi
@@ -46,36 +52,33 @@ fi
 GOBIN=$(pwd) go install github.com/gitpod-io/observability/installer@main
 mv installer observability-installer
 
-tmpdir=$(mktemp -d)
-
 HONEYCOMB_API_KEY="$(readWerftSecret honeycomb-api-key apikey)" \
 PROM_REMOTE_WRITE_USER="$(readWerftSecret prometheus-remote-write-auth user)" \
 PROM_REMOTE_WRITE_PASSWORD="$(readWerftSecret prometheus-remote-write-auth password)" \
 PREVIEW_NAME="${PREVIEW_NAME}" \
 envsubst <"${ROOT}/dev/preview/workflow/config/monitoring-satellite.yaml" \
-| ./observability-installer render --app monitoring-satellite --output-split-files "${tmpdir}" --config -
-
-pushd "${tmpdir}"
+| ./observability-installer render --app monitoring-satellite --output-split-files "${manifests_dir}" --config -
 
 # we have to apply the CRDs first and wait until they are available before we can apply the rest
-find . -name "*CustomResourceDefinition*" -exec kubectl --kubeconfig "${PREVIEW_K3S_KUBE_PATH}" --context "${PREVIEW_K3S_KUBE_CONTEXT}" apply -f {} --server-side \;
+find "${manifests_dir}" -name "*CustomResourceDefinition*" -exec kubectl --kubeconfig "${PREVIEW_K3S_KUBE_PATH}" --context "${PREVIEW_K3S_KUBE_CONTEXT}" apply -f {} --server-side \;
 
-# wait for the CRDs
+echo "Waiting for CRDs"
 kubectl \
     --kubeconfig "${PREVIEW_K3S_KUBE_PATH}" \
     --context "${PREVIEW_K3S_KUBE_CONTEXT}" \
     -n monitoring-satellite wait --for condition=established --timeout=60s crd/servicemonitors.monitoring.coreos.com
 
-kubectl \
-    --kubeconfig "${PREVIEW_K3S_KUBE_PATH}" \
-    --context "${PREVIEW_K3S_KUBE_CONTEXT}" \
-    apply --server-side -f .
+echo "Applying generated manifests"
+for f in "${manifests_dir}"/*.yaml; do
+    echo "Applying $f"
+    kubectl \
+        --kubeconfig "${PREVIEW_K3S_KUBE_PATH}" \
+        --context "${PREVIEW_K3S_KUBE_CONTEXT}" \
+        apply --server-side -f "${f}"
+done
 
+echo "Patching grafana deployment"
 kubectl \
     --kubeconfig "${PREVIEW_K3S_KUBE_PATH}" \
     --context "${PREVIEW_K3S_KUBE_CONTEXT}" \
     patch deployments.apps -n monitoring-satellite grafana --type=json -p="[{'op': 'remove', 'path': '/spec/template/spec/nodeSelector'}]"
-
-popd
-
-rm observability-installer
