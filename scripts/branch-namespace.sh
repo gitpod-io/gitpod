1b48aa91a12782aa676e465fe3c0adb0964f1d57
Mads Hartmann <mads379@gmail.com>
2022-11-08 13:23:17 +0000

    Remove legacy branch-namespace script

--- scripts/branch-namespace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ./dev/preview/util/preview-name-from-branch.sh
-
-currentContext=$(kubectl config current-context 2>&1) || error "cannot set kubectl namespace: no current context"
-namespace="staging-$(preview-name-from-branch)"
-
-echo "Setting kubectl namespace: $namespace"
-kubectl config set-context "$currentContext" --namespace "$namespace"
