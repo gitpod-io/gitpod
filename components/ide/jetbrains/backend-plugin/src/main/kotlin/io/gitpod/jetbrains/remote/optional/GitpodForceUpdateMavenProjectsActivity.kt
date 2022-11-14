ca2876d730b08fb6ce0dd8b11b87ede49cc5084d
Anton Kosyakov <anton@gitpod.io>
2022-11-14 12:03:53 +0200

    [jetbrains]: force initialize maven project
    to work around initialization after warmup in prebuilds
    
    Co-authored-by: Victor Nogueira <victor@gitpod.io>

---
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/optional/GitpodForceUpdateMavenProjectsActivity.kt
@@ -0,0 +1,22 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package io.gitpod.jetbrains.remote.optional
+
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import org.jetbrains.idea.maven.project.MavenProjectsManager
+
+class GitpodForceUpdateMavenProjectsActivity : StartupActivity.RequiredForSmartMode {
+    override fun runActivity(project: Project) {
+        val mavenProjectManager = MavenProjectsManager.getInstance(project)
+
+        if (!mavenProjectManager.isMavenizedProject) return
+
+        mavenProjectManager.forceUpdateAllProjectsOrFindAllAvailablePomFiles()
+
+        thisLogger().warn("gitpod: Forced the update of Maven Project.")
+    }
+}
