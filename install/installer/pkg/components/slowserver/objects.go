c298adda339becb5de2703a8f078d76c489f3a78
Andrew Farries <andrew@gitpod.io>
2022-11-14 13:15:00 +0000

    Render components if experimental flag is set

--- install/installer/pkg/components/slowserver/objects.go
+++ install/installer/pkg/components/slowserver/objects.go
@@ -7,40 +7,64 @@ package slowserver
 import (
 	"github.com/gitpod-io/gitpod/common-go/baseserver"
 	"github.com/gitpod-io/gitpod/installer/pkg/common"
+	"github.com/gitpod-io/gitpod/installer/pkg/config/v1/experimental"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
-var Objects = common.CompositeRenderFunc(
-	configmap,
-	deployment,
-	networkpolicy,
-	role,
-	rolebinding,
-	common.GenerateService(Component, []common.ServicePort{
-		{
-			Name:          ContainerPortName,
-			ContainerPort: ContainerPort,
-			ServicePort:   ServicePort,
-		},
-		{
-			Name:          baseserver.BuiltinMetricsPortName,
-			ContainerPort: baseserver.BuiltinMetricsPort,
-			ServicePort:   baseserver.BuiltinMetricsPort,
-		},
-		{
-			Name:          InstallationAdminName,
-			ContainerPort: InstallationAdminPort,
-			ServicePort:   InstallationAdminPort,
-		},
-		{
-			Name:          DebugPortName,
-			ContainerPort: baseserver.BuiltinDebugPort,
-			ServicePort:   baseserver.BuiltinDebugPort,
-		},
-		{
-			Name:          DebugNodePortName,
-			ContainerPort: common.DebugNodePort,
-			ServicePort:   common.DebugNodePort,
-		},
-	}),
-	common.DefaultServiceAccount(Component),
-)
+func Objects(ctx *common.RenderContext) ([]runtime.Object, error) {
+	cfg := getExperimentalWebAppConfig(ctx)
+	if cfg == nil || !cfg.SlowDatabase {
+		return nil, nil
+	}
+
+	return common.CompositeRenderFunc(
+		configmap,
+		deployment,
+		networkpolicy,
+		role,
+		rolebinding,
+		common.GenerateService(Component, []common.ServicePort{
+			{
+				Name:          ContainerPortName,
+				ContainerPort: ContainerPort,
+				ServicePort:   ServicePort,
+			},
+			{
+				Name:          baseserver.BuiltinMetricsPortName,
+				ContainerPort: baseserver.BuiltinMetricsPort,
+				ServicePort:   baseserver.BuiltinMetricsPort,
+			},
+			{
+				Name:          InstallationAdminName,
+				ContainerPort: InstallationAdminPort,
+				ServicePort:   InstallationAdminPort,
+			},
+			{
+				Name:          DebugPortName,
+				ContainerPort: baseserver.BuiltinDebugPort,
+				ServicePort:   baseserver.BuiltinDebugPort,
+			},
+			{
+				Name:          DebugNodePortName,
+				ContainerPort: common.DebugNodePort,
+				ServicePort:   common.DebugNodePort,
+			},
+		}),
+		common.DefaultServiceAccount(Component),
+	)(ctx)
+}
+
+func getExperimentalWebAppConfig(ctx *common.RenderContext) *experimental.WebAppConfig {
+	var experimentalCfg *experimental.Config
+
+	_ = ctx.WithExperimental(func(ucfg *experimental.Config) error {
+		experimentalCfg = ucfg
+		return nil
+	})
+
+	if experimentalCfg == nil || experimentalCfg.WebApp == nil {
+		return nil
+	}
+
+	return experimentalCfg.WebApp
+}
