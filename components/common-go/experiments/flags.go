6c883c70ab109a6d259e331e0d72a411b10cfa01
Milan Pavlik <milan@gitpod.io>
2022-11-15 14:42:32 +0000

    [public-api] Add test for CreateToken when feature flag enabled

--- components/common-go/experiments/flags.go
+++ components/common-go/experiments/flags.go
@@ -6,11 +6,10 @@ package experiments
 
 import "context"
 
-// IsMyFirstFeatureFlagEnabled example usage of a flag
-func IsMyFirstFeatureFlagEnabled(ctx context.Context, client Client, attributes Attributes) bool {
-	return client.GetBoolValue(ctx, "isMyFirstFeatureEnabled", false, attributes)
-}
+const (
+	PersonalAccessTokensEnabledFlag = "personalAccessTokensEnabled"
+)
 
 func IsPersonalAccessTokensEnabled(ctx context.Context, client Client, attributes Attributes) bool {
-	return client.GetBoolValue(ctx, "personalAccessTokensEnabled", false, attributes)
+	return client.GetBoolValue(ctx, PersonalAccessTokensEnabledFlag, false, attributes)
 }
