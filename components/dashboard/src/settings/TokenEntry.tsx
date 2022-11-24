f4b15612d0d4b281b935162af25f942b21e39bed
Laurie T. Malau <laurie@gitpod.io>
2022-11-24 14:08:55 +0000

    m

--- components/dashboard/src/settings/TokenEntry.tsx
+++ components/dashboard/src/settings/TokenEntry.tsx
@@ -5,23 +5,46 @@
  */
 
 import { PersonalAccessToken } from "@gitpod/public-api/lib/gitpod/experimental/v1/tokens_pb";
+import { ContextMenuEntry } from "../components/ContextMenu";
+import { ItemFieldContextMenu } from "../components/ItemsList";
+
+const menuEntries: ContextMenuEntry[] = [
+    {
+        title: "Edit",
+        href: "",
+        onClick: () => {},
+    },
+    {
+        title: "Regenerate",
+        href: "",
+        onClick: () => {},
+    },
+    {
+        title: "Delete",
+        href: "",
+        onClick: () => {},
+    },
+];
 
 function TokenEntry(t: { token: PersonalAccessToken }) {
     if (!t) {
         return <></>;
     }
     return (
-        <div className="rounded-xl whitespace-nowrap flex space-x-2 py-6 px-6 w-full justify-between hover:bg-gray-100 dark:hover:bg-gray-800 focus:bg-gitpod-kumquat-light group">
-            <div className="pr-3 self-center w-1/12">
-                <span>{t.token.name || ""}</span>
+        <>
+            <div className="rounded-xl whitespace-nowrap flex space-x-2 py-6 px-6 w-full justify-between hover:bg-gray-100 dark:hover:bg-gray-800 focus:bg-gitpod-kumquat-light group">
+                <div className="pr-3 self-center w-1/12">
+                    <span>{t.token.name || ""}</span>
+                </div>
+                <div className="flex flex-col w-6/12">
+                    <span>{t.token.scopes || ""}</span>
+                </div>
+                <div className="flex w-5/12 self-center">
+                    <span>{t.token.expirationTime || ""}</span>
+                </div>
             </div>
-            <div className="flex flex-col w-6/12">
-                <span>{t.token.scopes || ""}</span>
-            </div>
-            <div className="flex w-5/12 self-center">
-                <span>{t.token.expirationTime || ""}</span>
-            </div>
-        </div>
+            <ItemFieldContextMenu menuEntries={menuEntries} />
+        </>
     );
 }
 
