1cf27b3a69480270222dfd157e20b81595bcc9fd
Sven Efftinge <sven@gitpod.io>
2022-11-25 12:10:52 +0000

    migrate oss users to ubp

---
+++ components/gitpod-db/src/typeorm/migration/1669370193752-MigrateToUBP.ts
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MigrateToUBP1669370193752 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // migrate OSS users, the group by and having clause makes sure that we only add open-source users that are not on other plans as well.
+        queryRunner.query(`
+            INSERT INTO d_b_cost_center
+                (id, spendingLimit, creationTime, billingStrategy, billingCycleStart, nextBillingTime)
+            SELECT
+                CONCAT('user:', userId),
+                1000,
+                DATE_FORMAT(now(), '%Y-%m-%dT%TZ'),
+                'other',
+                DATE_FORMAT(now(), '%Y-%m-%dT%TZ'),
+                DATE_FORMAT(DATE_ADD(now(),INTERVAL 1 month), '%Y-%m-%dT%TZ')
+            FROM d_b_subscription
+            WHERE
+                cancellationDate=''
+                AND endDate=''
+                AND deleted=0
+            GROUP BY userid
+            HAVING max(planId) = 'free-open-source'
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
