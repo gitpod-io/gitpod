4c41f27830d2cbe09d9cdcde563998e4a5470baa
Pudong Zheng <tianshi8650@gmail.com>
2022-11-28 12:01:33 +0000

    update stable code image

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-adf78e4cd7f0309b567a0c849bee9d473b37496a" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-7588b82c91ad977c97dbd51e64694981261c2aaf" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
