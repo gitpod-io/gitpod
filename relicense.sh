9b3c6f4f5804a044ecefeaf9694a5653d3b6d6c5
Moritz Eysholdt <moritz@gitpod.io>
2022-12-05 13:49:19 +0000

    wip

--- relicense.sh
+++ relicense.sh
@@ -14,12 +14,14 @@
 
 
 leeway collect files --format-string '{{ range . }}{{ .Name }}{{"\n"}}{{ end }}' | uniq > files.txt
+find . -iname "*.libsonnet" -or  -iname "*.jsonnet" >> files.txt
 
 L1="Licensed under the GNU Affero General Public License (AGPL)."
 L2="See License.AGPL.txt in the project root for license information."
 
 cat < files.txt | while IFS= read -r line
 do
+  echo "$line"
   # ------ MIT -------
   # bash, YAML, Dockerfile
   sed -i "s/# Licensed under the MIT License. See License-MIT.txt in the project root for license information./# $L1\n# $L2/" "$line"
