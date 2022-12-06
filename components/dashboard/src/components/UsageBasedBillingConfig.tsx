3ee281fe96e49993998a7658b78a4069dd38b80a
Sven Efftinge <sven@gitpod.io>
2022-12-06 08:18:54 +0000

    [dashboard] update description

--- components/dashboard/src/components/UsageBasedBillingConfig.tsx
+++ components/dashboard/src/components/UsageBasedBillingConfig.tsx
@@ -211,7 +211,11 @@ export default function UsageBasedBillingConfig({ attributionId }: Props) {
 
     return (
         <div className="mb-16">
-            <h2 className="text-gray-500">Manage pay-as-you-go billing.</h2>
+            <h2 className="text-gray-500">
+                {attributionId && AttributionId.parse(attributionId)?.kind === "user"
+                    ? "Manage billing for your personal account."
+                    : "Manage billing for your team."}
+            </h2>
             <div className="max-w-xl flex flex-col">
                 {errorMessage && (
                     <Alert className="max-w-xl mt-2" closable={false} showIcon={true} type="error">
