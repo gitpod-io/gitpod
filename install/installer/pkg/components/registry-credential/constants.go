28b5a76c68ee9cb32361c29c43a1523385a6943a
JenTing Hsiao <hsiaoairplane@gmail.com>
2022-12-06 07:19:50 +0000

    installer: add registry-credential component
    Signed-off-by: JenTing Hsiao <hsiaoairplane@gmail.com>

---
+++ install/installer/pkg/components/registry-credential/constants.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package registry_credential
+
+const (
+	Component = "registry-credential"
+
+	SecretNameAWSIAMUserCredentials = "aws-iam-user-credentials"
+
+	CronSchedule = "* */6 * * *"
+)
