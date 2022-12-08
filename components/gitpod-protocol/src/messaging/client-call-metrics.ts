abdf31d7e271b74d58f47aba8153514a97473ccc
Pudong Zheng <tianshi8650@gmail.com>
2022-12-08 05:52:34 +0000

    add grpc_client_handling_seconds metrics for nice-grpc

--- components/gitpod-protocol/src/messaging/client-call-metrics.ts
+++ components/gitpod-protocol/src/messaging/client-call-metrics.ts
@@ -14,6 +14,7 @@ export class PrometheusClientCallMetrics implements IClientCallMetrics {
     readonly sentCounter: prometheusClient.Counter<string>;
     readonly receivedCounter: prometheusClient.Counter<string>;
     readonly handledCounter: prometheusClient.Counter<string>;
+    readonly handledSecondsHistogram: prometheusClient.Histogram<string>;
 
     constructor() {
         this.startedCounter = new prometheusClient.Counter({
@@ -37,6 +38,12 @@ export class PrometheusClientCallMetrics implements IClientCallMetrics {
         this.handledCounter = new prometheusClient.Counter({
             name: "grpc_client_handled_total",
             help: "Total number of RPCs completed by the client, regardless of success or failure.",
+            labelNames: ["grpc_service", "grpc_method", "grpc_type", "grpc_code"],
+            registers: [prometheusClient.register],
+        });
+        this.handledSecondsHistogram = new prometheusClient.Histogram({
+            name: "grpc_client_handling_seconds",
+            help: "Histogram of response latency (seconds) of the gRPC until it is finished by the application.",
             labelNames: ["grpc_service", "grpc_method", "grpc_type", "grpc_code"],
             registers: [prometheusClient.register],
         });
@@ -72,6 +79,16 @@ export class PrometheusClientCallMetrics implements IClientCallMetrics {
             grpc_method: labels.method,
             grpc_type: labels.type,
             grpc_code: labels.code,
+        });
+    }
+
+    startHandleTimer(
+        labels: IGrpcCallMetricsLabelsWithCode,
+    ): (labels?: Partial<Record<string, string | number>> | undefined) => number {
+        return this.handledSecondsHistogram.startTimer({
+            grpc_service: labels.service,
+            grpc_method: labels.method,
+            grpc_type: labels.type,
         });
     }
 }
