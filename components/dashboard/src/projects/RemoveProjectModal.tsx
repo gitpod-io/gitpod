cca8a7a08263f9463a7eedd2db314a813f56ea0e
Brad Harris <bmharris@gmail.com>
2022-12-12 21:59:55 +0000

    wip for remove project on settings page

---
+++ components/dashboard/src/projects/RemoveProjectModal.tsx
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License.AGPL.txt in the project root for license information.
+ */
+
+import { FunctionComponent, useCallback, useState } from "react";
+import type { Project } from "@gitpod/gitpod-protocol";
+import { projectsService } from "../service/public-api";
+import { getGitpodService } from "../service/service";
+import ConfirmationModal from "../components/ConfirmationModal";
+import { useFeatureFlags } from "../contexts/FeatureFlagContext";
+
+type RemoveProjectModalProps = {
+    project: Project;
+    onClose: () => void;
+    onRemoved: () => void;
+};
+
+export const RemoveProjectModal: FunctionComponent<RemoveProjectModalProps> = ({ project, onClose, onRemoved }) => {
+    const { usePublicApiProjectsService } = useFeatureFlags();
+
+    const [disabled, setDisabled] = useState(false);
+
+    const removeProject = useCallback(async () => {
+        setDisabled(true);
+        setDisabled(false);
+        onRemoved();
+    }, [onRemoved]);
+
+    const removeProject2 = useCallback(async () => {
+        setDisabled(true);
+        usePublicApiProjectsService
+            ? await projectsService.deleteProject({ projectId: project.id })
+            : await getGitpodService().server.deleteProject(project.id);
+        setDisabled(false);
+        onRemoved();
+        // onClose();
+    }, [onRemoved, project.id, usePublicApiProjectsService]);
+    console.log("derp", removeProject2);
+
+    return (
+        <ConfirmationModal
+            title="Remove Project"
+            areYouSureText="Are you sure you want to remove this project from this team? Team members will also lose access to this project."
+            children={{
+                name: project?.name ?? "",
+                description: project?.cloneUrl ?? "",
+            }}
+            buttonText="Remove Project"
+            buttonDisabled={disabled}
+            onClose={onClose}
+            onConfirm={removeProject}
+            visible
+        />
+    );
+};
