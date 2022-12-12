9560c81abece5e3f47b6f62c40db4719ab1b1bac
JenTing Hsiao <hsiaoairplane@gmail.com>
2022-12-12 06:37:14 +0000

    installer: use container image from leeway built
    Signed-off-by: JenTing Hsiao <hsiaoairplane@gmail.com>

--- install/installer/pkg/config/versions/versions.go
+++ install/installer/pkg/config/versions/versions.go
@@ -36,6 +36,7 @@ type Components struct {
 	PaymentEndpoint       Versioned `json:"paymentEndpoint"`
 	Proxy                 Versioned `json:"proxy"`
 	PublicAPIServer       Versioned `json:"public-api-server"`
+	RegistryCredential    Versioned `json:"registryCredential"`
 	RegistryFacade        Versioned `json:"registryFacade"`
 	Server                Versioned `json:"server"`
 	ServiceWaiter         Versioned `json:"serviceWaiter"`
