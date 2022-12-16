cd65787189572549aed61c0d5ae34f0b608b718c
Anton Kosyakov <anton@gitpod.io>
2022-12-16 09:56:12 +0000

    [jb]: remove custom JDK detection
    Use built-in instead during warmup and regular workspace start.
    Also disable refresh of Maven project in headless mode, i.e warmup or install plugins.
    
    Co-authored-by: Andrea Falzetti <andrea@gitpod.io>
    Co-authored-by: Victor Nogueira <victor@gitpod.io>

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/optional/GitpodForceUpdateMavenProjectsActivity.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/optional/GitpodForceUpdateMavenProjectsActivity.kt
@@ -7,12 +7,16 @@ package io.gitpod.jetbrains.remote.optional
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.util.application
 import org.jetbrains.idea.maven.project.MavenProjectsManager
 
 class GitpodForceUpdateMavenProjectsActivity : StartupActivity.RequiredForSmartMode {
     override fun runActivity(project: Project) {
+        if (application.isHeadlessEnvironment  || Registry.get("gitpod.forceUpdateMavenProjects.disabled").asBoolean()) {
+            return
+        }
         application.invokeLater {
             VirtualFileManager.getInstance().asyncRefresh {
                 MavenProjectsManager.getInstance(project).forceUpdateAllProjectsOrFindAllAvailablePomFiles()
