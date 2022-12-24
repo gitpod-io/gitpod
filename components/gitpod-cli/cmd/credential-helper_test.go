1d3bf3065fdb6dc89d3f03e8d8bbb7894958315f
Siddhant Khare <siddhant@gitpod.io>
2022-12-24 05:19:31 +0000

    fix: context URLs of JetBrains ex. repo.
    Signed-off-by: Siddhant Khare <siddhant@gitpod.io>

--- components/gitpod-cli/cmd/credential-helper_test.go
+++ components/gitpod-cli/cmd/credential-helper_test.go
@@ -63,10 +63,10 @@ func Test_parse_git_command_and_remote(t *testing.T) {
 		{
 			Name: "JB push command",
 			Commands: []string{
-				"/usr/lib/git-core/git remote-https origin https://github.com/gitpod-io/spring-petclinic.git",
+				"/usr/lib/git-core/git remote-https origin https://github.com/gitpod-samples/spring-petclinic.git",
 				"/bin/git -c core.quotepath=false -c log.showSignature=false push --progress --porcelain origin refs/heads/master:master",
 			},
-			Expected: gitCommandInfo{RepoUrl: "https://github.com/gitpod-io/spring-petclinic.git", GitCommand: "push"},
+			Expected: gitCommandInfo{RepoUrl: "https://github.com/gitpod-samples/spring-petclinic.git", GitCommand: "push"},
 		},
 	}
 	for _, tt := range tests {
