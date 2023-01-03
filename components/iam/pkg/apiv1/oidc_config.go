4500032f7faceb25f908849f8811d9bd48e388e7
Milan Pavlik <milan@gitpod.io>
2023-01-03 09:45:06 +0000

    [iam] Add unimplemented CreateOIDCConfig RPC

--- components/iam/pkg/apiv1/oidc_config.go
+++ components/iam/pkg/apiv1/oidc_config.go
@@ -5,13 +5,29 @@
 package apiv1
 
 import (
+	"context"
+
+	db "github.com/gitpod-io/gitpod/components/gitpod-db/go"
 	v1 "github.com/gitpod-io/gitpod/components/iam-api/go/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
 )
 
-func NewOIDCClientConfigService() *OIDCClientConfigService {
-	return &OIDCClientConfigService{}
+func NewOIDCClientConfigService(dbConn *gorm.DB, cipher db.Cipher) *OIDCClientConfigService {
+	return &OIDCClientConfigService{
+		dbConn: dbConn,
+		cipher: cipher,
+	}
 }
 
 type OIDCClientConfigService struct {
+	dbConn *gorm.DB
+	cipher db.Cipher
+
 	v1.UnimplementedOIDCServiceServer
+}
+
+func (s *OIDCClientConfigService) CreateClientConfig(ctx context.Context, req *v1.CreateClientConfigRequest) (*v1.CreateClientConfigResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateClientConfig not implemented")
 }
