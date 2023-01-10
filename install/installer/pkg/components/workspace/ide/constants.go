bb49607a3ab5584b547c6d7f0f44970241f738d1
Filip Troníček <filip@gitpod.io>
2023-01-10 19:42:51 +0100

    Update VS Code Browser to `1.74.3`

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-2b5b022a4d10e518d55b5a893dab492f2a33ba89" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-f861b9d093fc8cdc728a0179b3a0c43a9155d9b4" // stable version that will be updated manually on demand
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
