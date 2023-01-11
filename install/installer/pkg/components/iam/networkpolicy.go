f2244f8d0e2203205aed6b8e057c9c09fa7c9f25
Alex Tugarev <alex@gitpod.io>
2023-01-11 10:41:12 +0000

    [installer/iam] allow ingress from public-api-server

--- install/installer/pkg/components/iam/networkpolicy.go
+++ install/installer/pkg/components/iam/networkpolicy.go
@@ -46,6 +46,13 @@ func networkpolicy(ctx *common.RenderContext) ([]runtime.Object, error) {
 									},
 								},
 							},
+							{
+								PodSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"component": common.PublicApiComponent,
+									},
+								},
+							},
 						},
 					},
 				},
