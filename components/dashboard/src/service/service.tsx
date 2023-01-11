659889f09acc14735d431c5f6837b36c6b22db32
Huiwen <mhqnwt@gmail.com>
2023-01-11 11:26:09 +0000

    [dashboard] support supervisor frontend server
    Co-authored-by: mustard <mhqnwt@gmail.com>
    Co-authored-by: Jean Pierre <jeanp413@hotmail.com>

--- components/dashboard/src/service/service.tsx
+++ components/dashboard/src/service/service.tsx
@@ -5,32 +5,24 @@
  */
 
 import {
+    Emitter,
     GitpodClient,
     GitpodServer,
     GitpodServerPath,
     GitpodService,
     GitpodServiceImpl,
+    User,
+    WorkspaceInfo,
 } from "@gitpod/gitpod-protocol";
 import { WebSocketConnectionProvider } from "@gitpod/gitpod-protocol/lib/messaging/browser/connection";
-import { createWindowMessageConnection } from "@gitpod/gitpod-protocol/lib/messaging/browser/window-connection";
-import { JsonRpcProxyFactory } from "@gitpod/gitpod-protocol/lib/messaging/proxy-factory";
 import { GitpodHostUrl } from "@gitpod/gitpod-protocol/lib/util/gitpod-host-url";
 import { log } from "@gitpod/gitpod-protocol/lib/util/logging";
+import { IDEFrontendDashboardService } from "@gitpod/gitpod-protocol/lib/frontend-dashboard-service";
+import { RemoteTrackMessage } from "@gitpod/gitpod-protocol/lib/analytics";
 
 export const gitpodHostUrl = new GitpodHostUrl(window.location.toString());
 
 function createGitpodService<C extends GitpodClient, S extends GitpodServer>() {
-    if (window.top !== window.self && process.env.NODE_ENV === "production") {
-        const connection = createWindowMessageConnection("gitpodServer", window.parent, "*");
-        const factory = new JsonRpcProxyFactory<S>();
-        const proxy = factory.createProxy();
-        factory.listen(connection);
-        return new GitpodServiceImpl<C, S>(proxy, {
-            onReconnect: async () => {
-                await connection.sendRequest("$reconnectServer");
-            },
-        });
-    }
     let host = gitpodHostUrl.asWebsocket().with({ pathname: GitpodServerPath }).withApi();
 
     const connectionProvider = new WebSocketConnectionProvider();
@@ -53,7 +45,7 @@ function createGitpodService<C extends GitpodClient, S extends GitpodServer>() {
     return new GitpodServiceImpl<C, S>(proxy, { onReconnect });
 }
 
-function getGitpodService(): GitpodService {
+export function getGitpodService(): GitpodService {
     const w = window as any;
     const _gp = w._gp || (w._gp = {});
     if (window.location.search.includes("service=mock")) {
@@ -64,4 +56,143 @@ function getGitpodService(): GitpodService {
     return service;
 }
 
-export { getGitpodService };
+let ideFrontendService: IDEFrontendService | undefined;
+export function getIDEFrontendService(workspaceID: string, sessionId: string, service: GitpodService) {
+    if (!ideFrontendService) {
+        ideFrontendService = new IDEFrontendService(workspaceID, sessionId, service, window.parent);
+    }
+    return ideFrontendService;
+}
+
+export class IDEFrontendService implements IDEFrontendDashboardService.IServer {
+    private instanceID: string | undefined;
+    private ideUrl: URL | undefined;
+    private user: User | undefined;
+
+    private latestStatus?: IDEFrontendDashboardService.Status;
+
+    private readonly onDidChangeEmitter = new Emitter<IDEFrontendDashboardService.SetStateData>();
+    readonly onSetState = this.onDidChangeEmitter.event;
+
+    constructor(
+        private workspaceID: string,
+        private sessionId: string,
+        private service: GitpodService,
+        private clientWindow: Window,
+    ) {
+        this.processServerInfo();
+        window.addEventListener("message", (event: MessageEvent) => {
+            if (event.origin !== this.ideUrl?.origin) {
+                return;
+            }
+
+            if (IDEFrontendDashboardService.isTrackEventData(event.data)) {
+                this.trackEvent(event.data.msg);
+            }
+            if (IDEFrontendDashboardService.isHeartbeatEventData(event.data)) {
+                this.activeHeartbeat();
+            }
+            if (IDEFrontendDashboardService.isSetStateEventData(event.data)) {
+                this.onDidChangeEmitter.fire(event.data.state);
+            }
+        });
+        window.addEventListener("unload", () => {
+            if (!this.instanceID) {
+                return;
+            }
+            // send last heartbeat (wasClosed: true)
+            const data = { sessionId: this.sessionId };
+            const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+            const gitpodHostUrl = new GitpodHostUrl(new URL(window.location.toString()));
+            const url = gitpodHostUrl.withApi({ pathname: `/auth/workspacePageClose/${this.instanceID}` }).toString();
+            navigator.sendBeacon(url, blob);
+        });
+    }
+
+    private async processServerInfo() {
+        this.user = await this.service.server.getLoggedInUser();
+        const workspace = await this.service.server.getWorkspace(this.workspaceID);
+        this.instanceID = workspace.latestInstance?.id;
+        if (this.instanceID) {
+            this.auth();
+        }
+
+        const listener = await this.service.listenToInstance(this.workspaceID);
+        listener.onDidChange(() => {
+            this.ideUrl = listener.info.latestInstance?.ideUrl
+                ? new URL(listener.info.latestInstance?.ideUrl)
+                : undefined;
+            const status = this.getWorkspaceStatus(listener.info);
+            this.latestStatus = status;
+            this.sendStatusUpdate(this.latestStatus);
+            if (this.instanceID !== status.instanceId) {
+                this.instanceID = status.instanceId;
+                this.auth();
+            }
+        });
+    }
+
+    getWorkspaceStatus(workspace: WorkspaceInfo): IDEFrontendDashboardService.Status {
+        return {
+            loggedUserId: this.user!.id,
+            workspaceID: this.workspaceID,
+            instanceId: workspace.latestInstance?.id,
+            ideUrl: workspace.latestInstance?.ideUrl,
+            statusPhase: workspace.latestInstance?.status.phase,
+            workspaceDescription: workspace.workspace.description,
+            workspaceType: workspace.workspace.type,
+        };
+    }
+
+    // implements
+
+    async auth() {
+        if (!this.instanceID) {
+            return;
+        }
+        const url = gitpodHostUrl.asWorkspaceAuth(this.instanceID).toString();
+        await fetch(url, {
+            credentials: "include",
+        });
+    }
+
+    trackEvent(msg: RemoteTrackMessage): void {
+        msg.properties = {
+            ...msg.properties,
+            sessionId: this.sessionId,
+            instanceId: this.latestStatus?.instanceId,
+            workspaceId: this.workspaceID,
+            type: this.latestStatus?.workspaceType,
+        };
+        this.service.server.trackEvent(msg);
+    }
+
+    activeHeartbeat(): void {
+        if (this.instanceID) {
+            this.service.server.sendHeartBeat({ instanceId: this.instanceID });
+        }
+    }
+
+    sendStatusUpdate(status: IDEFrontendDashboardService.Status): void {
+        if (!this.ideUrl) {
+            return;
+        }
+        this.clientWindow.postMessage(
+            {
+                type: "ide-status-update",
+                status,
+            } as IDEFrontendDashboardService.StatusUpdateEventData,
+            this.ideUrl.origin,
+        );
+    }
+
+    relocate(url: string): void {
+        if (!this.ideUrl) {
+            return;
+        }
+        this.clientWindow.postMessage(
+            { type: "ide-relocate", url } as IDEFrontendDashboardService.RelocateEventData,
+            this.ideUrl.origin,
+        );
+    }
+}
