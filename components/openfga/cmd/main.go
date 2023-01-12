974f9212b40fdaaae6318f0fa2b10327e563ce79
Milan Pavlik <milan@gitpod.io>
2023-01-12 16:07:27 +0000

    Fix

--- components/openfga/cmd/main.go
+++ components/openfga/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
@@ -9,10 +11,7 @@ import (
 
 func main() {
 
-	err := load(loadOpts{
-		teams:   15000,
-		members: 155,
-	})
+	err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +23,7 @@ type loadOpts struct {
 	members int
 }
 
-func load(opts loadOpts) error {
+func run() error {
 	configuration, err := openfga.NewConfiguration(openfga.Configuration{
 		ApiScheme: os.Getenv("OPENFGA_API_SCHEME"), // optional, defaults to "https"
 		ApiHost:   os.Getenv("OPENFGA_API_HOST"),   // required, define without the scheme (e.g. api.fga.example instead of https://api.fga.example)
@@ -34,6 +33,52 @@ func load(opts loadOpts) error {
 		return err
 	}
 
+	client := openfga.NewAPIClient(configuration)
+
 	return nil
 
+}
+
+func load(client openfga.OpenFgaApi, opts loadOpts) error {
+	ctx := context.Background()
+	for teamID := 0; teamID < opts.teams; teamID++ {
+		for userID := 0; userID < opts.members; userID++ {
+			var membership *openfga.TupleKey
+
+			if userID < 5 {
+				// first 5 members are owners
+				membership = tup(user(userID), "owner", team(teamID))
+			} else {
+				membership = tup(user(userID), "member", team(teamID))
+			}
+
+			cellOwnership := tup("cell:global", "cell", team(teamID))
+
+			req := openfga.WriteRequest{
+				Writes: openfga.NewTupleKeys([]openfga.TupleKey{
+					membership,
+					cellOwnership,
+				}),
+				Deletes: nil,
+			}
+			client.Write(ctx).Body()
+
+		}
+	}
+}
+
+func user(id int) string {
+	return fmt.Sprintf("user:%d", id)
+}
+
+func team(id int) string {
+	return fmt.Sprintf("team:%d", id)
+}
+
+func tup(user, relation, object string) openfga.TupleKey {
+	return openfga.TupleKey{
+		User:     openfga.PtrString(user),
+		Relation: openfga.PtrString(relation),
+		Object:   openfga.PtrString(object),
+	}
 }
