7a2a28b13c243689ad79f16fa23b4de6299fa935
Milan Pavlik <milan@gitpod.io>
2023-01-12 15:01:06 +0000

    fix

--- components/openfga/cmd/main.go
+++ components/openfga/cmd/main.go
@@ -9,14 +9,22 @@ import (
 
 func main() {
 
-	err := run()
+	err := load(loadOpts{
+		teams:   15000,
+		members: 155,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
 }
 
-func run() error {
+type loadOpts struct {
+	teams   int
+	members int
+}
+
+func load(opts loadOpts) error {
 	configuration, err := openfga.NewConfiguration(openfga.Configuration{
 		ApiScheme: os.Getenv("OPENFGA_API_SCHEME"), // optional, defaults to "https"
 		ApiHost:   os.Getenv("OPENFGA_API_HOST"),   // required, define without the scheme (e.g. api.fga.example instead of https://api.fga.example)
@@ -26,5 +34,6 @@ func run() error {
 		return err
 	}
 
-	client := openfga.NewAPIClient(configuration)
+	return nil
+
 }
