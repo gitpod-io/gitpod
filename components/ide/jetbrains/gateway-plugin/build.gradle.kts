eca774c9e505c95dc45e45a0a6721e59140940fe
勇里 <yongli.zzp@antgroup.com>
2023-01-12 10:28:43 +0800

    fix-IDEA2022.3-gradle-runIde
    Signed-off-by: 勇里 <yongli.zzp@antgroup.com>

--- components/ide/jetbrains/gateway-plugin/build.gradle.kts
+++ components/ide/jetbrains/gateway-plugin/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     // Kotlin support - check the latest version at https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.11.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.1.2"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
