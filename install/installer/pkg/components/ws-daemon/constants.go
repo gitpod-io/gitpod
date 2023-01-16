77515c0e8818683717ff4223a53a4157e79e9187
Thomas Schubart <thomas@gitpod.io>
2023-01-16 10:56:49 +0000

    [installer] Fix content init

--- install/installer/pkg/components/ws-daemon/constants.go
+++ install/installer/pkg/components/ws-daemon/constants.go
@@ -7,12 +7,14 @@ package wsdaemon
 import "github.com/gitpod-io/gitpod/common-go/baseserver"
 
 const (
-	Component            = "ws-daemon"
-	ServicePort          = 8080
-	HostWorkingArea      = "/var/gitpod/workspaces"
-	ContainerWorkingArea = "/mnt/workingarea"
-	HostBackupPath       = "/var/gitpod/tmp/backup"
-	TLSSecretName        = "ws-daemon-tls"
-	VolumeTLSCerts       = "ws-daemon-tls-certs"
-	ReadinessPort        = baseserver.BuiltinHealthPort
+	Component               = "ws-daemon"
+	ServicePort             = 8080
+	HostWorkingArea         = "/var/gitpod/workspaces"
+	HostWorkingAreaMk2      = "/var/gitpod/workspaces-mk2"
+	ContainerWorkingArea    = "/mnt/workingarea"
+	ContainerWorkingAreaMk2 = "/mnt/workingarea-mk2"
+	HostBackupPath          = "/var/gitpod/tmp/backup"
+	TLSSecretName           = "ws-daemon-tls"
+	VolumeTLSCerts          = "ws-daemon-tls-certs"
+	ReadinessPort           = baseserver.BuiltinHealthPort
 )
