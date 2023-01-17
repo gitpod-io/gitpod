511edf803453383b568b2e37dd1190b15d66650a
AXON <axonasif@gmail.com>
2023-01-17 12:22:58 +0000

    Revert "feat: Enable and autoinstall shell completions for gp CLI"
    This reverts commit 0e36968b02778af78b4a4a609349d4f39a135527.

--- components/gitpod-cli/cmd/completion.go
+++ components/gitpod-cli/cmd/completion.go
@@ -5,8 +5,6 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -14,55 +12,21 @@ import (
 
 // completionCmd represents the completion command
 var completionCmd = &cobra.Command{
-	Use:   "completion",
-	Short: "Generates shell completion",
-	Long: fmt.Sprintf(`Bash:
+	Use:    "completion",
+	Hidden: true,
+	Short:  "Generates bash completion scripts",
+	Long: `To load completion run
 
-	$ source <(%[1]s completion bash)
+. <(gp completion)
 
-	# To load completions for each session, execute once:
-	$ %[1]s completion bash | sudo tee /etc/bash_completion.d/%[1]s
+To configure your bash shell to load completions for each session add to your bashrc
 
-fish:
-
-	$ %[1]s completion fish | source
-
-	# To load completions for each session, execute once:
-	$ mkdir -p ~/.config/fish/completions && %[1]s completion fish > ~/.config/fish/completions/%[1]s.fish
-
-Zsh:
-
-	# If shell completion is not already enabled in your environment,
-	# you will need to enable it.  You can execute the following once:
-	$ echo "autoload -U compinit; compinit" >> ~/.zshrc
-
-	# To load completions for each session, execute once:
-	$ echo "%[1]s completion zsh" | sudo tee "${fpath[1]}/_%[1]s"
-
-	# You will need to start a new shell for this setup to take effect.
-`, rootCmd.Root().Name()),
-	ValidArgs: []string{"bash", "fish", "zsh"},
-	Args:      cobra.ExactValidArgs(1),
+# ~/.bashrc or ~/.profile
+. <(gp completion)
+`,
 	Run: func(cmd *cobra.Command, args []string) {
-		switch args[0] {
-		case "bash":
-			if err := cmd.Root().GenBashCompletion(os.Stdout); err != nil {
-				err_log("bash")
-			}
-		case "fish":
-			if err := cmd.Root().GenFishCompletion(os.Stdout, true); err != nil {
-				err_log("fish")
-			}
-		case "zsh":
-			if err := cmd.Root().GenZshCompletion(os.Stdout); err != nil {
-				err_log("zsh")
-			}
-		}
+		rootCmd.GenBashCompletion(os.Stdout)
 	},
-}
-
-func err_log(shell string) {
-	log.Fatalf("Failed to generate completion for %s", shell)
 }
 
 func init() {
