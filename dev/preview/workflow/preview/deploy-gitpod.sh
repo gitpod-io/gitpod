8decb6fd8e0cc76f1f4f39b055c8be73f18d7310
Mads Hartmann <mads379@gmail.com>
2023-01-18 14:15:15 +0000

    Rename OpenFGA to SpiceDB

--- dev/preview/workflow/preview/deploy-gitpod.sh
+++ dev/preview/workflow/preview/deploy-gitpod.sh
@@ -477,9 +477,9 @@ yq w -i "${INSTALLER_CONFIG_PATH}" "experimental.webapp.server.stripeConfig" "st
 #
 
 #
-# OpenFGA
+# SpiceDB
 #
-yq w -i "${INSTALLER_CONFIG_PATH}" experimental.webapp.openfga.enabled "true"
+yq w -i "${INSTALLER_CONFIG_PATH}" experimental.webapp.spicedb.enabled "true"
 
 # copy secret from werft's space
 kubectl --kubeconfig "${DEV_KUBE_PATH}" --context "${DEV_KUBE_CONTEXT}" -n werft get secret preview-envs-oidc-clients-config-secret -o yaml > preview-envs-oidc-clients-config-secret.secret.yaml
