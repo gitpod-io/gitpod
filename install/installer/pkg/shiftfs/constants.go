9e173ca7a47d9768f3ce708d3aaa2c90da7d7703
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2023-01-17 21:50:02 -0300

    Remove shiftfs module loader

--- install/installer/pkg/shiftfs/constants.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
-/// Licensed under the GNU Affero General Public License (AGPL).
-// See License.AGPL.txt in the project root for license information.
-
-package shiftfs
-
-const (
-	Component = "shiftfs-module-loader"
-)
