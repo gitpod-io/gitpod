faca22485658ed3e9996929caa97c0eb3e386afa
Sven Efftinge <sven@gitpod.io>
2023-01-19 08:48:58 +0000

    [dashboard] never redirect to projects from /

--- components/dashboard/src/hooks/use-user-and-teams-loader.ts
+++ components/dashboard/src/hooks/use-user-and-teams-loader.ts
@@ -6,21 +6,18 @@
 
 import { useState, useContext, useEffect } from "react";
 import { User } from "@gitpod/gitpod-protocol";
-import { useHistory } from "react-router-dom";
 import { UserContext } from "../user-context";
-import { getSelectedTeamSlug, TeamsContext } from "../teams/teams-context";
+import { TeamsContext } from "../teams/teams-context";
 import { getGitpodService } from "../service/service";
 import { publicApiTeamsToProtocol, teamsService } from "../service/public-api";
 import { ErrorCodes } from "@gitpod/gitpod-protocol/lib/messaging/error";
 import { trackLocation } from "../Analytics";
 import { refreshSearchData } from "../components/RepositoryFinder";
-import { getURLHash } from "../utils";
 
 export const useUserAndTeamsLoader = () => {
     const [loading, setLoading] = useState<boolean>(true);
     const { user, setUser, refreshUserBillingMode } = useContext(UserContext);
     const { teams, setTeams } = useContext(TeamsContext);
-    const history = useHistory();
     const [isSetupRequired, setSetupRequired] = useState(false);
 
     useEffect(() => {
@@ -31,24 +28,7 @@ export const useUserAndTeamsLoader = () => {
                 setUser(loggedInUser);
                 refreshSearchData();
 
-                // TODO: atm this feature-flag won't have been set yet, as it's dependant on user/teams
-                // so it will always be false when this runs
                 const loadedTeams = publicApiTeamsToProtocol((await teamsService.listTeams({})).teams);
-
-                {
-                    // if a team was selected previously and we call the root URL (e.g. "gitpod.io"),
-                    // let's continue with the team page
-                    const hash = getURLHash();
-                    const isRoot = window.location.pathname === "/" && hash === "";
-                    if (isRoot) {
-                        try {
-                            const teamSlug = getSelectedTeamSlug();
-                            if (loadedTeams.some((t) => t.slug === teamSlug)) {
-                                history.push(`/t/${teamSlug}`);
-                            }
-                        } catch {}
-                    }
-                }
                 setTeams(loadedTeams);
             } catch (error) {
                 console.error(error);
