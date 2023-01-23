1d0740e85e46d5ebf565f4cfc08df3a598536adb
Mads Hartmann <mads379@gmail.com>
2023-01-23 14:32:41 +0000

    Download previewctl from image instead of building it

--- .github/actions/deploy-gitpod/entrypoint.sh
+++ .github/actions/deploy-gitpod/entrypoint.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 
 export HOME=/home/gitpod
 export PREVIEW_ENV_DEV_SA_KEY_PATH="$HOME/.config/gcloud/preview-environment-dev-sa.json"
-
+# shellcheck disable=SC2155
+export LEEWAY_WORKSPACE_ROOT="$(pwd)"
 export VERSION="${INPUT_VERSION}"
+export PATH="$PATH:$HOME/bin"
 
 mkdir $HOME/bin
-export PATH="$PATH:$HOME/bin"
 
 echo "Downloading installer for ${VERSION}"
 oci-tool fetch file -o $HOME/bin/installer --platform=linux-amd64 "eu.gcr.io/gitpod-core-dev/build/installer:${VERSION}" app/installer
@@ -20,11 +21,7 @@ oci-tool fetch file -o /tmp/versions.yaml --platform=linux-amd64 "eu.gcr.io/gitp
 echo "${INPUT_SA_KEY}" > "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 gcloud auth activate-service-account --key-file "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 
-# Hack alert: We're building previewctl here until we decide how to properly distribute internal tools
-# Also, LEEWAY_WORKSPACE_ROOT is set to /workspace/gitpod in our dev image, but that's not the path GH actions use
-# shellcheck disable=SC2155
-export LEEWAY_WORKSPACE_ROOT="$(pwd)"
-leeway run dev/preview/previewctl:install --dont-test
+leeway run dev/preview/previewctl:download
 
 echo "Setting up access to core-dev and harvester"
 previewctl get-credentials --gcp-service-account "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
