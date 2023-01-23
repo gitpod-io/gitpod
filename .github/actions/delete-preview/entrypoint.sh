464f37778ffa3384568d77d620e5574a9af7cfd6
Mads Hartmann <mads379@gmail.com>
2023-01-23 14:32:41 +0000

    Download previewctl from image instead of building it

--- .github/actions/delete-preview/entrypoint.sh
+++ .github/actions/delete-preview/entrypoint.sh
@@ -4,17 +4,16 @@ set -euo pipefail
 
 export HOME=/home/gitpod
 export PREVIEW_ENV_DEV_SA_KEY_PATH="$HOME/.config/gcloud/preview-environment-dev-sa.json"
+export PATH="$PATH:$HOME/bin"
+
+mkdir $HOME/bin
 
 echo "${INPUT_SA_KEY}" > "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 gcloud auth activate-service-account --key-file "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 
-# Hack alert: We're building previewctl here until we decide how to properly distribute internal tools
-# Also, LEEWAY_WORKSPACE_ROOT is set to /workspace/gitpod in our dev image, but that's not the path GH actions use
-# shellcheck disable=SC2155
-export LEEWAY_WORKSPACE_ROOT="$(pwd)"
-leeway run dev/preview/previewctl:install --dont-test
+leeway run dev/preview/previewctl:download
 
-/workspace/bin/previewctl get-credentials --gcp-service-account "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
+previewctl get-credentials --gcp-service-account "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 
 export TF_INPUT=0
 export TF_IN_AUTOMATION=true
