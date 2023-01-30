6b89711e290b41784e90493a2f9ef671561182c1
Mads Hartmann <mads379@gmail.com>
2023-01-30 14:59:24 +0000

    Spin up and delete preview env in regression check

--- .github/actions/preview-create/entrypoint.sh
+++ .github/actions/preview-create/entrypoint.sh
@@ -6,8 +6,6 @@ export HOME=/home/gitpod
 export PREVIEW_ENV_DEV_SA_KEY_PATH="$HOME/.config/gcloud/preview-environment-dev-sa.json"
 # shellcheck disable=SC2155
 export LEEWAY_WORKSPACE_ROOT="$(pwd)"
-export TF_INPUT=0
-export TF_IN_AUTOMATION=true
 export PATH="$PATH:$HOME/bin"
 
 mkdir $HOME/bin
@@ -18,4 +16,9 @@ gcloud auth activate-service-account --key-file "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 leeway run dev/preview/previewctl:download
 previewctl get-credentials --gcp-service-account "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 
+TF_VAR_preview_name="$(previewctl get-name --branch "${INPUT_NAME}")"
+export TF_VAR_preview_name
+export TF_VAR_infra_provider="${INPUT_INFRASTRUCTURE_PROVIDER}"
+export TF_INPUT=0
+export TF_IN_AUTOMATION=true
 leeway run dev/preview:create-preview
