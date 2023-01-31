86024e436ed360963fafab594b61144c1ad3e5a9
Milan Pavlik <milan@gitpod.io>
2023-01-31 11:10:33 +0000

    [server] Add config for interacting with spicedb

--- install/installer/pkg/components/slowserver/deployment.go
+++ install/installer/pkg/components/slowserver/deployment.go
@@ -14,6 +14,7 @@ import (
 	"github.com/gitpod-io/gitpod/installer/pkg/cluster"
 	contentservice "github.com/gitpod-io/gitpod/installer/pkg/components/content-service"
 	"github.com/gitpod-io/gitpod/installer/pkg/components/server"
+	"github.com/gitpod-io/gitpod/installer/pkg/components/spicedb"
 	"github.com/gitpod-io/gitpod/installer/pkg/components/usage"
 	wsmanager "github.com/gitpod-io/gitpod/installer/pkg/components/ws-manager"
 
@@ -84,6 +85,7 @@ func deployment(ctx *common.RenderContext) ([]runtime.Object, error) {
 		common.AnalyticsEnv(&ctx.Config),
 		common.MessageBusEnv(&ctx.Config),
 		common.ConfigcatEnv(ctx),
+		spicedb.Env(ctx),
 		[]corev1.EnvVar{
 			{
 				Name:  "CONFIG_PATH",
