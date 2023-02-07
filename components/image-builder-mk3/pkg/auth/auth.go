d9ea2c8c8ffdf78ab1baba2ed2d07fa3a5624263
Kyle Brennan <kyle@gitpod.io>
2023-02-07 21:00:58 +0000

    [image-builder-mk3] log registry not allowed as a warning
    This happens often, at a high rate, making it harder to find errors.
    
    Ref: https://cloudlogging.app.goo.gl/U6jHijAXqzUbcMes5

--- components/image-builder-mk3/pkg/auth/auth.go
+++ components/image-builder-mk3/pkg/auth/auth.go
@@ -233,7 +233,7 @@ func (a AllowedAuthFor) GetAuthFor(auth RegistryAuthenticator, refstr string) (r
 		}
 	}
 	if !regAllowed {
-		log.WithField("reg", reg).WithField("ref", ref).WithField("a", a).Error("registry not allowed - you may want to add this to the list of allowed registries in your installation config")
+		log.WithField("reg", reg).WithField("ref", ref).WithField("a", a).Warn("registry not allowed - you may want to add this to the list of allowed registries in your installation config")
 		return nil, nil
 	}
 
