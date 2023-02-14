1d0928e0f6009481170f728e3ece9d8efd3a8039
Filip Troníček <filip@gitpod.io>
2023-02-14 08:48:55 +0000

    add template from metric provider

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodTimeoutControlCenterTabProvider.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodTimeoutControlCenterTabProvider.kt
@@ -1,31 +1,121 @@
-// Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
 // Licensed under the GNU Affero General Public License (AGPL).
 // See License.AGPL.txt in the project root for license information.
 
 package io.gitpod.jetbrains.remote
 
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.components.service
-import io.gitpod.jetbrains.remote.GitpodManager
-import org.apache.http.client.utils.URIBuilder
+import com.jetbrains.ide.model.uiautomation.BeControl
+import com.jetbrains.rd.ui.bedsl.dsl.*
+import com.jetbrains.rd.util.lifetime.Lifetime
+import com.jetbrains.rd.util.reactive.Property
+import com.jetbrains.rdserver.diagnostics.BackendDiagnosticsService
+import com.jetbrains.rdserver.unattendedHost.customization.controlCenter.GatewayControlCenterTabProvider
+import com.jetbrains.rdserver.unattendedHost.customization.controlCenter.performance.createProgressBar
+import com.jetbrains.rdserver.unattendedHost.customization.controlCenter.performance.createProgressRow
 
 class GitpodTimeoutControlCenterTabProvider : GatewayControlCenterTabProvider {
-    override val id = "gitpodTimeoutTab"
-    override val title = "Timeout"
-
+    override val title: String = "Timeout"
+    override val id: String = "gitpodTimeoutTab"
     override fun getControl(lifetime: Lifetime): BeControl {
-        return scrollablePanel(lifetime) {
-            verticalLayout {
-                label("Gitpod Tab")
-                label("Current workspace timeout: 30 minutes")
-                button("Extend workspace timeout") {
-                    action {
-                        actionManager.tryToExecute("io.gitpod.jetbrains.remote.actions.ExtendWorkspaceTimeoutAction", null, null, null, true)
+        val backendDiagnosticsService = BackendDiagnosticsService.Companion.getInstance()
+
+        return verticalGrid {
+            row {
+                horizontalGrid {
+                    column {
+                        label("Workspace")
                     }
                 }
             }
+            createWorkspaceHeaderRow(this, backendDiagnosticsService, lifetime)
+            row {
+                verticalGrid {
+                    createCpuControl(this, backendDiagnosticsService, lifetime)
+                    createMemoryControl(this, backendDiagnosticsService, lifetime)
+                }.withMargin { margin(0, 15, 0, 25) }
+            }
+            row {
+                horizontalGrid {
+                    column {
+                        label("Shared Node Resources")
+                    }
+                }.withMargin { margin(0, 0, 0, 15) }.withHelpTooltip("Shared Node Resources", "The shared metrics represent the used and available resources of the cluster node on which your workspace is running")
+            }
         }
+    }
+
+    private fun createWorkspaceHeaderRow(ctx: VerticalGridBuilder, backendDiagnosticsService: BackendDiagnosticsService, lifetime: Lifetime) {
+        val labelProperty = Property("")
+
+        val workspaceClassMetric = backendDiagnosticsService.getMetric("gitpod_workspace_class")
+        val workspaceClass = workspaceClassMetric.toString()
+
+        fun updateLabel() {
+            if (workspaceClass != "") {
+                labelProperty.set(workspaceClass)
+            }
+        }
+        updateLabel()
+
+        workspaceClassMetric.valueProperty.change.advise(lifetime) {
+            updateLabel()
+        }
+
+        if (workspaceClass == "") {
+            return
+        }
+
+        return ctx.row {
+            horizontalGrid {
+                column {
+                    label(workspaceClass)
+                }
+            }.withMargin { margin(0, 15, 0, 0) }
+        }
+    }
+
+    private fun createCpuControl(ctx: VerticalGridBuilder, backendDiagnosticsService: BackendDiagnosticsService, lifetime: Lifetime) {
+        val cpuUsed = backendDiagnosticsService.getMetric("gitpod_workspace_cpu_used")
+        val cpuTotal = backendDiagnosticsService.getMetric("gitpod_workspace_cpu_total")
+        val cpuPercentage = backendDiagnosticsService.getMetric("gitpod_workspace_cpu_percentage")
+        val cpuPercentageProperty = Property("$cpuPercentage %")
+        val label = "CPU"
+        val progressBar = createProgressBar(lifetime, cpuPercentage.valueProperty, cpuPercentageProperty)
+        val labelProperty = Property("")
+
+        fun updateLabel() {
+            labelProperty.set("${cpuUsed}m / ${cpuTotal}m")
+        }
+        updateLabel()
+        cpuUsed.valueProperty.change.advise(lifetime) {
+            updateLabel()
+        }
+        cpuTotal.valueProperty.change.advise(lifetime) {
+            updateLabel()
+        }
+        createProgressRow(ctx, lifetime, label, cpuPercentage.statusProperty, labelProperty, cpuPercentageProperty, progressBar)
+    }
+
+    private fun createMemoryControl(ctx: VerticalGridBuilder, backendDiagnosticsService: BackendDiagnosticsService, lifetime: Lifetime) {
+        val memoryUsed = backendDiagnosticsService.getMetric("gitpod_workspace_memory_used")
+        val memoryTotal = backendDiagnosticsService.getMetric("gitpod_workspace_memory_total")
+        val memoryPercentage = backendDiagnosticsService.getMetric("gitpod_workspace_memory_percentage")
+        val memoryPercentageProperty = Property("$memoryPercentage %")
+        val label = "Memory"
+        val progressBar = createProgressBar(lifetime, memoryPercentage.valueProperty, memoryPercentageProperty)
+        val labelProperty = Property("")
+
+        fun updateLabel() {
+            labelProperty.set("${memoryUsed}GB / ${memoryTotal}GB")
+        }
+        updateLabel()
+        memoryUsed.valueProperty.change.advise(lifetime) {
+            updateLabel()
+        }
+        memoryTotal.valueProperty.change.advise(lifetime) {
+            updateLabel()
+        }
+
+        createProgressRow(ctx, lifetime, label, memoryPercentage.statusProperty, labelProperty, memoryPercentageProperty, progressBar)
     }
 }
