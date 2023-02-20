59b51a5b10cccd039a509a635236ca2c64f73a3e
Kyle Brennan <kyle@gitpod.io>
2023-02-20 17:45:21 +0000

    [docker-up] update Docker Compose to 2.10.0-gitpod.1
    Context: https://github.com/gitpod-io/gitpod/pull/16368#issuecomment-1437114679
    
    Related Docker Compose release: https://github.com/gitpod-io/compose/releases/tag/v2.10.0-gitpod.1
    
    Also, refactored source of truth for Docker Compose version to WORKSPACE.yaml for consistency and consideration when changing Docker version

--- components/docker-up/dependencies.sh
+++ components/docker-up/dependencies.sh
@@ -5,9 +5,12 @@
 
 set -euo pipefail
 
-DOCKER_COMPOSE_VERSION=2.8.0-gitpod.0
 RUNC_VERSION=v1.1.4
 
+# DOCKER_VERSION and DOCKER_COMPOSE_VERSION are defined in WORKSPACE.yaml
 curl -o docker.tgz      -fsSL "https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz"
-curl -o docker-compose  -fsSL "https://github.com/gitpod-io/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64"
+# Docker Compose is forked, we have to override the MTU
+curl -o docker-compose  -fsSL "https://github.com/gitpod-io/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64"
+curl -o checksums.txt  -fsSL "https://github.com/gitpod-io/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/checksums.txt"
+
 curl -o runc            -fsSL "https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.amd64"
