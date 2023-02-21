b21e2f276aee6b3b6ac81bf3fc97f0918851a58f
Gero Posmyk-Leinemann <gero@gitpod.io>
2023-02-21 08:19:55 +0000

    [server] Create nudges for all Chargebee Personal/Team Subscription/Team Subscription 2 owners

--- components/gitpod-protocol/src/gitpod-service.ts
+++ components/gitpod-protocol/src/gitpod-service.ts
@@ -320,6 +320,7 @@ export interface AppNotification {
         url: string;
         label: string;
     };
+    notClosable?: boolean;
 }
 export namespace AppNotification {
     export function is(data: any): data is AppNotification {
