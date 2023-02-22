74263bc8fc7a7ed11eeca39603dffe1fb33cc7ba
Brad Harris <bmharris@gmail.com>
2023-02-22 04:33:39 +0000

    removing old code

--- components/dashboard/src/onboarding/UserOnboarding.tsx
+++ components/dashboard/src/onboarding/UserOnboarding.tsx
@@ -27,24 +27,6 @@ const UserOnboarding: FunctionComponent<Props> = ({ user }) => {
     const [step, setStep] = useState(STEPS.ONE);
     // TODO: Remove this once current user is behind react-query
     const { setUser } = useContext(UserContext);
-    // const [profile, dispatch] = useReducer(
-    //     (state: OnboardingProfileDetails, action: Partial<OnboardingProfileDetails>) => {
-    //         return {
-    //             ...state,
-    //             ...action,
-    //         };
-    //     },
-    //     getInitialProfileState(user),
-    // );
-
-    // const handleSubmit = useCallback(() => {
-    //     console.log("Saving profile updates", profile);
-    //     // TODO: add api calls to update profile
-
-    //     // For now, send to workspaces once done
-    //     // This will get updated though so we don't need to redirect and can leave them on their current page once complete
-    //     history.push("/workspaces");
-    // }, [history, profile]);
 
     return (
         <div className="container">
@@ -54,13 +36,12 @@ const UserOnboarding: FunctionComponent<Props> = ({ user }) => {
                 </div>
                 <Separator />
                 <div className="mt-24">
-                    {step === STEPS.TWO && (
+                    {step === STEPS.ONE && (
                         <StepUserInfo
                             user={user}
                             onComplete={(updatedUser) => {
                                 setUser(updatedUser);
                                 setStep(STEPS.TWO);
-                                // history.push("/workspaces");
                             }}
                         />
                     )}
@@ -70,63 +51,22 @@ const UserOnboarding: FunctionComponent<Props> = ({ user }) => {
                             onComplete={(updatedUser) => {
                                 setUser(updatedUser);
                                 setStep(STEPS.THREE);
-                                history.push("/workspaces");
                             }}
                         />
                     )}
-                    {step === STEPS.ONE && (
+                    {step === STEPS.THREE && (
                         <StepPersonalize
                             user={user}
                             onComplete={(updatedUser) => {
                                 setUser(updatedUser);
-                                // setStep(STEPS.THREE);
+                                // TODO: should be able to remove this once state that shows this flow is updated
                                 history.push("/workspaces");
                             }}
                         />
                     )}
-
-                    {/* {user.avatarUrl && (
-                        <div className="mt-4">
-                            <img
-                                className="rounded-full w-24 h-24"
-                                src={user.avatarUrl}
-                                alt={user.fullName || user.name}
-                            />
-                        </div>
-                    )}
-                    <OnboardingForm profile={profile} onUpdate={dispatch} onSubmit={handleSubmit} /> */}
                 </div>
             </div>
         </div>
     );
 };
 export default UserOnboarding;
-
-// const getInitialNameParts = (user: User) => {
-//     const name = user.fullName || user.name || "";
-//     let firstName = name;
-//     let lastName = "";
-
-//     const parts = name.split(" ");
-//     if (parts.length > 1) {
-//         firstName = parts.shift() || "";
-//         lastName = parts.join(" ");
-//     }
-
-//     return { firstName, lastName };
-// };
-
-// const getInitialProfileState = (user: User): OnboardingProfileDetails => {
-//     const { firstName, lastName } = getInitialNameParts(user);
-
-//     return {
-//         firstName,
-//         lastName,
-//         emailAddress: user.additionalData?.profile?.emailAddress ?? "",
-//         companyWebsite: user.additionalData?.profile?.companyWebsite ?? "",
-//         jobRole: user.additionalData?.profile?.jobRole ?? "",
-//         jobRoleOther: user.additionalData?.profile?.jobRoleOther ?? "",
-//         signupGoals: user.additionalData?.profile?.signupGoals || "",
-//         signupGoalsOther: user.additionalData?.profile?.signupGoalsOther ?? "",
-//     };
-// };
