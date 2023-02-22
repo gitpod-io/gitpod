cec5b963f025cede47154153c0642cc481417f92
Thomas Schubart <shater@gmx.net>
2023-02-22 21:09:58 +0000

    [installer] Setup seperate image builder deployment for mk2

---
+++ install/installer/pkg/components/image-builder-mk3-wsman/constants.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License.AGPL.txt in the project root for license information.
+
+package image_builder_mk3_wsman
+
+import "github.com/gitpod-io/gitpod/installer/pkg/common"
+
+const (
+	BuilderImage   = "image-builder-mk3/bob"
+	Component      = "image-builder-mk3-wsman"
+	RPCPort        = common.ImageBuilderRPCPort
+	RPCPortName    = "service"
+	TLSSecretName  = common.ImageBuilderTLSSecret
+	VolumeTLSCerts = common.ImageBuilderVolumeTLSCerts
+)
