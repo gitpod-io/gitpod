4d6ebc20f3c102270be33fabfc41421198eb1c2c
Milan Pavlik <milan@gitpod.io>
2023-03-01 22:12:29 +0000

    fix

--- components/public-api/generate.sh
+++ components/public-api/generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -n "$DEBUG" ]; then
+# if [ -n "$DEBUG" ]; then
   set -x
-fi
+# fi
 
 set -o errexit
 set -o nounset
@@ -14,12 +14,18 @@ ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/../../
 # shellcheck disable=SC1090,SC1091
 source "$ROOT_DIR"/scripts/protoc-generator.sh
 
+pushd "go"
+  go install github.com/gitpod-io/gitpod/components/public-api/go/protoc-server-proxy-gen
+popd
+
 install_dependencies
 
 lint
 
 # Run breaking change detector
 buf breaking --against "https://github.com/gitpod-io/gitpod.git#branch=main,subdir=components/public-api"
+
+rm -rf go/experimental/v1
 
 protoc_buf_generate
 
