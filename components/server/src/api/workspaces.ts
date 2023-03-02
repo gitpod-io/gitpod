ee8b928942ce945b5d6642796779a16fd84f0e49
Milan Pavlik <milan@gitpod.io>
2023-03-02 20:50:52 +0000

    Fix

--- components/server/src/api/workspaces.ts
+++ components/server/src/api/workspaces.ts
@@ -11,4 +11,40 @@ export class WorkspacesService implements ServiceImpl<typeof experimental.Worksp
     async createWorkspace(req: experimental.CreateWorkspaceRequest): Promise<experimental.CreateWorkspaceResponse> {
         throw new ConnectError("unimplemented", Code.Unimplemented);
     }
+
+    async listWorkspaces(req: experimental.ListWorkspacesRequest): Promise<experimental.ListWorkspacesResponse> {
+        throw new ConnectError("unimplemented", Code.Unimplemented);
+    }
+
+    async getWorkspace(req: experimental.GetWorkspaceRequest): Promise<experimental.GetWorkspaceResponse> {
+        throw new ConnectError("unimplemented", Code.Unimplemented);
+    }
+
+    async *streamWorkspaceStatus(req: experimental.StreamWorkspaceStatusRequest) {
+        // yield { sentence: `Hi ${req.name}, I'm eliza` };
+        // yield { sentence: `How are you feeling today?` };
+        throw new ConnectError("unimplemented", Code.Unimplemented);
+    }
+
+    async getOwnerToken(req: experimental.GetOwnerTokenRequest): Promise<experimental.GetOwnerTokenResponse> {
+        throw new ConnectError("unimplemented", Code.Unimplemented);
+    }
+
+    async createAndStartWorkspace(
+        req: experimental.CreateAndStartWorkspaceRequest,
+    ): Promise<experimental.CreateAndStartWorkspaceResponse> {
+        throw new ConnectError("unimplemented", Code.Unimplemented);
+    }
+
+    async stopWorkspace(req: experimental.StopWorkspaceRequest): Promise<experimental.StopWorkspaceResponse> {
+        throw new ConnectError("unimplemented", Code.Unimplemented);
+    }
+
+    async deleteWorkspace(req: experimental.DeleteWorkspaceRequest): Promise<experimental.DeleteWorkspaceResponse> {
+        throw new ConnectError("unimplemented", Code.Unimplemented);
+    }
+
+    async updatePort(req: experimental.UpdatePortRequest): Promise<experimental.UpdatePortResponse> {
+        throw new ConnectError("unimplemented", Code.Unimplemented);
+    }
 }
