b634c16c7419021a5379986e808ce1dd687dcce4
Milan Pavlik <milan@gitpod.io>
2023-03-02 20:30:48 +0000

    Fix

--- components/server/src/api/routing.ts
+++ components/server/src/api/routing.ts
@@ -3,17 +3,3 @@
  * Licensed under the GNU Affero General Public License (AGPL).
  * See License.AGPL.txt in the project root for license information.
  */
-
-import { ConnectRouter } from "@bufbuild/connect";
-import { experimental } from "@gitpod/public-api";
-
-export default (router: ConnectRouter) =>
-    // registers buf.connect.demo.eliza.v1.ElizaService
-    router.service(experimental.WorkspacesService, {
-        // implements rpc Say
-        async say(req) {
-            return {
-                sentence: `You said: ${req.sentence}`,
-            };
-        },
-    });
