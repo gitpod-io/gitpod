32adcb9772144e6d55b80865d22a51e65dfbf2be
Filip Troníček <filip@gitpod.io>
2023-03-02 15:24:00 +0000

    Update VS Code Browser to `1.76` (#16590)
    * Update VS Code Browser to `1.76`
    
    * also change `codeVersion`
    
    * Update commit
    
    * Update commit
    
    * Update commit
    
    * Update VS Codde Browser stable
    
    ---------
    
    Co-authored-by: Jean Pierre <jeanp413@hotmail.com>

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-b83d82b9ee293b0559f11d09977d0e8e914d73f5" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-0f3f6b068fc5aba9abdf02af45a8cfda067cb6d0" // stable version that will be updated manually on demand
 	CodeHelperIDEImage          = "ide/code-codehelper"
 	CodeWebExtensionImage       = "ide/gitpod-code-web"
 	CodeWebExtensionVersion     = "commit-266edb36ec96f3f0613715c6967fd4591fdd0569" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
