6774bf3338c55551dacb66f37e141a16acd1c7fe
Milan Pavlik <milan@gitpod.io>
2023-03-06 09:27:03 +0100

    [public-api] Auto-generate proxy handler (#16626)
    * fix
    
    * fix
    
    * Fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Fix
    
    * Fix
    
    * Fix

--- components/public-api/generate.sh
+++ components/public-api/generate.sh
@@ -14,12 +14,19 @@ ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/../../
 # shellcheck disable=SC1090,SC1091
 source "$ROOT_DIR"/scripts/protoc-generator.sh
 
+pushd "go"
+  go install github.com/gitpod-io/gitpod/components/public-api/go/protoc-proxy-gen
+popd
+
 install_dependencies
 
 lint
 
 # Run breaking change detector
 buf breaking --against "https://github.com/gitpod-io/gitpod.git#branch=main,subdir=components/public-api"
+
+# Remove generated files, so they are re-created
+rm -rf go/experimental
 
 protoc_buf_generate
 
