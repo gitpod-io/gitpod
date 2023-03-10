8f29dcb1f35383db8700d5aa0d2f2da8896b3b8e
Aleksandar Aleksandrov <a.g.aleksandrov@gmail.com>
2023-03-10 16:39:55 +0000

    (╯°Д°)╯︵/(.□ . \)

--- .github/actions/deploy-monitoring-satellite/entrypoint.sh
+++ .github/actions/deploy-monitoring-satellite/entrypoint.sh
@@ -7,10 +7,11 @@ export PREVIEW_ENV_DEV_SA_KEY_PATH="$HOME/.config/gcloud/preview-environment-dev
 # shellcheck disable=SC2155
 export LEEWAY_WORKSPACE_ROOT="$(pwd)"
 export PATH="$PATH:$HOME/bin"
+export TEMPO_BASIC_AUTH_PASSWORD="${INPUT_TEMPO_BASIC_AUTH_PASSWORD:-""}"
 
 mkdir $HOME/bin
 
-echo "${INPUT_SA_KEY}" > "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
+echo "${INPUT_SA_KEY}" >"${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 gcloud auth activate-service-account --key-file "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 
 leeway run dev/preview/previewctl:download
@@ -25,8 +26,8 @@ echo "leeway run dev/preview:deploy-monitoring-satellite"
 leeway run dev/preview:deploy-monitoring-satellite
 
 {
-    echo '<p>Monitoring satellite has been installed in your preview environment.</p>'
-    echo '<ul>'
-    echo '<li><b>📚 Documentation</b> - See our <a href="https://www.notion.so/gitpod/f2938b2bcb0c4c8c99afe1d2b872380e" target="_blank">internal documentation</a> on how to use it.</li>'
-    echo '</ul>'
-} >> "${GITHUB_STEP_SUMMARY}"
+  echo '<p>Monitoring satellite has been installed in your preview environment.</p>'
+  echo '<ul>'
+  echo '<li><b>📚 Documentation</b> - See our <a href="https://www.notion.so/gitpod/f2938b2bcb0c4c8c99afe1d2b872380e" target="_blank">internal documentation</a> on how to use it.</li>'
+  echo '</ul>'
+} >>"${GITHUB_STEP_SUMMARY}"
