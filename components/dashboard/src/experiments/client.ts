c217402876334f49839744a6a09286e825e66823
Milan Pavlik <pavlik.mil@gmail.com>
2023-03-13 10:40:57 +0000

    [dashboard] Use proxy endpoint to fetch feature flags

--- components/dashboard/src/experiments/client.ts
+++ components/dashboard/src/experiments/client.ts
@@ -4,7 +4,6 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { newAlwaysReturningDefaultValueClient } from "@gitpod/gitpod-protocol/lib/experiments/always-default";
 import * as configcat from "configcat-js";
 import { ConfigCatClient } from "@gitpod/gitpod-protocol/lib/experiments/configcat";
 import { Client } from "@gitpod/gitpod-protocol/lib/experiments/types";
@@ -18,40 +17,17 @@ export function getExperimentsClient(): Client {
         return client;
     }
 
-    const host = window.location.hostname;
-    if (host === "gitpod.io") {
-        client = newProductionConfigCatClient();
-    } else if (host === "gitpod-staging.com" || host.endsWith("gitpod-dev.com") || host.endsWith("gitpod-io-dev.com")) {
-        client = newNonProductionConfigCatClient();
-    } else {
-        // We're gonna use a client which always returns the default value.
-        client = newAlwaysReturningDefaultValueClient();
-    }
-
+    client = newProxyConfigCatClient();
     return client;
 }
 
-// newProductionConfigCatClient constructs a new ConfigCat client with production configuration.
-function newProductionConfigCatClient(): Client {
-    // clientKey is an identifier of our ConfigCat application. It is not a secret.
-    const clientKey = "WBLaCPtkjkqKHlHedziE9g/TwAe6YyftEGPnGxVRXd0Ig";
+function newProxyConfigCatClient(): Client {
+    const clientKey = ""; // the client key is populated by the proxy
     const client = configcat.createClientWithLazyLoad(clientKey, {
         logger: configcat.createConsoleLogger(LogLevel.Error),
         cacheTimeToLiveSeconds: 60 * 3, // 3 minutes
         requestTimeoutMs: 1500,
-    });
-
-    return new ConfigCatClient(client);
-}
-
-// newNonProductionConfigCatClient constructs a new ConfigCat client with non-production configuration.
-function newNonProductionConfigCatClient(): Client {
-    // clientKey is an identifier of our ConfigCat application. It is not a secret.
-    const clientKey = "WBLaCPtkjkqKHlHedziE9g/LEAOCNkbuUKiqUZAcVg7dw";
-    const client = configcat.createClientWithLazyLoad(clientKey, {
-        logger: configcat.createConsoleLogger(LogLevel.Error),
-        cacheTimeToLiveSeconds: 60 * 3, // 3 minutes
-        requestTimeoutMs: 1500,
+        proxy: "/configcat",
     });
 
     return new ConfigCatClient(client);
