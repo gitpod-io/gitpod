3ba2dad2fdd27a03e263879d29fb269259eefac5
Filip Troníček <filip@gitpod.io>
2023-03-15 19:32:01 +0000

    Show JB beta notice

--- components/dashboard/src/user-settings/SelectIDE.tsx
+++ components/dashboard/src/user-settings/SelectIDE.tsx
@@ -10,6 +10,7 @@ import { UserContext } from "../user-context";
 import CheckBox from "../components/CheckBox";
 import { User } from "@gitpod/gitpod-protocol";
 import SelectIDEComponent from "../components/SelectIDEComponent";
+import PillLabel from "../components/PillLabel";
 
 export type IDEChangedTrackLocation = "workspace_list" | "workspace_start" | "preferences";
 interface SelectIDEProps {
@@ -70,6 +71,9 @@ export default function SelectIDE(props: SelectIDEProps) {
         setUseLatestVersion(value);
     };
 
+    //todo(ft): find a better way to group IDEs by vendor
+    const shouldShowJetbrainsNotice = !["code", "code-desktop"].includes(defaultIde); // a really hacky way to get just JetBrains IDEs
+
     return (
         <>
             <div className="w-112">
@@ -81,6 +85,27 @@ export default function SelectIDE(props: SelectIDEProps) {
                     useLatest={useLatestVersion}
                 />
             </div>
+
+            {shouldShowJetbrainsNotice && (
+                <p className="text-left w-full text-gray-400 dark:text-gray-500">
+                    <strong>JetBrains </strong> integration is currently in{" "}
+                    <PillLabel type="warn" className="font-semibold mt-2 ml-0 py-0.5 px-1 self-center">
+                        <a href="https://www.gitpod.io/docs/references/gitpod-releases">
+                            <span className="text-xs">Beta</span>
+                        </a>
+                    </PillLabel>
+                    &nbsp;&middot;&nbsp;
+                    <a
+                        href="https://github.com/gitpod-io/gitpod/issues/6576"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="gp-link"
+                    >
+                        Send feedback
+                    </a>
+                </p>
+            )}
+
             <CheckBox
                 title="Latest Release (Unstable)"
                 desc={
