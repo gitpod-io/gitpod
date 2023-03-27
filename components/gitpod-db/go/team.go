8d69099147c00bcfc2f222145158bcabb638b610
Alex Tugarev <alex@gitpod.io>
2023-03-27 10:49:35 +0000

    [gitpod-db] get team/org by slug

--- components/gitpod-db/go/team.go
+++ components/gitpod-db/go/team.go
@@ -5,9 +5,13 @@
 package db
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Team struct {
@@ -27,4 +31,47 @@ type Team struct {
 // TableName sets the insert table name for this struct type
 func (d *Team) TableName() string {
 	return "d_b_team"
+}
+
+func CreateTeam(ctx context.Context, conn *gorm.DB, newTeam Team) (Team, error) {
+	if newTeam.ID == uuid.Nil {
+		return Team{}, errors.New("id must be set")
+	}
+
+	if newTeam.Name == "" {
+		return Team{}, errors.New("name must be set")
+	}
+	if newTeam.Slug == "" {
+		return Team{}, errors.New("slug must be set")
+	}
+
+	tx := conn.
+		WithContext(ctx).
+		Create(&newTeam)
+	if tx.Error != nil {
+		return Team{}, fmt.Errorf("failed to create team: %w", tx.Error)
+	}
+
+	return newTeam, nil
+}
+
+func GetTeamBySlug(ctx context.Context, conn *gorm.DB, slug string) (Team, error) {
+	if slug == "" {
+		return Team{}, fmt.Errorf("Slug is required")
+	}
+
+	var team Team
+
+	tx := conn.WithContext(ctx).
+		Where("slug = ?", slug).
+		Find(&team)
+
+	if tx.Error != nil {
+		if errors.Is(tx.Error, gorm.ErrRecordNotFound) {
+			return Team{}, fmt.Errorf("Team with slug %s does not exist: %w", slug, ErrorNotFound)
+		}
+		return Team{}, fmt.Errorf("Failed to retrieve team: %v", tx.Error)
+	}
+
+	return team, nil
 }
