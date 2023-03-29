6288b460f5d9736e9098c0a10d28b3df1757a437
Brad Harris <bmharris@gmail.com>
2023-03-29 22:01:02 +0000

    handle api errors in error boundary

--- components/dashboard/src/App.tsx
+++ components/dashboard/src/App.tsx
@@ -8,28 +8,26 @@ import * as GitpodCookie from "@gitpod/gitpod-protocol/lib/util/gitpod-cookie";
 import React, { FC, Suspense } from "react";
 import { AppLoading } from "./app/AppLoading";
 import { AppRoutes } from "./app/AppRoutes";
-import { GitpodErrorBoundary } from "./components/ErrorBoundary";
+import { TopLevelErrorBoundary } from "./components/ErrorBoundary";
 import { useCurrentOrg } from "./data/organizations/orgs-query";
 import { useAnalyticsTracking } from "./hooks/use-analytics-tracking";
 import { useUserLoader } from "./hooks/use-user-loader";
 import { Login } from "./Login";
 import { isGitpodIo } from "./utils";
 
-const Setup = React.lazy(() => import(/* webpackPrefetch: true */ "./Setup"));
-
 // Wrap the App in an ErrorBoundary to catch User/Org loading errors
 // This will also catch any errors that happen to bubble all the way up to the top
 const AppWithErrorBoundary: FC = () => {
     return (
-        <GitpodErrorBoundary>
+        <TopLevelErrorBoundary>
             <App />
-        </GitpodErrorBoundary>
+        </TopLevelErrorBoundary>
     );
 };
 
 // Top level Dashboard App component
 const App: FC = () => {
-    const { user, isSetupRequired, loading } = useUserLoader();
+    const { user, loading } = useUserLoader();
     const currentOrgQuery = useCurrentOrg();
 
     // Setup analytics/tracking
@@ -37,15 +35,6 @@ const App: FC = () => {
 
     if (loading) {
         return <AppLoading />;
-    }
-
-    // This may be flagged true during user/teams loading
-    if (isSetupRequired) {
-        return (
-            <Suspense fallback={<AppLoading />}>
-                <Setup />
-            </Suspense>
-        );
     }
 
     // Redirects to www site if it's the root, no user, and no gp cookie present (has logged in recently)
--- components/dashboard/src/components/ErrorBoundary.tsx
+++ components/dashboard/src/components/ErrorBoundary.tsx
@@ -4,11 +4,26 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { FC } from "react";
+import { ErrorCodes } from "@gitpod/gitpod-protocol/lib/messaging/error";
+import { FC, lazy, Suspense } from "react";
 import { ErrorBoundary, FallbackProps, ErrorBoundaryProps } from "react-error-boundary";
+import { AppLoading } from "../app/AppLoading";
 import gitpodIcon from "../icons/gitpod.svg";
+import { Login } from "../Login";
 import { getGitpodService } from "../service/service";
 import { Heading1, Subheading } from "./typography/headings";
+
+const Setup = lazy(() => import(/* webpackPrefetch: true */ "../Setup"));
+
+// This is the outermost error boundary, which catches user and org loading errors
+// or others that we let bubble up to the top
+export const TopLevelErrorBoundary: FC = ({ children }) => {
+    return (
+        <ErrorBoundary FallbackComponent={TopLevelErrorFallback} onReset={handleReset} onError={handleError}>
+            {children}
+        </ErrorBoundary>
+    );
+};
 
 export const GitpodErrorBoundary: FC = ({ children }) => {
     return (
@@ -19,6 +34,27 @@ export const GitpodErrorBoundary: FC = ({ children }) => {
 };
 
 type CaughtError = Error & { code?: number };
+
+const TopLevelErrorFallback: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
+    // adjust typing, as we may have caught an api error here w/ a code property
+    const caughtError = error as CaughtError;
+
+    // Handle any expected errors here
+    if (caughtError.code === ErrorCodes.NOT_AUTHENTICATED) {
+        return <Login />;
+    }
+
+    if (caughtError.code === ErrorCodes.SETUP_REQUIRED) {
+        return (
+            <Suspense fallback={<AppLoading />}>
+                <Setup />
+            </Suspense>
+        );
+    }
+
+    // Otherwise fall back to default error view
+    return <DefaultErrorFallback error={error} resetErrorBoundary={resetErrorBoundary} />;
+};
 
 export const DefaultErrorFallback: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
     // adjust typing, as we may have caught an api error here w/ a code property
