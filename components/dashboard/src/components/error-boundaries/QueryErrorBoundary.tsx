9a4864463f5c40da672de8454672afdaa15d001b
Brad Harris <bmharris@gmail.com>
2023-03-30 17:03:07 +0000

    splitting out error boundary types

---
+++ components/dashboard/src/components/error-boundaries/QueryErrorBoundary.tsx
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License.AGPL.txt in the project root for license information.
+ */
+
+import * as GitpodCookie from "@gitpod/gitpod-protocol/lib/util/gitpod-cookie";
+import { ErrorCodes } from "@gitpod/gitpod-protocol/lib/messaging/error";
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { FC, lazy, Suspense } from "react";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+import { AppLoading } from "../../app/AppLoading";
+import { Login } from "../../Login";
+import { isGitpodIo } from "../../utils";
+import { CaughtError } from "./ReloadPageErrorBoundary";
+
+const Setup = lazy(() => import(/* webpackPrefetch: true */ "../../Setup"));
+
+// Error boundary intended to catch and handle expected errors from api calls
+export const QueryErrorBoundary: FC = ({ children }) => {
+    return (
+        <QueryErrorResetBoundary>
+            {({ reset }) => (
+                // Passing reset to onReset so any queries know to retry after boundary is reset
+                <ErrorBoundary FallbackComponent={ExpectedQueryErrorsFallback} onReset={reset}>
+                    {children}
+                </ErrorBoundary>
+            )}
+        </QueryErrorResetBoundary>
+    );
+};
+
+// This handles any expected errors, i.e. errors w/ a code that an api call produced
+const ExpectedQueryErrorsFallback: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
+    // adjust typing, as we may have caught an api error here w/ a code property
+    const caughtError = error as CaughtError;
+
+    // User needs to Login
+    if (caughtError.code === ErrorCodes.NOT_AUTHENTICATED) {
+        // Before we show a Login screen, check to see if we need to redirect to www site
+        // Redirects if it's the root, no user, and no gp cookie present (has logged in recently)
+        if (isGitpodIo() && window.location.pathname === "/" && window.location.hash === "") {
+            // If there's no gp cookie, bounce to www site
+            if (!GitpodCookie.isPresent(document.cookie)) {
+                window.location.href = `https://www.gitpod.io`;
+                return <div></div>;
+            }
+        }
+
+        return <Login onLoggedIn={resetErrorBoundary} />;
+    }
+
+    // Setup needed before proceeding
+    if (caughtError.code === ErrorCodes.SETUP_REQUIRED) {
+        return (
+            <Suspense fallback={<AppLoading />}>
+                <Setup onComplete={resetErrorBoundary} />
+            </Suspense>
+        );
+    }
+
+    // Otherwise throw the error for default error boundary to catch and handle
+    throw error;
+};
--- components/dashboard/src/components/error-boundaries/ReloadPageErrorBoundary.tsx
+++ components/dashboard/src/components/error-boundaries/ReloadPageErrorBoundary.tsx
@@ -4,65 +4,24 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { ErrorCodes } from "@gitpod/gitpod-protocol/lib/messaging/error";
-import { QueryErrorResetBoundary } from "@tanstack/react-query";
-import { FC, lazy, Suspense, useCallback } from "react";
+import { FC, useCallback } from "react";
 import { ErrorBoundary, FallbackProps, ErrorBoundaryProps } from "react-error-boundary";
-import { AppLoading } from "../app/AppLoading";
-import gitpodIcon from "../icons/gitpod.svg";
-import { Login } from "../Login";
-import { getGitpodService } from "../service/service";
-import { Heading1, Subheading } from "./typography/headings";
+import gitpodIcon from "../../icons/gitpod.svg";
+import { getGitpodService } from "../../service/service";
+import { Heading1, Subheading } from "../typography/headings";
 
-const Setup = lazy(() => import(/* webpackPrefetch: true */ "../Setup"));
+export type CaughtError = Error & { code?: number };
 
-// This is the outermost error boundary, which catches user and org loading errors
-// or others that we let bubble up to the top
-export const TopLevelErrorBoundary: FC = ({ children }) => {
+// Catches any unexpected errors w/ a UI to reload the page. Also reports errors to api
+export const ReloadPageErrorBoundary: FC = ({ children }) => {
     return (
-        <QueryErrorResetBoundary>
-            {({ reset }) => (
-                // Passing reset to onReset so any queries know to retry after boundary is reset
-                <ErrorBoundary FallbackComponent={TopLevelErrorFallback} onReset={reset} onError={handleError}>
-                    {children}
-                </ErrorBoundary>
-            )}
-        </QueryErrorResetBoundary>
-    );
-};
-
-export const GitpodErrorBoundary: FC = ({ children }) => {
-    return (
-        <ErrorBoundary FallbackComponent={DefaultErrorFallback} onError={handleError}>
+        <ErrorBoundary FallbackComponent={ReloadPageErrorFallback} onError={handleError}>
             {children}
         </ErrorBoundary>
     );
 };
 
-type CaughtError = Error & { code?: number };
-
-const TopLevelErrorFallback: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
-    // adjust typing, as we may have caught an api error here w/ a code property
-    const caughtError = error as CaughtError;
-
-    // Handle any expected errors here
-    if (caughtError.code === ErrorCodes.NOT_AUTHENTICATED) {
-        return <Login onLoggedIn={resetErrorBoundary} />;
-    }
-
-    if (caughtError.code === ErrorCodes.SETUP_REQUIRED) {
-        return (
-            <Suspense fallback={<AppLoading />}>
-                <Setup onComplete={resetErrorBoundary} />
-            </Suspense>
-        );
-    }
-
-    // Otherwise fall back to default error view
-    return <DefaultErrorFallback error={error} />;
-};
-
-export const DefaultErrorFallback: FC<Pick<FallbackProps, "error">> = ({ error }) => {
+export const ReloadPageErrorFallback: FC<Pick<FallbackProps, "error">> = ({ error }) => {
     // adjust typing, as we may have caught an api error here w/ a code property
     const caughtError = error as CaughtError;
 
@@ -91,7 +50,7 @@ export const DefaultErrorFallback: FC<Pick<FallbackProps, "error">> = ({ error }
             <div>
                 <button onClick={handleReset}>Reload</button>
             </div>
-            <div>
+            <div className="flex flex-col items-center space-y-2">
                 {caughtError.code && (
                     <span>
                         <strong>Code:</strong> {caughtError.code}
