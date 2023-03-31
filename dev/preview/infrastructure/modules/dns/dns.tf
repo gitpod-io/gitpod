7db3030f81984e91e5a6d708f3d6cf90c29dfb34
Huiwen <huiwen@gitpod.io>
2023-03-31 15:55:37 +0000

    [preview] add local ssh dns to preview env

--- dev/preview/infrastructure/modules/dns/dns.tf
+++ dev/preview/infrastructure/modules/dns/dns.tf
@@ -52,3 +52,25 @@ resource "google_dns_record_set" "root-wc-ws-dev-ssh" {
   managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
   rrdatas      = [var.workspace_ip]
 }
+
+resource "google_dns_record_set" "root-wc-local-ssh-a" {
+  provider = google
+
+  name = "*.lssh.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
+  type = "A"
+  ttl  = 86400 # 1day
+
+  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
+  rrdatas      = ["127.0.0.1"]
+}
+
+resource "google_dns_record_set" "root-wc-local-ssh-aaaa" {
+  provider = google
+
+  name = "*.lssh.${var.preview_name}.${data.google_dns_managed_zone.preview-gitpod-dev.dns_name}"
+  type = "AAAA"
+  ttl  = 86400 # 1day
+
+  managed_zone = data.google_dns_managed_zone.preview-gitpod-dev.name
+  rrdatas      = ["::1"]
+}
