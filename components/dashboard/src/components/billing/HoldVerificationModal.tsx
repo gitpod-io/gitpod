dfd751d8fa5ffe494b4c077e444e3760197d1b9b
Brad Harris <bmharris@gmail.com>
2023-04-14 22:11:22 +0000

    splitting up the billing related modals

---
+++ components/dashboard/src/components/billing/HoldVerificationModal.tsx
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License.AGPL.txt in the project root for license information.
+ */
+
+import { AttributionId } from "@gitpod/gitpod-protocol/lib/attribution";
+import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { FC, useCallback, useContext } from "react";
+import Modal, { ModalBody, ModalFooter, ModalFooterAlert } from "../Modal";
+import { ReactComponent as Spinner } from "../../icons/Spinner.svg";
+import { ThemeContext } from "../../theme-context";
+import { Heading2 } from "../typography/headings";
+import { getStripeAppearance } from "./BillingSetupModal";
+import { useStripePromise } from "./use-stripe-promise";
+import { Button } from "../Button";
+import { useMutation } from "@tanstack/react-query";
+
+type Props = {
+    attributionId: string;
+    holdPaymentIntentClientSecret: string;
+    onClose: () => void;
+};
+export const HoldVerificationModal: FC<Props> = ({ attributionId, holdPaymentIntentClientSecret, onClose }) => {
+    const { isDark } = useContext(ThemeContext);
+    const { stripePromise } = useStripePromise();
+
+    return (
+        <Modal visible={true} onClose={onClose}>
+            <Heading2 className="flex">Confirm Payment Method</Heading2>
+            {!stripePromise && (
+                <ModalBody>
+                    <div className="h-80 flex items-center justify-center">
+                        <Spinner className="h-5 w-5 animate-spin" />
+                    </div>
+                </ModalBody>
+            )}
+            {!!stripePromise && (
+                <Elements
+                    stripe={stripePromise}
+                    options={{
+                        appearance: getStripeAppearance(isDark),
+                        clientSecret: holdPaymentIntentClientSecret,
+                    }}
+                >
+                    <HoldVerificationForm attributionId={attributionId} />
+                </Elements>
+            )}
+        </Modal>
+    );
+};
+
+function HoldVerificationForm(props: { attributionId: string }) {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const confirmPayment = useMutation(async () => {
+        const attrId = AttributionId.parse(props.attributionId);
+        if (!stripe || !elements || !attrId) {
+            return;
+        }
+
+        try {
+            const url = new URL(window.location.href);
+            url.searchParams.set("step", "subscribe");
+
+            // Event though we might not request a payment right away, we want to use this sync opportunity to prepare clients as much as possible.
+            // E.g., if they have to go through a flow like 3DS or iDEAL, they should do it ("online flow"), instead of ending up in the (email-based) "offline flow".
+            const result = await stripe.confirmPayment({
+                elements,
+                confirmParams: {
+                    return_url: url.toString(),
+                },
+            });
+            console.log("confirmPayment", result);
+            if (result.error) {
+                // Show error to your customer (for example, payment details incomplete)
+                throw result.error;
+            } else {
+                // Your customer will be redirected to your `return_url`. For some payment
+                // methods like iDEAL, your customer will be redirected to an intermediate
+                // site first to authorize the payment, then redirected to the `return_url`.
+                console.log("RESULT: " + JSON.stringify(result));
+            }
+        } catch (error) {
+            console.error("Failed to submit form.", error);
+            let message = `Failed to submit form. ${error?.message || String(error)}`;
+            throw new Error(message);
+        }
+    });
+
+    const handleSubmit = useCallback(
+        (e) => {
+            e.preventDefault();
+            confirmPayment.mutate();
+        },
+        [confirmPayment],
+    );
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <ModalBody>
+                <PaymentElement id="payment-element" />
+            </ModalBody>
+            <ModalFooter
+                alert={
+                    confirmPayment.isError && (
+                        <ModalFooterAlert closable={false} type="danger">
+                            {(confirmPayment.error as Error).message}
+                        </ModalFooterAlert>
+                    )
+                }
+            >
+                <Button disabled={!stripe} loading={confirmPayment.isLoading}>
+                    Confirm
+                </Button>
+            </ModalFooter>
+        </form>
+    );
+}
