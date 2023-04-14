fd48916dec37b5359a3c11ebcb2a81b25525b8c3
Milan Pavlik <milan@gitpod.io>
2023-04-14 14:03:56 +0000

    [installer] Add key id for each auth keypair

---
+++ install/installer/pkg/components/auth/contants.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License.AGPL.txt in the project root for license information.
+
+package auth
+
+const (
+	Component = "auth"
+)
