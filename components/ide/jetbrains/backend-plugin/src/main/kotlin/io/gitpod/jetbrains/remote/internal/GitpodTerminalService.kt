5403b97318b3ef755046e5bf390ca9f39cb86674
Huiwen <huiwen@gitpod.io>
2023-04-17 08:22:10 +0000

    Stable latest implements

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/internal/GitpodTerminalService.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/internal/GitpodTerminalService.kt
@@ -2,24 +2,24 @@
 // Licensed under the GNU Affero General Public License (AGPL).
 // See License.AGPL.txt in the project root for license information.
 
-package io.gitpod.jetbrains.remote.stable
+package io.gitpod.jetbrains.remote.internal
 
 import com.intellij.openapi.project.Project
 import com.jetbrains.rdserver.terminal.BackendTerminalManager
 import io.gitpod.jetbrains.remote.AbstractGitpodTerminalService
 import org.jetbrains.plugins.terminal.ShellTerminalWidget
-import org.jetbrains.plugins.terminal.TerminalView
+import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 import java.util.*
 
 @Suppress("UnstableApiUsage")
-class StableGitpodTerminalService(project: Project): AbstractGitpodTerminalService(project) {
+class GitpodTerminalService(project: Project): AbstractGitpodTerminalService(project) {
 
-    private val terminalView = TerminalView.getInstance(project)
+    private val terminalToolWindowManager = TerminalToolWindowManager.getInstance(project)
     private val backendTerminalManager = BackendTerminalManager.getInstance(project)
 
     override fun createSharedTerminal(title: String): ShellTerminalWidget {
-        val shellTerminalWidget = terminalView.createLocalShellWidget(null, title, true, false)
-        backendTerminalManager.shareTerminal(shellTerminalWidget, UUID.randomUUID().toString())
+        val shellTerminalWidget = terminalToolWindowManager.createLocalShellWidget(null, title, true, false)
+        backendTerminalManager.shareTerminal(shellTerminalWidget.asNewWidget(), UUID.randomUUID().toString())
         return shellTerminalWidget
     }
 
