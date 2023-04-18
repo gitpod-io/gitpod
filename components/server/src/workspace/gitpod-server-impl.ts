973c455964458a44d7bc5e199ae810de7ae7347c
Milan Pavlik <milan@gitpod.io>
2023-04-18 11:48:50 +0200

    [server] Remove TermsProvider - unused - WEB-167 (#17252)
    * [server] Remove TermsProvider - unused
    
    * fix
    
    * fix
    
    * fix

--- components/server/src/workspace/gitpod-server-impl.ts
+++ components/server/src/workspace/gitpod-server-impl.ts
@@ -43,7 +43,6 @@ import {
     SetWorkspaceTimeoutResult,
     StartPrebuildContext,
     StartWorkspaceResult,
-    Terms,
     Token,
     User,
     UserEnvVar,
@@ -141,7 +140,6 @@ import { GuardedResource, ResourceAccessGuard, ResourceAccessOp } from "../auth/
 import { Config } from "../config";
 import { NotFoundError, UnauthorizedError } from "../errors";
 import { RepoURL } from "../repohost/repo-url";
-import { TermsProvider } from "../terms/terms-provider";
 import { AuthorizationService } from "../user/authorization-service";
 import { TokenProvider } from "../user/token-provider";
 import { UserDeletionService } from "../user/user-deletion-service";
@@ -253,8 +251,6 @@ export class GitpodServerImpl implements GitpodServerWithTracing, Disposable {
     @inject(IClientDataPrometheusAdapter) protected readonly clientDataPrometheusAdapter: IClientDataPrometheusAdapter;
 
     @inject(AuthProviderService) protected readonly authProviderService: AuthProviderService;
-
-    @inject(TermsProvider) protected readonly termsProvider: TermsProvider;
 
     @inject(CachingBlobServiceClientProvider)
     protected readonly blobServiceClientProvider: CachingBlobServiceClientProvider;
@@ -3313,12 +3309,6 @@ export class GitpodServerImpl implements GitpodServerWithTracing, Disposable {
             context: event.context,
         };
         this.analytics.identify(identifyMessage);
-    }
-
-    async getTerms(ctx: TraceContext): Promise<Terms> {
-        // Terms are publicly available, thus no user check here.
-
-        return this.termsProvider.getCurrent();
     }
 
     async getIDEOptions(ctx: TraceContext): Promise<IDEOptions> {
