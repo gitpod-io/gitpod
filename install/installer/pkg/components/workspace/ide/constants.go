3f9844047486b040f3088a862257f020d607f6f0
Anton Kosyakov <anton@gitpod.io>
2023-04-19 21:32:52 +0200

    bump up gitpod code extension (#17260)
    to pick up changes to validate links: https://github.com/gitpod-io/gitpod-code/commit/0b2fb39d35def04293397c07b29cb44cc3b14612

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -9,7 +9,7 @@ const (
 	CodeIDEImageStableVersion   = "commit-5999c87e5c9bed5dcb7397820ef95b89ace55dab" // stable version that will be updated manually on demand
 	CodeHelperIDEImage          = "ide/code-codehelper"
 	CodeWebExtensionImage       = "ide/gitpod-code-web"
-	CodeWebExtensionVersion     = "commit-3b076b91039c47404d98c4a3198edf2982e24af7" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
+	CodeWebExtensionVersion     = "commit-993573cfcc9f02dec825f8e333d52fa0e95ada8d" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	IntelliJDesktopIDEImage     = "ide/intellij"
