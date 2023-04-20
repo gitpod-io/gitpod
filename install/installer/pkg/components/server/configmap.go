2ccb84f62c5982974d6756928f4a8aeae3de1d9a
Milan Pavlik <pavlik.mil@gmail.com>
2023-04-20 09:54:35 +0000

    [installer] First user is no longer admin

--- install/installer/pkg/components/server/configmap.go
+++ install/installer/pkg/components/server/configmap.go
@@ -287,7 +287,7 @@ func configmap(ctx *common.RenderContext) ([]runtime.Object, error) {
 		InactivityPeriodForReposInDays: inactivityPeriodForReposInDays,
 		PATSigningKeyFile:              personalAccessTokenSigningKeyPath,
 		Admin: AdminConfig{
-			GrantFirstUserAdminRole: true, // existing default
+			GrantFirstUserAdminRole: false, // Use AdminCredentialsPath to seed the installation with admin credentials
 			CredentialsPath:         adminCredentialsPath,
 		},
 		ShowSetupModal: showSetupModal,
