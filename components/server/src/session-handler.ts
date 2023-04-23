b065c7692f3a88562854a066ec8a0671d5c2fd03
Milan Pavlik <milan@gitpod.io>
2023-04-23 07:41:44 +0200

    Revert "[server] Metrics for sessions with JWTs WEB-102 (#17302)" (#17329)
    This reverts commit ec8c66e948d8dce9963d4051bc04e1c183263710.

--- components/server/src/session-handler.ts
+++ components/server/src/session-handler.ts
@@ -16,7 +16,6 @@ import { log } from "@gitpod/gitpod-protocol/lib/util/logging";
 import { Config as DBConfig } from "@gitpod/gitpod-db/lib/config";
 import { Config } from "./config";
 import { GitpodHostUrl } from "@gitpod/gitpod-protocol/lib/util/gitpod-host-url";
-import { reportSessionHasJWT } from "./prometheus-metrics";
 
 @injectable()
 export class SessionHandlerProvider {
@@ -41,14 +40,7 @@ export class SessionHandlerProvider {
 
         options.store = this.createStore();
 
-        this.sessionHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-            const jwt = req.cookies[SessionHandlerProvider.getJWTCookieName(this.config.hostUrl)];
-            const containedJWT = !!jwt;
-
-            reportSessionHasJWT(containedJWT);
-
-            return session(options)(req, res, next);
-        };
+        this.sessionHandler = session(options);
     }
 
     protected getCookieOptions(config: Config): express.CookieOptions {
