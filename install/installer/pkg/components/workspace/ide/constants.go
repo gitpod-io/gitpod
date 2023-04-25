13cbd5c5c46790ca4f38b19b61d727a2a9dbd0a8
Anton Kosyakov <anton@gitpod.io>
2023-04-25 12:24:33 +0200

    bum up code
    to address missing initial analytics events: https://github.com/gitpod-io/gitpod-code/commit/150ee83c1f1c51cd4d07b85418e2c3070b0f1f9b

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -9,7 +9,7 @@ const (
 	CodeIDEImageStableVersion   = "commit-5999c87e5c9bed5dcb7397820ef95b89ace55dab" // stable version that will be updated manually on demand
 	CodeHelperIDEImage          = "ide/code-codehelper"
 	CodeWebExtensionImage       = "ide/gitpod-code-web"
-	CodeWebExtensionVersion     = "commit-3ed3cec73b5602ea3b9481d6b0f62bbfefe7d1e1" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
+	CodeWebExtensionVersion     = "commit-150ee83c1f1c51cd4d07b85418e2c3070b0f1f9b" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
 	CodeDesktopIDEImage         = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
 	XtermIDEImage               = "ide/xterm-web"
