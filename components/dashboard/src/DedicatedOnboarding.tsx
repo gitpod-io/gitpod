a95a03f20282625871744c2a414eb94cce98b4f2
Brad Harris <bmharris@gmail.com>
2023-04-26 20:15:18 +0000

    updating getting started view

--- components/dashboard/src/DedicatedOnboarding.tsx
+++ components/dashboard/src/DedicatedOnboarding.tsx
@@ -4,51 +4,26 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import { Button } from "./components/Button";
-import Modal, { ModalBody, ModalFooter, ModalHeader } from "./components/Modal";
-import { OIDCClientConfigModal } from "./teams/sso/OIDCClientConfigModal";
+import { Heading1, Subheading } from "./components/typography/headings";
 
 type Props = {
     onComplete?: () => void;
 };
 export default function DedicatedOnboarding({ onComplete }: Props) {
-    const [showModal] = useState<boolean>(false);
-
-    const acceptAndContinue = useCallback(() => {
-        // // setShowModal(true);
+    const handleGetStarted = useCallback(() => {
+        console.log("getting started");
     }, []);
-
-    // const onAuthorize = useCallback(
-    //     (payload?: string) => {
-    //         onComplete && onComplete();
-
-    //         // run without await, so the integrated closing of new tab isn't blocked
-    //         (async () => {
-    //             window.location.href = gitpodHostUrl.asDashboard().toString();
-    //         })();
-    //     },
-    //     [onComplete],
-    // );
 
     return (
         <div>
-            {!showModal && (
-                <Modal visible={true} onClose={() => {}} closeable={false}>
-                    <ModalHeader>Welcome to Gitpod Dedicated 🚀</ModalHeader>
-                    <ModalBody>
-                        <p className="pb-4 text-gray-500 text-base">
-                            Before this instance can be used, you need to set up Single Sign-on.
-                        </p>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button className={"ml-2"} onClick={acceptAndContinue}>
-                            Continue
-                        </Button>
-                    </ModalFooter>
-                </Modal>
-            )}
-            {showModal && <OIDCClientConfigModal onClose={() => {}} />}
+            <Heading1>Let's get started</Heading1>
+            {/* TODO: verify this is the copy we want to use */}
+            <Subheading>
+                Spin up fresh cloud development environments for each task, full automated, in seconds.
+            </Subheading>
+            <Button onClick={handleGetStarted}>Get Started</Button>
         </div>
     );
 }
