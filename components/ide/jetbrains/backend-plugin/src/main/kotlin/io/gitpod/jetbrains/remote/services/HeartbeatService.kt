1130b565ac53d53984f35493303feef01303ee2b
Huiwen <huiwen@gitpod.io>
2023-04-26 09:29:19 +0000

    mutex

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/services/HeartbeatService.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/services/HeartbeatService.kt
@@ -13,6 +13,8 @@ import io.gitpod.jetbrains.remote.GitpodManager
 import io.gitpod.jetbrains.remote.services.ControllerStatusService.ControllerStatus
 import kotlinx.coroutines.*
 import kotlinx.coroutines.future.await
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlin.random.Random.Default.nextInt
 
 @Service
@@ -23,6 +25,7 @@ class HeartbeatService : Disposable {
     // define property to store successfulCount and totalCount
     private var successfulCount = 0
     private var totalCount = 0
+    private val counterMutex = Mutex()
 
     private val job = GlobalScope.launch {
         val info = manager.pendingInfo.await()
@@ -32,6 +35,7 @@ class HeartbeatService : Disposable {
                 secondsSinceLastActivity = 0
         )
         while (isActive) {
+            var hbSucceed = false
             try {
                 val previous = current;
                 current = ControllerStatusService.fetch()
@@ -44,7 +48,7 @@ class HeartbeatService : Disposable {
                 }
                 if (wasClosed != null) {
                     manager.client.server.sendHeartBeat(SendHeartBeatOptions(info.instanceId, wasClosed)).await()
-                    successfulCount += 1
+                    hbSucceed = true
                     if (wasClosed) {
                         manager.trackEvent("ide_close_signal", mapOf(
                                 "clientKind" to "jetbrains"
@@ -54,7 +58,12 @@ class HeartbeatService : Disposable {
             } catch (t: Throwable) {
                 thisLogger().error("gitpod: failed to check activity:", t)
             } finally {
-                totalCount += 1
+                counterMutex.withLock {
+                    if (hbSucceed) {
+                        successfulCount += 1
+                    }
+                    totalCount += 1
+                }
             }
 
             delay(intervalInSeconds * 1000L)
@@ -63,21 +72,35 @@ class HeartbeatService : Disposable {
 
     // define job to start interval per 15 minutes
     private val telemetryJob = GlobalScope.launch {
-        val info = manager.pendingInfo.await()
-        while (isActive) {
-            try {
-                manager.trackEvent("ide_heartbeat", mapOf(
+        try {
+            while (isActive) {
+                sendIDEHeartbeat()
+                delay(15 * 60 * 1000L)
+            }
+        } finally {
+            sendIDEHeartbeat()
+        }
+    }
+
+    private suspend fun sendIDEHeartbeat() {
+        try {
+            var props: Map<String, Any> = mapOf()
+            val info = manager.pendingInfo.await()
+            counterMutex.withLock {
+                props = mapOf(
                         "clientKind" to "jetbrains",
                         "totalCount" to totalCount,
                         "successfulCount" to successfulCount,
                         "gitpodHost" to info.gitpodApi.host,
-                // TODO: Identify if it's debug workspace
-                // "debugWorkspace" to "false"
-                ))
-            } catch (t: Throwable) {
-                thisLogger().error("gitpod: failed to send heartbeat telemetry:", t)
+                        // TODO: Identify if it's debug workspace
+                        // "debugWorkspace" to "false"
+                )
+                totalCount = 0
+                successfulCount = 0
             }
-            delay(15 * 60 * 1000L)
+            manager.trackEvent("ide_heartbeat", props)
+        } catch (t: Throwable) {
+            thisLogger().error("gitpod: failed to send heartbeat telemetry:", t)
         }
     }
 
