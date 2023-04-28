edde826c495a275d890a8d4747c57bdb4672d730
Brad Harris <bmharris@gmail.com>
2023-04-28 16:18:46 +0000

    only show dedicated setup if query param is set for now

--- components/dashboard/src/hooks/use-check-dedicated-setup.ts
+++ components/dashboard/src/hooks/use-check-dedicated-setup.ts
@@ -4,29 +4,20 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { useFeatureFlag } from "../data/featureflag-query";
-import { useOrganizations } from "../data/organizations/orgs-query";
 import { useQueryParams } from "./use-query-param";
 
 const FORCE_SETUP_PARAM = "dedicated-setup";
 const FORCE_SETUP_PARAM_VALUE = "force";
 
 export const useCheckDedicatedSetup = () => {
-    const orgs = useOrganizations();
     const params = useQueryParams();
-    const enableDedicatedOnboardingFlow = useFeatureFlag("enableDedicatedOnboardingFlow");
 
     const forceSetup = params.get(FORCE_SETUP_PARAM) === FORCE_SETUP_PARAM_VALUE;
 
-    const hasOrgs = (orgs.data || [])?.length > 0;
-
-    // Show setup if:
-    // - query param set to force
-    // - or
-    // - feature flag is on and no orgs present for user
+    // For now to aid dev only show if query param is set
     // TODO: update this once a new backend method is ready that will let us know if dedicated setup is needed
     return {
-        needsOnboarding: forceSetup || (enableDedicatedOnboardingFlow && !hasOrgs),
-        isLoading: orgs.isLoading,
+        needsOnboarding: forceSetup,
+        isLoading: false,
     };
 };
