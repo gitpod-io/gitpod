aee006bbc772287d398b3addf9db2be8f65890bb
Milan Pavlik <pavlik.mil@gmail.com>
2023-05-02 08:44:34 +0000

    [server] Fix parsing of go duration

--- components/gitpod-protocol/src/util/timeutil.spec.ts
+++ components/gitpod-protocol/src/util/timeutil.spec.ts
@@ -7,7 +7,7 @@
 import * as chai from "chai";
 const expect = chai.expect;
 import { suite, test } from "mocha-typescript";
-import { daysBefore, hoursBefore, oneMonthLater } from "./timeutil";
+import { daysBefore, goDurationToHumanReadable, hoursBefore, oneMonthLater, parseGoDurationToMs } from "./timeutil";
 
 @suite()
 export class TimeutilSpec {
@@ -64,6 +64,40 @@ export class TimeutilSpec {
         for (const t of tests) {
             const actual = daysBefore(t.date.toISOString(), t.daysEarlier);
             expect(actual).to.equal(t.expectation, `expected ${actual} to be equal ${t.expectation}`);
+        }
+    }
+
+    @test
+    testParseGoDurationToMs() {
+        const scenarios = [
+            { input: "1ms", out: 1 },
+            { input: "1m", out: 1 * 60 * 1000 },
+            { input: "1m0s", out: 1 * 60 * 1000 },
+            { input: "1m30s", out: 1 * 60 * 1000 + 30 * 1000 },
+            { input: "1h1m30s", out: 1 * 60 * 60 * 1000 + 1 * 60 * 1000 + 30 * 1000 },
+        ];
+
+        for (const t of scenarios) {
+            const actual = parseGoDurationToMs(t.input);
+            expect(actual).to.equal(t.out, `expected ${t.input} to be ${t.out} in MS`);
+        }
+    }
+
+    @test
+    testGoDurationToHumanReadable() {
+        const scenarios = [
+            { input: "1m", out: "1 minute" },
+            { input: "2m", out: "2 minutes" },
+            { input: "1m0s", out: "1 minute" },
+            { input: "2m0s", out: "2 minutes" },
+            { input: "1m30s", out: "1 minute 30 seconds" },
+            { input: "2m30s", out: "2 minutes 30 seconds" },
+            { input: "1h1m30s", out: "1 hour 1 minute 30 seconds" },
+        ];
+
+        for (const t of scenarios) {
+            const actual = goDurationToHumanReadable(t.input);
+            expect(actual).to.equal(t.out, `expected ${t.input} to be ${t.out} in human readable form`);
         }
     }
 }
