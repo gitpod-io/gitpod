1edbd227f0dc8b7c34ac5eaf75157d2a99f37a12
Thomas Schubart <thomas@gitpod.io>
2023-05-04 12:35:13 +0000

    [ws-daemon] Add tests

--- components/ws-daemon/pkg/controller/workspace_controller_test.go
+++ components/ws-daemon/pkg/controller/workspace_controller_test.go
@@ -24,7 +24,7 @@ import (
 )
 
 const (
-	timeout            = time.Second * 5
+	timeout            = time.Second * 20
 	duration           = time.Second * 2
 	interval           = time.Millisecond * 250
 	workspaceNamespace = "default"
@@ -32,13 +32,11 @@ const (
 
 var _ = Describe("WorkspaceController", func() {
 	Context("with regular workspace", func() {
-		It("should handle content init", func() {
+		It("should handle regular content init", func() {
 			name := uuid.NewString()
 
-			// ops := &FakeWorkspaceOperations{}
-			// workspaceCtrl.operations = ops
-
 			mockCtrl := gomock.NewController(GinkgoT())
+			defer mockCtrl.Finish()
 			ops := NewMockWorkspaceOperations(mockCtrl)
 
 			ops.EXPECT().InitWorkspace(gomock.Any(), gomock.Any()).Return("", nil).Times(1)
@@ -55,11 +53,85 @@ var _ = Describe("WorkspaceController", func() {
 				}
 			})
 
+			expectConditionEventually(ws, string(workspacev1.WorkspaceConditionContentReady), metav1.ConditionTrue, "InitializationSuccess")
+		})
+
+		It("should handle regular content backup", func() {
+			name := uuid.NewString()
+
+			mockCtrl := gomock.NewController(GinkgoT())
+			defer mockCtrl.Finish()
+			ops := NewMockWorkspaceOperations(mockCtrl)
+
+			gitStatus := &csapi.GitStatus{
+				Branch:               "main",
+				LatestCommit:         "991300e0cf199116685f25561702a145d40ae462",
+				UncommitedFiles:      []string{"git", "pod"},
+				TotalUncommitedFiles: 2,
+				UntrackedFiles:       []string{"kumquat"},
+				TotalUntrackedFiles:  1,
+				UnpushedCommits:      []string{"df591ed557c9afa8b6bcd1f51809d83d3f48fc43"},
+				TotalUnpushedCommits: 1,
+			}
+
+			ops.EXPECT().BackupWorkspace(gomock.Any(), gomock.Any()).Return(gitStatus, nil).Times(1)
+			ops.EXPECT().DeleteWorkspace(gomock.Any(), gomock.Any())
+			workspaceCtrl.operations = ops
+
+			_ = createSecret(fmt.Sprintf("%s-tokens", name), secretsNamespace)
+			ws := newWorkspace(name, workspaceNamespace, workspacev1.WorkspacePhaseCreating)
+			createWorkspace(ws)
+			markContentReady(ws)
+
+			expectConditionEventually(ws, string(workspacev1.WorkspaceConditionBackupComplete), metav1.ConditionTrue, "BackupComplete")
+			expectGitStatusEventually(ws, gitStatus)
+		})
+
+		It("should report backup failure", func() {
+			name := uuid.NewString()
+
+			mockCtrl := gomock.NewController(GinkgoT())
+			defer mockCtrl.Finish()
+			ops := NewMockWorkspaceOperations(mockCtrl)
+
+			ops.EXPECT().BackupWorkspace(gomock.Any(), gomock.Any()).Return(nil, fmt.Errorf("BOOM!")).Times(1)
+			ops.EXPECT().DeleteWorkspace(gomock.Any(), gomock.Any())
+			workspaceCtrl.operations = ops
+
+			_ = createSecret(fmt.Sprintf("%s-tokens", name), secretsNamespace)
+			ws := newWorkspace(name, workspaceNamespace, workspacev1.WorkspacePhaseCreating)
+			createWorkspace(ws)
+			markContentReady(ws)
+
+			expectConditionEventually(ws, string(workspacev1.WorkspaceConditionBackupFailure), metav1.ConditionTrue, "BackupFailed")
+		})
+
+		It("should report snapshot url on snapshot", func() {
+			name := uuid.NewString()
+
+			mockCtrl := gomock.NewController(GinkgoT())
+			defer mockCtrl.Finish()
+			ops := NewMockWorkspaceOperations(mockCtrl)
+
+			ops.EXPECT().BackupWorkspace(gomock.Any(), gomock.Any()).Return(nil, nil).Times(1)
+			ops.EXPECT().SnapshotIDs(gomock.Any(), gomock.Any()).Return("snapshotUrl", "snapshotName", nil)
+			ops.EXPECT().DeleteWorkspace(gomock.Any(), gomock.Any()).Return(nil).Times(1)
+			workspaceCtrl.operations = ops
+
+			_ = createSecret(fmt.Sprintf("%s-tokens", name), secretsNamespace)
+			ws := newWorkspace(name, workspaceNamespace, workspacev1.WorkspacePhaseCreating)
+			ws.Spec.Type = workspacev1.WorkspaceTypePrebuild
+			createWorkspace(ws)
+			markContentReady(ws)
+
 			Eventually(func(g Gomega) {
 				g.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: ws.Name, Namespace: ws.Namespace}, ws)).To(Succeed())
-				g.Expect(wsk8s.ConditionPresentAndTrue(ws.Status.Conditions, string(workspacev1.WorkspaceConditionContentReady)))
+				g.Expect(ws.Status.Snapshot).ToNot(BeEmpty())
 			}, timeout, interval).Should(Succeed())
+
+			expectConditionEventually(ws, string(workspacev1.WorkspaceConditionBackupComplete), metav1.ConditionTrue, "BackupComplete")
 		})
+
 	})
 })
 
@@ -149,4 +221,48 @@ func updateObjWithRetries[O client.Object](c client.Client, obj O, updateStatus 
 		}
 		return err
 	}, timeout, interval).Should(Succeed())
+}
+
+func expectConditionEventually(ws *workspacev1.Workspace, tpe string, status metav1.ConditionStatus, reason string) {
+	GinkgoHelper()
+	By(fmt.Sprintf("expect workspace condition %s to be %s", tpe, status))
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: ws.Name, Namespace: ws.Namespace}, ws)).To(Succeed())
+		c := wsk8s.GetCondition(ws.Status.Conditions, tpe)
+		g.Expect(c).ToNot(BeNil(), fmt.Sprintf("expected condition %s to be present", tpe))
+		g.Expect(c.Status).To(Equal(status))
+		if reason != "" {
+			g.Expect(c.Reason).To(Equal(reason))
+		}
+	}, timeout, interval).Should(Succeed())
+}
+
+func expectGitStatusEventually(ws *workspacev1.Workspace, gitStatus *csapi.GitStatus) {
+	GinkgoHelper()
+	By("expect git status")
+	Eventually(func(g Gomega) {
+		g.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: ws.Name, Namespace: ws.Namespace}, ws)).To(Succeed())
+		g.Expect(ws.Status.GitStatus.Branch).To(Equal(gitStatus.Branch))
+		g.Expect(ws.Status.GitStatus.LatestCommit).To(Equal(gitStatus.LatestCommit))
+		g.Expect(ws.Status.GitStatus.UncommitedFiles).To(Equal(gitStatus.UncommitedFiles))
+		g.Expect(ws.Status.GitStatus.TotalUncommitedFiles).To(Equal(gitStatus.TotalUncommitedFiles))
+		g.Expect(ws.Status.GitStatus.UntrackedFiles).To(Equal(gitStatus.UntrackedFiles))
+		g.Expect(ws.Status.GitStatus.TotalUntrackedFiles).To(Equal(gitStatus.TotalUntrackedFiles))
+		g.Expect(ws.Status.GitStatus.UnpushedCommits).To(Equal(gitStatus.UnpushedCommits))
+		g.Expect(ws.Status.GitStatus.TotalUnpushedCommits).To(Equal(gitStatus.TotalUnpushedCommits))
+	}, timeout, interval).Should(Succeed())
+}
+
+func markContentReady(ws *workspacev1.Workspace) {
+	GinkgoHelper()
+	By("adding content ready condition")
+	updateObjWithRetries(k8sClient, ws, true, func(ws *workspacev1.Workspace) {
+		ws.Status.Phase = workspacev1.WorkspacePhaseStopping
+		ws.Status.Conditions = []metav1.Condition{
+			workspacev1.NewWorkspaceConditionContentReady(metav1.ConditionTrue, "InitializationSuccess", ""),
+		}
+		ws.Status.Runtime = &workspacev1.WorkspaceRuntimeStatus{
+			NodeName: NodeName,
+		}
+	})
 }
