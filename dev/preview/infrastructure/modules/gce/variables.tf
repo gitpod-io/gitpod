0e8322699d76bb1f71339ed08d5463907f470ef2
Kyle Brennan <kyle@gitpod.io>
2023-05-09 19:28:55 -0400

    [preview] update VM image to match gen97 (#17554)
    Ref: https://github.com/gitpod-io/gitpod-packer-gcp-image/releases/tag/gitpod-k3s-202305091902

--- dev/preview/infrastructure/modules/gce/variables.tf
+++ dev/preview/infrastructure/modules/gce/variables.tf
@@ -44,7 +44,7 @@ variable "harvester_ingress_ip" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202304191605"
+  default     = "gitpod-k3s-202305091902"
 }
 
 variable "cert_issuer" {
