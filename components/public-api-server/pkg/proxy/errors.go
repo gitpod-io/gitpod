99c0eaf72d0e5b157d93efa76032754a2e7898c3
akosyakov <anton@gitpod.io>
2023-05-12 07:36:15 +0000

    [public-api] refine mapping of json rpc error codes
    - map 4xx and 650 codes as invalid argument (client errors)
    - map 401, 411 related to unauthedicated and permission denied
    - mapp 429 to resource exhausted

--- components/public-api-server/pkg/proxy/errors.go
+++ components/public-api-server/pkg/proxy/errors.go
@@ -43,15 +43,28 @@ func categorizeRPCError(err error) *connect.Error {
 		// components/gitpod-protocol/src/messaging/error.ts
 		case 409:
 			return connect.NewError(connect.CodeAlreadyExists, fmt.Errorf(rpcErr.Message))
+		// components/gitpod-protocol/src/messaging/error.ts - SETUP_REQUIRED (no user)
+		case 410:
+			return connect.NewError(connect.CodeUnauthenticated, fmt.Errorf(rpcErr.Message))
+		// components/gitpod-protocol/src/messaging/error.ts - NEEDS_VERIFICATION (no user)
+		case 411:
+			return connect.NewError(connect.CodePermissionDenied, fmt.Errorf(rpcErr.Message))
+		case 429:
+			return connect.NewError(connect.CodeResourceExhausted, fmt.Errorf(rpcErr.Message))
 		// components/gitpod-messagebus/src/jsonrpc-server.ts#47
 		case -32603:
 			return connect.NewError(connect.CodeInternal, fmt.Errorf(rpcErr.Message))
 		case 470:
 			return connect.NewError(connect.CodePermissionDenied, fmt.Errorf(rpcErr.Message))
-
-		default:
-			return connect.NewError(connect.CodeInternal, fmt.Errorf(rpcErr.Message))
+		// components/gitpod-protocol/src/messaging/error.ts - INVALID_VALUE (no user)
+		case 650:
+			return connect.NewError(connect.CodeInvalidArgument, fmt.Errorf(rpcErr.Message))
 		}
+		// components/gitpod-protocol/src/messaging/error.ts - user errors
+		if rpcErr.Code >= 400 && rpcErr.Code < 500 {
+			return connect.NewError(connect.CodeInvalidArgument, fmt.Errorf(rpcErr.Message))
+		}
+		return connect.NewError(connect.CodeInternal, fmt.Errorf(rpcErr.Message))
 	}
 
 	if errors.Is(err, context.Canceled) {
