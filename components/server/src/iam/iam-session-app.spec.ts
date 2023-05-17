97e881fbaab4f6d25a12349281a58df49d2fa22f
Alex Tugarev <alex@gitpod.io>
2023-05-17 10:48:15 +0000

    adding simple tests for update of email attribute

--- components/server/src/iam/iam-session-app.spec.ts
+++ components/server/src/iam/iam-session-app.spec.ts
@@ -93,6 +93,7 @@ class TestIamSessionApp {
 
     public before() {
         this.teamDbMock.memberships.clear();
+        this.knownUser.identities = [];
 
         const container = new Container();
         container.load(
@@ -235,6 +236,58 @@ class TestIamSessionApp {
         // assert no admin is member of the org
         expect(this.teamDbMock.memberships.has(BUILTIN_INSTLLATION_ADMIN_USER_ID)).to.be.false;
         expect(this.teamDbMock.memberships.has("id-new-user")).to.be.true;
+    }
+
+    @test public async testSessionRequest_updates_existing_user() {
+        const payload: OIDCCreateSessionPayload = { ...this.payload };
+        payload.claims.sub = this.knownSubjectID; // `userServiceMock.findUserForLogin` will match this value
+
+        this.knownUser.identities = [
+            {
+                authId: payload.claims.sub,
+                authProviderId: payload.claims.aud,
+                authName: "Test User",
+                primaryEmail: "random-user@any.org",
+            },
+        ];
+
+        let newEmail: string | undefined;
+        this.userServiceMock.updateUserIdentity = async (user, updatedIdentity) => {
+            newEmail = updatedIdentity.primaryEmail;
+        };
+
+        const result = await request(this.app.create())
+            .post("/session")
+            .set("Content-Type", "application/json")
+            .send(JSON.stringify(payload));
+
+        expect(result.statusCode, JSON.stringify(result.body)).to.equal(200);
+        expect(newEmail, "update was not called").not.to.be.undefined;
+        expect(newEmail).to.equal(payload.claims.email);
+    }
+
+    @test public async testSessionRequest_no_update_if_same_email() {
+        this.knownUser.identities = [
+            {
+                authId: this.payload.claims.sub,
+                authProviderId: this.payload.claims.aud,
+                authName: "Test User",
+                primaryEmail: this.payload.claims.email,
+            },
+        ];
+
+        let updateUserIdentityCalled = false;
+        this.userServiceMock.updateUserIdentity = async () => {
+            updateUserIdentityCalled = true;
+        };
+
+        const result = await request(this.app.create())
+            .post("/session")
+            .set("Content-Type", "application/json")
+            .send(JSON.stringify(this.payload));
+
+        expect(result.statusCode, JSON.stringify(result.body)).to.equal(200);
+        expect(updateUserIdentityCalled).to.be.false;
     }
 }
 
