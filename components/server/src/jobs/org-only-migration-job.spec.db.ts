322548c254e0a960bf329eae15b376ce0f88e9cd
Sven Efftinge <sven@gitpod.io>
2023-05-23 20:41:57 +0200

    [server] install job for orgonly migration (#17692)
    * [server] install job for orgonly migration
    
    * [dashboard] handle no orgs (#17697)

---
+++ components/server/src/jobs/org-only-migration-job.spec.db.ts
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License.AGPL.txt in the project root for license information.
+ */
+
+import { TypeORM, UserDB } from "@gitpod/gitpod-db/lib";
+import { AdditionalUserData } from "@gitpod/gitpod-protocol";
+import * as chai from "chai";
+import { Container, ContainerModule } from "inversify";
+import { RedlockAbortSignal } from "redlock";
+import { UserToTeamMigrationService } from "../migration/user-to-team-migration-service";
+import { RedisMutex } from "../redis/mutex";
+import { StripeService } from "../user/stripe-service";
+import { OrgOnlyMigrationJob } from "./org-only-migration-job";
+import { dbContainerModule } from "@gitpod/gitpod-db/lib/container-module";
+const expect = chai.expect;
+
+const mockedStripe = new StripeService();
+mockedStripe.updateAttributionId = async (
+    stripeCustomerId: string,
+    attributionId: string,
+    oldAttributionId: string,
+) => {
+    return true;
+};
+class TestingRedisMutex extends RedisMutex {
+    public using<T>(
+        resources: string[],
+        duration: number,
+        routine: (signal: RedlockAbortSignal) => Promise<T>,
+    ): Promise<T> {
+        return routine(undefined!);
+    }
+}
+export const testContainer = new Container();
+testContainer.load(dbContainerModule);
+testContainer.load(
+    new ContainerModule((bind) => {
+        bind(StripeService).toConstantValue(mockedStripe);
+        bind(OrgOnlyMigrationJob).toSelf().inSingletonScope();
+        bind(UserToTeamMigrationService).toSelf().inSingletonScope();
+        bind(RedisMutex).toConstantValue(new TestingRedisMutex());
+    }),
+);
+
+describe("Migration Job", () => {
+    const typeORM = testContainer.get<TypeORM>(TypeORM);
+    const migrationJob = testContainer.get<OrgOnlyMigrationJob>(OrgOnlyMigrationJob);
+    const userDB = testContainer.get<UserDB>(UserDB);
+
+    it("should migrate non migrated user", async () => {
+        const migratedUser = await userDB.newUser();
+        AdditionalUserData.set(migratedUser, { isMigratedToTeamOnlyAttribution: true });
+        await userDB.storeUser(migratedUser);
+        const nonMigratedUser = await userDB.newUser();
+        AdditionalUserData.set(nonMigratedUser, { isMigratedToTeamOnlyAttribution: false });
+        await userDB.storeUser(nonMigratedUser);
+
+        const users = await migrationJob.migrateUsers(1000);
+
+        expect(
+            users.some((u) => u.id === nonMigratedUser.id),
+            "should migrate non migrated user",
+        ).to.be.true;
+        expect(
+            users.some((u) => u.id === migratedUser.id),
+            "should not migrate already migrated user",
+        ).to.be.false;
+        expect(
+            users.find((u) => u.id === nonMigratedUser.id)?.additionalData?.isMigratedToTeamOnlyAttribution,
+            "user should be migrated",
+        ).to.be.true;
+
+        const c = await typeORM.getConnection();
+        await c.query("DELETE FROM d_b_user WHERE id in (?)", [[migratedUser.id, nonMigratedUser.id]]);
+        await c.close();
+    });
+});
