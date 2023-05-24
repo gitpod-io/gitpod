309a275685f29902f79367b58ba492c9262e529b
Brad Harris <bmharris@gmail.com>
2023-05-24 17:23:29 +0000

    updating to use onSubmit

--- components/dashboard/src/user-settings/ShowTokenModal.tsx
+++ components/dashboard/src/user-settings/ShowTokenModal.tsx
@@ -8,8 +8,9 @@ import { PersonalAccessToken } from "@gitpod/public-api/lib/gitpod/experimental/
 import dayjs from "dayjs";
 import { useState } from "react";
 import DateSelector from "../components/DateSelector";
-import Modal from "../components/Modal";
+import Modal, { ModalBody, ModalFooter, ModalHeader } from "../components/Modal";
 import { TokenExpirationDays } from "./PersonalAccessTokens";
+import { Button } from "../components/Button";
 
 interface TokenModalProps {
     token: PersonalAccessToken;
@@ -30,51 +31,49 @@ function ShowTokenModal(props: TokenModalProps) {
 
     const onEnter = () => {
         props.onSave({ expirationDate: expiration.expirationDate });
-        return true;
+        props.onClose();
     };
 
     return (
-        <Modal
-            title={props.title}
-            buttons={[
-                <button className="secondary" onClick={() => props.onClose()}>
-                    Cancel
-                </button>,
-                <button className="danger" onClick={onEnter}>
-                    {props.actionDescription}
-                </button>,
-            ]}
-            visible={true}
-            onClose={() => props.onClose()}
-            onEnter={onEnter}
-        >
-            <div className="text-gray-500 dark:text-gray-400 text-md">
-                <span>{props.description}</span> <span className="font-semibold">{props.descriptionImportant}</span>
-            </div>
-            <div className="p-4 mt-2 rounded-xl bg-gray-50 dark:bg-gray-800">
-                <div className="font-semibold text-gray-700 dark:text-gray-200">{props.token.name}</div>
-                <div className="font-medium text-gray-400 dark:text-gray-300">
-                    Expires on {dayjs(props.token.expirationTime!.toDate()).format("MMM D, YYYY")}
+        <Modal visible onClose={props.onClose} onSubmit={onEnter}>
+            <ModalHeader>{props.title}</ModalHeader>
+            <ModalBody>
+                <div className="text-gray-500 dark:text-gray-400 text-md">
+                    <span>{props.description}</span> <span className="font-semibold">{props.descriptionImportant}</span>
                 </div>
-            </div>
-            <div className="mt-4">
-                {props.showDateSelector && (
-                    <DateSelector
-                        title="Expiration Date"
-                        description={`The token will expire on ${dayjs(expiration.expirationDate).format(
-                            "MMM D, YYYY",
-                        )}`}
-                        options={TokenExpirationDays}
-                        value={TokenExpirationDays.find((i) => i.value === expiration.expirationDays)?.value}
-                        onChange={(value) =>
-                            setExpiration({
-                                expirationDays: value,
-                                expirationDate: new Date(Date.now() + Number(value) * 24 * 60 * 60 * 1000),
-                            })
-                        }
-                    />
-                )}
-            </div>
+                <div className="p-4 mt-2 rounded-xl bg-gray-50 dark:bg-gray-800">
+                    <div className="font-semibold text-gray-700 dark:text-gray-200">{props.token.name}</div>
+                    <div className="font-medium text-gray-400 dark:text-gray-300">
+                        Expires on {dayjs(props.token.expirationTime!.toDate()).format("MMM D, YYYY")}
+                    </div>
+                </div>
+                <div className="mt-4">
+                    {props.showDateSelector && (
+                        <DateSelector
+                            title="Expiration Date"
+                            description={`The token will expire on ${dayjs(expiration.expirationDate).format(
+                                "MMM D, YYYY",
+                            )}`}
+                            options={TokenExpirationDays}
+                            value={TokenExpirationDays.find((i) => i.value === expiration.expirationDays)?.value}
+                            onChange={(value) =>
+                                setExpiration({
+                                    expirationDays: value,
+                                    expirationDate: new Date(Date.now() + Number(value) * 24 * 60 * 60 * 1000),
+                                })
+                            }
+                        />
+                    )}
+                </div>
+            </ModalBody>
+            <ModalFooter>
+                <Button type="secondary" onClick={props.onClose}>
+                    Cancel
+                </Button>
+                <Button htmlType="submit" type="danger">
+                    {props.actionDescription}
+                </Button>
+            </ModalFooter>
         </Modal>
     );
 }
