94766810825ee822601398a6b9cbdc067186094c
Brad Harris <bmharris@gmail.com>
2023-05-30 23:50:14 +0000

    handle no records

--- components/dashboard/src/usage/download/DownloadUsage.tsx
+++ components/dashboard/src/usage/download/DownloadUsage.tsx
@@ -6,10 +6,11 @@
 
 import { FC, useCallback, useState } from "react";
 import { Button } from "../../components/Button";
-import { buildUsageCSV } from "./build-usage-csv";
+import { downloadUsageCSV } from "./download-usage-csv";
 import { AttributionId } from "@gitpod/gitpod-protocol/lib/attribution";
 import { Dayjs } from "dayjs";
 import { useToast } from "../../components/toasts/Toasts";
+import { useCurrentOrg } from "../../data/organizations/orgs-query";
 
 type Props = {
     attributionId: AttributionId;
@@ -17,24 +18,35 @@ type Props = {
     endDate: Dayjs;
 };
 export const DownloadUsage: FC<Props> = ({ attributionId, startDate, endDate }) => {
+    const { data: org } = useCurrentOrg();
     const [isSaving, setIsSaving] = useState(false);
     const { toast } = useToast();
 
     const handleDownload = useCallback(async () => {
+        if (!org) {
+            return;
+        }
+
         setIsSaving(true);
 
         try {
-            await buildUsageCSV({
+            const downloaded = await downloadUsageCSV({
+                orgName: org?.slug ?? org?.id,
                 attributionId: AttributionId.render(attributionId),
                 from: startDate.startOf("day").valueOf(),
                 to: endDate.startOf("day").valueOf(),
             });
+
+            if (!downloaded) {
+                toast("There are no usage records for that date range");
+            }
         } catch (e) {
-            toast("There was a problem downloading usage data");
+            console.error(e);
+            toast(`There was a problem downloading your usage data: ${e.message}`);
         }
 
         setIsSaving(false);
-    }, [attributionId, endDate, startDate, toast]);
+    }, [attributionId, endDate, org, startDate, toast]);
 
     return (
         <Button type="secondary" loading={isSaving} onClick={handleDownload}>
--- components/dashboard/src/usage/download/download-usage-csv.ts
+++ components/dashboard/src/usage/download/download-usage-csv.ts
@@ -8,15 +8,22 @@ import { ListUsageRequest } from "@gitpod/gitpod-protocol/lib/usage";
 import { getAllUsageRecords } from "./get-usage-records";
 import { UsageCSVRow, transformUsageRecord } from "./transform-usage-record";
 import { saveAs } from "file-saver";
+import dayjs from "dayjs";
 
-type Args = Pick<ListUsageRequest, "attributionId" | "from" | "to">;
+type Args = Pick<ListUsageRequest, "attributionId" | "from" | "to"> & {
+    orgName: string;
+};
 
-export const buildUsageCSV = async ({ attributionId, from, to }: Args) => {
+export const downloadUsageCSV = async ({ attributionId, from, to, orgName }: Args) => {
     const records = await getAllUsageRecords({
         attributionId,
         from,
         to,
     });
+
+    if (records.length === 0) {
+        return false;
+    }
 
     const rows = records.map(transformUsageRecord).filter(Boolean) as UsageCSVRow[];
     const fields = Object.keys(rows[0]) as (keyof UsageCSVRow)[];
@@ -36,8 +43,13 @@ export const buildUsageCSV = async ({ attributionId, from, to }: Args) => {
         type: "text/csv;charset=utf-8",
     });
 
+    const start = dayjs(from).format("YYYYMMDD");
+    const end = dayjs(to).format("YYYYMMDD");
+
     // TODO: generate filename based on args
-    const filename = `usage.csv`;
+    const filename = `gitpod-usage-${orgName}-${start}-${end}.csv`;
 
     saveAs(blob, filename);
+
+    return true;
 };
