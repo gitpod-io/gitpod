43d6fe6f7a1ad0f671f47c9fc3e62f7e0473a4cd
Milan Pavlik <pavlik.mil@gmail.com>
2023-05-31 10:02:27 +0000

    fix

--- components/server/src/session-handler.ts
+++ components/server/src/session-handler.ts
@@ -101,11 +101,25 @@ export class SessionHandler {
                 // req.session.id is alias for req.sessionID
                 // https://github.com/expressjs/session/blob/master/README.md?plain=1#LL396C9-L396C19
                 id: req.sessionID,
-                regenerate: (cb) => session,
-                destroy: (cb) => session,
-                reload: (cb) => session,
+                regenerate: (cb) => {
+                    cb(null);
+                    return session;
+                },
+                destroy: (cb) => {
+                    cb(null);
+                    return session;
+                },
+                reload: (cb) => {
+                    cb(null);
+                    return session;
+                },
                 resetMaxAge: () => session,
-                save: (cb) => session,
+                save: (cb) => {
+                    if (cb) {
+                        cb(null);
+                    }
+                    return session;
+                },
                 touch: () => session,
             };
             req.session = session;
