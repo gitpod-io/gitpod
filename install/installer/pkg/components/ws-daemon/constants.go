4f37b4ccc387150b02e7fc59774d4ecf3c00baff
Thomas Schubart <thomas@gitpod.io>
2023-06-06 14:41:47 +0000

    [installer] Remove constant

--- install/installer/pkg/components/ws-daemon/constants.go
+++ install/installer/pkg/components/ws-daemon/constants.go
@@ -9,7 +9,6 @@ import "github.com/gitpod-io/gitpod/common-go/baseserver"
 const (
 	Component               = "ws-daemon"
 	ServicePort             = 8080
-	HostWorkingArea         = "/var/gitpod/workspaces"
 	HostWorkingAreaMk2      = "/var/gitpod/workspaces-mk2"
 	ContainerWorkingAreaMk2 = "/mnt/workingarea-mk2"
 	HostBackupPath          = "/var/gitpod/tmp/backup"
