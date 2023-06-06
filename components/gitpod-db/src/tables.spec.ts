0c4be908db896e3b7a02b923832e7738ccdd006b
Alex Tugarev <alex@gitpod.io>
2023-06-06 18:08:57 +0200

    [gitpod-db] Fix tables.ts (#17841)

---
+++ components/gitpod-db/src/tables.spec.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License.AGPL.txt in the project root for license information.
+ */
+
+import * as chai from "chai";
+const expect = chai.expect;
+import { suite, test, timeout } from "mocha-typescript";
+
+import { GitpodTableDescriptionProvider } from "./tables";
+
+@suite.only
+class TablesSpec {
+    async before() {}
+
+    async after() {}
+
+    @test(timeout(10000))
+    public async createAndFindATeam() {
+        const thing = new GitpodTableDescriptionProvider();
+        try {
+            thing.getSortedTables();
+        } catch (error) {
+            expect.fail(error);
+        }
+    }
+}
+
+module.exports = new TablesSpec();
