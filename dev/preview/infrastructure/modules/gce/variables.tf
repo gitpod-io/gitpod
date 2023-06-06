b0e598e9055bb0f3c67e6d00cb1bfcca9b125209
Kyle Brennan <kyle@gitpod.io>
2023-06-06 19:57:00 +0000

    [preview] update image for gen99

--- dev/preview/infrastructure/modules/gce/variables.tf
+++ dev/preview/infrastructure/modules/gce/variables.tf
@@ -44,7 +44,7 @@ variable "harvester_ingress_ip" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202305232029"
+  default     = "gitpod-k3s-202306061841"
 }
 
 variable "cert_issuer" {
--- dev/preview/infrastructure/variables.tf
+++ dev/preview/infrastructure/variables.tf
@@ -35,7 +35,7 @@ variable "vm_type" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202305232029"
+  default     = "gitpod-k3s-202306061841"
 }
 
 variable "cert_issuer" {
