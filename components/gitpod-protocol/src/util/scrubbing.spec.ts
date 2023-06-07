3e7affa06c04b44e05ccc572d01b5d41243a2ea6
akosyakov <anton@gitpod.io>
2023-06-07 11:32:01 +0000

    keyvalue tests

--- components/gitpod-protocol/src/util/scrubbing.spec.ts
+++ components/gitpod-protocol/src/util/scrubbing.spec.ts
@@ -6,7 +6,7 @@
 
 import * as chai from "chai";
 import { suite, test } from "mocha-typescript";
-import { scrubValue } from "./scrubbing";
+import { scrubKeyValue, scrubValue } from "./scrubbing";
 const expect = chai.expect;
 
 @suite
@@ -21,6 +21,19 @@ export class ScrubbingTest {
         expect(scrubValue("The email is foo@bar.com or bar@foo.com")).to.equal(
             "The email is [redacted:md5:f3ada405ce890b6f8204094deb12d8a8:email] or [redacted:md5:dc8a42aba3651b0b1f088ef928ff3b1d:email]",
         );
+    }
+
+    @test public testKeyValue_Email() {
+        expect(scrubKeyValue("email", "testvalue")).to.equal("[redacted:md5:e9de89b0a5e9ad6efd5e5ab543ec617c]");
+    }
+    @test public testKeyValue_AuthorEmail() {
+        expect(scrubKeyValue("author_email", "testvalue")).to.equal("[redacted:md5:e9de89b0a5e9ad6efd5e5ab543ec617c]");
+    }
+    @test public testKeyValue_Token() {
+        expect(scrubKeyValue("token", "testvalue")).to.equal("[redacted]");
+    }
+    @test public testKeyValue_OwnerToken() {
+        expect(scrubKeyValue("owner_token", "testvalue")).to.equal("[redacted]");
     }
 }
 module.exports = new ScrubbingTest();
