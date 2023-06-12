fc9ed7034106e618beb53648d5d8cdfdbef5c543
Milan Pavlik <pavlik.mil@gmail.com>
2023-06-12 06:59:09 +0000

    [dashboard] Always use Public API for workspace stop

--- components/dashboard/src/data/workspaces/stop-workspace-mutation.ts
+++ components/dashboard/src/data/workspaces/stop-workspace-mutation.ts
@@ -6,22 +6,16 @@
 
 import { useMutation } from "@tanstack/react-query";
 import { workspacesService } from "../../service/public-api";
-import { getGitpodService } from "../../service/service";
-import { useFeatureFlag } from "../featureflag-query";
 
 type StopWorkspaceArgs = {
     workspaceId: string;
 };
 
 export const useStopWorkspaceMutation = () => {
-    const usePublicApiWorkspacesService = useFeatureFlag("publicApiExperimentalWorkspaceService");
-
     // No need to manually update workspace in cache here, we'll receive messages over the ws that will update it
     return useMutation({
         mutationFn: async ({ workspaceId }: StopWorkspaceArgs) => {
-            return usePublicApiWorkspacesService
-                ? workspacesService.stopWorkspace({ workspaceId })
-                : getGitpodService().server.stopWorkspace(workspaceId);
+            return workspacesService.stopWorkspace({ workspaceId });
         },
     });
 };
