f312a0f6cc8786905c16695d73e4b24129fb500f
filiptronicek <filip@gitpod.io>
2023-06-21 09:24:57 +0000

    Remove access token method

--- components/gitpod-cli/cmd/ssh.go
+++ components/gitpod-cli/cmd/ssh.go
@@ -10,25 +10,18 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gitpod-io/gitpod/common-go/util"
 	"github.com/gitpod-io/gitpod/gitpod-cli/pkg/gitpod"
-	serverapi "github.com/gitpod-io/gitpod/gitpod-protocol"
-	supervisor "github.com/gitpod-io/gitpod/supervisor/api"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"golang.org/x/xerrors"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
-
-var accessToken bool
 
 // sshCmd commands collection
 var sshCmd = &cobra.Command{
 	Use:   "ssh",
 	Short: "Show the SSH connection command for the current workspace",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Long: `The command which is outputted requires SSH keys to be configured with Gitpod.
+See https://www.gitpod.io/docs/configure/user-settings/ssh for a guide on setting them up.
+`, RunE: func(cmd *cobra.Command, args []string) error {
 		ctx, cancel := context.WithTimeout(cmd.Context(), 10*time.Second)
 		defer cancel()
 
@@ -41,47 +34,6 @@ var sshCmd = &cobra.Command{
 		sshKeyHost := fmt.Sprintf(`%s@%s`, wsInfo.WorkspaceId, host)
 
 		sshHost := sshKeyHost
-
-		if accessToken {
-			var err error
-
-			supervisorConn, err := grpc.Dial(util.GetSupervisorAddress(), grpc.WithTransportCredentials(insecure.NewCredentials()))
-			if err != nil {
-				return xerrors.Errorf("failed connecting to supervisor: %w", err)
-			}
-			defer supervisorConn.Close()
-			clientToken, err := supervisor.NewTokenServiceClient(supervisorConn).GetToken(ctx, &supervisor.GetTokenRequest{
-				Host: wsInfo.GitpodApi.Host,
-				Kind: "gitpod",
-				Scope: []string{
-					"function:getOwnerToken",
-				},
-			})
-			if err != nil {
-				return xerrors.Errorf("failed getting token from supervisor: %w", err)
-			}
-
-			serverLog := log.NewEntry(log.StandardLogger())
-
-			client, err := serverapi.ConnectToServer(wsInfo.GitpodApi.Endpoint, serverapi.ConnectToServerOpts{
-				Token:   clientToken.Token,
-				Context: ctx,
-				Log:     serverLog,
-			})
-			if err != nil {
-				return xerrors.Errorf("failed connecting to server: %w", err)
-			}
-
-			ownerToken, err := client.GetOwnerToken(ctx, wsInfo.WorkspaceId)
-			if err != nil {
-				fmt.Println("failed getting owner token from server: %w", err)
-			}
-
-			fmt.Println(ownerToken)
-
-			sshHost = fmt.Sprintf(`%s#%s@%s`, wsInfo.WorkspaceId, ownerToken, host)
-		}
-
 		sshCommand := fmt.Sprintf(`ssh '%s'`, sshHost)
 		fmt.Println(sshCommand)
 
@@ -91,5 +43,4 @@ var sshCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(sshCmd)
-	sshCmd.Flags().BoolVar(&accessToken, "access-token", false, "Show the SSH access token command instead")
 }
