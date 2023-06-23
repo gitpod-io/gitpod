828f1c6a7a09df8b30ef070ce105f8821bbd331e
geropl <gero@gitpod.io>
2023-06-23 08:33:54 +0000

    [dashboard] Add doRetryUserLoader feature flag

--- components/dashboard/src/hooks/use-user-loader.ts
+++ components/dashboard/src/hooks/use-user-loader.ts
@@ -12,9 +12,11 @@ import { refreshSearchData } from "../components/RepositoryFinder";
 import { useQuery } from "@tanstack/react-query";
 import { noPersistence } from "../data/setup";
 import { ErrorCodes } from "@gitpod/gitpod-protocol/lib/messaging/error";
+import { useFeatureFlag } from "../data/featureflag-query";
 
 export const useUserLoader = () => {
     const { user, setUser } = useContext(UserContext);
+    const doRetryUserLoader = useFeatureFlag("doRetryUserLoader");
 
     // For now, we're using the user context to store the user, but letting react-query handle the loading
     // In the future, we should remove the user context and use react-query to access the user
@@ -29,8 +31,14 @@ export const useUserLoader = () => {
         useErrorBoundary: true,
         // It's important we don't retry as we want to show the login screen as quickly as possible if a 401
         retry: (_failureCount: number, error: Error & { code?: number }) => {
+            if (!doRetryUserLoader) {
+                return false;
+            }
             return error.code !== ErrorCodes.NOT_AUTHENTICATED;
         },
+        // docs: https://tanstack.com/query/v4/docs/react/guides/query-retries
+        // backoff by doubling, max. 10s
+        retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 10000),
         cacheTime: 1000 * 60 * 60 * 1, // 1 hour
         staleTime: 1000 * 60 * 60 * 1, // 1 hour
         onSuccess: (loadedUser) => {
