41abfc1a86ddf84114165b063a6529b348c74735
Gero Posmyk-Leinemann <32448529+geropl@users.noreply.github.com>
2023-06-23 11:35:13 +0200

    [dashboard] retry useUserQuery if error code is not 401 – WEB-560 (#18033)
    * [dashboard] retry useUserQuery if error code is not 401
    
    * [dashboard] Add doRetryUserLoader feature flag
    
    ---------
    
    Co-authored-by: Alex Tugarev <alex@gitpod.io>

--- components/dashboard/src/data/featureflag-query.ts
+++ components/dashboard/src/data/featureflag-query.ts
@@ -24,6 +24,7 @@ const featureFlags = {
     enableDedicatedOnboardingFlow: false,
     usageDownload: false,
     phoneVerificationByCall: false,
+    doRetryUserLoader: true,
 };
 
 export const useFeatureFlag = (featureFlag: keyof typeof featureFlags) => {
