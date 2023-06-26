8c2faaab241925c97fe26889a18541d439fd078d
WVerlaek <wouter@gitpod.io>
2023-06-26 09:07:04 +0000

    WIP

--- test/tests/components/ws-daemon/network_limiting_test.go
+++ test/tests/components/ws-daemon/network_limiting_test.go
@@ -7,7 +7,10 @@ package wsdaemon
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/gitpod-io/gitpod/test/pkg/integration"
+	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -17,7 +20,48 @@ func TestNetworkdLimiting(t *testing.T) {
 		WithLabel("component", "ws-daemon").
 		Assess("verify if network limiting works fine", func(testCtx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			t.Parallel()
-			t.Skip("unimplemented")
+
+			ctx, cancel := context.WithTimeout(testCtx, 5*time.Minute)
+			defer cancel()
+
+			api := integration.NewComponentAPI(ctx, cfg.Namespace(), kubeconfig, cfg.Client())
+			t.Cleanup(func() {
+				api.Done(t)
+			})
+
+			ws, stopWs, err := integration.LaunchWorkspaceDirectly(t, ctx, api)
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Cleanup(func() {
+				sctx, scancel := context.WithTimeout(context.Background(), 5*time.Minute)
+				defer scancel()
+
+				sapi := integration.NewComponentAPI(sctx, cfg.Namespace(), kubeconfig, cfg.Client())
+				defer sapi.Done(t)
+
+				_, err = stopWs(true, sapi)
+				if err != nil {
+					t.Fatal(err)
+				}
+			})
+
+			daemonClient, daemonCloser, err := integration.Instrument(integration.ComponentWorkspaceDaemon, "daemon", cfg.Namespace(), kubeconfig, cfg.Client(),
+				integration.WithWorkspacekitLift(false),
+				integration.WithContainer("ws-daemon"))
+			if err != nil {
+				t.Fatalf("unexpected error instrumenting daemon: %v", err)
+			}
+			defer daemonClient.Close()
+			integration.DeferCloser(t, daemonCloser)
+
+			var pod corev1.Pod
+			if err := cfg.Client().Resources().Get(ctx, "ws-"+ws.Req.Id, cfg.Namespace(), &pod); err != nil {
+				t.Fatal(err)
+			}
+
+			// containerId := getWorkspaceContainerId(&pod)
+
 			return testCtx
 		}).Feature()
 
