ae9037cab3d77da5f7de7db7e83f5282ccbacf00
Milan Pavlik <pavlik.mil@gmail.com>
2023-06-27 17:03:41 +0200

    Revert "[redis] Use chainguard image  WEB-574 (#18067)"
    This reverts commit 664e19c741e7842a70dee25fdd87efa2c609ea73.

--- install/installer/pkg/components/redis/constants.go
+++ install/installer/pkg/components/redis/constants.go
@@ -4,27 +4,21 @@
 
 package redis
 
-import "github.com/opencontainers/go-digest"
-
 const (
 	Component = "redis"
 
 	PortName = "api"
 	Port     = 6379
 
-	RegistryRepo  = "cgr.dev"
-	RegistryImage = "chainguard/redis"
+	RegistryRepo  = "registry.hub.docker.com"
+	RegistryImage = "library/redis"
+	ImageTag      = "7.0.8"
 
 	ContainerName = "redis"
 
-	ExporterRegistryImage = "chainguard/prometheus-redis-exporter"
-
+	ExporterRegistryImage = "oliver006/redis_exporter"
+	ExporterImageTag      = "v1.48.0"
 	ExporterContainerName = "exporter"
 	ExporterPortName      = "exporter"
 	ExporterPort          = 9500
-)
-
-var (
-	ImageDigest         = digest.Digest("sha256:d2802dec2f97b47c7fb34eee0b4d658fd0574577271e5e426e2d949cc3d47110")
-	ExporterImageDigest = digest.Digest("sha256:30948f2fe1bee4bd13d33061f0abbbd1d045b69ec4e02b6213af8709ea7bb710")
 )
