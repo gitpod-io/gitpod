5c721b3f94d9f4fd2d6c562b9cd15e6d10b5341c
Milan Pavlik <milan@gitpod.io>
2023-06-27 08:05:18 +0000

    Fix

--- install/installer/pkg/components/redis/constants.go
+++ install/installer/pkg/components/redis/constants.go
@@ -17,13 +17,14 @@ const (
 
 	ContainerName = "redis"
 
-	ExporterRegistryImage = "oliver006/redis_exporter"
-	ExporterImageTag      = "v1.48.0"
+	ExporterRegistryImage = "chainguard/prometheus-redis-exporter"
+
 	ExporterContainerName = "exporter"
 	ExporterPortName      = "exporter"
 	ExporterPort          = 9500
 )
 
 var (
-	ImageDigest = digest.FromString("sha256:d2802dec2f97b47c7fb34eee0b4d658fd0574577271e5e426e2d949cc3d47110")
+	ImageDigest         = digest.FromString("sha256:d2802dec2f97b47c7fb34eee0b4d658fd0574577271e5e426e2d949cc3d47110")
+	ExporterImageDigest = digest.FromString("sha256:30948f2fe1bee4bd13d33061f0abbbd1d045b69ec4e02b6213af8709ea7bb710")
 )
