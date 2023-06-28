cd13f40111c6fa55cf67cc1a059179a74f2c4937
Alex Tugarev <alex@gitpod.io>
2023-06-28 08:06:00 +0000

    try withouth persist

--- components/dashboard/src/data/setup.tsx
+++ components/dashboard/src/data/setup.tsx
@@ -4,21 +4,21 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { get, set, del } from "idb-keyval";
-import {
-    PersistedClient,
-    Persister,
-    PersistQueryClientProvider,
-    PersistQueryClientProviderProps,
-} from "@tanstack/react-query-persist-client";
-import { QueryCache, QueryClient, QueryKey } from "@tanstack/react-query";
+// import { get, set, del } from "idb-keyval";
+// import {
+//     PersistedClient,
+//     Persister,
+//     PersistQueryClientProvider,
+//     PersistQueryClientProviderProps,
+// } from "@tanstack/react-query-persist-client";
+import { QueryCache, QueryClient, QueryClientProvider, QueryKey } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { FunctionComponent } from "react";
 
 // This is used to version the cache
 // If data we cache changes in a non-backwards compatible way, increment this version
 // That will bust any previous cache versions a client may have stored
-const CACHE_VERSION = "1";
+// const CACHE_VERSION = "1";
 
 export function noPersistence(queryKey: QueryKey): QueryKey {
     return [...queryKey, "no-persistence"];
@@ -36,28 +36,28 @@ export const setupQueryClientProvider = () => {
             },
         }),
     });
-    const queryClientPersister = createIDBPersister();
+    // const queryClientPersister = createIDBPersister();
 
-    const persistOptions: PersistQueryClientProviderProps["persistOptions"] = {
-        persister: queryClientPersister,
-        // This allows the client to persist up to 24 hours
-        // Individual queries may expire prior to this though
-        maxAge: 1000 * 60 * 60 * 24, // 24 hours
-        buster: CACHE_VERSION,
-        dehydrateOptions: {
-            shouldDehydrateQuery: (query) => {
-                return !isNoPersistence(query.queryKey) && query.state.status === "success";
-            },
-        },
-    };
+    // const persistOptions: PersistQueryClientProviderProps["persistOptions"] = {
+    //     persister: queryClientPersister,
+    //     // This allows the client to persist up to 24 hours
+    //     // Individual queries may expire prior to this though
+    //     maxAge: 1000 * 60 * 60 * 24, // 24 hours
+    //     buster: CACHE_VERSION,
+    //     dehydrateOptions: {
+    //         shouldDehydrateQuery: (query) => {
+    //             return !isNoPersistence(query.queryKey) && query.state.status === "success";
+    //         },
+    //     },
+    // };
 
     // Return a wrapper around PersistQueryClientProvider w/ the query client options we setp
     const GitpodQueryClientProvider: FunctionComponent = ({ children }) => {
         return (
-            <PersistQueryClientProvider client={client} persistOptions={persistOptions}>
+            <QueryClientProvider client={client}>
                 {children}
                 <ReactQueryDevtools initialIsOpen={false} />
-            </PersistQueryClientProvider>
+            </QueryClientProvider>
         );
     };
 
@@ -65,41 +65,41 @@ export const setupQueryClientProvider = () => {
 };
 
 // Persister that uses IndexedDB
-function createIDBPersister(idbValidKey: IDBValidKey = "gitpodQueryClient"): Persister {
-    // Track a flag that indicates if we're attempting to persist the client
-    // Some browsers/versions don't support using indexed-db w/ certain settings or in private mode
-    // If we get an error performing an operation, we'll disable persistance and assume it's not supported
-    let persistanceActive = true;
+// function createIDBPersister(idbValidKey: IDBValidKey = "gitpodQueryClient"): Persister {
+//     // Track a flag that indicates if we're attempting to persist the client
+//     // Some browsers/versions don't support using indexed-db w/ certain settings or in private mode
+//     // If we get an error performing an operation, we'll disable persistance and assume it's not supported
+//     let persistanceActive = true;
 
-    return {
-        persistClient: async (client: PersistedClient) => {
-            if (!persistanceActive) {
-                return;
-            }
+//     return {
+//         persistClient: async (client: PersistedClient) => {
+//             if (!persistanceActive) {
+//                 return;
+//             }
 
-            try {
-                await set(idbValidKey, client);
-            } catch (e) {
-                console.error("unable to persist query client");
-                persistanceActive = false;
-            }
-        },
-        restoreClient: async () => {
-            try {
-                const client = await get<PersistedClient>(idbValidKey);
-                return client;
-            } catch (e) {
-                console.error("unable to load query client from cache");
-                persistanceActive = false;
-            }
-        },
-        removeClient: async () => {
-            try {
-                await del(idbValidKey);
-            } catch (e) {
-                console.error("unable to remove query client");
-                persistanceActive = false;
-            }
-        },
-    };
-}
+//             try {
+//                 await set(idbValidKey, client);
+//             } catch (e) {
+//                 console.error("unable to persist query client");
+//                 persistanceActive = false;
+//             }
+//         },
+//         restoreClient: async () => {
+//             try {
+//                 const client = await get<PersistedClient>(idbValidKey);
+//                 return client;
+//             } catch (e) {
+//                 console.error("unable to load query client from cache");
+//                 persistanceActive = false;
+//             }
+//         },
+//         removeClient: async () => {
+//             try {
+//                 await del(idbValidKey);
+//             } catch (e) {
+//                 console.error("unable to remove query client");
+//                 persistanceActive = false;
+//             }
+//         },
+//     };
+// }
