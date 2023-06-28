d47884f702490d2e26d611d0a925601e4bb06247
Milan Pavlik <milan@gitpod.io>
2023-06-28 13:28:44 +0000

    fix

--- components/server/src/projects/projects-service.ts
+++ components/server/src/projects/projects-service.ts
@@ -235,14 +235,6 @@ export class ProjectsService {
             }
             throw err;
         }
-
-        this.analytics.track({
-            userId: callerID,
-            event: "project_deleted",
-            properties: {
-                project_id: projectId,
-            },
-        });
     }
 
     async findPrebuilds(params: FindPrebuildsParams): Promise<PrebuildWithStatus[]> {
