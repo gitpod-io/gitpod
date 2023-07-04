56d95d7fab2d3bcc5b398d94b826bccc5fc4163f
Milan Pavlik <milan@gitpod.io>
2023-07-04 15:51:21 +0200

    [ws-man-bridge] Publish ws-instance-update - WEB-592 (#18162)
    * [ws-man-bridge] Publish ws-instance-update
    
    * tests
    
    * fix
    
    * fix

--- components/ws-manager-bridge/src/redis/publisher.ts
+++ components/ws-manager-bridge/src/redis/publisher.ts
@@ -7,19 +7,33 @@
 import { inject, injectable } from "inversify";
 import { log } from "@gitpod/gitpod-protocol/lib/util/logging";
 import { Metrics } from "../metrics";
+import { RedisClient } from "./client";
+import { RedisWorkspaceInstanceUpdate, WorkspaceInstanceUpdatesChannel } from "@gitpod/gitpod-protocol";
 
 @injectable()
 export class RedisPublisher {
-    constructor(@inject(Metrics) private readonly metrics: Metrics) {}
+    constructor(
+        @inject(RedisClient) private readonly client: RedisClient,
+        @inject(Metrics) private readonly metrics: Metrics,
+    ) {}
 
     async publishPrebuildUpdate(): Promise<void> {
         log.debug("[redis] Publish prebuild udpate invoked.");
         this.metrics.reportUpdatePublished("prebuild");
     }
 
-    async publishInstanceUpdate(): Promise<void> {
-        log.debug("[redis] Publish instance udpate invoked.");
-        this.metrics.reportUpdatePublished("workspace-instance");
+    async publishInstanceUpdate(update: RedisWorkspaceInstanceUpdate): Promise<void> {
+        let err: Error | undefined;
+        try {
+            const serialized = JSON.stringify(update);
+            await this.client.get().publish(WorkspaceInstanceUpdatesChannel, serialized);
+            log.debug("[redis] Succesfully published instance update.", update);
+        } catch (e) {
+            err = e;
+            log.error("[redis] Failed to publish instance update.", e, update);
+        } finally {
+            this.metrics.reportUpdatePublished("workspace-instance", err);
+        }
     }
 
     async publishHeadlessUpdate(): Promise<void> {
