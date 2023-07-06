b7b98d47ffd23bad5d39e6863281284199b3ce21
Pudong Zheng <tianshi8650@gmail.com>
2023-07-06 15:34:38 +0000

    move retry logic to listener

--- components/supervisor/pkg/supervisor/docker.go
+++ components/supervisor/pkg/supervisor/docker.go
@@ -40,6 +40,10 @@ const (
 	dockerUpLogFilePath = logsDir + "/docker-up.log"
 )
 
+var (
+	failedForRetryError = errors.New("failed for retry start docker-up")
+)
+
 func socketActivationForDocker(parentCtx context.Context, wg *sync.WaitGroup, term *terminal.Mux, cfg *Config, telemetry analytics.Writer, notifications *NotificationService) {
 	defer wg.Done()
 
@@ -101,74 +105,42 @@ func socketActivationForDocker(parentCtx context.Context, wg *sync.WaitGroup, te
 		return gpCmd.Run()
 	}
 
-	burstAttempts := 0
-	backoffStrategy := backoff.NewExponentialBackOff()
-	backoffStrategy.MaxInterval = maxIntervalBetweenDockerStart
-	var lastExitErrorTime time.Time
+	var stdout, stderr io.Writer
+
+	stdout = os.Stdout
+	stderr = os.Stderr
+	if cfg.WorkspaceLogRateLimit > 0 {
+		limit := int64(cfg.WorkspaceLogRateLimit)
+		stdout = dropwriter.Writer(stdout, dropwriter.NewBucket(limit*1024*3, limit*1024))
+		stderr = dropwriter.Writer(stderr, dropwriter.NewBucket(limit*1024*3, limit*1024))
+		log.WithField("limit_kb_per_sec", limit).Info("docker-up: rate limiting log output")
+	}
+
+	if logFile != nil {
+		stdout = io.MultiWriter(stdout, logFile)
+		stderr = io.MultiWriter(stderr, logFile)
+	}
 
 	for ctx.Err() == nil {
-		err := listenToDockerSocket(ctx, term, cfg, telemetry, logFile)
+		err := listenToDockerSocket(ctx, term, cfg, telemetry, logFile, stdout, stderr)
 		if ctx.Err() != nil {
 			return
 		}
 
-		var exitError *exec.ExitError
-		if err != nil {
-			exitError, _ = err.(*exec.ExitError)
-		}
-		if exitError != nil && exitError.ExitCode() > 0 {
-			// docker-up or daemon exited with an error - we'll try again
-			// it can be a transient condition like apt is locked by another process to install prerequisites
-			// or permament like misconfigured env var, image which does not allow to install prerequisites at all
-			// or any general issue with docker daemon startup
-			if time.Since(lastExitErrorTime) <= dockerStartErrorBurstDuration {
-				// we're in the eixt error burst
-				burstAttempts++
-			} else {
-				// no burst, reset the counter
-				burstAttempts = 0
-				backoffStrategy.Reset()
-			}
-			lastExitErrorTime = time.Now()
-
+		if errors.Is(err, failedForRetryError) {
 			go func() {
 				notifyErr := notifyDockerUpFail()
 				if notifyErr != nil {
 					log.WithError(notifyErr).Error("cannot notify about docker-up failure")
 				}
 			}()
-		} else {
-			// transient condition like a docker daemon get killed because of OOM
-			burstAttempts = 0
-			backoffStrategy.Reset()
 		}
-		nextBackOff := backoffStrategy.NextBackOff()
-
-		if nextBackOff == backoff.Stop || burstAttempts >= maxBurstDockerStartAttempts {
-			cancel()
-			log.WithError(err).WithField("attempts", burstAttempts).Error("cannot activate docker after maximum attempts, stopping trying permanently")
-			if logFile != nil {
-				fmt.Fprintln(logFile, "Cannot activate docker after maximum attempts, stopping trying permanently.")
-				fmt.Fprintln(logFile, "Please check logs above, fix the configuration, use `gp validate` to verify change, and commit to apply.")
-				fmt.Fprintf(logFile, "If it does not help, please reach out to the support. Don't forget to share your workspace ID: %s.\n", cfg.WorkspaceID)
-			}
-			continue
-		}
-
-		if err != nil &&
-			!errors.Is(err, context.Canceled) &&
-			!process.IsNotChildProcess(err) &&
-			!strings.Contains(err.Error(), "signal: ") {
-			// don't log typical transient errors
-			log.WithError(err).WithField("attempts", burstAttempts).WithField("backoff", nextBackOff.String()).Error("cannot activate docker, retrying...")
-		}
-		time.Sleep(nextBackOff)
 	}
 }
 
 // listenToDockerSocket listens to the docker socket and starts the docker-up process
-// if it fails to start then listener is closed to requests all incoming requests and prevent infinite try
-func listenToDockerSocket(parentCtx context.Context, term *terminal.Mux, cfg *Config, telemetry analytics.Writer, logFile *os.File) error {
+// if it fails to start with sometimes retry then listener is closed to requests all incoming requests and prevent infinite try
+func listenToDockerSocket(parentCtx context.Context, term *terminal.Mux, cfg *Config, telemetry analytics.Writer, logFile *os.File, stdout, stderr io.Writer) error {
 	ctx, cancel := context.WithCancel(parentCtx)
 	defer cancel()
 
@@ -184,60 +156,106 @@ func listenToDockerSocket(parentCtx context.Context, term *terminal.Mux, cfg *Co
 	}()
 
 	_ = os.Chown(fn, gitpodUID, gitpodGID)
-	return activation.Listen(ctx, l, func(socketFD *os.File) error {
-		defer socketFD.Close()
-		startTime := time.Now()
-		telemetry.Track(analytics.TrackMessage{
-			Identity: analytics.Identity{UserID: cfg.OwnerId},
-			Event:    "gitpod_activate_docker",
-			Properties: map[string]interface{}{
-				"instanceId":     cfg.WorkspaceInstanceID,
-				"workspaceId":    cfg.WorkspaceID,
-				"debugWorkspace": cfg.isDebugWorkspace(),
-			},
+
+	var lastExitErrorTime time.Time
+	burstAttempts := 0
+	backoffStrategy := backoff.NewExponentialBackOff()
+	backoffStrategy.MaxInterval = maxIntervalBetweenDockerStart
+
+	for ctx.Err() == nil {
+		err = activation.Listen(ctx, l, func(socketFD *os.File) error {
+			defer socketFD.Close()
+			startTime := time.Now()
+			telemetry.Track(analytics.TrackMessage{
+				Identity: analytics.Identity{UserID: cfg.OwnerId},
+				Event:    "gitpod_activate_docker",
+				Properties: map[string]interface{}{
+					"instanceId":     cfg.WorkspaceInstanceID,
+					"workspaceId":    cfg.WorkspaceID,
+					"debugWorkspace": cfg.isDebugWorkspace(),
+				},
+			})
+
+			if logFile != nil {
+				defer fmt.Fprintf(logFile, "\n======= Stop docker-up at %s =======\n", time.Now().Format(time.RFC3339))
+				fmt.Fprintf(logFile, "======= Start docker-up at %s =======\n", startTime.Format(time.RFC3339))
+			}
+
+			cmd := exec.CommandContext(ctx, "/usr/bin/docker-up")
+			cmd.Env = append(os.Environ(), "LISTEN_FDS=1")
+			cmd.ExtraFiles = []*os.File{socketFD}
+			cmd.Stdout = stdout
+			cmd.Stderr = stderr
+
+			err = cmd.Start()
+			if err != nil {
+				return err
+			}
+			ptyCtx, cancel := context.WithCancel(ctx)
+			go func(ptyCtx context.Context) {
+				select {
+				case <-ctx.Done():
+					_ = cmd.Process.Signal(syscall.SIGTERM)
+				case <-ptyCtx.Done():
+				}
+			}(ptyCtx)
+			err = cmd.Wait()
+			cancel()
+			return err
 		})
 
-		var stdout, stderr io.Writer
-
-		stdout = os.Stdout
-		stderr = os.Stderr
-		if cfg.WorkspaceLogRateLimit > 0 {
-			limit := int64(cfg.WorkspaceLogRateLimit)
-			stdout = dropwriter.Writer(stdout, dropwriter.NewBucket(limit*1024*3, limit*1024))
-			stderr = dropwriter.Writer(stderr, dropwriter.NewBucket(limit*1024*3, limit*1024))
-			log.WithField("limit_kb_per_sec", limit).Info("docker-up: rate limiting log output")
+		if ctx.Err() != nil {
+			return ctx.Err()
 		}
 
-		if logFile != nil {
-			defer fmt.Fprintf(logFile, "\n======= Stop docker-up at %s =======\n", time.Now().Format(time.RFC3339))
-			fmt.Fprintf(logFile, "======= Start docker-up at %s =======\n", startTime.Format(time.RFC3339))
-
-			stdout = io.MultiWriter(stdout, logFile)
-			stderr = io.MultiWriter(stderr, logFile)
-		}
-
-		cmd := exec.CommandContext(ctx, "/usr/bin/docker-up")
-		cmd.Env = append(os.Environ(), "LISTEN_FDS=1")
-		cmd.ExtraFiles = []*os.File{socketFD}
-		cmd.Stdout = stdout
-		cmd.Stderr = stderr
-
-		err = cmd.Start()
+		var exitError *exec.ExitError
 		if err != nil {
-			return err
+			exitError, _ = err.(*exec.ExitError)
 		}
-		ptyCtx, cancel := context.WithCancel(ctx)
-		go func(ptyCtx context.Context) {
-			select {
-			case <-ctx.Done():
-				_ = cmd.Process.Signal(syscall.SIGTERM)
-			case <-ptyCtx.Done():
+		if exitError != nil && exitError.ExitCode() > 0 {
+			// docker-up or daemon exited with an error - we'll try again
+			// it can be a transient condition like apt is locked by another process to install prerequisites
+			// or permament like misconfigured env var, image which does not allow to install prerequisites at all
+			// or any general issue with docker daemon startup
+			if time.Since(lastExitErrorTime) <= dockerStartErrorBurstDuration {
+				// we're in the exit error burst
+				burstAttempts++
+			} else {
+				// no burst, reset the counter
+				burstAttempts = 0
+				backoffStrategy.Reset()
 			}
-		}(ptyCtx)
-		err = cmd.Wait()
-		cancel()
-		return err
-	})
+			lastExitErrorTime = time.Now()
+
+		} else {
+			// transient condition like a docker daemon get killed because of OOM
+			burstAttempts = 0
+			backoffStrategy.Reset()
+		}
+
+		nextBackOff := backoffStrategy.NextBackOff()
+
+		if nextBackOff == backoff.Stop || burstAttempts >= maxBurstDockerStartAttempts {
+			cancel()
+			log.WithError(err).WithField("attempts", burstAttempts).Error("cannot activate docker after maximum attempts, stopping trying permanently")
+			if logFile != nil {
+				fmt.Fprintln(logFile, "Cannot activate docker after maximum attempts, stopping trying permanently.")
+				fmt.Fprintln(logFile, "Please check logs above, fix the configuration, use `gp validate` to verify change, and commit to apply.")
+				fmt.Fprintf(logFile, "If it does not help, please reach out to the support. Don't forget to share your workspace ID: %s.\n", cfg.WorkspaceID)
+			}
+			return failedForRetryError
+		}
+
+		if err != nil &&
+			!errors.Is(err, context.Canceled) &&
+			!process.IsNotChildProcess(err) &&
+			!strings.Contains(err.Error(), "signal: ") {
+			// don't log typical transient errors
+			log.WithError(err).WithField("attempts", burstAttempts).WithField("backoff", nextBackOff.String()).Error("cannot activate docker, retrying...")
+		}
+		time.Sleep(nextBackOff)
+	}
+	return ctx.Err()
 }
 
 func openDockerUpLogFile() (*os.File, error) {
