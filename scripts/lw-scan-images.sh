2882ae5e97f8d1ee389ce64df0e965eb867058e6
geropl <gero@gitpod.io>
2023-07-06 09:23:36 +0000

    EXCLUDE_DOCKER_IO

--- scripts/lw-scan-images.sh
+++ scripts/lw-scan-images.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
+if [[ -z "$VERSION" ]]; then
+  echo "VERSION env var is required"
+  exit 1
+fi
+
+if [[ -z "$LW_ACCESS_TOKEN" ]]; then
+  echo "LW_ACCESS_TOKEN env var is required"
+  exit 1
+fi
+
+EXCLUDE_DOCKER_IO="${EXCLUDE_DOCKER_IO:-"false"}"
+
 TMP=$(mktemp -d)
 echo "workdir: $TMP"
 
@@ -27,11 +39,12 @@ echo "Found $(cat "$TMP/images.txt" | wc -l) images to scan"
 # There, we can see the results in the "Vulnerabilities" tab, by searching for the Gitpod version
 # Note: Does not fail on CVEs!
 while IFS= read -r IMAGE_REF; do
-  # Skip images with this prefix
-  # TODO(gpl) Unclear why we can't access the docker.io images; it works from a workspace?
-  if [[ "$IMAGE_REF" == "docker.io/"* ]]; then
-    echo "Skipping $IMAGE_REF"
-    continue
+  # TODO(gpl) Unclear why we can't access the docker.io images the GitHub workflow; it works from a workspace?
+  if [[ "$EXCLUDE_DOCKER_IO" == "true" ]]; then
+    if [[ "$IMAGE_REF" == "docker.io/"* ]]; then
+      echo "Skipping docker.io image: $IMAGE_REF"
+      continue
+    fi
   fi
 
   NAME=$(echo "$IMAGE_REF" | cut -d ":" -f 1)
