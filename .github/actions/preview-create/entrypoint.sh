05d41b4da9ea639ad122a446bb77890615f44b59
Alejandro de Brito Fontes <aledbf@gmail.com>
2023-07-15 07:02:30 +0000

    cleanup

--- .github/actions/preview-create/entrypoint.sh
+++ .github/actions/preview-create/entrypoint.sh
@@ -2,14 +2,11 @@
 
 set -euo pipefail
 
-export PREVIEW_ENV_DEV_SA_KEY_PATH="$HOME/.config/gcloud/preview-environment-dev-sa.json"
 # shellcheck disable=SC2155
-export LEEWAY_WORKSPACE_ROOT="$(pwd)"
 export PATH="$PATH:$HOME/bin"
 
 mkdir "$HOME/bin"
 
-echo "${INPUT_SA_KEY}" >"${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 gcloud auth activate-service-account --key-file "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 
 leeway run dev/preview/previewctl:download
