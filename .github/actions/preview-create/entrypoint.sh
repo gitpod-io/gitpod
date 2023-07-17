06ee1ef8ff6dbef9e65f562e5509fc195fd06f79
Alejandro de Brito Fontes <aledbf@gmail.com>
2023-07-17 15:52:05 +0000

    24

--- .github/actions/preview-create/entrypoint.sh
+++ .github/actions/preview-create/entrypoint.sh
@@ -13,17 +13,14 @@ echo "Previewctl install-context"
 previewctl install-context --log-level debug --timeout 10m --gcp-service-account "${GOOGLE_APPLICATION_CREDENTIALS}"
 
 replace="module.preview_gce[0].google_compute_instance.default"
-if [[ "${INPUT_INFRASTRUCTURE_PROVIDER}" = "harvester " ]]; then
-  replace="module.preview_harvester[0].harvester_virtualmachine.harvester"
-fi
 
 if [[ "${INPUT_RECREATE_VM:-x}" == "true" ]]; then
   export TF_CLI_ARGS_plan="-replace=${replace}"
 fi
 
 export TF_VAR_preview_name="$(previewctl get-name --branch "${INPUT_NAME}")"
-export TF_VAR_infra_provider="${INPUT_INFRASTRUCTURE_PROVIDER}"
 export TF_VAR_with_large_vm="${INPUT_LARGE_VM}"
 export TF_INPUT=0
 export TF_IN_AUTOMATION=true
+
 leeway run dev/preview:create-preview
