f96ba5011dc1a90c3ad0699a38d354e5477e1b36
Gero Posmyk-Leinemann <32448529+geropl@users.noreply.github.com>
2023-07-28 09:23:29 +0200

    [.werft] Remove (almost) entirely (#18361)
    * [.werft] Delete superfluous files
    
    * Move post-process.sh
    
    * move vm folder
    
    * fix

--- .werft/jobs/build/const.ts
+++
@@ -1,5 +0,0 @@
-export const GCLOUD_SERVICE_ACCOUNT_PATH = "/mnt/secrets/gcp-sa/service-account.json";
-export const CORE_DEV_KUBECONFIG_PATH = "/workspace/gitpod/kubeconfigs/core-dev";
-export const HARVESTER_KUBECONFIG_PATH = "/workspace/gitpod/kubeconfigs/harvester";
-export const PREVIEW_K3S_KUBECONFIG_PATH = "/workspace/gitpod/kubeconfigs/k3s";
-export const GLOBAL_KUBECONFIG_PATH = process.env.HOME + "/.kube/config"
