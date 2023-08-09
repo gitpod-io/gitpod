b6297b30b390da9caaa3f27955089817ab0efadc
Kyle Brennan <kyle@gitpod.io>
2023-08-09 20:54:23 +0000

    [ws-daemon] Improve logging for `cannot unmount mark mount from within ws-daemon`
    Reduce the severity from error to warn

--- components/ws-daemon/pkg/daemon/markunmount.go
+++ components/ws-daemon/pkg/daemon/markunmount.go
@@ -112,7 +112,7 @@ func (c *MarkUnmountFallback) WorkspaceUpdated(ctx context.Context, ws *dispatch
 
 		err := unmountMark(ws.InstanceID)
 		if err != nil {
-			log.WithFields(ws.OWI()).WithError(err).Error("cannot unmount mark mount from within ws-daemon")
+			log.WithFields(ws.OWI()).WithError(err).Warn("cannot unmount mark mount from within ws-daemon")
 			c.activityCounter.WithLabelValues("false").Inc()
 		} else {
 			c.activityCounter.WithLabelValues("true").Inc()
