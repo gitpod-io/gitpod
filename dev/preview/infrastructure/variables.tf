f4d5b81c07b8a0efa11b47daa7fd8794e7e08d5d
Kyle Brennan <kyle@gitpod.io>
2023-08-10 13:07:42 +0000

    [preview] update VM image (again) for gen103

--- dev/preview/infrastructure/variables.tf
+++ dev/preview/infrastructure/variables.tf
@@ -35,7 +35,7 @@ variable "vm_type" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202308091714"
+  default     = "gitpod-k3s-202308101245"
 }
 
 variable "cert_issuer" {
