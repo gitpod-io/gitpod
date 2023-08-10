147586518d2103bcaf9d44cc89dd3f90ff8124ca
filiptronicek <filip@gitpod.io>
2023-08-10 21:31:45 +0000

    Update Stable

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-8c60cd3938c5559680cabc90337c4c0ed4639bec" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-722b22e60cbc82dde713d4892eeebbbe5fc348c0" // stable version that will be updated manually on demand
 	CodeHelperIDEImage          = "ide/code-codehelper"
 	CodeWebExtensionImage       = "ide/gitpod-code-web"
 	CodeWebExtensionVersion     = "commit-38829640a8b23058ea46930d861d9179cad971b7" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
