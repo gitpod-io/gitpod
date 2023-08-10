4708339890b01d8521ab802b72a12167fe31d9eb
Kyle Brennan <kyle@gitpod.io>
2023-08-10 21:43:22 +0000

    [preview] update again for gen103

--- dev/preview/infrastructure/modules/harvester/variables.tf
+++ dev/preview/infrastructure/modules/harvester/variables.tf
@@ -33,7 +33,7 @@ variable "ssh_key" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202308091714"
+  default     = "gitpod-k3s-202308102015"
 }
 
 variable "harvester_ingress_ip" {
