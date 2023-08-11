37ad723d47494a586174788390361dcf3945869b
Kyle Brennan <kyle@gitpod.io>
2023-08-11 17:12:14 +0000

    [preview] update VM image to account for kernel update
    Depends on https://github.com/gitpod-io/gitpod-packer-gcp-image/pull/245

--- dev/preview/infrastructure/variables.tf
+++ dev/preview/infrastructure/variables.tf
@@ -35,7 +35,7 @@ variable "vm_type" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202308102015"
+  default     = "gitpod-k3s-202308111617"
 }
 
 variable "cert_issuer" {
