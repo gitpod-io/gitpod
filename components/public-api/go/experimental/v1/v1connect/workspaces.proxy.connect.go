8ced600eea17928f2047de41eea57380ae9cf5d8
filiptronicek <filip@gitpod.io>
2023-08-18 08:22:00 +0000

    [papi] List workspace classes
    Adds the `ListWorkspaceClasses` method on `WorkspacesService`.

--- components/public-api/go/experimental/v1/v1connect/workspaces.proxy.connect.go
+++ components/public-api/go/experimental/v1/v1connect/workspaces.proxy.connect.go
@@ -98,3 +98,13 @@ func (s *ProxyWorkspacesServiceHandler) UpdatePort(ctx context.Context, req *con
 
 	return connect_go.NewResponse(resp), nil
 }
+
+func (s *ProxyWorkspacesServiceHandler) ListWorkspaceClasses(ctx context.Context, req *connect_go.Request[v1.ListWorkspaceClassesRequest]) (*connect_go.Response[v1.ListWorkspaceClassesResponse], error) {
+	resp, err := s.Client.ListWorkspaceClasses(ctx, req.Msg)
+	if err != nil {
+		// TODO(milan): Convert to correct status code
+		return nil, err
+	}
+
+	return connect_go.NewResponse(resp), nil
+}
