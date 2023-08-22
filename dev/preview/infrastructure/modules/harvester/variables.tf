35f98d928961c89b5deb1c0e163b473c695b237d
Kyle Brennan <kyle@gitpod.io>
2023-08-22 15:51:48 -0400

    [preview] use k3s 1.26 (#18521)
    Related to https://github.com/gitpod-io/gitpod-packer-gcp-image/pull/248

--- dev/preview/infrastructure/modules/harvester/variables.tf
+++ dev/preview/infrastructure/modules/harvester/variables.tf
@@ -33,7 +33,7 @@ variable "ssh_key" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202308150341"
+  default     = "gitpod-k3s-202308221555"
 }
 
 variable "harvester_ingress_ip" {
