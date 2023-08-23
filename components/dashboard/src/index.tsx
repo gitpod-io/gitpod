983acf68a802cb7aa180b6a379d4461582689497
Jean Pierre <jeanp413@hotmail.com>
2023-08-23 17:09:18 +0000

    Add comment

--- components/dashboard/src/index.tsx
+++ components/dashboard/src/index.tsx
@@ -4,7 +4,7 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import "setimmediate";
+import "setimmediate"; // important!, required by vscode-jsonrpc
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import relativeTime from "dayjs/plugin/relativeTime";
