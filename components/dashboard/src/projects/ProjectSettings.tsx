00b7a11c14eb99d9f80970f2cde1e32f645f0457
Alex Tugarev <alex@gitpod.io>
2023-09-13 09:39:43 +0000

    show detailed prebuild setting only if prebuilds are enabled

--- components/dashboard/src/projects/ProjectSettings.tsx
+++ components/dashboard/src/projects/ProjectSettings.tsx
@@ -123,9 +123,11 @@ export default function ProjectSettingsView() {
     // TODO: Render a generic error screen for when an entity isn't found
     if (!project) return null;
 
-    // TODO(at): (out of scope for now, but) once we've migrated the settings of existings projects
-    // we can remove the implicit enablement here
-    const enablePrebuilds = !project.settings || !!project.settings?.enablePrebuilds;
+    const enablePrebuilds =
+        !!project.settings?.enablePrebuilds ||
+        // TODO(at): out of scope for now, but once we've migrated the settings of existings projects
+        // we can remove the implicit enablement here
+        !project.settings;
 
     return (
         <ProjectSettingsPage project={project}>
@@ -150,96 +152,105 @@ export default function ProjectSettingsView() {
                     checked={enablePrebuilds}
                     onChange={(checked) => updateProjectSettings({ enablePrebuilds: checked })}
                 />
-                <InputField label="Workspace machine type" disabled={!enablePrebuilds}>
-                    <div className="max-w-md">
-                        <SelectWorkspaceClassComponent
-                            disabled={!enablePrebuilds}
-                            selectedWorkspaceClass={project.settings?.workspaceClasses?.prebuild}
-                            onSelectionChange={setWorkspaceClassForPrebuild}
-                        />
-                    </div>
-                </InputField>
-                <CheckboxInputField
-                    label="Enable Incremental Prebuilds"
-                    hint={
-                        <span>
-                            When possible, use an earlier successful prebuild as a base to create new prebuilds. This
-                            can make your prebuilds significantly faster, especially if they normally take longer than
-                            10 minutes.{" "}
-                            <a className="gp-link" href="https://www.gitpod.io/changelog/faster-incremental-prebuilds">
-                                Learn more
-                            </a>
-                        </span>
-                    }
-                    disabled={!enablePrebuilds}
-                    checked={project.settings?.useIncrementalPrebuilds ?? false}
-                    onChange={(checked) => updateProjectSettings({ useIncrementalPrebuilds: checked })}
-                />
-                <CheckboxInputField
-                    label="Cancel Prebuilds on Outdated Commits"
-                    hint="Cancel pending or running prebuilds on the same branch when new commits are pushed."
-                    disabled={!enablePrebuilds}
-                    checked={!project.settings?.keepOutdatedPrebuildsRunning}
-                    onChange={(checked) => updateProjectSettings({ keepOutdatedPrebuildsRunning: !checked })}
-                />
-                <CheckboxInputField
-                    label={
-                        <span>
-                            Use Last Successful Prebuild{" "}
-                            <PillLabel type="warn" className="font-semibold mt-2 ml-2 py-0.5 px-2 self-center">
-                                Alpha
-                            </PillLabel>
-                        </span>
-                    }
-                    hint="Skip waiting for prebuilds in progress and use the last successful prebuild from previous
-                    commits on the same branch."
-                    disabled={!enablePrebuilds}
-                    checked={!!project.settings?.allowUsingPreviousPrebuilds}
-                    onChange={(checked) =>
-                        updateProjectSettings({
-                            allowUsingPreviousPrebuilds: checked,
-                            // we are disabling prebuild cancellation when incremental workspaces are enabled
-                            keepOutdatedPrebuildsRunning: checked || project?.settings?.keepOutdatedPrebuildsRunning,
-                        })
-                    }
-                />
-                <div className="flex mt-4 max-w-2xl">
-                    <div className="flex flex-col ml-6">
-                        <label
-                            htmlFor="prebuildNthCommit"
-                            className={classNames(
-                                "text-sm font-semibold cursor-pointer tracking-wide",
-                                !enablePrebuilds || !project.settings?.allowUsingPreviousPrebuilds
-                                    ? "text-gray-400 dark:text-gray-400"
-                                    : "text-gray-600 dark:text-gray-100",
-                            )}
-                        >
-                            Skip Prebuilds
-                        </label>
-                        <input
-                            type="number"
-                            id="prebuildNthCommit"
-                            min="0"
-                            max="100"
-                            step="5"
-                            className="mt-2"
-                            disabled={!project.settings?.allowUsingPreviousPrebuilds}
-                            value={
-                                project.settings?.prebuildEveryNthCommit === undefined
-                                    ? 0
-                                    : project.settings?.prebuildEveryNthCommit
+                {enablePrebuilds && (
+                    <>
+                        <InputField label="Workspace machine type" disabled={!enablePrebuilds}>
+                            <div className="max-w-md">
+                                <SelectWorkspaceClassComponent
+                                    disabled={!enablePrebuilds}
+                                    selectedWorkspaceClass={project.settings?.workspaceClasses?.prebuild}
+                                    onSelectionChange={setWorkspaceClassForPrebuild}
+                                />
+                            </div>
+                        </InputField>
+                        <CheckboxInputField
+                            label="Enable Incremental Prebuilds"
+                            hint={
+                                <span>
+                                    When possible, use an earlier successful prebuild as a base to create new prebuilds.
+                                    This can make your prebuilds significantly faster, especially if they normally take
+                                    longer than 10 minutes.{" "}
+                                    <a
+                                        className="gp-link"
+                                        href="https://www.gitpod.io/changelog/faster-incremental-prebuilds"
+                                    >
+                                        Learn more
+                                    </a>
+                                </span>
                             }
-                            onChange={({ target }) =>
+                            disabled={!enablePrebuilds}
+                            checked={project.settings?.useIncrementalPrebuilds ?? false}
+                            onChange={(checked) => updateProjectSettings({ useIncrementalPrebuilds: checked })}
+                        />
+                        <CheckboxInputField
+                            label="Cancel Prebuilds on Outdated Commits"
+                            hint="Cancel pending or running prebuilds on the same branch when new commits are pushed."
+                            disabled={!enablePrebuilds}
+                            checked={!project.settings?.keepOutdatedPrebuildsRunning}
+                            onChange={(checked) => updateProjectSettings({ keepOutdatedPrebuildsRunning: !checked })}
+                        />
+                        <CheckboxInputField
+                            label={
+                                <span>
+                                    Use Last Successful Prebuild{" "}
+                                    <PillLabel type="warn" className="font-semibold mt-2 ml-2 py-0.5 px-2 self-center">
+                                        Alpha
+                                    </PillLabel>
+                                </span>
+                            }
+                            hint="Skip waiting for prebuilds in progress and use the last successful prebuild from previous
+                    commits on the same branch."
+                            disabled={!enablePrebuilds}
+                            checked={!!project.settings?.allowUsingPreviousPrebuilds}
+                            onChange={(checked) =>
                                 updateProjectSettings({
-                                    prebuildEveryNthCommit: Math.abs(Math.min(Number.parseInt(target.value), 100)) || 0,
+                                    allowUsingPreviousPrebuilds: checked,
+                                    // we are disabling prebuild cancellation when incremental workspaces are enabled
+                                    keepOutdatedPrebuildsRunning:
+                                        checked || project?.settings?.keepOutdatedPrebuildsRunning,
                                 })
                             }
                         />
-                        <div className="text-gray-500 dark:text-gray-400 text-sm mt-2">
-                            The number of commits that are skipped between prebuilds.
+                        <div className="flex mt-4 max-w-2xl">
+                            <div className="flex flex-col ml-6">
+                                <label
+                                    htmlFor="prebuildNthCommit"
+                                    className={classNames(
+                                        "text-sm font-semibold cursor-pointer tracking-wide",
+                                        !enablePrebuilds || !project.settings?.allowUsingPreviousPrebuilds
+                                            ? "text-gray-400 dark:text-gray-400"
+                                            : "text-gray-600 dark:text-gray-100",
+                                    )}
+                                >
+                                    Skip Prebuilds
+                                </label>
+                                <input
+                                    type="number"
+                                    id="prebuildNthCommit"
+                                    min="0"
+                                    max="100"
+                                    step="5"
+                                    className="mt-2"
+                                    disabled={!project.settings?.allowUsingPreviousPrebuilds}
+                                    value={
+                                        project.settings?.prebuildEveryNthCommit === undefined
+                                            ? 0
+                                            : project.settings?.prebuildEveryNthCommit
+                                    }
+                                    onChange={({ target }) =>
+                                        updateProjectSettings({
+                                            prebuildEveryNthCommit:
+                                                Math.abs(Math.min(Number.parseInt(target.value), 100)) || 0,
+                                        })
+                                    }
+                                />
+                                <div className="text-gray-500 dark:text-gray-400 text-sm mt-2">
+                                    The number of commits that are skipped between prebuilds.
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </>
+                )}
                 <div>
                     <Heading2 className="mt-12">Workspaces</Heading2>
                     <Subheading>Choose the workspace machine type for your workspaces.</Subheading>
