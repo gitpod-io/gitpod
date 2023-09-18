ef9c9f50981f85abc1490e5f672e63101a5c2dd0
Alex Tugarev <alex@gitpod.io>
2023-09-18 12:34:16 +0000

    fixup c&p error

--- components/gitpod-protocol/go/gitpod-service.go
+++ components/gitpod-protocol/go/gitpod-service.go
@@ -2293,7 +2293,7 @@ type Project struct {
 type ProjectSettings struct {
 	EnablePrebuilds              *bool                     `json:"enablePrebuilds,omitempty"`
 	PrebuildDefaultBranchOnly    *bool                     `json:"prebuildDefaultBranchOnly,omitempty"`
-	PrebuildBranchPattern        *bool                     `json:"prebuildBranchPattern,omitempty"`
+	PrebuildBranchPattern        *string                   `json:"prebuildBranchPattern,omitempty"`
 	UseIncrementalPrebuilds      bool                      `json:"useIncrementalPrebuilds,omitempty"`
 	UsePersistentVolumeClaim     bool                      `json:"usePersistentVolumeClaim,omitempty"`
 	KeepOutdatedPrebuildsRunning bool                      `json:"keepOutdatedPrebuildsRunning,omitempty"`
