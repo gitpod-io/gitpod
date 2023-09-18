5f0daea4d67852b9d3a1e350dd48529109e55126
Huiwen <mhqnwt@gmail.com>
2023-09-18 15:30:40 +0000

    fixup

--- components/gitpod-db/src/typeorm/team-db-impl.ts
+++ components/gitpod-db/src/typeorm/team-db-impl.ts
@@ -390,7 +390,7 @@ export class TeamDBImpl extends TransactionalDBImpl<TeamDB> implements TeamDB {
                 orgId,
             });
         } else {
-            repo.save({
+            await repo.save({
                 ...team,
                 ...update,
             });
