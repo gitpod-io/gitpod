6260269c97baa821c43df38c64e86586b6125aa7
Huiwen <mhqnwt@gmail.com>
2023-09-20 11:41:42 +0000

    hid change button for members

--- components/dashboard/src/teams/TeamSettings.tsx
+++ components/dashboard/src/teams/TeamSettings.tsx
@@ -274,21 +274,19 @@ function WorkspaceImageButton(props: { image: string; onClick: () => void; disab
                     <span className="w-5 h-5 mr-1">
                         <Stack />
                     </span>
-                    <span className="truncate font-medium text-gray-700 dark:text-gray-200">{parseDockerImage(props.image).repository}</span>
+                    <span className="truncate font-medium text-gray-700 dark:text-gray-200">
+                        {parseDockerImage(props.image).repository}
+                    </span>
                     &nbsp;&middot;&nbsp;
                     <span className="flex-none w-16 truncate text-gray-500 dark:text-gray-400">
                         {parseDockerImage(props.image).tag}
                     </span>
                 </div>
-                <Button
-                    disabled={props.disabled}
-                    htmlType="button"
-                    type="transparent"
-                    className="text-blue-500"
-                    onClick={props.onClick}
-                >
-                    Change
-                </Button>
+                {!props.disabled && (
+                    <Button htmlType="button" type="transparent" className="text-blue-500" onClick={props.onClick}>
+                        Change
+                    </Button>
+                )}
             </div>
         </InputField>
     );
@@ -331,7 +329,8 @@ function OrgDefaultWorkspaceImageModal(props: OrgDefaultWorkspaceImageModalProps
             <ModalHeader>Workspace Default Image</ModalHeader>
             <ModalBody>
                 <Alert type="warning" className="mb-2">
-                    <span className="font-medium">Warning:</span> You are setting a default image for all workspaces within the organization.
+                    <span className="font-medium">Warning:</span> You are setting a default image for all workspaces
+                    within the organization.
                 </Alert>
                 {errorMsg.length > 0 && (
                     <Alert type="error" className="mb-2">
