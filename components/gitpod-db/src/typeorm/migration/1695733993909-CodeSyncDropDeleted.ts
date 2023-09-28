a2dfdb88f27ca1809662f503c0a9b8b516fce75b
Pudong Zheng <tianshi8650@gmail.com>
2023-09-28 04:11:19 +0000

    Fix compatibility issues with db-migration

--- components/gitpod-db/src/typeorm/migration/1695733993909-CodeSyncDropDeleted.ts
+++ components/gitpod-db/src/typeorm/migration/1695733993909-CodeSyncDropDeleted.ts
@@ -10,22 +10,22 @@ import { columnExists } from "./helper/helper";
 export class CodeSyncDropDeleted1695733993909 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         if (await columnExists(queryRunner, "d_b_code_sync_resource", "deleted")) {
-            await queryRunner.query("ALTER TABLE `d_b_code_sync_resource` DROP COLUMN `deleted`, ALGORITHM=INSTANT");
+            await queryRunner.query("ALTER TABLE `d_b_code_sync_resource` DROP COLUMN `deleted`");
         }
         if (await columnExists(queryRunner, "d_b_code_sync_collection", "deleted")) {
-            await queryRunner.query("ALTER TABLE `d_b_code_sync_collection` DROP COLUMN `deleted`, ALGORITHM=INSTANT");
+            await queryRunner.query("ALTER TABLE `d_b_code_sync_collection` DROP COLUMN `deleted`");
         }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         if (!(await columnExists(queryRunner, "d_b_code_sync_collection", "deleted"))) {
             await queryRunner.query(
-                "ALTER TABLE `d_b_code_sync_collection` ADD COLUMN `deleted` tinyint(4) NOT NULL DEFAULT '0', ALGORITHM=INSTANT",
+                "ALTER TABLE `d_b_code_sync_collection` ADD COLUMN `deleted` tinyint(4) NOT NULL DEFAULT '0'",
             );
         }
         if (!(await columnExists(queryRunner, "d_b_code_sync_resource", "deleted"))) {
             await queryRunner.query(
-                "ALTER TABLE `d_b_code_sync_resource` ADD COLUMN `deleted` tinyint(4) NOT NULL DEFAULT '0', ALGORITHM=INSTANT",
+                "ALTER TABLE `d_b_code_sync_resource` ADD COLUMN `deleted` tinyint(4) NOT NULL DEFAULT '0'",
             );
         }
     }
