8491d27e5106aa9aa7d2fba3c28806855a9873c8
Gero Posmyk-Leinemann <gero@gitpod.io>
2023-10-04 13:51:47 +0000

    [db] Make backwards-compatible to mysql 5.7

--- components/gitpod-db/src/typeorm/migration/1695820427730-TokenEntryDropDeleted.ts
+++ components/gitpod-db/src/typeorm/migration/1695820427730-TokenEntryDropDeleted.ts
@@ -13,14 +13,14 @@ const COLUMN_NAME = "deleted";
 export class TokenEntryDropDeleted1695820427730 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         if (await columnExists(queryRunner, TABLE_NAME, COLUMN_NAME)) {
-            await queryRunner.query(`ALTER TABLE \`${TABLE_NAME}\` DROP COLUMN \`${COLUMN_NAME}\`, ALGORITHM=INSTANT`);
+            await queryRunner.query(`ALTER TABLE \`${TABLE_NAME}\` DROP COLUMN \`${COLUMN_NAME}\``);
         }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         if (!(await columnExists(queryRunner, TABLE_NAME, COLUMN_NAME))) {
             await queryRunner.query(
-                `ALTER TABLE \`${TABLE_NAME}\` ADD COLUMN \`${COLUMN_NAME}\` tinyint(4) NOT NULL DEFAULT '0', ALGORITHM=INSTANT`,
+                `ALTER TABLE \`${TABLE_NAME}\` ADD COLUMN \`${COLUMN_NAME}\` tinyint(4) NOT NULL DEFAULT '0'`,
             );
         }
     }
