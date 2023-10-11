791b7592969cfd43354e5ad20201240c0d6ec3d3
Brad Harris <bmharris@gmail.com>
2023-10-11 05:11:00 +0000

    remove project distinction for now

--- components/dashboard/src/components/RepositoryFinder.tsx
+++ components/dashboard/src/components/RepositoryFinder.tsx
@@ -146,7 +146,7 @@ const SuggestedRepositoryOption: FC<SuggestedRepositoryOptionProps> = ({ repo })
             aria-label={`${repo.projectId ? "Project" : "Repo"}: ${repo.url}`}
         >
             <span className={"pr-2"}>
-                <RepositoryIcon className={`w-5 h-5 ${repo.projectId ? "text-gray-400" : "text-gray-400/50"}`} />
+                <RepositoryIcon className={`w-5 h-5 text-gray-400`} />
             </span>
 
             {name && (
