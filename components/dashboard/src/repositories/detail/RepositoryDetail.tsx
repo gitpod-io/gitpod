1cdab1b2830f18f0c3a79b9a99b5417cb5124b9f
Brad Harris <bmharris@gmail.com>
2023-10-19 10:46:27 -0600

    Adding name form to repository config detail page (#18943)
    * Adding repo config name form
    
    * fixing validation logic
    
    * use a toast vs. disabling button when there's an error
    
    * update comment with more context
    
    * cleanup
    
    * remove protocol
    
    * bumping up name limit to 100 to account for recent changes
    
    ---------
    
    Co-authored-by: Filip Troníček <filip@gitpod.io>

--- components/dashboard/src/repositories/detail/RepositoryDetail.tsx
+++ components/dashboard/src/repositories/detail/RepositoryDetail.tsx
@@ -7,18 +7,43 @@
 import { FC } from "react";
 import Header from "../../components/Header";
 import { useParams } from "react-router";
+import { useProject } from "../../data/projects/project-queries";
+import { Button } from "../../components/Button";
+import { RepositoryNameForm } from "./RepositoryName";
+import { Loader2 } from "lucide-react";
+import Alert from "../../components/Alert";
 
 type PageRouteParams = {
     id: string;
 };
 const RepositoryDetailPage: FC = () => {
     const { id } = useParams<PageRouteParams>();
+    const { data, error, isLoading, refetch } = useProject({ id });
 
     return (
         <>
             <Header title="Repository Detail" subtitle="" />
             <div className="app-container">
-                <span>id: {id}</span>
+                {isLoading && <Loader2 className="animate-spin" />}
+                {error && (
+                    <div className="gap-4">
+                        <Alert type="error">
+                            <span>Failed to load repository configuration</span>
+                            <pre>{error.message}</pre>
+                        </Alert>
+
+                        <Button type="danger" onClick={refetch}>
+                            Retry
+                        </Button>
+                    </div>
+                )}
+                {!isLoading &&
+                    (!data ? (
+                        // TODO: add a better not-found UI w/ link back to repositories
+                        <div>Sorry, we couldn't find that repository configuration.</div>
+                    ) : (
+                        <RepositoryNameForm project={data} />
+                    ))}
             </div>
         </>
     );
---
+++ components/dashboard/src/repositories/detail/RepositoryName.tsx
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License.AGPL.txt in the project root for license information.
+ */
+
+// TODO: fix mismatched project types when we build repo configuration apis
+import { Project } from "@gitpod/gitpod-protocol/lib/teams-projects-protocol";
+import { Button } from "../../components/Button";
+import { TextInputField } from "../../components/forms/TextInputField";
+import { FC, useCallback, useState } from "react";
+import { useUpdateProject } from "../../data/projects/project-queries";
+import { useToast } from "../../components/toasts/Toasts";
+import { useOnBlurError } from "../../hooks/use-onblur-error";
+
+const MAX_LENGTH = 100;
+
+type Props = {
+    project: Project;
+};
+
+export const RepositoryNameForm: FC<Props> = ({ project }) => {
+    const { toast } = useToast();
+    const updateProject = useUpdateProject();
+    const [projectName, setProjectName] = useState(project.name);
+
+    const nameError = useOnBlurError("Sorry, this name is too long.", projectName.length <= MAX_LENGTH);
+
+    const updateName = useCallback(
+        async (e: React.FormEvent) => {
+            e.preventDefault();
+
+            if (!nameError.isValid) {
+                toast("Please correct the errors with the name.");
+                return;
+            }
+
+            updateProject.mutate(
+                {
+                    id: project.id,
+                    name: projectName,
+                },
+                {
+                    onSuccess: () => {
+                        toast(`Configuration name set to "${projectName}".`);
+                    },
+                },
+            );
+        },
+        [nameError.isValid, updateProject, project.id, projectName, toast],
+    );
+
+    return (
+        <form onSubmit={updateName}>
+            <TextInputField
+                hint={`The name can be up to ${MAX_LENGTH} characters long.`}
+                value={projectName}
+                error={nameError.message}
+                onChange={setProjectName}
+                onBlur={nameError.onBlur}
+            />
+
+            <Button
+                className="mt-4"
+                htmlType="submit"
+                disabled={project.name === projectName}
+                loading={updateProject.isLoading}
+            >
+                Update Name
+            </Button>
+        </form>
+    );
+};
