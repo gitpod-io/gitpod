b24ccdd56fc88eced4be1dbe913b3f33d9378d35
Filip Troníček <filip@gitpod.io>
2023-10-28 14:23:16 +0000

    Add additional focus ring classes

--- components/dashboard/src/components/podkit/button/Button.tsx
+++ components/dashboard/src/components/podkit/button/Button.tsx
@@ -10,7 +10,7 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@podkit/lib/cn";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+    "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-medium ring-offset-zinc-400 dark:ring-offset-neutral-300 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
     {
         variants: {
             variant: {
