3ad68b0b32717c0d768d033961490559cf22b753
Filip Troníček <filip@gitpod.io>
2023-10-28 19:27:12 +0000

    Fix destructive button styles

--- components/dashboard/src/components/podkit/button/Button.tsx
+++ components/dashboard/src/components/podkit/button/Button.tsx
@@ -18,8 +18,7 @@ export const buttonVariants = cva(
             variant: {
                 default:
                     "bg-gray-900 hover:bg-gray-800 dark:bg-kumquat-base dark:hover:bg-kumquat-ripe text-gray-50 dark:text-gray-900",
-                destructive:
-                    "bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 text-gray-500 dark:text-gray-100 hover:text-gray-600",
+                destructive: "bg-red-600 hover:bg-red-700 text-gray-100 dark:text-red-100",
                 outline: "border border-input bg-transparent hover:bg-kumquat-ripe hover:text-gray-600",
                 secondary:
                     "bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 text-gray-500 dark:text-gray-100 hover:text-gray-600",
