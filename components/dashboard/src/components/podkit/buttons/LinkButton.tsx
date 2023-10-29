7c1c3a706d0c5d6086a03785a8319ad24c32a50a
Filip Troníček <filip@gitpod.io>
2023-10-29 10:55:18 +0000

    Init Button, LinkButton, Loading Button, DropDow.. n, DropDownActions, Headings

---
+++ components/dashboard/src/components/podkit/buttons/LinkButton.tsx
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License.AGPL.txt in the project root for license information.
+ */
+
+import { Link } from "react-router-dom";
+import { Button, ButtonProps } from "@podkit/buttons/Button";
+import React from "react";
+
+export interface LinkButtonProps extends ButtonProps {
+    asChild?: false;
+    href: string;
+}
+
+/**
+ * A HTML anchor element styled as a button.
+ */
+export const LinkButton = React.forwardRef<HTMLButtonElement, LinkButtonProps>(
+    ({ className, variant, size, asChild, children, href, ...props }, ref) => {
+        return (
+            <Button className="transition-width" asChild {...props} ref={ref}>
+                <Link to={href}>{children}</Link>
+            </Button>
+        );
+    },
+);
+LinkButton.displayName = "LinkButton";
