203f4ec8f45af0588574607cdc5eff58a9739f15
Alex Tugarev <alex@gitpod.io>
2023-11-06 16:39:54 +0000

    WIP: get and list authProviders

--- components/server/src/api/auth-provider-service-api.ts
+++ components/server/src/api/auth-provider-service-api.ts
@@ -22,41 +22,77 @@ import {
     DeleteAuthProviderRequest,
     DeleteAuthProviderResponse,
 } from "@gitpod/public-api/lib/gitpod/v1/authprovider_pb";
+import { AuthProviderService } from "../auth/auth-provider-service";
+import { AuthProviderEntry } from "@gitpod/gitpod-protocol";
 
 @injectable()
 export class AuthProviderServiceAPI implements ServiceImpl<typeof AuthProviderServiceInterface> {
-    @inject(PublicAPIConverter)
-    private readonly apiConverter: PublicAPIConverter;
+    constructor(
+        @inject(PublicAPIConverter) private readonly apiConverter: PublicAPIConverter,
+        @inject(AuthProviderService) private readonly authProviderService: AuthProviderService,
+    ) {}
 
     async createAuthProvider(
-        req: CreateAuthProviderRequest,
+        request: CreateAuthProviderRequest,
         context: HandlerContext,
     ): Promise<CreateAuthProviderResponse> {
         throw new ConnectError("unimplemented", Code.Unimplemented);
     }
-    async getAuthProvider(req: GetAuthProviderRequest, context: HandlerContext): Promise<GetAuthProviderResponse> {
-        throw new ConnectError("unimplemented", Code.Unimplemented);
+    async getAuthProvider(request: GetAuthProviderRequest, context: HandlerContext): Promise<GetAuthProviderResponse> {
+        if (!request.authProviderId) {
+            throw new ConnectError("authProviderId is required", Code.InvalidArgument);
+        }
+
+        const authProvider = await this.authProviderService.getAuthProvider(context.user.id, request.authProviderId);
+        if (!authProvider) {
+            throw new ConnectError("Provider not found.", Code.NotFound);
+        }
+
+        return new GetAuthProviderResponse({
+            authProvider: this.apiConverter.toAuthProvider(authProvider),
+        });
     }
+
     async listAuthProviders(
-        req: ListAuthProvidersRequest,
+        request: ListAuthProvidersRequest,
         context: HandlerContext,
     ): Promise<ListAuthProvidersResponse> {
-        throw new ConnectError("unimplemented", Code.Unimplemented);
+        const target = request.id;
+        const ownerId = target.case === "userId" ? target.value : undefined;
+        const organizationId = target.case === "organizationId" ? target.value : undefined;
+
+        if (!organizationId && !ownerId) {
+            throw new ConnectError("organizationId or ownerId is required", Code.InvalidArgument);
+        }
+
+        const authProviders = organizationId
+            ? await this.authProviderService.getAuthProvidersOfOrg(context.user.id, organizationId)
+            : await this.authProviderService.getAuthProvidersOfUser(context.user.id);
+
+        const redacted = authProviders.map(AuthProviderEntry.redact.bind(AuthProviderEntry));
+
+        const result = new ListAuthProvidersResponse({
+            list: redacted.map((ap) => this.apiConverter.toAuthProvider(ap)),
+        });
+        return result;
     }
+
     async listAuthProviderDescriptions(
-        req: ListAuthProviderDescriptionsRequest,
+        request: ListAuthProviderDescriptionsRequest,
         context: HandlerContext,
     ): Promise<ListAuthProviderDescriptionsResponse> {
         throw new ConnectError("unimplemented", Code.Unimplemented);
     }
+
     async updateAuthProvider(
-        req: UpdateAuthProviderRequest,
+        request: UpdateAuthProviderRequest,
         context: HandlerContext,
     ): Promise<UpdateAuthProviderResponse> {
         throw new ConnectError("unimplemented", Code.Unimplemented);
     }
+
     async deleteAuthProvider(
-        req: DeleteAuthProviderRequest,
+        request: DeleteAuthProviderRequest,
         context: HandlerContext,
     ): Promise<DeleteAuthProviderResponse> {
         throw new ConnectError("unimplemented", Code.Unimplemented);
