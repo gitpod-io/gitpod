befffa8924722dad8654a5f28e1160b3cdae28d4
Filip Troníček <filip@gitpod.io>
2023-11-08 23:05:13 +0000

    Cancel buttons

--- components/dashboard/src/configurations/detail/ConfigurationName.tsx
+++ components/dashboard/src/configurations/detail/ConfigurationName.tsx
@@ -4,13 +4,14 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { Button } from "../../components/Button";
 import { TextInputField } from "../../components/forms/TextInputField";
 import { FC, useCallback, useState } from "react";
 import { useUpdateProject } from "../../data/projects/project-queries";
 import { useToast } from "../../components/toasts/Toasts";
 import { useOnBlurError } from "../../hooks/use-onblur-error";
 import type { Configuration } from "@gitpod/public-api/lib/gitpod/v1/configuration_pb";
+import { Button } from "@podkit/buttons/Button";
+import { LoadingButton } from "@podkit/buttons/LoadingButton";
 
 const MAX_LENGTH = 100;
 
@@ -52,21 +53,33 @@ export const ConfigurationNameForm: FC<Props> = ({ configuration }) => {
     return (
         <form onSubmit={updateName}>
             <TextInputField
+                label="Configuration name"
                 hint={`The name can be up to ${MAX_LENGTH} characters long.`}
                 value={projectName}
                 error={nameError.message}
                 onChange={setProjectName}
                 onBlur={nameError.onBlur}
             />
-
-            <Button
-                className="mt-4"
-                htmlType="submit"
-                disabled={configuration.name === projectName}
-                loading={updateProject.isLoading}
-            >
-                Update Name
-            </Button>
+            <div className="flex flex-row items-center justify-start gap-2 w-full">
+                <LoadingButton
+                    className="mt-4"
+                    type="submit"
+                    disabled={configuration.name === projectName}
+                    loading={updateProject.isLoading}
+                >
+                    Save
+                </LoadingButton>
+                <Button
+                    className="mt-4 ml-2"
+                    variant="secondary"
+                    disabled={configuration.name === projectName}
+                    onClick={() => {
+                        setProjectName(configuration.name);
+                    }}
+                >
+                    Cancel
+                </Button>
+            </div>
         </form>
     );
 };
