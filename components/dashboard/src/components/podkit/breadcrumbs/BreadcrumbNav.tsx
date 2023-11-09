6094f5011b6bf31e228eec72b7f9232a79964c71
Filip Troníček <filip@gitpod.io>
2023-11-09 22:31:31 +0000

    Fix text color in BreadcrumbNav component

--- components/dashboard/src/components/podkit/breadcrumbs/BreadcrumbNav.tsx
+++ components/dashboard/src/components/podkit/breadcrumbs/BreadcrumbNav.tsx
@@ -39,7 +39,7 @@ export const BreadcrumbNav: FC<BreadcrumbPageNavProps> = ({ pageTitle, pageDescr
             )}
             <h1 className="text-lg font-bold">{pageTitle}</h1>
             <span>·</span>
-            <p className="gray-900 dark:gray-300 text-md">{pageDescription}</p>
+            <p className="text-gray-900 dark:text-gray-300 text-md">{pageDescription}</p>
         </section>
     );
 };
