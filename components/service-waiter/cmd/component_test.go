e1dcb63fc343a097219f2b6bbbe68a2548324424
Huiwen <mhqnwt@gmail.com>
2023-11-10 12:26:34 +0000

    mock feature flag hang

--- components/service-waiter/cmd/component_test.go
+++ components/service-waiter/cmd/component_test.go
@@ -27,6 +27,7 @@ func Test_actualWaitFeatureFlag(t *testing.T) {
 		values          []*string
 		defaultValue    bool
 		nilClient       bool
+		clientHanging   time.Duration
 	}
 	tests := []struct {
 		name          string
@@ -42,6 +43,7 @@ func Test_actualWaitFeatureFlag(t *testing.T) {
 				values:          []*string{nil, nil, &trueValue},
 				defaultValue:    false,
 				nilClient:       false,
+				clientHanging:   time.Second * 0,
 			},
 			wantFlagValue: true,
 			wantOk:        true,
@@ -54,6 +56,20 @@ func Test_actualWaitFeatureFlag(t *testing.T) {
 				values:          []*string{nil, nil, nil, &falseValue},
 				defaultValue:    false,
 				nilClient:       false,
+				clientHanging:   time.Second * 0,
+			},
+			wantFlagValue: false,
+			wantOk:        true,
+			fetchTimes:    4,
+		},
+		{
+			name: "happy path 3",
+			args: args{
+				timeoutDuration: baseDuration * 1000,
+				values:          []*string{nil, nil, nil, &falseValue},
+				defaultValue:    false,
+				nilClient:       false,
+				clientHanging:   baseDuration * 10,
 			},
 			wantFlagValue: false,
 			wantOk:        true,
@@ -66,6 +82,7 @@ func Test_actualWaitFeatureFlag(t *testing.T) {
 				values:          []*string{nil, nil, nil, &unknownValue, &falseValue},
 				defaultValue:    false,
 				nilClient:       false,
+				clientHanging:   time.Second * 0,
 			},
 			wantFlagValue: false,
 			wantOk:        true,
@@ -78,6 +95,7 @@ func Test_actualWaitFeatureFlag(t *testing.T) {
 				values:          []*string{nil, nil, nil, &unknownValue, &falseValue},
 				defaultValue:    false,
 				nilClient:       true,
+				clientHanging:   time.Second * 0,
 			},
 			wantFlagValue: false,
 			wantOk:        false,
@@ -90,58 +108,75 @@ func Test_actualWaitFeatureFlag(t *testing.T) {
 				values:          []*string{nil, nil, nil, &unknownValue, &falseValue},
 				defaultValue:    true,
 				nilClient:       true,
+				clientHanging:   time.Second * 0,
 			},
 			wantFlagValue: true,
 			wantOk:        false,
 			fetchTimes:    0,
 		},
 		{
-			name: "timed out with default value: true",
+			name: "ctx timed out should get default value: true",
 			args: args{
 				timeoutDuration: baseDuration * 30,
 				values:          []*string{nil, nil, nil, &unknownValue, &falseValue},
 				defaultValue:    true,
 				nilClient:       false,
+				clientHanging:   time.Second * 0,
 			},
 			wantFlagValue: true,
 			wantOk:        false,
 			fetchTimes:    2,
 		},
 		{
-			name: "timed out with default value: false",
+			name: "ctx timed out should get default value: false",
 			args: args{
 				timeoutDuration: baseDuration * 30,
 				values:          []*string{nil, nil, nil, &unknownValue, &falseValue},
 				defaultValue:    false,
 				nilClient:       false,
+				clientHanging:   time.Second * 0,
 			},
 			wantFlagValue: false,
 			wantOk:        false,
 			fetchTimes:    2,
+		},
+		{
+			name: "client hanging should get default value",
+			args: args{
+				timeoutDuration: baseDuration * 30,
+				values:          []*string{nil, nil, nil, &unknownValue, &falseValue},
+				defaultValue:    true,
+				nilClient:       false,
+				clientHanging:   baseDuration * 60,
+			},
+			wantFlagValue: true,
+			wantOk:        false,
+			fetchTimes:    1,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ctx, cancel := context.WithTimeout(context.Background(), tt.args.timeoutDuration)
 			defer cancel()
-			client := newMockClient(tt.args.values, tt.args.nilClient)
-			gotFlagValue, gotOk, gotWaitTimes := cmd.ActualWaitFeatureFlag(ctx, client, tt.args.defaultValue)
+			client := newMockClient(tt.args.values, tt.args.nilClient, tt.args.clientHanging)
+			gotFlagValue, gotOk, gotFetchTimes := cmd.ActualWaitFeatureFlag(ctx, client, tt.args.defaultValue)
 			if gotFlagValue != tt.wantFlagValue {
 				t.Errorf("actualWaitFeatureFlag() gotFlagValue = %v, want %v", gotFlagValue, tt.wantFlagValue)
 			}
 			if gotOk != tt.wantOk {
 				t.Errorf("actualWaitFeatureFlag() gotOk = %v, want %v", gotOk, tt.wantOk)
 			}
-			if gotWaitTimes != tt.fetchTimes {
-				t.Errorf("actualWaitFeatureFlag() gotWaitTimes = %v, want %v", gotWaitTimes, tt.fetchTimes)
+			if gotFetchTimes != tt.fetchTimes {
+				t.Errorf("actualWaitFeatureFlag() gotFetchTimes = %v, want %v", gotFetchTimes, tt.fetchTimes)
 			}
 		})
 	}
 }
 
 type mockClient struct {
-	values []*string
-	i      int
+	values          []*string
+	i               int
+	hangingDuration time.Duration
 }
 
 // GetBoolValue implements experiments.Client.
@@ -159,11 +194,11 @@ func (*mockClient) GetIntValue(ctx context.Context, experimentName string, defau
 	panic("unimplemented")
 }
 
-func newMockClient(values []*string, isNil bool) experiments.Client {
+func newMockClient(values []*string, isNil bool, hangingDuration time.Duration) experiments.Client {
 	if isNil {
 		return nil
 	}
-	return &mockClient{values: values, i: 0}
+	return &mockClient{values: values, i: 0, hangingDuration: hangingDuration}
 }
 
 // GetStringValue implements experiments.Client.
@@ -171,6 +206,7 @@ func (c *mockClient) GetStringValue(ctx context.Context, experimentName string, 
 	defer func() {
 		c.i += 1
 	}()
+	time.Sleep(c.hangingDuration)
 	if c.i >= len(c.values) {
 		return defaultValue
 	}
