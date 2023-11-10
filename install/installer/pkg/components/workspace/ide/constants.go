6dd8988acda492b0952a4a4ba429864fad5b9588
Huiwen <mhqnwt@gmail.com>
2023-11-10 23:02:48 +0800

    Gitpod VS Code Browser Release `1.84.2` (#19055)
    * Build stable code
    
    * upgrade stable image
    
    ---------
    
    Co-authored-by: Filip Troníček <filip@gitpod.io>

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-0bfb871a1d539936e3ef9959d7db453ab59af352" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-3bb09152fc5248f5b99b33b4d8a7f101aa082441" // stable version that will be updated manually on demand
 	CodeHelperIDEImage          = "ide/code-codehelper"
 	CodeWebExtensionImage       = "ide/gitpod-code-web"
 	CodeWebExtensionVersion     = "commit-49bb715b599dce2356dd02a6ede7ae8cf10d8d12" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
