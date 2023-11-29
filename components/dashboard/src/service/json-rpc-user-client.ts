06e5b4c849ecda58962b613864278afebe4a80eb
Alex Tugarev <alex@gitpod.io>
2023-11-29 16:21:06 +0000

    fixup unimplemented methods

--- components/dashboard/src/service/json-rpc-user-client.ts
+++ components/dashboard/src/service/json-rpc-user-client.ts
@@ -8,9 +8,17 @@ import { UserService } from "@gitpod/public-api/lib/gitpod/v1/user_connect";
 
 import { PromiseClient } from "@connectrpc/connect";
 import { PartialMessage } from "@bufbuild/protobuf";
-import { GetAuthenticatedUserRequest, GetAuthenticatedUserResponse } from "@gitpod/public-api/lib/gitpod/v1/user_pb";
+import {
+    GetAuthenticatedUserRequest,
+    GetAuthenticatedUserResponse,
+    SetWorkspaceAutoStartOptionsRequest,
+    SetWorkspaceAutoStartOptionsResponse,
+    UpdateUserRequest,
+    UpdateUserResponse,
+} from "@gitpod/public-api/lib/gitpod/v1/user_pb";
 import { getGitpodService } from "./service";
 import { converter } from "./public-api";
+import { ApplicationError, ErrorCodes } from "@gitpod/gitpod-protocol/lib/messaging/error";
 
 export class JsonRpcUserClient implements PromiseClient<typeof UserService> {
     async getAuthenticatedUser(
@@ -20,5 +28,15 @@ export class JsonRpcUserClient implements PromiseClient<typeof UserService> {
         return new GetAuthenticatedUserResponse({
             user: converter.toUser(user),
         });
+    }
+
+    async updateUser(request: PartialMessage<UpdateUserRequest>): Promise<UpdateUserResponse> {
+        throw new ApplicationError(ErrorCodes.UNIMPLEMENTED, "not implemented");
+    }
+
+    async setWorkspaceAutoStartOptions(
+        request: PartialMessage<SetWorkspaceAutoStartOptionsRequest>,
+    ): Promise<SetWorkspaceAutoStartOptionsResponse> {
+        throw new ApplicationError(ErrorCodes.UNIMPLEMENTED, "not implemented");
     }
 }
