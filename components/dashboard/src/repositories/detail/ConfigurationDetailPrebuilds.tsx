7b5a42445167910a0c04abaf232d1b079f97cc76
Brad Harris <bmharris@gmail.com>
2023-11-30 00:19:08 +0000

    adding Switch podkit component

--- components/dashboard/src/repositories/detail/ConfigurationDetailPrebuilds.tsx
+++ components/dashboard/src/repositories/detail/ConfigurationDetailPrebuilds.tsx
@@ -4,15 +4,20 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Configuration } from "@gitpod/public-api/lib/gitpod/v1/configuration_pb";
 import { ConfigurationSettingsField } from "./ConfigurationSettingsField";
 import { Heading3, Subheading } from "@podkit/typography/Headings";
+import { Switch } from "@podkit/switch/Switch";
+import { TextMuted } from "@podkit/typography/TextMuted";
 
 type Props = {
     configuration: Configuration;
 };
 export const ConfigurationDetailPrebuilds: FC<Props> = ({ configuration }) => {
+    // TODO: hook this up to just use configuration as state and wire up optimistic update for mutation
+    const [enabled, setEnabled] = useState(!!configuration.prebuildSettings?.enabled);
+
     return (
         <>
             <ConfigurationSettingsField>
@@ -20,11 +25,38 @@ export const ConfigurationDetailPrebuilds: FC<Props> = ({ configuration }) => {
                 <Subheading className="max-w-lg">
                     Prebuilds reduce wait time for new workspaces. Enabling requires permissions to configure repository
                     webhooks.{" "}
-                    <a href="/docs/configure/projects/prebuilds" className="gp-link">
+                    <a
+                        href="/docs/configure/projects/prebuilds"
+                        className="gp-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         Learn more
                     </a>
                     .
                 </Subheading>
+
+                <div className="flex gap-4 mt-6">
+                    {/* TODO: wrap this in a SwitchInputField that handles the switch, label and description and htmlFor/id automatically */}
+                    <Switch checked={enabled} onCheckedChange={setEnabled} id="prebuilds-enabled" />
+                    <div>
+                        <label className="font-semibold" htmlFor="prebuilds-enabled">
+                            Prebuilds are enabled
+                        </label>
+                        <TextMuted>
+                            Enabling requires permissions to configure repository webhooks.{" "}
+                            <a
+                                href="/docs/configure/projects/prebuilds"
+                                className="gp-link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Learn more
+                            </a>
+                            .
+                        </TextMuted>
+                    </div>
+                </div>
             </ConfigurationSettingsField>
         </>
     );
