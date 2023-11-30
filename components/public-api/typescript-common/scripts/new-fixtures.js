82c5bb6d91f213d5a9d12e5b6e12b7d746b178fe
Huiwen <mhqnwt@gmail.com>
2023-11-30 17:34:04 +0000

    Add copyright

--- components/public-api/typescript-common/scripts/new-fixtures.js
+++ components/public-api/typescript-common/scripts/new-fixtures.js
@@ -1,4 +1,10 @@
 /**
+ * Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License.AGPL.txt in the project root for license information.
+ */
+
+/**
  * @example node scripts/new-fixtures.js toPrebuildSettings 2
  */
 
