8a4fc01e200c3a73a573988ac334a3c73c5ca96c
Brad Harris <bmharris@gmail.com>
2023-12-07 22:11:12 +0000

    drop animations for now

--- components/dashboard/src/components/podkit/select/Select.tsx
+++ components/dashboard/src/components/podkit/select/Select.tsx
@@ -82,8 +82,6 @@ const SelectContent = React.forwardRef<
                 "relative z-50 max-h-96 min-w-[8rem] overflow-hidden",
                 "rounded-lg border border-pk-border-base",
                 "bg-pk-surface-primary text-pk-content-primary shadow-md",
-                // TODO: setup these animations or remove them
-                "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
                 position === "popper" &&
                     "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
                 className,
