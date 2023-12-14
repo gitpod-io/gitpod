888d5fd1355406644103ef5f6daaabb12d56619d
Filip Troníček <filip@gitpod.io>
2023-12-14 17:36:58 +0000

    Try disabled switch

--- components/dashboard/src/components/podkit/switch/Switch.tsx
+++ components/dashboard/src/components/podkit/switch/Switch.tsx
@@ -22,7 +22,6 @@ export const Switch = React.forwardRef<
                 "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background",
                 "focus-visible:outline-kumquat-ripe",
                 "disabled:cursor-default disabled:bg-pk-surface-labels",
-                // TODO: try and make kumquat-gradient work here for the bg
                 // checked state colors
                 "data-[state=checked]:bg-kumquat-gradient",
                 // unchecked state colors
