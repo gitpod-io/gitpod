6c999463efc98aefce2bdcdd5a80482707d65723
Filip Troníček <filip@gitpod.io>
2023-12-14 17:05:46 +0000

    Fix disabled state

--- components/dashboard/src/components/podkit/switch/Switch.tsx
+++ components/dashboard/src/components/podkit/switch/Switch.tsx
@@ -17,7 +17,7 @@ export const Switch = React.forwardRef<
             className={cn(
                 // this gives the switch a line-height of 24px that matches the height of our base font size
                 "my-0.5",
-                "peer inline-flex h-[20px] w-[36px] shrink-0 cursor-pointer items-center",
+                "peer group inline-flex h-[20px] w-[36px] shrink-0 cursor-pointer items-center",
                 "rounded-2xl transition-colors",
                 "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background",
                 "focus-visible:outline-kumquat-ripe",
@@ -36,7 +36,7 @@ export const Switch = React.forwardRef<
                 className={cn(
                     "pointer-events-none block h-[16px] w-[16px] rounded-full",
                     "bg-pk-surface-primary drop-shadow ring-0",
-                    "disabled:bg-pk-surface-tertiary",
+                    "group-disabled:bg-pk-surface-tertiary",
                     // Positioning
                     "transition-transform data-[state=checked]:translate-x-[17px] data-[state=unchecked]:translate-x-[3px]",
                 )}
