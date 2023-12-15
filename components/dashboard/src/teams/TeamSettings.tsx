d2919ee0f0571d2403ea708b2f354b3650051434
Huiwen <mhqnwt@gmail.com>
2023-12-15 21:42:22 +0800

    Revert and fixing "Revert "Add org-level workspace class restrictions"" - Dashboard (#19276)
    * Revert "Revert "Add org-level workspace class restrictions (#19191)" (#19272)" - dashboard
    
    * Fixing

--- components/dashboard/src/teams/TeamSettings.tsx
+++ components/dashboard/src/teams/TeamSettings.tsx
@@ -4,7 +4,7 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
-import { Organization, OrganizationSettings } from "@gitpod/public-api/lib/gitpod/v1/organization_pb";
+import { OrganizationSettings } from "@gitpod/public-api/lib/gitpod/v1/organization_pb";
 import React, { Children, ReactNode, useCallback, useMemo, useState } from "react";
 import Alert from "../components/Alert";
 import ConfirmationModal from "../components/ConfirmationModal";
@@ -13,7 +13,7 @@ import Modal, { ModalBody, ModalFooter, ModalHeader } from "../components/Modal"
 import { CheckboxInputField } from "../components/forms/CheckboxInputField";
 import { InputField } from "../components/forms/InputField";
 import { TextInputField } from "../components/forms/TextInputField";
-import { Heading2, Subheading } from "../components/typography/headings";
+import { Heading2, Heading3, Subheading } from "../components/typography/headings";
 import { useIsOwner } from "../data/organizations/members-query";
 import { useOrgSettingsQuery } from "../data/organizations/org-settings-query";
 import { useCurrentOrg, useOrganizationsInvalidator } from "../data/organizations/orgs-query";
@@ -28,6 +28,13 @@ import { OrgSettingsPage } from "./OrgSettingsPage";
 import { ErrorCode } from "@gitpod/gitpod-protocol/lib/messaging/error";
 import { Button } from "@podkit/buttons/Button";
 import { useInstallationDefaultWorkspaceImageQuery } from "../data/installation/default-workspace-image-query";
+import { useToast } from "../components/toasts/Toasts";
+import { useWorkspaceClasses } from "../data/workspaces/workspace-classes-query";
+import { LoadingState } from "@podkit/loading/LoadingState";
+import { LoadingButton } from "@podkit/buttons/LoadingButton";
+import { ConfigurationSettingsField } from "../repositories/detail/ConfigurationSettingsField";
+import { SwitchInputField } from "@podkit/switch/Switch";
+import { useFeatureFlag } from "../data/featureflag-query";
 
 export default function TeamSettingsPage() {
     const user = useCurrentUser();
@@ -39,6 +46,8 @@ export default function TeamSettingsPage() {
     const [teamName, setTeamName] = useState(org?.name || "");
     const [updated, setUpdated] = useState(false);
     const updateOrg = useUpdateOrgMutation();
+
+    const enableOrgWorkspaceClassRestrictions = useFeatureFlag("org_workspace_class_restrictions");
 
     const close = () => setModal(false);
 
@@ -83,56 +92,143 @@ export default function TeamSettingsPage() {
         document.location.href = gitpodHostUrl.asDashboard().toString();
     }, [invalidateOrgs, org, user]);
 
+    const { data: settings, isLoading } = useOrgSettingsQuery();
+    const { data: installationDefaultImage } = useInstallationDefaultWorkspaceImageQuery();
+    const updateTeamSettings = useUpdateOrgSettingsMutation();
+
+    const [showImageEditModal, setShowImageEditModal] = useState(false);
+
+    const handleUpdateTeamSettings = useCallback(
+        async (newSettings: Partial<OrganizationSettings>) => {
+            if (!org?.id) {
+                throw new Error("no organization selected");
+            }
+            if (!isOwner) {
+                throw new Error("no organization settings change permission");
+            }
+            try {
+                await updateTeamSettings.mutateAsync({
+                    ...settings,
+                    ...newSettings,
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        [updateTeamSettings, org?.id, isOwner, settings],
+    );
+
     return (
         <>
             <OrgSettingsPage>
-                <Heading2>Organization Details</Heading2>
-                <Subheading className="max-w-2xl">Details of your organization within Gitpod.</Subheading>
+                <div className="space-y-4">
+                    <div>
+                        <Heading2>Organization Details</Heading2>
+                        <Subheading>Details of your organization within Gitpod.</Subheading>
+                    </div>
+                    <ConfigurationSettingsField>
+                        {updateOrg.isError && (
+                            <Alert type="error" closable={true} className="mb-2 max-w-xl rounded-md">
+                                <span>Failed to update organization information: </span>
+                                <span>{updateOrg.error.message || "unknown error"}</span>
+                            </Alert>
+                        )}
+                        {updated && (
+                            <Alert type="message" closable={true} className="mb-2 max-w-xl rounded-md">
+                                Organization name has been updated.
+                            </Alert>
+                        )}
+                        <TextInputField
+                            label="Display Name"
+                            hint="The name of your company or organization"
+                            value={teamName}
+                            error={teamNameError.message}
+                            onChange={setTeamName}
+                            disabled={!isOwner}
+                            topMargin={false}
+                            onBlur={teamNameError.onBlur}
+                        />
 
-                {updateOrg.isError && (
-                    <Alert type="error" closable={true} className="mb-2 max-w-xl rounded-md">
-                        <span>Failed to update organization information: </span>
-                        <span>{updateOrg.error.message || "unknown error"}</span>
-                    </Alert>
-                )}
-                {updated && (
-                    <Alert type="message" closable={true} className="mb-2 max-w-xl rounded-md">
-                        Organization name has been updated.
-                    </Alert>
-                )}
-                <form onSubmit={updateTeamInformation}>
-                    <TextInputField
-                        label="Name"
-                        hint="The name of your company or organization"
-                        value={teamName}
-                        error={teamNameError.message}
-                        onChange={setTeamName}
-                        disabled={!isOwner}
-                        onBlur={teamNameError.onBlur}
-                    />
+                        {org && (
+                            <InputField label="Organization ID">
+                                <InputWithCopy value={org.id} tip="Copy Organization ID" />
+                            </InputField>
+                        )}
 
-                    {isOwner && (
-                        <Button className="mt-4" type="submit" disabled={org?.name === teamName || !orgFormIsValid}>
-                            Update Organization
-                        </Button>
+                        {isOwner && (
+                            <Button
+                                onClick={updateTeamInformation}
+                                className="mt-4"
+                                type="submit"
+                                disabled={org?.name === teamName || !orgFormIsValid}
+                            >
+                                Save
+                            </Button>
+                        )}
+                    </ConfigurationSettingsField>
+
+                    <ConfigurationSettingsField>
+                        <Heading3>Collaboration & Sharing</Heading3>
+
+                        {updateTeamSettings.isError && (
+                            <Alert type="error" closable={true} className="mb-2 max-w-xl rounded-md">
+                                <span>Failed to update organization settings: </span>
+                                <span>{updateTeamSettings.error.message || "unknown error"}</span>
+                            </Alert>
+                        )}
+
+                        <CheckboxInputField
+                            label="Workspace Sharing"
+                            hint="Allow workspaces created within an Organization to share the workspace with any authenticated user."
+                            checked={!settings?.workspaceSharingDisabled}
+                            onChange={(checked) => handleUpdateTeamSettings({ workspaceSharingDisabled: !checked })}
+                            disabled={isLoading || !isOwner}
+                        />
+                    </ConfigurationSettingsField>
+
+                    <ConfigurationSettingsField>
+                        <Heading3>Workspace Images</Heading3>
+                        <Subheading>Choose a default image for all workspaces in the organization.</Subheading>
+
+                        <WorkspaceImageButton
+                            disabled={!isOwner}
+                            settings={settings}
+                            installationDefaultWorkspaceImage={installationDefaultImage}
+                            onClick={() => setShowImageEditModal(true)}
+                        />
+                    </ConfigurationSettingsField>
+
+                    {showImageEditModal && (
+                        <OrgDefaultWorkspaceImageModal
+                            settings={settings}
+                            installationDefaultWorkspaceImage={installationDefaultImage}
+                            onClose={() => setShowImageEditModal(false)}
+                        />
                     )}
-                </form>
 
-                <OrgSettingsForm org={org} isOwner={isOwner} />
+                    {enableOrgWorkspaceClassRestrictions && (
+                        <ConfigurationSettingsField>
+                            <Heading3>Available Workspace Classes</Heading3>
+                            <Subheading>Limit the available workspace classes in your organization.</Subheading>
 
-                {user?.organizationId !== org?.id && isOwner && (
-                    <>
-                        <Heading2 className="pt-12">Delete Organization</Heading2>
-                        <Subheading className="pb-4 max-w-2xl">
-                            Deleting this organization will also remove all associated data, including projects and
-                            workspaces. Deleted organizations cannot be restored!
-                        </Subheading>
+                            {settings && <WorkspaceClassOptions disabled={!isOwner} settings={settings} />}
+                        </ConfigurationSettingsField>
+                    )}
 
-                        <Button variant="destructive" onClick={() => setModal(true)}>
-                            Delete Organization
-                        </Button>
-                    </>
-                )}
+                    {user?.organizationId !== org?.id && isOwner && (
+                        <ConfigurationSettingsField>
+                            <Heading3>Delete Organization</Heading3>
+                            <Subheading className="pb-4 max-w-2xl">
+                                Deleting this organization will also remove all associated data, including projects and
+                                workspaces. Deleted organizations cannot be restored!
+                            </Subheading>
+
+                            <Button variant="destructive" onClick={() => setModal(true)}>
+                                Delete Organization
+                            </Button>
+                        </ConfigurationSettingsField>
+                    )}
+                </div>
             </OrgSettingsPage>
 
             <ConfirmationModal
@@ -170,86 +266,6 @@ export default function TeamSettingsPage() {
                 ></input>
             </ConfirmationModal>
         </>
-    );
-}
-
-function OrgSettingsForm(props: { org?: Organization; isOwner: boolean }) {
-    const { org, isOwner } = props;
-    const { data: settings, isLoading } = useOrgSettingsQuery();
-    const { data: installationDefaultImage } = useInstallationDefaultWorkspaceImageQuery();
-    const updateTeamSettings = useUpdateOrgSettingsMutation();
-
-    const [showImageEditModal, setShowImageEditModal] = useState(false);
-
-    const handleUpdateTeamSettings = useCallback(
-        async (newSettings: Partial<OrganizationSettings>) => {
-            if (!org?.id) {
-                throw new Error("no organization selected");
-            }
-            if (!isOwner) {
-                throw new Error("no organization settings change permission");
-            }
-            try {
-                await updateTeamSettings.mutateAsync({
-                    ...settings,
-                    ...newSettings,
-                });
-            } catch (error) {
-                console.error(error);
-            }
-        },
-        [updateTeamSettings, org?.id, isOwner, settings],
-    );
-
-    return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault();
-            }}
-        >
-            {props.org && (
-                <InputField label="Organization ID">
-                    <InputWithCopy value={props.org.id} tip="Copy Organization ID" />
-                </InputField>
-            )}
-
-            <Heading2 className="pt-12">Collaboration & Sharing</Heading2>
-
-            {updateTeamSettings.isError && (
-                <Alert type="error" closable={true} className="mb-2 max-w-xl rounded-md">
-                    <span>Failed to update organization settings: </span>
-                    <span>{updateTeamSettings.error.message || "unknown error"}</span>
-                </Alert>
-            )}
-
-            <CheckboxInputField
-                label="Workspace Sharing"
-                hint="Allow workspaces created within an Organization to share the workspace with any authenticated user."
-                checked={!settings?.workspaceSharingDisabled}
-                onChange={(checked) => handleUpdateTeamSettings({ workspaceSharingDisabled: !checked })}
-                disabled={isLoading || !isOwner}
-            />
-
-            <Heading2 className="pt-12">Workspace Images</Heading2>
-            <Subheading className="max-w-2xl">
-                Choose a default image for all workspaces in the organization.
-            </Subheading>
-
-            <WorkspaceImageButton
-                disabled={!isOwner}
-                settings={settings}
-                installationDefaultWorkspaceImage={installationDefaultImage}
-                onClick={() => setShowImageEditModal(true)}
-            />
-
-            {showImageEditModal && (
-                <OrgDefaultWorkspaceImageModal
-                    settings={settings}
-                    installationDefaultWorkspaceImage={installationDefaultImage}
-                    onClose={() => setShowImageEditModal(false)}
-                />
-            )}
-        </form>
     );
 }
 
@@ -397,3 +413,93 @@ function OrgDefaultWorkspaceImageModal(props: OrgDefaultWorkspaceImageModalProps
         </Modal>
     );
 }
+
+interface WorkspaceClassOptionsProps {
+    settings: OrganizationSettings | undefined;
+    disabled: boolean;
+}
+const WorkspaceClassOptions = (props: WorkspaceClassOptionsProps) => {
+    const [validateError, setValidateError] = useState("");
+    const [selectedValue, setSelectedValue] = useState(props.settings?.allowedWorkspaceClasses ?? []);
+    const [isChanged, setIsChanged] = useState(false);
+    const updateTeamSettings = useUpdateOrgSettingsMutation();
+    const { data: classes, isError, isLoading } = useWorkspaceClasses();
+
+    const { toast } = useToast();
+    const handleUpdateTeamSettings = useCallback(
+        async (classes: string[]) => {
+            await updateTeamSettings.mutateAsync(
+                {
+                    ...props.settings,
+                    allowedWorkspaceClasses: classes,
+                },
+                {
+                    onSuccess: () => {
+                        toast({ message: "Available workspace classes updated." });
+                    },
+                },
+            );
+        },
+        [updateTeamSettings, props.settings, toast],
+    );
+
+    const noClassesSelected = useMemo(() => {
+        return (props.settings?.allowedWorkspaceClasses.length ?? 0) === 0;
+    }, [props.settings?.allowedWorkspaceClasses]);
+
+    if (isError || !classes) {
+        return <div>Something went wrong</div>;
+    }
+
+    if (isLoading) {
+        return <LoadingState />;
+    }
+
+    return (
+        <div className="space-y-4">
+            <div>
+                {classes.map((wsClass) => (
+                    <SwitchInputField
+                        className="mt-2"
+                        key={wsClass.id}
+                        id={wsClass.id}
+                        label={wsClass.displayName}
+                        description={wsClass.description}
+                        checked={(!isChanged && noClassesSelected) || selectedValue.includes(wsClass.id)}
+                        onCheckedChange={(checked) => {
+                            const previousValue =
+                                !isChanged && noClassesSelected ? classes.map((e) => e.id) : selectedValue;
+                            setIsChanged(true);
+                            const newVal = (
+                                checked ? [...previousValue, wsClass.id] : previousValue.filter((e) => e !== wsClass.id)
+                            ).filter((id) => classes.find((cls) => cls.id === id));
+                            setValidateError(
+                                newVal.length === 0 ? "At least one workspace class has to be selected." : "",
+                            );
+                            setSelectedValue(newVal);
+                        }}
+                        disabled={props.disabled || updateTeamSettings.isLoading}
+                    />
+                ))}
+            </div>
+
+            <div className="flex gap-2 items-center">
+                {!props.disabled && (
+                    <LoadingButton
+                        disabled={props.disabled || !isChanged || validateError.length > 0}
+                        loading={updateTeamSettings.isLoading}
+                        onClick={() => {
+                            handleUpdateTeamSettings(selectedValue);
+                        }}
+                    >
+                        Save
+                    </LoadingButton>
+                )}
+                {validateError.length > 0 && <span className="text-red-600 dark:text-red-400">{validateError}</span>}
+                {updateTeamSettings.isError && updateTeamSettings.error.message.length > 0 && (
+                    <span className="text-red-600 dark:text-red-400">{updateTeamSettings.error.message}</span>
+                )}
+            </div>
+        </div>
+    );
+};
