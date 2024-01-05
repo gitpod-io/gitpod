708571eb7a2c0280fc26f38186ec1f89e2262e06
Filip Troníček <filip@gitpod.io>
2024-01-05 12:30:55 +0000

    Fix button gap

--- components/dashboard/src/components/podkit/buttons/Button.tsx
+++ components/dashboard/src/components/podkit/buttons/Button.tsx
@@ -10,7 +10,7 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@podkit/lib/cn";
 
 export const buttonVariants = cva(
-    "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+    "inline-flex gap-1.5 items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
     {
         variants: {
             variant: {
