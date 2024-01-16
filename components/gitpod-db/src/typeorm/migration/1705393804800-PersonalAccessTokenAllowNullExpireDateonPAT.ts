e404a421b57f9b5b52be1da4a364fb3e7badda63
Huiwen Huang <huiwen@gitpod.io>
2024-01-16 09:55:26 +0000

    fix migration

--- components/gitpod-db/src/typeorm/migration/1705393804800-PersonalAccessTokenAllowNullExpireDateonPAT.ts
+++ components/gitpod-db/src/typeorm/migration/1705393804800-PersonalAccessTokenAllowNullExpireDateonPAT.ts
@@ -10,12 +10,12 @@ const table = "d_b_personal_access_token";
 
 export class PersonalAccessTokenAllowNullExpireDateonPAT1705393804800 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE ${table} MODIFY expirationTime expirationTime timestamp(6) NULL`);
+        await queryRunner.query(`ALTER TABLE ${table} MODIFY expirationTime timestamp(6) NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `ALTER TABLE ${table} CHANGE expirationTime expirationTime timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)`,
+            `ALTER TABLE ${table} CHANGE expirationTime timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)`,
         );
     }
 }
