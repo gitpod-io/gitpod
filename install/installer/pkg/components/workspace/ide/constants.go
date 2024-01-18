59d89d5e1c57151e72d273db792125ac66715272
Jean Pierre <jeanp413@hotmail.com>
2024-01-18 00:39:54 -0500

    Update code stable (#19332)

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-1fc02691286b404b664249d7c840cf8c84343741" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion   = "commit-cb1173f2a457633550a7fdc89af86d8d4da51876" // stable version that will be updated manually on demand
 	CodeHelperIDEImage          = "ide/code-codehelper"
 	CodeWebExtensionImage       = "ide/gitpod-code-web"
 	CodeWebExtensionVersion     = "commit-127d8686824d65c06a75e16d23052ebfbd8bc6a7" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
