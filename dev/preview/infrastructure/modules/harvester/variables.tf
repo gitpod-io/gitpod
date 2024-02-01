22c2ef291c739ed84a0420d6ad99560513028296
Kyle Brennan <kyle@gitpod.io>
2024-02-01 18:26:12 +0000

    [workspace-cluster] update image for gen108

--- dev/preview/infrastructure/modules/harvester/variables.tf
+++ dev/preview/infrastructure/modules/harvester/variables.tf
@@ -33,7 +33,7 @@ variable "ssh_key" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202312131438"
+  default     = "gitpod-k3s-202402010208"
 }
 
 variable "harvester_ingress_ip" {
