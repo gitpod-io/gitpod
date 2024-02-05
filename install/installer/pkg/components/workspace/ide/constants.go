06e02e4a9fe974a6457008f8946ea1fc449e9697
Jean Pierre <jeanp413@hotmail.com>
2024-02-05 04:24:47 -0500

    Enable vscode 1.85 for older linux distros (#19391)
    * Enable vscode for older linux distros
    
    * :lipstick:
    
    * Update code to 1.86
    
    * Update code to 1.86

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -5,22 +5,23 @@
 package ide
 
 const (
-	CodeIDEImage                = "ide/code"
-	CodeIDEImageStableVersion   = "commit-cb1173f2a457633550a7fdc89af86d8d4da51876" // stable version that will be updated manually on demand
-	CodeHelperIDEImage          = "ide/code-codehelper"
-	CodeWebExtensionImage       = "ide/gitpod-code-web"
-	CodeWebExtensionVersion     = "commit-127d8686824d65c06a75e16d23052ebfbd8bc6a7" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
-	CodeDesktopIDEImage         = "ide/code-desktop"
-	CodeDesktopInsidersIDEImage = "ide/code-desktop-insiders"
-	XtermIDEImage               = "ide/xterm-web"
-	IntelliJDesktopIDEImage     = "ide/intellij"
-	GoLandDesktopIdeImage       = "ide/goland"
-	PyCharmDesktopIdeImage      = "ide/pycharm"
-	PhpStormDesktopIdeImage     = "ide/phpstorm"
-	RubyMineDesktopIdeImage     = "ide/rubymine"
-	WebStormDesktopIdeImage     = "ide/webstorm"
-	RiderDesktopIdeImage        = "ide/rider"
-	CLionDesktopIdeImage        = "ide/clion"
-	JetBrainsBackendPluginImage = "ide/jb-backend-plugin"
-	JetBrainsLauncherImage      = "ide/jb-launcher"
+	CodeIDEImage                  = "ide/code"
+	CodeIDEImageStableVersion     = "commit-fc97f55eae648b06f4cbd03e67c52f229de17023" // stable version that will be updated manually on demand
+	Code1_85IDEImageStableVersion = "commit-cb1173f2a457633550a7fdc89af86d8d4da51876"
+	CodeHelperIDEImage            = "ide/code-codehelper"
+	CodeWebExtensionImage         = "ide/gitpod-code-web"
+	CodeWebExtensionVersion       = "commit-127d8686824d65c06a75e16d23052ebfbd8bc6a7" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
+	CodeDesktopIDEImage           = "ide/code-desktop"
+	CodeDesktopInsidersIDEImage   = "ide/code-desktop-insiders"
+	XtermIDEImage                 = "ide/xterm-web"
+	IntelliJDesktopIDEImage       = "ide/intellij"
+	GoLandDesktopIdeImage         = "ide/goland"
+	PyCharmDesktopIdeImage        = "ide/pycharm"
+	PhpStormDesktopIdeImage       = "ide/phpstorm"
+	RubyMineDesktopIdeImage       = "ide/rubymine"
+	WebStormDesktopIdeImage       = "ide/webstorm"
+	RiderDesktopIdeImage          = "ide/rider"
+	CLionDesktopIdeImage          = "ide/clion"
+	JetBrainsBackendPluginImage   = "ide/jb-backend-plugin"
+	JetBrainsLauncherImage        = "ide/jb-launcher"
 )
