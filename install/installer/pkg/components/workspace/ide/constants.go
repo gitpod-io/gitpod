8e20afd186d7dcd29ec824f7469cc9f07012c662
Jean Pierre <jeanp413@hotmail.com>
2024-02-15 16:05:57 -0500

    Update code-web extension (#19427)

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -10,7 +10,7 @@ const (
 	Code1_85IDEImageStableVersion = "commit-cb1173f2a457633550a7fdc89af86d8d4da51876"
 	CodeHelperIDEImage            = "ide/code-codehelper"
 	CodeWebExtensionImage         = "ide/gitpod-code-web"
-	CodeWebExtensionVersion       = "commit-736db6fb669a1e3dd3379c26e1aa932ab9c8f696" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
+	CodeWebExtensionVersion       = "commit-4e069a6195f3926ba8b84725bc806228f4cb94ec" // gitpod-web extension version comes from https://github.com/gitpod-io/gitpod-code
 	CodeDesktopIDEImage           = "ide/code-desktop"
 	CodeDesktopInsidersIDEImage   = "ide/code-desktop-insiders"
 	XtermIDEImage                 = "ide/xterm-web"
