bdad590b090ae449411bb97ec2637d7539347d88
Gero Posmyk-Leinemann <32448529+geropl@users.noreply.github.com>
2024-02-27 08:13:08 +0100

    [server] Missing SCM access: Filter out user error on workspace start (#19469)
    * [server] Missing SCM access: Filter out user error on workspace start
    
    to prevent false alerts (EXP-1434)
    
    * [proxy] api.: Handle /auth/*/callback

--- components/server/src/prometheus-metrics.ts
+++ components/server/src/prometheus-metrics.ts
@@ -203,6 +203,7 @@ export type FailedInstanceStartReason =
     | "startOnClusterFailed"
     | "imageBuildFailed"
     | "imageBuildFailedUser"
+    | "scmAccessFailed"
     | "resourceExhausted"
     | "workspaceClusterMaintenance"
     | "other";
