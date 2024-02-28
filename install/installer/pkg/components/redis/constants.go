39d36a9c1e420e533149b21669077f887baaef76
Pudong Zheng <pudong@gitpod.io>
2024-02-28 09:44:41 +0000

    [installer] use image digest for redis image

--- install/installer/pkg/components/redis/constants.go
+++ install/installer/pkg/components/redis/constants.go
@@ -14,10 +14,10 @@ const (
 	RegistryImage = "chainguard/redis"
 
 	ContainerName = "redis"
-	ImageTag      = "latest"
+	ImageDigest   = "sha256:cd0eb6030e86577bbf24c18e7d6ff1f84b2bce4b9f1d74f2bd0fba170e728165"
 
 	ExporterRegistryImage = "chainguard/prometheus-redis-exporter"
-	ExporterImageTag      = "latest"
+	ExporterImageDigest   = "sha256:68eeec479c749ffb9b9a3ac8913cedc8cb067040675570d721bc1d2f39051a02"
 
 	ExporterContainerName = "exporter"
 	ExporterPortName      = "exporter"
