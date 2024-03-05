4438c62d57c8bb35d2372ebf76e67444ccdbd3e7
Jean Pierre <jeanpierre@gitpod.io>
2024-03-05 08:16:48 +0000

    Add db migration

--- components/gitpod-db/src/typeorm/entity/db-team-settings.ts
+++ components/gitpod-db/src/typeorm/entity/db-team-settings.ts
@@ -24,6 +24,9 @@ export class DBOrgSettings implements OrganizationSettings {
     @Column("json", { nullable: true })
     allowedWorkspaceClasses?: string[] | null;
 
+    @Column("json", { nullable: true })
+    pinnedEditorVersions?: { [key: string]: string } | null;
+
     @Column()
     deleted: boolean;
 }
