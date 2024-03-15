2abf8db5ff3831de03cea3c8649baf039ffe3a29
Sven Efftinge <sven@gitpod.io>
2024-03-15 12:22:31 +0000

    [dashboard] update login screen and autostart

--- components/dashboard/src/workspaces/CreateWorkspacePage.tsx
+++ components/dashboard/src/workspaces/CreateWorkspacePage.tsx
@@ -29,7 +29,7 @@ import { useWorkspaceContext } from "../data/workspaces/resolve-context-query";
 import { useDirtyState } from "../hooks/use-dirty-state";
 import { openAuthorizeWindow } from "../provider-utils";
 import { gitpodHostUrl } from "../service/service";
-import { StartWorkspaceError } from "../start/StartPage";
+import { StartPage, StartWorkspaceError } from "../start/StartPage";
 import { VerifyModal } from "../start/VerifyModal";
 import { StartWorkspaceOptions } from "../start/start-workspace-options";
 import { UserContext, useCurrentUser } from "../user-context";
@@ -50,6 +50,7 @@ import { EditorReference } from "@gitpod/public-api/lib/gitpod/v1/editor_pb";
 import { converter } from "../service/public-api";
 import { useUpdateCurrentUserMutation } from "../data/current-user/update-mutation";
 import { useAllowedWorkspaceClassesMemo } from "../data/workspaces/workspace-classes-query";
+import Menu from "../menu/Menu";
 
 type NextLoadOption = "searchParams" | "autoStart" | "allDone";
 
@@ -419,90 +420,100 @@ export function CreateWorkspacePage() {
         );
     }
 
+    if (
+        (createWorkspaceMutation.isStarting || autostart) &&
+        !(createWorkspaceMutation.error || workspaceContext.error)
+    ) {
+        return <StartPage phase={WorkspacePhase_Phase.PREPARING} />;
+    }
+
     return (
-        <div className="flex flex-col mt-32 mx-auto ">
-            <div className="flex flex-col max-h-screen max-w-xl mx-auto items-center w-full">
-                <Heading1>New Workspace</Heading1>
-                <div className="text-gray-500 text-center text-base">
-                    Create a new workspace in the{" "}
-                    <span className="font-semibold text-gray-600 dark:text-gray-400">{currentOrg?.name}</span>{" "}
-                    organization.
-                </div>
-
-                <div className="-mx-6 px-6 mt-6 w-full">
-                    {createWorkspaceMutation.error || workspaceContext.error ? (
-                        <ErrorMessage
-                            error={
-                                (createWorkspaceMutation.error as StartWorkspaceError) ||
-                                (workspaceContext.error as StartWorkspaceError)
-                            }
-                            setSelectAccountError={setSelectAccountError}
-                            reset={() => {
-                                workspaceContext.refetch();
-                                createWorkspaceMutation.reset();
-                            }}
-                        />
-                    ) : null}
-
-                    <InputField>
-                        <RepositoryFinder
-                            onChange={handleContextURLChange}
-                            selectedContextURL={contextURL}
-                            selectedConfigurationId={selectedProjectID}
-                            expanded={!contextURL}
-                            disabled={createWorkspaceMutation.isStarting}
-                        />
-                    </InputField>
-
-                    <InputField error={errorIde}>
-                        <SelectIDEComponent
-                            onSelectionChange={onSelectEditorChange}
-                            setError={setErrorIde}
-                            selectedIdeOption={selectedIde}
-                            useLatest={useLatestIde}
-                            disabled={createWorkspaceMutation.isStarting}
-                            loading={workspaceContext.isLoading}
-                        />
-                    </InputField>
-
-                    <InputField error={errorWsClass}>
-                        <SelectWorkspaceClassComponent
-                            selectedConfigurationId={selectedProjectID}
-                            onSelectionChange={setSelectedWsClass}
-                            setError={setErrorWsClass}
-                            selectedWorkspaceClass={selectedWsClass}
-                            disabled={createWorkspaceMutation.isStarting}
-                            loading={workspaceContext.isLoading}
-                        />
-                    </InputField>
-                </div>
-                <div className="w-full flex justify-end mt-3 space-x-2 px-6">
-                    <LoadingButton
-                        onClick={onClickCreate}
-                        autoFocus={true}
-                        className="w-full"
-                        loading={createWorkspaceMutation.isStarting || !!autostart}
-                        disabled={continueButtonDisabled}
-                    >
-                        {createWorkspaceMutation.isStarting ? "Opening Workspace ..." : "Continue"}
-                    </LoadingButton>
-                </div>
-                {existingWorkspaces.length > 0 && !createWorkspaceMutation.isStarting && (
-                    <div className="w-full flex flex-col justify-end px-6">
-                        <p className="mt-6 text-center text-base">Running workspaces on this revision</p>
-                        {existingWorkspaces.map((w) => {
-                            return (
-                                <a
-                                    key={w.id}
-                                    href={w.status?.workspaceUrl || `/start/${w.id}}`}
-                                    className="rounded-xl group hover:bg-gray-100 dark:hover:bg-gray-800 flex"
-                                >
-                                    <WorkspaceEntry info={w} shortVersion={true} />
-                                </a>
-                            );
-                        })}
+        <div className="container">
+            <Menu />
+            <div className="flex flex-col mt-32 mx-auto ">
+                <div className="flex flex-col max-h-screen max-w-xl mx-auto items-center w-full">
+                    <Heading1>New Workspace</Heading1>
+                    <div className="text-gray-500 text-center text-base">
+                        Create a new workspace in the{" "}
+                        <span className="font-semibold text-gray-600 dark:text-gray-400">{currentOrg?.name}</span>{" "}
+                        organization.
                     </div>
-                )}
+
+                    <div className="-mx-6 px-6 mt-6 w-full">
+                        {createWorkspaceMutation.error || workspaceContext.error ? (
+                            <ErrorMessage
+                                error={
+                                    (createWorkspaceMutation.error as StartWorkspaceError) ||
+                                    (workspaceContext.error as StartWorkspaceError)
+                                }
+                                setSelectAccountError={setSelectAccountError}
+                                reset={() => {
+                                    workspaceContext.refetch();
+                                    createWorkspaceMutation.reset();
+                                }}
+                            />
+                        ) : null}
+
+                        <InputField>
+                            <RepositoryFinder
+                                onChange={handleContextURLChange}
+                                selectedContextURL={contextURL}
+                                selectedConfigurationId={selectedProjectID}
+                                expanded={!contextURL}
+                                disabled={createWorkspaceMutation.isStarting}
+                            />
+                        </InputField>
+
+                        <InputField error={errorIde}>
+                            <SelectIDEComponent
+                                onSelectionChange={onSelectEditorChange}
+                                setError={setErrorIde}
+                                selectedIdeOption={selectedIde}
+                                useLatest={useLatestIde}
+                                disabled={createWorkspaceMutation.isStarting}
+                                loading={workspaceContext.isLoading}
+                            />
+                        </InputField>
+
+                        <InputField error={errorWsClass}>
+                            <SelectWorkspaceClassComponent
+                                selectedConfigurationId={selectedProjectID}
+                                onSelectionChange={setSelectedWsClass}
+                                setError={setErrorWsClass}
+                                selectedWorkspaceClass={selectedWsClass}
+                                disabled={createWorkspaceMutation.isStarting}
+                                loading={workspaceContext.isLoading}
+                            />
+                        </InputField>
+                    </div>
+                    <div className="w-full flex justify-end mt-3 space-x-2 px-6">
+                        <LoadingButton
+                            onClick={onClickCreate}
+                            autoFocus={true}
+                            className="w-full"
+                            loading={createWorkspaceMutation.isStarting || !!autostart}
+                            disabled={continueButtonDisabled}
+                        >
+                            {createWorkspaceMutation.isStarting ? "Opening Workspace ..." : "Continue"}
+                        </LoadingButton>
+                    </div>
+                    {existingWorkspaces.length > 0 && !createWorkspaceMutation.isStarting && (
+                        <div className="w-full flex flex-col justify-end px-6">
+                            <p className="mt-6 text-center text-base">Running workspaces on this revision</p>
+                            {existingWorkspaces.map((w) => {
+                                return (
+                                    <a
+                                        key={w.id}
+                                        href={w.status?.workspaceUrl || `/start/${w.id}}`}
+                                        className="rounded-xl group hover:bg-gray-100 dark:hover:bg-gray-800 flex"
+                                    >
+                                        <WorkspaceEntry info={w} shortVersion={true} />
+                                    </a>
+                                );
+                            })}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
