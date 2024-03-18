f9aee7fe0e7faf6c3a05d3ab7c929399d0eff5d2
Filip Troníček <filip@gitpod.io>
2024-03-18 20:39:27 +0100

    Parse context URLs when creating configurations (#19552)
    * Parse context URLs when creating configurations
    
    * Rename `cloneUrl` to `contextUrl`
    
    * fix on server impl
    
    * Revert "fix on server impl"
    
    This reverts commit e61032d818f85565e328c21981d8f529220a8430.
    
    * Revert "Rename `cloneUrl` to `contextUrl`"
    
    This reverts commit 649ab77cc54b66ab7005317c9f5ebe3deb2b2152.

--- components/server/src/api/configuration-service-api.ts
+++ components/server/src/api/configuration-service-api.ts
@@ -29,6 +29,7 @@ import { UserService } from "../user/user-service";
 import { SortOrder } from "@gitpod/public-api/lib/gitpod/v1/sorting_pb";
 import { Project } from "@gitpod/gitpod-protocol";
 import { DeepPartial } from "@gitpod/gitpod-protocol/lib/util/deep-partial";
+import { ContextService } from "../workspace/context-service";
 
 function buildUpdateObject<T extends Record<string, any>>(obj: T): Partial<T> {
     const update: Partial<T> = {};
@@ -55,6 +56,8 @@ export class ConfigurationServiceAPI implements ServiceImpl<typeof Configuration
         private readonly apiConverter: PublicAPIConverter,
         @inject(UserService)
         private readonly userService: UserService,
+        @inject(ContextService)
+        private readonly contextService: ContextService,
     ) {}
 
     async createConfiguration(
@@ -73,11 +76,16 @@ export class ConfigurationServiceAPI implements ServiceImpl<typeof Configuration
             throw new ApplicationError(ErrorCodes.NOT_FOUND, "user not found");
         }
 
+        const cloneUrl = await this.contextService.parseContextUrlAsCloneUrl(installer, req.cloneUrl);
+        if (!cloneUrl) {
+            throw new ApplicationError(ErrorCodes.BAD_REQUEST, "clone_url is not valid");
+        }
+
         const project = await this.projectService.createProject(
             {
                 teamId: req.organizationId,
                 name: req.name,
-                cloneUrl: req.cloneUrl,
+                cloneUrl,
                 appInstallationId: "",
                 slug: "",
             },
