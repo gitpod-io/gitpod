24ea6eadec4f0af0d39f30a11eee6d20158a2fd7
Kyle Brennan <kyle@gitpod.io>
2024-04-15 23:33:23 +0000

    Too short

--- dev/loadgen/cmd/benchmark.go
+++ dev/loadgen/cmd/benchmark.go
@@ -55,7 +55,7 @@ var benchmarkCommand = &cobra.Command{
 		}
 
 		var load loadgen.LoadGenerator
-		load = loadgen.NewFixedLoadGenerator(500*time.Millisecond, 200*time.Millisecond)
+		load = loadgen.NewFixedLoadGenerator(800*time.Millisecond, 300*time.Millisecond)
 		load = loadgen.NewWorkspaceCountLimitingGenerator(load, scenario.Workspaces)
 
 		template := &api.StartWorkspaceRequest{
