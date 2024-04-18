a243cb4b7bf44e920a4e44afed4b664c9e1e5f3e
Filip Troníček <filip@gitpod.io>
2024-04-18 17:16:26 +0000

    Fix super long unauthed contexts

--- components/dashboard/src/utils.ts
+++ components/dashboard/src/utils.ts
@@ -117,3 +117,35 @@ export function isWebsiteSlug(pathName: string) {
     ];
     return slugs.some((slug) => pathName.startsWith("/" + slug + "/") || pathName === "/" + slug);
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#testing_for_availability
+export function storageAvailable(type: "localStorage" | "sessionStorage"): boolean {
+    let storage;
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        if (!storage) {
+            return false;
+        }
+
+        return (
+            e instanceof DOMException &&
+            // everything except Firefox
+            (e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === "QuotaExceededError" ||
+                // Firefox
+                e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage &&
+            storage.length !== 0
+        );
+    }
+}
