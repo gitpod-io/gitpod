b60cc2770121a8424960a37ff24bd7039b950134
Filip Troníček <filip@gitpod.io>
2024-04-23 13:49:46 +0000

    Dynamic auth provider message

--- components/dashboard/src/workspaces/BrowserExtensionBanner.tsx
+++ components/dashboard/src/workspaces/BrowserExtensionBanner.tsx
@@ -6,11 +6,16 @@
 
 import { useEffect, useMemo, useState } from "react";
 import UAParser from "ua-parser-js";
+import { useUserLoader } from "../hooks/use-user-loader";
+import { User } from "@gitpod/public-api/lib/gitpod/v1/user_pb";
+import { AuthProviderDescription, AuthProviderType } from "@gitpod/public-api/lib/gitpod/v1/authprovider_pb";
+import { useAuthProviderDescriptions } from "../data/auth-providers/auth-provider-descriptions-query";
 
 type BrowserOption = {
     aliases?: string[];
     url: string;
 };
+type UnifiedAuthProvider = "Bitbucket" | "GitLab" | "GitHub";
 
 const installationOptions: Record<string, BrowserOption> = {
     firefox: {
@@ -22,7 +27,49 @@ const installationOptions: Record<string, BrowserOption> = {
     },
 };
 
+const isIdentity = (identity?: AuthProviderDescription): identity is AuthProviderDescription => !!identity;
+const unifyProviderType = (type: AuthProviderType): UnifiedAuthProvider | undefined => {
+    switch (type) {
+        case AuthProviderType.BITBUCKET:
+        case AuthProviderType.BITBUCKET_SERVER:
+            return "Bitbucket";
+        case AuthProviderType.GITHUB:
+            return "GitHub";
+        case AuthProviderType.GITLAB:
+            return "GitLab";
+        default:
+            return undefined;
+    }
+};
+
+const isAuthProviderType = (type?: UnifiedAuthProvider): type is UnifiedAuthProvider => !!type;
+const getDeduplicatedScmProviders = (user: User, descriptions: AuthProviderDescription[]): UnifiedAuthProvider[] => {
+    const userIdentities = user.identities.map((identity) => identity.authProviderId);
+    const userProviders = userIdentities
+        .map((id) => descriptions?.find((provider) => provider.id === id))
+        .filter(isIdentity)
+        .map((provider) => provider.type);
+
+    return userProviders.map((type) => unifyProviderType(type)).filter(isAuthProviderType);
+};
+
+const displayScmProviders = (providers: UnifiedAuthProvider[]): string => {
+    const formatter = new Intl.ListFormat("en", { style: "long", type: "disjunction" });
+
+    return formatter.format(providers);
+};
+
 export function BrowserExtensionBanner() {
+    const { user } = useUserLoader();
+    const { data: authProviderDescriptions } = useAuthProviderDescriptions();
+
+    const scmProviderString = useMemo(() => {
+        if (!user || !authProviderDescriptions) return;
+
+        const usedProviders = getDeduplicatedScmProviders(user, authProviderDescriptions);
+        return displayScmProviders(usedProviders);
+    }, [user, authProviderDescriptions]);
+
     const parser = useMemo(() => new UAParser(), []);
     const browserName = useMemo(() => parser.getBrowser().name?.toLowerCase(), [parser]);
 
@@ -50,6 +97,10 @@ export function BrowserExtensionBanner() {
         return null;
     }
 
+    if (!scmProviderString) {
+        return null;
+    }
+
     let browserOption: BrowserOption | undefined = installationOptions[browserName];
     if (!browserOption) {
         browserOption = Object.values(installationOptions).find(
@@ -64,7 +115,7 @@ export function BrowserExtensionBanner() {
         <section className="flex justify-center w-full mt-20 mx-4">
             <div className="sm:flex justify-between border-2 rounded-xl hidden max-w-xl mt-4">
                 <div className="flex flex-col gap-1 py-4 px-2 justify-center">
-                    <span className="text-lg font-semibold">Open from Github</span>
+                    <span className="text-lg font-semibold">Open from {scmProviderString}</span>
                     <span>
                         <a href={browserOption.url} className="gp-link">
                             Install the Gitpod extension
