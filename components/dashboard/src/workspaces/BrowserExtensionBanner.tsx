549103e93ec5c374dac35a55685b6e57c7073b5a
Filip Troníček <filip@gitpod.io>
2024-04-26 10:39:24 +0000

    some design tweaks from pr review

--- components/dashboard/src/workspaces/BrowserExtensionBanner.tsx
+++ components/dashboard/src/workspaces/BrowserExtensionBanner.tsx
@@ -151,10 +151,12 @@ export function BrowserExtensionBanner() {
     }
 
     return (
-        <section className="flex justify-center mt-20 mx-4">
-            <div className="sm:flex justify-between border-pk-border-base border-2 rounded-xl hidden max-w-xl mt-4">
-                <div className="flex flex-col gap-1 py-5 pl-6 pr-4 justify-center">
-                    <span className="text-lg font-semibold">Open from {scmProviderString}</span>
+        <section className="flex justify-center mt-24 mx-4">
+            <div className="sm:flex justify-between border-pk-border-light border-2 rounded-xl hidden max-w-xl mt-4">
+                <div className="flex flex-col gap-1 py-5 pl-6 pr-6 justify-center">
+                    <span className="text-lg font-semibold text-pk-content-secondary">
+                        Open from {scmProviderString}
+                    </span>
                     <span className="text-xs">
                         <a
                             href={browserOption.url}
