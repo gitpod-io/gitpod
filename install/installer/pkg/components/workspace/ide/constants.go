bc05c6998d10d10ad1c6b89087d2bf1094851d04
Huiwen <mhqnwt@gmail.com>
2024-05-06 22:56:45 +0800

    [code] upgrade Browser VS Code to 1.89.0 (#19706)

--- install/installer/pkg/components/workspace/ide/constants.go
+++ install/installer/pkg/components/workspace/ide/constants.go
@@ -6,7 +6,7 @@ package ide
 
 const (
 	CodeIDEImage                  = "ide/code"
-	CodeIDEImageStableVersion     = "commit-2ca710524cf1d69bc014cbfd57865375a4f9a522" // stable version that will be updated manually on demand
+	CodeIDEImageStableVersion     = "commit-8a412095311a2ee0460abca3a4461704c4533374" // stable version that will be updated manually on demand
 	Code1_85IDEImageStableVersion = "commit-cb1173f2a457633550a7fdc89af86d8d4da51876"
 	CodeHelperIDEImage            = "ide/code-codehelper"
 	CodeWebExtensionImage         = "ide/gitpod-code-web"
