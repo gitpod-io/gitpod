cb3866af30b82f1de2ccf320c06508e0460991d7
Gero Posmyk-Leinemann <gero@gitpod.io>
2024-05-13 12:53:02 +0000

    [dev] copied over CI from main

--- .github/actions/deploy-gitpod/entrypoint.sh
+++ .github/actions/deploy-gitpod/entrypoint.sh
@@ -7,24 +7,20 @@ export PREVIEW_ENV_DEV_SA_KEY_PATH="$HOME/.config/gcloud/preview-environment-dev
 # shellcheck disable=SC2155
 export LEEWAY_WORKSPACE_ROOT="$(pwd)"
 export VERSION="${INPUT_VERSION}"
+export IMAGE_REPO_BASE="${INPUT_IMAGE_REPO_BASE}"
 export PATH="$PATH:$HOME/bin"
 
 mkdir $HOME/bin
 
 echo "Downloading installer for ${VERSION}"
-oci-tool fetch file -o $HOME/bin/installer --platform=linux-amd64 "eu.gcr.io/gitpod-core-dev/build/installer:${VERSION}" app/installer
+oci-tool fetch file -o $HOME/bin/installer --platform=linux-amd64 "${IMAGE_REPO_BASE}/installer:${VERSION}" app/installer
 chmod +x $HOME/bin/installer
 
 echo "Download versions.yaml"
-oci-tool fetch file -o /tmp/versions.yaml --platform=linux-amd64 "eu.gcr.io/gitpod-core-dev/build/versions:${VERSION}" versions.yaml
+oci-tool fetch file -o /tmp/versions.yaml --platform=linux-amd64 "${IMAGE_REPO_BASE}/versions:${VERSION}" versions.yaml
 
-echo "${INPUT_SA_KEY}" > "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
-gcloud auth activate-service-account --key-file "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
-
-leeway run dev/preview/previewctl:download
-
-echo "Setting up access to core-dev and harvester"
-previewctl get-credentials --gcp-service-account "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
+gcloud auth login --cred-file="$GOOGLE_APPLICATION_CREDENTIALS" --activate --quiet
+leeway run dev/preview/previewctl:install
 
 PREVIEW_NAME="$(previewctl get-name --branch "${INPUT_NAME}")"
 export PREVIEW_NAME
@@ -32,12 +28,11 @@ export PREVIEW_NAME
 for var in WITH_DEDICATED_EMU ANALYTICS WORKSPACE_FEATURE_FLAGS; do
   input_var="INPUT_${var}"
   if [[ -n "${!input_var:-}" ]];then
-    # shellcheck disable=SC2140
     export "GITPOD_${var}"="${!input_var}"
   fi
 done
 
-previewctl install-context --branch "${PREVIEW_NAME}" --log-level debug --timeout 10m --gcp-service-account "${PREVIEW_ENV_DEV_SA_KEY_PATH}"
+previewctl install-context --branch "${PREVIEW_NAME}" --log-level debug --timeout 10m
 leeway run dev/preview:deploy-gitpod
 previewctl report --branch "${PREVIEW_NAME}" >> "${GITHUB_STEP_SUMMARY}"
 
