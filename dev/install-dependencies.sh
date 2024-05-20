a7547c83e562a90343769aca806b0a390506aea0
Pudong <tianshi8650@gmail.com>
2024-05-20 15:45:01 +0800

    improve dev container (#19757)

--- dev/install-dependencies.sh
+++ dev/install-dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+leeway run dev/preview:configure-workspace
+leeway run dev:install-dev-utils
 leeway run dev/preview/previewctl:install
 pre-commit install --install-hooks
