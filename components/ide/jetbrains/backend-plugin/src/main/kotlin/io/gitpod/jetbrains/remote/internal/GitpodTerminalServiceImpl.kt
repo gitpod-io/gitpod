cbd48c9edda6d9dd4ec42f8af15e859cdc6fbd52
Huiwen <mhqnwt@gmail.com>
2024-05-23 18:29:39 +0000

    Make terminal service stable

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/internal/GitpodTerminalServiceImpl.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/internal/GitpodTerminalServiceImpl.kt
@@ -2,9 +2,8 @@
 // Licensed under the GNU Affero General Public License (AGPL).
 // See License.AGPL.txt in the project root for license information.
 
-package io.gitpod.jetbrains.remote.latest
+package io.gitpod.jetbrains.remote.internal
 
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rd.util.threading.coroutines.launch
@@ -15,8 +14,7 @@ import org.jetbrains.plugins.terminal.ShellTerminalWidget
 import org.jetbrains.plugins.terminal.TerminalToolWindowManager
 import java.util.*
 
-@Suppress("UnstableApiUsage")
-class GitpodTerminalService(val project: Project): AbstractGitpodTerminalService(project) {
+class GitpodTerminalServiceImpl(val project: Project) : AbstractGitpodTerminalService(project) {
 
     private val terminalToolWindowManager = TerminalToolWindowManager.getInstance(project)
     private val backendTerminalManager = BackendTerminalManager.getInstance(project)
@@ -24,7 +22,14 @@ class GitpodTerminalService(val project: Project): AbstractGitpodTerminalService
     override fun runJob(lifetime: Lifetime, block: suspend CoroutineScope.() -> Unit) = lifetime.launch { block() }
 
     override fun createSharedTerminal(title: String): ShellTerminalWidget {
-        val shellTerminalWidget = ShellTerminalWidget.toShellJediTermWidgetOrThrow(terminalToolWindowManager.createShellWidget(null, title, true, false))
+        val shellTerminalWidget = ShellTerminalWidget.toShellJediTermWidgetOrThrow(
+            terminalToolWindowManager.createShellWidget(
+                null,
+                title,
+                true,
+                false
+            )
+        )
         backendTerminalManager.shareTerminal(shellTerminalWidget.asNewWidget(), UUID.randomUUID().toString())
         return shellTerminalWidget
     }
