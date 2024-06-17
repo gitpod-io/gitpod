5fef799b5d27f3a5c23f80a46ddd2ede2b214304
mustard-mh <mhqnwt@gmail.com>
2024-06-17 10:13:30 +0000

    Add deps

--- test/tests/ide/jetbrains/warmup-indexing.sh
+++ test/tests/ide/jetbrains/warmup-indexing.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Copyright (c) 2024 Gitpod GmbH. All rights reserved.
+# Licensed under the GNU Affero General Public License (AGPL).
+# See License.AGPL.txt in the project root for license information.
+
 
 set -euo pipefail
 SystemDir=$1
