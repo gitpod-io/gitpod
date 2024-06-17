0a872c2e654435e554020d4bca673cba3aa9dfa1
mustard-mh <mhqnwt@gmail.com>
2024-06-17 10:40:52 +0000

    Add debug log

--- components/ide-service/pkg/server/ideconfig.go
+++ components/ide-service/pkg/server/ideconfig.go
@@ -121,6 +121,7 @@ func ParseConfig(ctx context.Context, res remotes.Resolver, b []byte) (*config.I
 		}
 		cfg.IdeOptions.Options[id] = option
 	}
+	log.WithField("cfg", log.TrustedValueWrap{Value: cfg}).Info("==============ParsedIDEConfig")
 
 	return &cfg, nil
 }
