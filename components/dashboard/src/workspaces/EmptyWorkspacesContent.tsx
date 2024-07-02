60f9db97936d7d663b8e8e8f608475f173d9614b
Siddhant Khare <siddhant@gitpod.io>
2024-07-02 21:33:15 +0530

    [dashboard] Onboarding: new video onboarding for new workspace (#19987)
    * [dashboard] Onboarding: new video onboarding for new workspace
    
    * New button and copy text updates
    
    * Update YT Video

--- components/dashboard/src/workspaces/EmptyWorkspacesContent.tsx
+++ components/dashboard/src/workspaces/EmptyWorkspacesContent.tsx
@@ -4,34 +4,81 @@
  * See License.AGPL.txt in the project root for license information.
  */
 
+import React, { useEffect } from "react";
 import { StartWorkspaceModalKeyBinding } from "../App";
 import { LinkButton } from "@podkit/buttons/LinkButton";
-import { Heading2, Subheading } from "@podkit/typography/Headings";
+import { Heading1, Heading2, Subheading } from "@podkit/typography/Headings";
+import { trackVideoClick } from "../Analytics";
+
+declare global {
+    interface Window {
+        onYouTubeIframeAPIReady: () => void;
+        YT: any;
+    }
+}
 
 export const EmptyWorkspacesContent = () => {
+    useEffect(() => {
+        // Load the YouTube IFrame Player API code asynchronously
+        const tag = document.createElement("script");
+        tag.src = "https://www.youtube.com/iframe_api";
+        const firstScriptTag = document.getElementsByTagName("script")[0];
+        firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
+
+        // Create YouTube player when API is ready
+        window.onYouTubeIframeAPIReady = () => {
+            new window.YT.Player("gitpod-video", {
+                events: {
+                    onStateChange: onPlayerStateChange,
+                },
+            });
+        };
+    }, []);
+
+    const onPlayerStateChange = (event: any) => {
+        if (event.data === window.YT.PlayerState.PLAYING) {
+            trackVideoClick("create-new-workspace");
+        }
+    };
+
     return (
         <div className="app-container flex flex-col space-y-2">
-            <div className="px-6 py-3 flex flex-col">
-                <div className="flex flex-col items-center justify-center h-96 w-96 mx-auto">
-                    <Heading2 className="text-center pb-3">No Workspaces</Heading2>
-                    <Subheading className="text-center pb-6">
-                        Prefix any Git repository URL with {window.location.host}/# or create a new workspace for a
-                        recently used project.{" "}
-                        <a
-                            className="gp-link"
-                            target="_blank"
-                            rel="noreferrer"
-                            href="https://www.gitpod.io/docs/getting-started/"
-                        >
-                            Learn more
-                        </a>
-                    </Subheading>
-                    <span>
-                        <LinkButton href={"/new"} className="gap-1.5">
-                            New Workspace{" "}
-                            <span className="opacity-60 hidden md:inline">{StartWorkspaceModalKeyBinding}</span>
-                        </LinkButton>
-                    </span>
+            <div className="px-6 mt-12 flex flex-row justify-center space-x-8">
+                <div>
+                    <iframe
+                        id="gitpod-video"
+                        width="535"
+                        height="307"
+                        src="https://www.youtube.com/embed/1ZBN-b2cIB8?enablejsapi=1&modestbranding=1&rel=0&controls=1&showinfo=0&fs=1"
+                        title="YouTube - Gitpod in 120 seconds"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                        className="rounded-xl"
+                    ></iframe>
+                </div>
+                <div>
+                    <div className="flex flex-col items-left h-96 w-96">
+                        <Heading1 className="text-left mb-4 !font-semibold !text-2xl">Welcome to Gitpod</Heading1>
+                        <Heading2 className="text-left !mb-0 !font-semibold !text-lg">
+                            Create your first workspace
+                        </Heading2>
+                        <Subheading className="text-left max-w-xs">
+                            A workspace is a cloud development environment
+                        </Subheading>
+                        <span className="flex flex-col space-y-4 w-fit">
+                            <LinkButton
+                                variant="secondary"
+                                className="mt-4 border border-pk-content-secondary text-pk-content-secondary bg-pk-surface-secondary"
+                                href={"/new?showExamples=true"}
+                            >
+                                Try a configured demo repo
+                            </LinkButton>
+                            <LinkButton href={"/new"} className="gap-1.5">
+                                Open your repository{" "}
+                                <span className="opacity-60 hidden md:inline">{StartWorkspaceModalKeyBinding}</span>
+                            </LinkButton>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
