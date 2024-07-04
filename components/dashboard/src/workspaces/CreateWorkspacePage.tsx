db13e482f38c22a9d907941db977a9fbd3d30286
Sven Efftinge <sven@gitpod.io>
2024-07-04 09:31:24 +0000

    [github] throw up error message from GH

--- components/dashboard/src/workspaces/CreateWorkspacePage.tsx
+++ components/dashboard/src/workspaces/CreateWorkspacePage.tsx
@@ -736,10 +736,13 @@ export const RepositoryNotFound: FC<{ error: StartWorkspaceError }> = ({ error }
         })
         .toString();
 
+    const errorMessage = error.data?.errorMessage || error.message;
+
     if (!userScopes.includes(missingScope)) {
         return (
             <RepositoryInputError
                 title="The repository may be private. Please authorize Gitpod to access private repositories."
+                message={errorMessage}
                 linkText="Grant access"
                 linkHref={authorizeURL}
             />
@@ -747,7 +750,7 @@ export const RepositoryNotFound: FC<{ error: StartWorkspaceError }> = ({ error }
     }
 
     if (userIsOwner) {
-        return <RepositoryInputError title="The repository was not found in your account." />;
+        return <RepositoryInputError title="The repository was not found in your account." message={errorMessage} />;
     }
 
     let updatedRecently = false;
@@ -764,6 +767,7 @@ export const RepositoryNotFound: FC<{ error: StartWorkspaceError }> = ({ error }
         return (
             <RepositoryInputError
                 title={`Permission to access private repositories has been granted. If you are a member of '${owner}', please try to request access for Gitpod.`}
+                message={errorMessage}
                 linkText="Request access"
                 linkHref={authorizeURL}
             />
@@ -773,6 +777,7 @@ export const RepositoryNotFound: FC<{ error: StartWorkspaceError }> = ({ error }
         return (
             <RepositoryInputError
                 title={`Although you appear to have the correct authorization credentials, the '${owner}' organization has enabled OAuth App access restrictions, meaning that data access to third-parties is limited. For more information on these restrictions, including how to enable this app, visit https://docs.github.com/articles/restricting-access-to-your-organization-s-data/.`}
+                message={errorMessage}
                 linkText="Check Organization Permissions"
                 linkHref={"https://github.com/settings/connections/applications/484069277e293e6d2a2a"}
             />
@@ -782,6 +787,7 @@ export const RepositoryNotFound: FC<{ error: StartWorkspaceError }> = ({ error }
     return (
         <RepositoryInputError
             title={`Your access token was updated recently. Please try again if the repository exists and Gitpod was approved for '${owner}'.`}
+            message={errorMessage}
             linkText="Authorize again"
             linkHref={authorizeURL}
         />
