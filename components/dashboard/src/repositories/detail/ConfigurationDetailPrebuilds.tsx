a2980a432e821abcf768a54cf5e6f91de02e4c91
Filip Troníček <filip@gitpod.io>
2024-07-12 08:10:43 +0000

    Re-add link to prebuild history of a repo

--- components/dashboard/src/repositories/detail/ConfigurationDetailPrebuilds.tsx
+++ components/dashboard/src/repositories/detail/ConfigurationDetailPrebuilds.tsx
@@ -13,6 +13,8 @@ import { PrebuildSettingsForm } from "./prebuilds/PrebuildSettingsForm";
 import { useConfigurationMutation } from "../../data/configurations/configuration-queries";
 import { LoadingState } from "@podkit/loading/LoadingState";
 import { EnablePrebuildsError } from "./prebuilds/EnablePrebuildsError";
+import { TextMuted } from "@podkit/typography/TextMuted";
+import { Link } from "react-router-dom";
 
 type Props = {
     configuration: Configuration;
@@ -64,6 +66,13 @@ export const ConfigurationDetailPrebuilds: FC<Props> = ({ configuration }) => {
                     checked={enabled}
                     onCheckedChange={updateEnabled}
                     label={configuration.prebuildSettings?.enabled ? "Prebuilds are enabled" : "Prebuilds are disabled"}
+                    description={
+                        <TextMuted>
+                            <Link to={`/prebuilds?configurationId=${configuration.id}`} className="gp-link">
+                                View prebuild history
+                            </Link>
+                        </TextMuted>
+                    }
                 />
             </ConfigurationSettingsField>
 
