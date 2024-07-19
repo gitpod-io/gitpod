910b13302c70790af7beb16c9ffe3aa7f9588e09
Huiwen <mhqnwt@gmail.com>
2024-07-19 14:11:57 +0800

    [TB] add oauth entry (#20049)

--- components/server/src/oauth-server/db.ts
+++ components/server/src/oauth-server/db.ts
@@ -141,6 +141,17 @@ const desktopClient: OAuthClient = {
     ],
 };
 
+const toolbox: OAuthClient = {
+    id: "toolbox-gateway-gitpod-plugin",
+    name: "JetBrains Toolbox Gitpod Plugin",
+    redirectUris: ["jetbrains://gateway/io.gitpod.toolbox.gateway/auth"],
+    allowedGrants: ["authorization_code"],
+    scopes: [
+        // We scope all so that it can work in papi like a PAT
+        { name: "function:*" },
+    ],
+};
+
 const vscode = createVSCodeClient("vscode", "VS Code");
 const vscodeInsiders = createVSCodeClient("vscode-insiders", "VS Code Insiders");
 
@@ -157,6 +168,7 @@ export const inMemoryDatabase: InMemory = {
         [vscodium.id]: vscodium,
         [cursor.id]: cursor,
         [desktopClient.id]: desktopClient,
+        [toolbox.id]: toolbox,
     },
     tokens: {},
     scopes: {},
