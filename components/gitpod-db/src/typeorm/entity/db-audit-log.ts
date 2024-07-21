84b04ad92abaf5ade1b3b0395804cd99e2470267
Gero Posmyk-Leinemann <gero@gitpod.io>
2024-07-21 16:55:33 +0000

    [audit] Properly serialize BigInts

--- components/gitpod-db/src/typeorm/entity/db-audit-log.ts
+++ components/gitpod-db/src/typeorm/entity/db-audit-log.ts
@@ -7,6 +7,7 @@
 import { Entity, Column, PrimaryColumn } from "typeorm";
 import { TypeORM } from "../typeorm";
 import { AuditLog } from "@gitpod/gitpod-protocol/lib/audit-log";
+import { Transformer } from "../transformer";
 
 @Entity()
 export class DBAuditLog implements AuditLog {
@@ -25,6 +26,24 @@ export class DBAuditLog implements AuditLog {
     @Column("varchar")
     action: string;
 
-    @Column("simple-json")
+    @Column({
+        type: "text", // it's JSON on DB, but we aim to disable the Typeorm-internal JSON-transformer we can't control and can't disable otherwise
+        transformer: Transformer.SIMPLE_JSON_CUSTOM(
+            [],
+            function (key: string, value: any) {
+                if (typeof value === "bigint") {
+                    return `bigint:${value.toString()}`;
+                }
+                return value;
+            },
+            function (key: string, value: any) {
+                if (typeof value === "string" && value.startsWith("bigint:")) {
+                    const v: string = value.substring(7);
+                    return BigInt(v);
+                }
+                return value;
+            },
+        ),
+    })
     args: object[];
 }
