70aab765a6086feb40383ad961a0f82800f7d33e
Huiwen <mhqnwt@gmail.com>
2024-08-15 11:39:56 +0000

    1

--- dev/preview/workflow/preview/patch-ide-configmap.js
+++ dev/preview/workflow/preview/patch-ide-configmap.js
@@ -6,10 +6,15 @@ const fs = require("fs");
 let json = JSON.parse(fs.readFileSync(process.argv[2]).toString());
 
 function replaceImage(image) {
-    if (image.includes("jb-backend-plugin:commit-2d67254d5aa110bc2c76cd807b85b272e3d54d97-latest")) {
-        return image;
-    }
     return image.replace("gitpod-dev-artifact", "gitpod-core-dev");
+}
+
+// TODO(hw): remove me
+function replaceImage2(image) {
+    if (image.includes("jb-backend-plugin:commit-2d67254d5aa110bc2c76cd807b85b272e3d54d97-latest")) {
+        return image.replace("gitpod-dev-artifact", "gitpod-core-dev");
+    }
+    return image
 }
 
 for (let ide in json.ideOptions.options) {
@@ -24,6 +29,18 @@ for (let ide in json.ideOptions.options) {
             return version;
         });
     }
+
+    // TODO(hw): remove me
+    if (
+        ["intellij"].includes(
+            ide,
+        )
+    ) {
+        json.ideOptions.options[ide].pluginImage = replaceImage2(json.ideOptions.options[ide].pluginImage)
+        json.ideOptions.options[ide].imageLayers = json.ideOptions.options[ide].imageLayers.map(replaceImage2)
+    }
+
+    json.ideOptions.options[]
     if (["code", "code1_85"].includes(ide)) {
         json.ideOptions.options[ide].image = replaceImage(json.ideOptions.options[ide].image);
         json.ideOptions.options[ide].imageLayers = json.ideOptions.options[ide].imageLayers.map(replaceImage);
