71498a4299b89ad0885f1badad7bf2fafc4cfca8
Huiwen <mhqnwt@gmail.com>
2024-08-23 16:28:08 +0000

    test

--- components/ide/jetbrains/backend-plugin/build.sh
+++ components/ide/jetbrains/backend-plugin/build.sh
@@ -7,10 +7,10 @@ set -e
 
 JB_GP_VERSION=${1:-debug}
 
-if [ "${NO_VERIFY_JB_PLUGIN}" == "true" ]; then
-    echo "build.sh: skip verify plugin step"
-else
+# if [ "${NO_VERIFY_JB_PLUGIN}" == "true" ]; then
+#     echo "build.sh: skip verify plugin step"
+# else
     ./gradlew -PsupervisorApiProjectPath=components-supervisor-api-java--lib/ -PgitpodProtocolProjectPath=components-gitpod-protocol-java--lib/ -PenvironmentName="$JB_QUALIFIER" -Dgradle.user.home="/workspace/.gradle-$JB_QUALIFIER" -Dplugin.verifier.home.dir="$HOME/.cache/pluginVerifier-$JB_QUALIFIER" -PgitpodVersion="$JB_GP_VERSION" runPluginVerifier
-fi
+# fi
 ./gradlew -PsupervisorApiProjectPath=components-supervisor-api-java--lib/ -PgitpodProtocolProjectPath=components-gitpod-protocol-java--lib/ -PenvironmentName="$JB_QUALIFIER" -Dgradle.user.home="/workspace/.gradle-$JB_QUALIFIER" -Dplugin.verifier.home.dir="$HOME/.cache/pluginVerifier-$JB_QUALIFIER" -PgitpodVersion="$JB_GP_VERSION" buildPlugin
 unzip ./build/distributions/gitpod-remote.zip -d ./build
