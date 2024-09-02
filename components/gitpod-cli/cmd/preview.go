f209a5bff968234ecb6009a2c70acc8d95472ff8
Huiwen <mhqnwt@gmail.com>
2024-09-02 12:47:41 +0000

    remove stdout pipe

--- components/gitpod-cli/cmd/preview.go
+++ components/gitpod-cli/cmd/preview.go
@@ -84,7 +84,6 @@ func openPreview(gpBrowserEnvVar string, url string) error {
 	args = append(args, url)
 
 	previewCmd := exec.Command(pcmd, args...)
-	previewCmd.Stdout = os.Stdout
 	previewCmd.Stderr = os.Stderr
 	err = previewCmd.Run()
 	if err != nil {
