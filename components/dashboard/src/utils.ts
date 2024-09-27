d7f086deff682b656dc76d11f47218c3df7776ad
Huiwen <mhqnwt@gmail.com>
2024-09-27 17:12:47 +0000

    Revert "revert me"
    This reverts commit 189c431eca33b06812ae33beaf3f5c95a28604ee.

--- components/dashboard/src/utils.ts
+++ components/dashboard/src/utils.ts
@@ -53,14 +53,12 @@ export const poll = async <T>(
 };
 
 export function isGitpodIo() {
-    return window.location.hostname === "gitpod.io";
-    // TODO(hw): Revert me before merging
-    // return (
-    //     window.location.hostname === "gitpod.io" ||
-    //     window.location.hostname === "gitpod-staging.com" ||
-    //     window.location.hostname.endsWith("gitpod-dev.com") ||
-    //     window.location.hostname.endsWith("gitpod-io-dev.com")
-    // );
+    return (
+        window.location.hostname === "gitpod.io" ||
+        window.location.hostname === "gitpod-staging.com" ||
+        window.location.hostname.endsWith("gitpod-dev.com") ||
+        window.location.hostname.endsWith("gitpod-io-dev.com")
+    );
 }
 
 function trimResource(resource: string): string {
