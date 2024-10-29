6a1111a2db6363f36f938ce87fbad34b153f2cc4
Pudong <tianshi8650@gmail.com>
2024-10-29 16:26:54 +0800

    Add flex oidc support (#20324)

--- dev/preview/workflow/preview/configure-workspace.sh
+++ dev/preview/workflow/preview/configure-workspace.sh
@@ -20,11 +20,11 @@ fi
 
 if [ -f "/usr/local/gitpod/config/initial-spec.json" ]; then
   gcloud iam workload-identity-pools create-cred-config \
-    projects/184212049955/locations/global/workloadIdentityPools/gitpod-next/providers/gitpod-next-provider \
+    projects/184212049955/locations/global/workloadIdentityPools/gitpod-flex/providers/gitpod-flex-provider \
     --service-account=preview-environmnet-dev@gitpod-dev-preview.iam.gserviceaccount.com \
     --service-account-token-lifetime-seconds=1h \
     --output-file="${PREVIEW_ENV_DEV_SA_KEY_PATH}" \
-    --executable-command='node /workspace/gitpod/dev/next-oidc/oidc.js' \
+    --executable-command='node /workspace/gitpod/dev/flex-oidc/oidc.js' \
     --executable-timeout-millis=5000
 elif [[ -n "${PREVIEW_ENV_DEV_CRED:-}" ]]; then
   echo "${PREVIEW_ENV_DEV_CRED}" >"${PREVIEW_ENV_DEV_SA_KEY_PATH}"
