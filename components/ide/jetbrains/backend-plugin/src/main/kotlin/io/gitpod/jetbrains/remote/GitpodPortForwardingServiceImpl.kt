87775d37ae55b659eb9096f4ba1c78e50dacc7aa
Huiwen <mhqnwt@gmail.com>
2024-11-15 07:56:53 +0000

    Make stable plugin build

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodPortForwardingServiceImpl.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodPortForwardingServiceImpl.kt
@@ -2,10 +2,10 @@
 // Licensed under the GNU Affero General Public License (AGPL).
 // See License.AGPL.txt in the project root for license information.
 
-package io.gitpod.jetbrains.remote.stable
+package io.gitpod.jetbrains.remote
 
 import com.jetbrains.rd.util.lifetime.Lifetime
-import com.jetbrains.rd.framework.util.launch
+import com.jetbrains.rd.util.threading.coroutines.launch
 import io.gitpod.jetbrains.remote.AbstractGitpodPortForwardingService
 import kotlinx.coroutines.CoroutineScope
 
