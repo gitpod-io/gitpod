c07687fdc761c308b5afc218885be795221aadde
Gero Posmyk-Leinemann <gero@gitpod.io>
2025-02-18 10:11:56 +0000

    [api, server, dashboard] Cleanup UpdateOrganizationSettings API
    Tool: gitpod/catfood.gitpod.cloud

--- components/gitpod-db/src/typeorm/entity/db-team-settings.ts
+++ components/gitpod-db/src/typeorm/entity/db-team-settings.ts
@@ -25,34 +25,34 @@ export class DBOrgSettings implements OrganizationSettings {
     workspaceSharingDisabled?: boolean;
 
     @Column("varchar", { nullable: true })
-    defaultWorkspaceImage?: string | null;
+    defaultWorkspaceImage?: string;
 
     @Column("json", { nullable: true })
-    allowedWorkspaceClasses?: string[] | null;
+    allowedWorkspaceClasses?: string[];
 
     @Column("json", { nullable: true })
-    pinnedEditorVersions?: { [key: string]: string } | null;
+    pinnedEditorVersions?: { [key: string]: string };
 
     @Column("json", { nullable: true })
-    restrictedEditorNames?: string[] | null;
+    restrictedEditorNames?: string[];
 
     @Column("varchar", { nullable: true })
-    defaultRole?: OrgMemberRole | undefined;
+    defaultRole?: OrgMemberRole;
 
     @Column("json", { nullable: true })
-    timeoutSettings?: TimeoutSettings | undefined;
+    timeoutSettings?: TimeoutSettings;
 
     @Column("json", { nullable: true })
-    roleRestrictions?: RoleRestrictions | undefined;
+    roleRestrictions?: RoleRestrictions;
 
     @Column({ type: "int", default: 0 })
     maxParallelRunningWorkspaces: number;
 
     @Column("json", { nullable: true })
-    onboardingSettings?: OnboardingSettings | undefined;
+    onboardingSettings?: OnboardingSettings;
 
     @Column({ type: "boolean", default: false })
-    annotateGitCommits?: boolean | undefined;
+    annotateGitCommits?: boolean;
 
     @Column()
     deleted: boolean;
