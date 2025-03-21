3f2d253988a02998ef3aa25e3d8a6a9d5c27ae2d
Kyle Brennan <kyle@gitpod.io>
2025-03-21 02:52:50 -0400

    Bump docker compose and docker (#20688)
    * Bump docker compose
    
    Tool: gitpod/catfood.gitpod.cloud
    
    * Bump docker
    
    Tool: gitpod/catfood.gitpod.cloud
    
    * Oops
    
    Tool: gitpod/catfood.gitpod.cloud
    
    * Fix build
    
    Tool: gitpod/catfood.gitpod.cloud

--- components/docker-up/dependencies.sh
+++ components/docker-up/dependencies.sh
@@ -11,6 +11,8 @@ RUNC_VERSION=v1.1.9
 curl -o docker.tgz      -fsSL "https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz"
 # Docker Compose is forked, we have to override the MTU
 curl -o docker-compose  -fsSL "https://github.com/gitpod-io/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64"
+curl -o docker-compose-linux-x86_64.provenance.json -fsSL "https://github.com/gitpod-io/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64.provenance.json"
+curl -o docker-compose-linux-x86_64.sbom.json -fsSL "https://github.com/gitpod-io/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64.sbom.json"
 curl -o checksums.txt  -fsSL "https://github.com/gitpod-io/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/checksums.txt"
 
 curl -o runc            -fsSL "https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.amd64"
