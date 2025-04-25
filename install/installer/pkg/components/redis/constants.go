1b1575b9d50dee4a3d014796e5e2ff42f4d2e15d
Robo Quat <87151047+roboquat@users.noreply.github.com>
2025-04-25 03:00:35 -0400

    update index (#20740)

--- install/installer/pkg/components/redis/constants.go
+++ install/installer/pkg/components/redis/constants.go
@@ -14,11 +14,11 @@ const (
 	RegistryImage = "chainguard/redis"
 
 	ContainerName = "redis"
-	ImageDigest   = "sha256:40896624ea61e7b4ae748825826f8d87099fd3802e3c0afd5d9b746849c733af"
+	ImageDigest   = "sha256:02837a7b96ae30691b72dc9b317c1b1f714994b4d0d34249992c8fc668d3bf30"
 
 	ExporterRegistryRepo  = "quay.io"
 	ExporterRegistryImage = "oliver006/redis_exporter"
-	ExporterImageDigest   = "sha256:dbd410eb9255016c9440b40b8fa18872fdf0e7972d06fc1df8d5b120d35efe05"
+	ExporterImageDigest   = "sha256:ba64da756cd5b76b31e8237aaa40d4126e04625acadd98f76eceb9313e66379f"
 
 	ExporterContainerName = "exporter"
 	ExporterPortName      = "exporter"
