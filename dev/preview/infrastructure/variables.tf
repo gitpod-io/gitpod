f271ea28d33466f80188a79a47e979d4e01b1f68
Kyle Brennan <kyle@gitpod.io>
2025-05-22 23:18:07 -0400

    [dev/preview] bump GCP AMI (#20833)
    * [workspace clusters] new GCP AMI
    
    * Fix

--- dev/preview/infrastructure/variables.tf
+++ dev/preview/infrastructure/variables.tf
@@ -23,7 +23,7 @@ variable "vm_type" {
 variable "vm_image" {
   type        = string
   description = "The VM image"
-  default     = "gitpod-k3s-202411272101"
+  default     = "gitpod-k3s-202505222027"
 }
 
 variable "cert_issuer" {
